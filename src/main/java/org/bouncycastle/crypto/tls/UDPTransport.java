3b8554e0120f654ef3c7ae9614984bc1ff01e105
Peter Dettman <peter.dettman@bouncycastle.org>
2013-02-17 18:25:13 +0700

    Add basic transport layer interface for DTLS

---
+++ src/main/java/org/bouncycastle/crypto/tls/UDPTransport.java
@@ -0,0 +1,39 @@
+package org.bouncycastle.crypto.tls;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPTransport implements DatagramTransport {
+
+    private final DatagramSocket socket;
+    private final int mtu;
+
+    public UDPTransport(DatagramSocket socket, int mtu) throws IOException {
+        if (!socket.isBound() || !socket.isConnected()) {
+            throw new IllegalArgumentException("'socket' must be bound and connected");
+        }
+        this.socket = socket;
+        this.mtu = mtu;
+    }
+
+    public int getReceiveLimit() {
+        return mtu;
+    }
+
+    public int getSendLimit() {
+        // TODO[DTLS] Maybe use a more conservative number, or implement Path-MTU discovery?
+        return getReceiveLimit();
+    }
+
+    public void receive(byte[] buf, int off, int len, int waitMillis) throws IOException {
+        socket.setSoTimeout(waitMillis);
+        DatagramPacket packet = new DatagramPacket(buf, off, len);
+        socket.receive(packet);
+    }
+
+    public void send(byte[] buf, int off, int len) throws IOException {
+        DatagramPacket packet = new DatagramPacket(buf, off, len);
+        socket.send(packet);
+    }
+}
