8aacaa8f4c03f7ee173733bea1adc5483f735046
Peter Dettman <peter.dettman@bouncycastle.org>
2013-02-23 14:33:03 +0700

    DTLS will have to vet attempts to use RC4

--- src/main/java/org/bouncycastle/crypto/tls/DTLSProtocolHandler.java
+++ src/main/java/org/bouncycastle/crypto/tls/DTLSProtocolHandler.java
@@ -12,5 +12,29 @@ public class DTLSProtocolHandler {
 
     public DTLSTransport connect(DatagramTransport transport) {
         return new DTLSTransport(transport);
+        
+//        if (client_version.isDTLS())
+//        {
+//            for (int cipherSuite : offeredCipherSuites)
+//            {
+//                switch (cipherSuite)
+//                {
+//                case CipherSuite.TLS_RSA_EXPORT_WITH_RC4_40_MD5:
+//                case CipherSuite.TLS_RSA_WITH_RC4_128_MD5:
+//                case CipherSuite.TLS_RSA_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_DH_anon_EXPORT_WITH_RC4_40_MD5:
+//                case CipherSuite.TLS_DH_anon_WITH_RC4_128_MD5:
+//                case CipherSuite.TLS_PSK_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_DHE_PSK_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_RSA_PSK_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_ECDH_ECDSA_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_ECDH_RSA_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_ECDHE_RSA_WITH_RC4_128_SHA:
+//                case CipherSuite.TLS_ECDH_anon_WITH_RC4_128_SHA:
+//                    throw new IllegalStateException("Client offered an RC4 cipher suite: RC4 MUST NOT be used with DTLS");
+//                }
+//            }
+//        }
     }
 }
