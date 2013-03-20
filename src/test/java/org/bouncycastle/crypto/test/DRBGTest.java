93d3da955bfd2a91236f457d475cd35b255a08cb
Jon Eaves <jon.eaves@rea-group.com>
2013-03-20 17:32:12 +1100

    Added a simple test framework for the DRGB vectors

--- src/test/java/org/bouncycastle/crypto/test/DRBGTest.java
+++ src/test/java/org/bouncycastle/crypto/test/DRBGTest.java
@@ -1,5 +1,9 @@
 package org.bouncycastle.crypto.test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.prng.DRBG;
@@ -35,27 +39,99 @@ public class DRBGTest extends SimpleTest
         }
     }
 
+    private class TestVector 
+    {
+        
+        private String _entropy;
+        private boolean _pr;
+        private String _nonce;
+        private String _personalisation;
+        private int _ss;
+        private String _ev;
+
+        public TestVector(String entropy, boolean predictionResistance, String nonce, String personalisation, int securityStrength, String expected)
+        {
+            _entropy = entropy;
+            _pr = predictionResistance;
+            _nonce = nonce;
+            _personalisation = personalisation;
+            _ss = securityStrength;
+            _ev = expected;
+        }
+        
+        public String entropy()
+        {
+            return _entropy;
+        }
+        
+        public boolean predictionResistance()
+        {
+            return _pr;
+        }
+        
+        public String nonce()
+        {
+            return _nonce;
+        }
+        
+        public String personalisation() 
+        {
+            return _personalisation;
+        }
+        
+        public int securityStrength() 
+        {
+            return _ss;
+        }
+        
+        public String expectedValue() 
+        {
+            return _ev;
+        }
+    }
+    
+    private Collection createTestVectorData() 
+    {
+        Collection rv = new ArrayList();
+
+        TestVector tv = new TestVector("a37a3e08d8393feb01c4d78cb6a4d1e210c288c89e9838176bc78946745f1c5bea44cf15e061601bfd45f7b3b95be924", true, "8243299805c0877e", "", 128, "a05002f98d5676e1b2e3b3d4686bb9055a830a39"); 
+
+        rv.add(tv);
+        
+        return rv;
+        
+    }
+        
     public void performTest() throws Exception
     {
-        Digest digest = new SHA1Digest();
-        HashDerivationFunction hf = new HashDerivationFunction(digest, 440);
-        EntropySource tes = new TestEntropySource(Hex.decode("a37a3e08d8393feb01c4d78cb6a4d1e210c288c89e9838176bc78946745f1c5bea44cf15e061601bfd45f7b3b95be924"), true);
-        byte[] nonce = Hex.decode("8243299805c0877e");
-        byte[] personalisationString = new byte[0];
-        int securityStrength = 128;
-        DRBG d = new SP800DRBG(hf, tes, nonce, personalisationString, securityStrength);
+        Collection<TestVector> tests = createTestVectorData();
         
-        byte[] output = new byte[20];
-        
-        int rv = d.generate(output, null, true);
-        String out = new String(Hex.encode(output));
-        
-        System.out.println(out);
-
-        rv = d.generate(output, null, true);
-        out = new String(Hex.encode(output));
-        
-        System.out.println(out);
-
+        for (TestVector tv : tests)
+        {
+            tv.entropy();
+            Digest digest = new SHA1Digest();
+            HashDerivationFunction hf = new HashDerivationFunction(digest, 440);
+            EntropySource tes = new TestEntropySource(Hex.decode(tv.entropy()), tv.predictionResistance());
+            byte[] nonce = Hex.decode(tv.nonce());
+            byte[] personalisationString = Hex.decode(tv.personalisation());
+            int securityStrength = tv.securityStrength();
+            DRBG d = new SP800DRBG(hf, tes, nonce, personalisationString, securityStrength);
+            
+            byte[] output = new byte[20];
+            
+            int rv = d.generate(output, null, true);
+            String out = new String(Hex.encode(output));
+            System.out.println(out);
+            rv = d.generate(output, null, true);
+            out = new String(Hex.encode(output));
+            System.out.println(out);
+            
+            byte[] expected = Hex.decode(tv.expectedValue());
+            
+            if (!areEqual(expected, output)) 
+            {
+                throw new Exception("Test Vector Failed, expected "+tv.expectedValue()+" got "+out);
+            }
+        }
     }
 }
