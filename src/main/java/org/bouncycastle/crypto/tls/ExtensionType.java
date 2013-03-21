1ea47fbff7a4e108fb5f5b703f02d7a1633beacf
Peter Dettman <peter.dettman@bouncycastle.org>
2013-03-21 20:15:08 +0700

    Add extension type from RFC 5246

--- src/main/java/org/bouncycastle/crypto/tls/ExtensionType.java
+++ src/main/java/org/bouncycastle/crypto/tls/ExtensionType.java
@@ -1,10 +1,10 @@
 package org.bouncycastle.crypto.tls;
 
-/**
- * RFC 4366 2.3
- */
 public class ExtensionType
 {
+    /*
+     * RFC 6066 1.1.
+     */
     public static final int server_name = 0;
     public static final int max_fragment_length = 1;
     public static final int client_certificate_url = 2;
@@ -13,18 +13,23 @@ public class ExtensionType
     public static final int status_request = 5;
 
     /*
-     * RFC 4492
+     * RFC 4492 5.1.
      */
     public static final int elliptic_curves = 10;
     public static final int ec_point_formats = 11;
 
     /*
-     * RFC 5054 2.8.1
+     * RFC 5054 2.8.1.
      */
     public static final int srp = 12;
 
     /*
-     * RFC 5746 6
+     * RFC 5246 7.4.1.4.
+     */
+    public static final int signature_algorithms = 13;
+
+    /*
+     * RFC 5746 3.2.
      */
     public static final int renegotiation_info = 0xff01;
 }
