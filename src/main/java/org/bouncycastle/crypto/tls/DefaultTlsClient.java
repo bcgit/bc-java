0c53fd70436eba6fb129e07f84d35b75d8ecbc90
Peter Dettman <peter.dettman@bouncycastle.org>
2013-03-26 19:49:02 +0700

    Remove RC4 from DefaultTlsClient so that it's safe to use with DTLS too

--- src/main/java/org/bouncycastle/crypto/tls/DefaultTlsClient.java
+++ src/main/java/org/bouncycastle/crypto/tls/DefaultTlsClient.java
@@ -26,7 +26,6 @@ public abstract class DefaultTlsClient extends AbstractTlsClient
             CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
             CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
             CipherSuite.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
-            CipherSuite.TLS_RSA_WITH_RC4_128_SHA,
         };
     }
 
