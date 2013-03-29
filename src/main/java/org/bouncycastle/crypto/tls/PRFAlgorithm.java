4cb78dc6a7fd6403f645453a60d4e5e92a6bc522
Peter Dettman <peter.dettman@bouncycastle.org>
2013-03-29 14:59:47 +0700

    Add new enumeration from RFC 5246

---
+++ src/main/java/org/bouncycastle/crypto/tls/PRFAlgorithm.java
@@ -0,0 +1,12 @@
+package org.bouncycastle.crypto.tls;
+
+/**
+ * RFC 5246
+ */
+public class PRFAlgorithm {
+    /*
+     * Note that the values here are implementation-specific and arbitrary. It is recommended not to
+     * depend on the particular values (e.g. serialization).
+     */
+    public static final int tls_prf_sha256 = 0;
+}
