0b4be86fb72a4b720be70b85265b0bc0ba64a8f2
Peter Dettman <peter.dettman@bouncycastle.org>
2013-03-30 16:31:20 +0700

    Add a value to represent the legacy TLS PRF algorithm

--- src/main/java/org/bouncycastle/crypto/tls/PRFAlgorithm.java
+++ src/main/java/org/bouncycastle/crypto/tls/PRFAlgorithm.java
@@ -1,17 +1,22 @@
 package org.bouncycastle.crypto.tls;
 
 /**
- * RFC 2246
+ * RFC 5246
  * 
  * Note that the values here are implementation-specific and arbitrary. It is recommended not to
  * depend on the particular values (e.g. serialization).
  */
 public class PRFAlgorithm {
 
-    public static final int tls_prf_sha256 = 0;
+    /*
+     * Placeholder to refer to the legacy TLS algorithm
+     */
+    public static final int tls_prf_legacy = 0;
+
+    public static final int tls_prf_sha256 = 1;
 
     /*
      * Implied by RFC 5288
      */
-    public static final int tls_prf_sha384 = 1;
+    public static final int tls_prf_sha384 = 2;
 }
