b9bb8f1a9fd9745271e0e636666cca13b9caa3f3
David Hook <dgh@autochthonous.org>
2013-04-01 13:17:00 +1100

    made byte[] constructor public, added extra JavaDoc

---
+++ src/main/java/org/bouncycastle/dvcs/MessageImprint.java
@@ -0,0 +1,12 @@
+package org.bouncycastle.dvcs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dgh
+ * Date: 31/03/13
+ * Time: 1:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageImprint
+{
+}
