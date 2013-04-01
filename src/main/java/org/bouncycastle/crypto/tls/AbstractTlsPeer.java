e794706f41060220db1be81155dd1824019cf162
Peter Dettman <peter.dettman@bouncycastle.org>
2013-04-01 19:01:39 +0700

    Add methods for notifying the peer of alerts raised or received.

--- src/main/java/org/bouncycastle/crypto/tls/AbstractTlsPeer.java
+++ src/main/java/org/bouncycastle/crypto/tls/AbstractTlsPeer.java
@@ -2,4 +2,9 @@ package org.bouncycastle.crypto.tls;
 
 public abstract class AbstractTlsPeer implements TlsPeer {
 
+    public void notifyAlertRaised(short alertLevel, short alertDescription, String details, Exception cause) {
+    }
+
+    public void notifyAlertReceived(short alertLevel, short alertDescription) {
+    }
 }
--- src/main/java/org/bouncycastle/crypto/tls/TlsPeer.java
+++ src/main/java/org/bouncycastle/crypto/tls/TlsPeer.java
@@ -2,4 +2,27 @@ package org.bouncycastle.crypto.tls;
 
 public interface TlsPeer {
 
+    /**
+     * This method will be called when an alert is raised by the protocol.
+     * 
+     * @param alertLevel
+     *            {@link AlertLevel}
+     * @param alertDescription
+     *            {@link AlertDescription}
+     * @param details
+     *            A human-readable message explaining what caused this alert. May be null.
+     * @param cause
+     *            The exception that caused this alert to be raised. May be null.
+     */
+    void notifyAlertRaised(short alertLevel, short alertDescription, String details, Exception cause);
+
+    /**
+     * This method will be called when an alert is received from the remote peer.
+     * 
+     * @param alertLevel
+     *            {@link AlertLevel}
+     * @param alertDescription
+     *            {@link AlertDescription}
+     */
+    void notifyAlertReceived(short alertLevel, short alertDescription);
 }
