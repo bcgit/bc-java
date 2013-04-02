21c37c10d971dda644e48540202b06d6f22f3ab6
Peter Dettman <peter.dettman@bouncycastle.org>
2013-04-02 19:40:09 +0700

    Factor out createEngine method

--- src/main/java/org/bouncycastle/crypto/tls/TlsRSASigner.java
+++ src/main/java/org/bouncycastle/crypto/tls/TlsRSASigner.java
@@ -12,25 +12,29 @@ import org.bouncycastle.crypto.params.ParametersWithRandom;
 import org.bouncycastle.crypto.params.RSAKeyParameters;
 import org.bouncycastle.crypto.signers.GenericSigner;
 
-class TlsRSASigner extends AbstractTlsSigner
-{
+class TlsRSASigner extends AbstractTlsSigner {
+
     public byte[] calculateRawSignature(SecureRandom random, AsymmetricKeyParameter privateKey, byte[] md5andsha1)
-        throws CryptoException
-    {
-        AsymmetricBlockCipher engine = new PKCS1Encoding(new RSABlindedEngine());
+        throws CryptoException {
+
+        AsymmetricBlockCipher engine = createEngine();
         engine.init(true, new ParametersWithRandom(privateKey, random));
         return engine.processBlock(md5andsha1, 0, md5andsha1.length);
     }
 
-    public Signer createVerifyer(AsymmetricKeyParameter publicKey)
-    {
-        Signer s = new GenericSigner(new PKCS1Encoding(new RSABlindedEngine()), new CombinedHash());
+    public Signer createVerifyer(AsymmetricKeyParameter publicKey) {
+
+        AsymmetricBlockCipher engine = createEngine();
+        Signer s = new GenericSigner(engine, new CombinedHash());
         s.init(false, publicKey);
         return s;
     }
 
-    public boolean isValidPublicKey(AsymmetricKeyParameter publicKey)
-    {
+    public boolean isValidPublicKey(AsymmetricKeyParameter publicKey) {
         return publicKey instanceof RSAKeyParameters && !publicKey.isPrivate();
+    }
+
+    protected AsymmetricBlockCipher createEngine() {
+        return new PKCS1Encoding(new RSABlindedEngine());
     }
 }
