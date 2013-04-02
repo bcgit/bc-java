5cd8db3da4084d7cabf765b29034ae9aedc27480
Peter Dettman <peter.dettman@bouncycastle.org>
2013-04-02 18:55:11 +0700

    Simplify raw RSA signing

--- src/main/java/org/bouncycastle/crypto/tls/TlsRSASigner.java
+++ src/main/java/org/bouncycastle/crypto/tls/TlsRSASigner.java
@@ -2,9 +2,9 @@ package org.bouncycastle.crypto.tls;
 
 import java.security.SecureRandom;
 
+import org.bouncycastle.crypto.AsymmetricBlockCipher;
 import org.bouncycastle.crypto.CryptoException;
 import org.bouncycastle.crypto.Signer;
-import org.bouncycastle.crypto.digests.NullDigest;
 import org.bouncycastle.crypto.encodings.PKCS1Encoding;
 import org.bouncycastle.crypto.engines.RSABlindedEngine;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
@@ -17,10 +17,9 @@ class TlsRSASigner implements TlsSigner
     public byte[] calculateRawSignature(SecureRandom random, AsymmetricKeyParameter privateKey, byte[] md5andsha1)
         throws CryptoException
     {
-        Signer sig = new GenericSigner(new PKCS1Encoding(new RSABlindedEngine()), new NullDigest());
-        sig.init(true, new ParametersWithRandom(privateKey, random));
-        sig.update(md5andsha1, 0, md5andsha1.length);
-        return sig.generateSignature();
+        AsymmetricBlockCipher engine = new PKCS1Encoding(new RSABlindedEngine());
+        engine.init(true, new ParametersWithRandom(privateKey, random));
+        return engine.processBlock(md5andsha1, 0, md5andsha1.length);
     }
 
     public Signer createVerifyer(AsymmetricKeyParameter publicKey)
