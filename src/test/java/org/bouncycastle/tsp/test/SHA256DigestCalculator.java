105550efbb675e245e3ced3a30e6d2c3135810c2
David Hook <dgh@cryptoworkshop.com>
2013-05-29 13:42:46 +1000

    clean up

--- src/test/java/org/bouncycastle/tsp/test/SHA256DigestCalculator.java
+++ src/test/java/org/bouncycastle/tsp/test/SHA256DigestCalculator.java
@@ -4,10 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
-import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.crypto.Digest;
-import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.operator.DigestCalculator;
 
