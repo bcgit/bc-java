aca5df90f022735b379b40091d5658bab02498df
David Hook <dgh@cryptoworkshop.com>
2013-06-06 10:41:51 +1000

    moved ocsp test and mozilla test

---
+++ pkix/src/main/java/org/bouncycastle/operator/BufferingContentSigner.java
@@ -0,0 +1,12 @@
+package org.bouncycastle.operator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dgh
+ * Date: 6/06/13
+ * Time: 8:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BufferingContentSigner
+{
+}
