706483c3860de7c062a28603a3766b9304f2f4f4
David Hook <dgh@cryptoworkshop.com>
2013-06-07 17:23:47 +1000

    removed useless getEncoded() method.

--- core/src/main/java/org/bouncycastle/crypto/ec/ECPair.java
+++ core/src/main/java/org/bouncycastle/crypto/ec/ECPair.java
@@ -22,17 +22,4 @@ public class ECPair
     {
         return y;
     }
-
-    public byte[] getEncoded()
-    {
-        byte[] xEnc = x.getEncoded();
-        byte[] yEnc = y.getEncoded();
-
-        byte[] full = new byte[xEnc.length + yEnc.length];
-
-        System.arraycopy(xEnc, 0, full, 0, xEnc.length);
-        System.arraycopy(yEnc, 0, full, xEnc.length, yEnc.length);
-
-        return full;
-    }
 }
