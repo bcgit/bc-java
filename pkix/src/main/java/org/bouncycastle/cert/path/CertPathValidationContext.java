291a4f410d841d6007f516520a9bc345c8becc96
David Hook <dgh@cryptoworkshop.com>
2013-06-15 13:57:09 +1000

    more tweaking

--- pkix/src/main/java/org/bouncycastle/cert/path/CertPathValidationContext.java
+++ pkix/src/main/java/org/bouncycastle/cert/path/CertPathValidationContext.java
@@ -1,10 +1,21 @@
 package org.bouncycastle.cert.path;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.util.Memoable;
 
 public class CertPathValidationContext
     implements Memoable
 {
+    private Set handledExtensions = new HashSet();
+
+    public void addHandledExtension(ASN1ObjectIdentifier extensionIdentifier)
+    {
+        this.handledExtensions.add(extensionIdentifier);
+    }
+
     public Memoable copy()
     {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
