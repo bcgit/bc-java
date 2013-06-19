6ab17e7ea46ebf593c165279a568f23dcd2b8c55
Peter Dettman <peter.dettman@bouncycastle.org>
2013-06-19 16:24:46 +0700

    Add DigitallySigned struct

---
+++ core/src/main/java/org/bouncycastle/crypto/tls/DigitallySigned.java
@@ -0,0 +1,67 @@
+package org.bouncycastle.crypto.tls;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DigitallySigned
+{
+    protected SignatureAndHashAlgorithm algorithm;
+    protected byte[] signature;
+
+    public DigitallySigned(SignatureAndHashAlgorithm algorithm, byte[] signature)
+    {
+        if (signature == null)
+        {
+            throw new IllegalArgumentException("'signature' cannot be null");
+        }
+
+        this.algorithm = algorithm;
+        this.signature = signature;
+    }
+
+    public SignatureAndHashAlgorithm getAlgorithm()
+    {
+        return algorithm;
+    }
+
+    public byte[] getSignature()
+    {
+        return signature;
+    }
+
+    /**
+     * Encode this {@link DigitallySigned} to an {@link OutputStream}.
+     * 
+     * @param output
+     *            the {@link OutputStream} to encode to.
+     * @throws IOException
+     */
+    public void encode(OutputStream output) throws IOException
+    {
+        if (algorithm != null)
+        {
+            algorithm.encode(output);
+        }
+        TlsUtils.writeOpaque16(signature, output);
+    }
+
+    /**
+     * Parse a {@link DigitallySigned} from an {@link InputStream}.
+     * 
+     * @param input
+     *            the {@link InputStream} to parse from.
+     * @return a {@link DigitallySigned} object.
+     * @throws IOException
+     */
+    public static DigitallySigned parse(TlsContext context, InputStream input) throws IOException
+    {
+        SignatureAndHashAlgorithm algorithm = null;
+        if (ProtocolVersion.TLSv12.isEqualOrEarlierVersionOf(context.getServerVersion().getEquivalentTLSVersion()))
+        {
+            algorithm = SignatureAndHashAlgorithm.parse(input);
+        }
+        byte[] signature = TlsUtils.readOpaque16(input);
+        return new DigitallySigned(algorithm, signature);
+    }
+}
