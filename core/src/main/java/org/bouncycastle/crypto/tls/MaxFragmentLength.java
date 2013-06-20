207b6312707a17f24f3a528e3a1311637b793269
Peter Dettman <peter.dettman@bouncycastle.org>
2013-06-20 15:09:17 +0700

    Add MaxFragmentLength enumeration from RFC 3546

---
+++ core/src/main/java/org/bouncycastle/crypto/tls/MaxFragmentLength.java
@@ -0,0 +1,12 @@
+package org.bouncycastle.crypto.tls;
+
+public class MaxFragmentLength
+{
+    /*
+     * RFC 3546 3.2.
+     */
+    public static short pow2_9 = 1;
+    public static short pow2_10 = 2;
+    public static short pow2_11 = 3;
+    public static short pow2_12 = 4;
+}
