fedb4452deceefce415c628e4e601b1ae7756910
Peter Dettman <peter.dettman@bouncycastle.org>
2013-06-20 15:23:10 +0700

    Add utility methods for working with max_fragment_length extensions

--- core/src/main/java/org/bouncycastle/crypto/tls/MaxFragmentLength.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/MaxFragmentLength.java
@@ -9,4 +9,9 @@ public class MaxFragmentLength
     public static short pow2_10 = 2;
     public static short pow2_11 = 3;
     public static short pow2_12 = 4;
+
+    public static boolean isValid(short s)
+    {
+        return s >= pow2_9 && s <= pow2_12;
+    }
 }
