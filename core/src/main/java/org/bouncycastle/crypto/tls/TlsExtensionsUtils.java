bc3ab6397c3bd2fcf68aa9b597eb6014b33f6050
Peter Dettman <peter.dettman@bouncycastle.org>
2013-06-20 12:12:37 +0700

    Add utility methods for working with server_name extensions

--- core/src/main/java/org/bouncycastle/crypto/tls/TlsExtensionsUtils.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/TlsExtensionsUtils.java
@@ -9,12 +9,34 @@ import org.bouncycastle.util.Integers;
 
 public class TlsExtensionsUtils
 {
+    public static final Integer EXT_server_name = Integers.valueOf(ExtensionType.server_name);
     public static final Integer EXT_status_request = Integers.valueOf(ExtensionType.status_request);
+
+    public static void addServerNameExtension(Hashtable extensions, ServerNameList serverNameList)
+        throws IOException
+    {
+        extensions.put(EXT_server_name, createServerNameExtension(serverNameList));
+    }
 
     public static void addStatusRequestExtension(Hashtable extensions, CertificateStatusRequest statusRequest)
         throws IOException
     {
         extensions.put(EXT_status_request, createStatusRequestExtension(statusRequest));
+    }
+
+    public static ServerNameList getServerNameExtension(Hashtable extensions)
+        throws IOException
+    {
+        if (extensions == null)
+        {
+            return null;
+        }
+        byte[] extensionValue = (byte[])extensions.get(EXT_server_name);
+        if (extensionValue == null)
+        {
+            return null;
+        }
+        return readServerNameExtension(extensionValue);
     }
 
     public static CertificateStatusRequest getStatusRequestExtension(Hashtable extensions)
@@ -32,6 +54,21 @@ public class TlsExtensionsUtils
         return readStatusRequestExtension(extensionValue);
     }
 
+    public static byte[] createServerNameExtension(ServerNameList serverNameList)
+        throws IOException
+    {
+        if (serverNameList == null)
+        {
+            throw new TlsFatalAlert(AlertDescription.internal_error);
+        }
+        
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        
+        serverNameList.encode(buf);
+
+        return buf.toByteArray();
+    }
+
     public static byte[] createStatusRequestExtension(CertificateStatusRequest statusRequest)
         throws IOException
     {
@@ -45,6 +82,23 @@ public class TlsExtensionsUtils
         statusRequest.encode(buf);
 
         return buf.toByteArray();
+    }
+
+    public static ServerNameList readServerNameExtension(byte[] extensionValue)
+        throws IOException
+    {
+        if (extensionValue == null)
+        {
+            throw new IllegalArgumentException("'extensionValue' cannot be null");
+        }
+
+        ByteArrayInputStream buf = new ByteArrayInputStream(extensionValue);
+
+        ServerNameList serverNameList = ServerNameList.parse(buf);
+
+        TlsProtocol.assertEmpty(buf);
+
+        return serverNameList;
     }
 
     public static CertificateStatusRequest readStatusRequestExtension(byte[] extensionValue)
