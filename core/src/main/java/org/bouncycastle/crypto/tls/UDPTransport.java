48a2d573f0d25417c1a61c6cc1f8e1f4e941f470
Peter Dettman <peter.dettman@bouncycastle.org>
2013-06-22 12:45:06 +0700

    Make fields protected Throw internal_error for sends exceeding limit

--- core/src/main/java/org/bouncycastle/crypto/tls/UDPTransport.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/UDPTransport.java
@@ -7,18 +7,16 @@ import java.net.DatagramSocket;
 public class UDPTransport
     implements DatagramTransport
 {
+    protected final static int MIN_IP_OVERHEAD = 20;
+    protected final static int MAX_IP_OVERHEAD = MIN_IP_OVERHEAD + 64;
+    protected final static int UDP_OVERHEAD = 8;
 
-    private final static int MIN_IP_OVERHEAD = 20;
-    private final static int MAX_IP_OVERHEAD = MIN_IP_OVERHEAD + 64;
-    private final static int UDP_OVERHEAD = 8;
-
-    private final DatagramSocket socket;
-    private final int receiveLimit, sendLimit;
+    protected final DatagramSocket socket;
+    protected final int receiveLimit, sendLimit;
 
     public UDPTransport(DatagramSocket socket, int mtu)
         throws IOException
     {
-
         if (!socket.isBound() || !socket.isConnected())
         {
             throw new IllegalArgumentException("'socket' must be bound and connected");
@@ -62,7 +60,7 @@ public class UDPTransport
              * the DTLS implementation SHOULD generate an error, thus avoiding sending a packet
              * which will be fragmented."
              */
-            // TODO Exception
+            throw new TlsFatalAlert(AlertDescription.internal_error);
         }
 
         DatagramPacket packet = new DatagramPacket(buf, off, len);
