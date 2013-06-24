3586ae333aa71729a09abb830601e1504535c663
Peter Dettman <peter.dettman@bouncycastle.org>
2013-06-24 14:47:18 +0700

    Tidy up ChangeCipherSpace processing

--- core/src/main/java/org/bouncycastle/crypto/tls/DTLSRecordLayer.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/DTLSRecordLayer.java
@@ -258,14 +258,18 @@ class DTLSRecordLayer
                 {
                     // Implicitly receive change_cipher_spec and change to pending cipher state
 
-                    if (plaintext.length != 1 || plaintext[0] != 1)
+                    for (int i = 0; i < plaintext.length; ++i)
                     {
-                        continue;
-                    }
+                        short message = TlsUtils.readUint8(plaintext, i);
+                        if (message != ChangeCipherSpec.change_cipher_spec)
+                        {
+                            continue;
+                        }
 
-                    if (pendingEpoch != null)
-                    {
-                        readEpoch = pendingEpoch;
+                        if (pendingEpoch != null)
+                        {
+                            readEpoch = pendingEpoch;
+                        }
                     }
 
                     continue;
