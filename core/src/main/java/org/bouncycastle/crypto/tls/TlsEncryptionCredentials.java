d2b8d72dd8264dd0b7ba568348668b973f5f8c47
Peter Dettman <peter.dettman@bouncycastle.org>
2013-09-17 10:23:49 +0700

    Formatting

--- core/src/main/java/org/bouncycastle/crypto/tls/TlsEncryptionCredentials.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/TlsEncryptionCredentials.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public interface TlsEncryptionCredentials
     extends TlsCredentials
 {
-
     byte[] decryptPreMasterSecret(byte[] encryptedPreMasterSecret)
         throws IOException;
 }
