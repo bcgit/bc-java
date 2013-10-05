2c78c0d487309509ba5ab162d118536120ee88b7
Peter Dettman <peter.dettman@bouncycastle.org>
2013-10-05 12:30:29 +0700

    Implement first-digit optimization for WNaf (disabled for the moment)

--- core/src/main/java/org/bouncycastle/math/ec/WNafL2RMultiplier.java
+++ core/src/main/java/org/bouncycastle/math/ec/WNafL2RMultiplier.java
@@ -29,13 +29,51 @@ public class WNafL2RMultiplier extends AbstractECMultiplier
         ECPoint R = p.getCurve().getInfinity();
 
         int i = wnaf.length;
-        while (--i >= 0)
+
+        /*
+         * NOTE This code optimizes the first window using the precomputed points to substitute an
+         * addition for 2 or more doublings. Not enabled until performance can be analyzed more,
+         * especially for coordinate systems where doubling is significantly cheaper than addition.
+         */
+        //        if (i > 1)
+//        {
+//            int wi = wnaf[--i];
+//            int digit = wi >> 16, zeroes = wi & 0xFFFF;
+//
+//            int n = Math.abs(digit);
+//            ECPoint[] table = digit < 0 ? preCompNeg : preComp;
+//
+//            int window = width - 1;
+//            if ((n << 1) < (1 << window))
+//            {
+//                int highest = IntArray.bitLengths[n] - 1;
+//                int lowBits =  n ^ (1 << highest);
+//                int scale = window - highest;
+//
+//                int i1 = ((1 << window) - 1);
+//                int i2 = (lowBits << scale) + 1;
+//
+//                R = table[i1 >>> 1].add(table[i2 >>> 1]);
+//                zeroes -= scale;
+//
+////              System.out.println("Optimized: 2^" + scale + " * " + n + " = " + i1 + " + " + i2);
+//            }
+//            else
+//            {
+//                R = table[n >>> 1];
+//            }
+//
+//            R = R.timesPow2(zeroes);
+//        }
+
+        while (i > 0)
         {
-            int wi = wnaf[i];
+            int wi = wnaf[--i];
             int digit = wi >> 16, zeroes = wi & 0xFFFF;
 
-            int index = Math.abs(digit) >>> 1;
-            ECPoint r = digit < 0 ? preCompNeg[index] : preComp[index];
+            int n = Math.abs(digit);
+            ECPoint[] table = digit < 0 ? preCompNeg : preComp;
+            ECPoint r = table[n >>> 1];
 
             R = R.twicePlus(r);
             R = R.timesPow2(zeroes);
