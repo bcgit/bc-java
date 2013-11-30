762585f0c5d3939b5c1a1badb92bd05ad009d394
David Hook <dgh@cryptoworkshop.com>
2013-11-30 16:56:19 +1100

    initial version

---
+++ pkix/src/main/java/org/bouncycastle/cert/path/validations/ValidationUtils.java
@@ -0,0 +1,11 @@
+package org.bouncycastle.cert.path.validations;
+
+import org.bouncycastle.cert.X509CertificateHolder;
+
+class ValidationUtils
+{
+    static boolean isSelfIssued(X509CertificateHolder cert)
+    {
+        return cert.getSubject().equals(cert.getIssuer());
+    }
+}
