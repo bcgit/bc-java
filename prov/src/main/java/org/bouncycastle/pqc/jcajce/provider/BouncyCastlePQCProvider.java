73379da9daa7fc8fd2a61fd7ee83c833ec999b32
David Hook <dgh@cryptoworkshop.com>
2013-12-02 16:35:26 +1100

    moved to 1.50

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/BouncyCastlePQCProvider.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/BouncyCastlePQCProvider.java
@@ -21,7 +21,7 @@ public class BouncyCastlePQCProvider
     extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Post-Quantum Security Provider v1.48";
+    private static String info = "BouncyCastle Post-Quantum Security Provider v1.50";
 
     public static String PROVIDER_NAME = "BCPQC";
 
@@ -46,7 +46,7 @@ public class BouncyCastlePQCProvider
      */
     public BouncyCastlePQCProvider()
     {
-        super(PROVIDER_NAME, 1.48, info);
+        super(PROVIDER_NAME, 1.50, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
