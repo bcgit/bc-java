588f8203bfe4c71d3c3be9e388d6334d99e5b62a
Peter Dettman <peter.dettman@bouncycastle.org>
2013-12-02 21:24:23 +0700

    Update (D)TLS tests to demonstrate TLS 1.2 server functionality

--- core/src/main/java/org/bouncycastle/crypto/tls/DefaultTlsClient.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/DefaultTlsClient.java
@@ -19,6 +19,7 @@ public abstract class DefaultTlsClient
     {
         return new int[] { CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
             CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256, CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+            CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256, CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA256,
             CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA };
     }
 
--- core/src/test/java/org/bouncycastle/crypto/tls/test/TlsProtocolTest.java
+++ core/src/test/java/org/bouncycastle/crypto/tls/test/TlsProtocolTest.java
@@ -12,9 +12,11 @@ import junit.framework.TestCase;
 import org.bouncycastle.asn1.x509.Certificate;
 import org.bouncycastle.crypto.tls.AlertLevel;
 import org.bouncycastle.crypto.tls.CertificateRequest;
+import org.bouncycastle.crypto.tls.CipherSuite;
 import org.bouncycastle.crypto.tls.ClientCertificateType;
 import org.bouncycastle.crypto.tls.DefaultTlsClient;
 import org.bouncycastle.crypto.tls.DefaultTlsServer;
+import org.bouncycastle.crypto.tls.HashAlgorithm;
 import org.bouncycastle.crypto.tls.ProtocolVersion;
 import org.bouncycastle.crypto.tls.SignatureAlgorithm;
 import org.bouncycastle.crypto.tls.SignatureAndHashAlgorithm;
@@ -24,6 +26,7 @@ import org.bouncycastle.crypto.tls.TlsCredentials;
 import org.bouncycastle.crypto.tls.TlsEncryptionCredentials;
 import org.bouncycastle.crypto.tls.TlsServerProtocol;
 import org.bouncycastle.crypto.tls.TlsSignerCredentials;
+import org.bouncycastle.crypto.tls.TlsUtils;
 import org.bouncycastle.util.Arrays;
 
 public class TlsProtocolTest
@@ -195,14 +198,44 @@ public class TlsProtocolTest
                 + alertDescription + ")");
         }
 
-//        protected ProtocolVersion getMaximumVersion()
-//        {
-//            return ProtocolVersion.TLSv12;
-//        }
-
-        public CertificateRequest getCertificateRequest()
+        protected int[] getCipherSuites()
         {
-            return new CertificateRequest(new short[]{ ClientCertificateType.rsa_sign }, null, null);
+            return new int[] { CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256, CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA, CipherSuite.TLS_RSA_WITH_AES_256_GCM_SHA384,
+                CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256, CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA256,
+                CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA256, CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
+                CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA };
+        }
+
+        protected ProtocolVersion getMaximumVersion()
+        {
+            return ProtocolVersion.TLSv12;
+        }
+
+        public CertificateRequest getCertificateRequest() throws IOException
+        {
+            Vector serverSigAlgs = null;
+
+            if (TlsUtils.isSignatureAlgorithmsExtensionAllowed(serverVersion))
+            {
+                short[] hashAlgorithms = new short[]{ HashAlgorithm.sha512, HashAlgorithm.sha384, HashAlgorithm.sha256,
+                    HashAlgorithm.sha224, HashAlgorithm.sha1 };
+                short[] signatureAlgorithms = new short[]{ SignatureAlgorithm.rsa };
+
+                serverSigAlgs = new Vector();
+                for (int i = 0; i < hashAlgorithms.length; ++i)
+                {
+                    for (int j = 0; j < signatureAlgorithms.length; ++j)
+                    {
+                        serverSigAlgs.addElement(new SignatureAndHashAlgorithm(hashAlgorithms[i],
+                            signatureAlgorithms[j]));
+                    }
+                }
+            }
+
+            return new CertificateRequest(new short[]{ ClientCertificateType.rsa_sign }, serverSigAlgs, null);
         }
 
         public void notifyClientCertificate(org.bouncycastle.crypto.tls.Certificate clientCertificate)
@@ -229,8 +262,33 @@ public class TlsProtocolTest
         protected TlsSignerCredentials getRSASignerCredentials()
             throws IOException
         {
+            /*
+             * TODO Note that this code fails to provide default value for the client supported
+             * algorithms if it wasn't sent.
+             */
+            SignatureAndHashAlgorithm signatureAndHashAlgorithm = null;
+            Vector sigAlgs = supportedSignatureAlgorithms;
+            if (sigAlgs != null)
+            {
+                for (int i = 0; i < sigAlgs.size(); ++i)
+                {
+                    SignatureAndHashAlgorithm sigAlg = (SignatureAndHashAlgorithm)
+                        sigAlgs.elementAt(i);
+                    if (sigAlg.getSignature() == SignatureAlgorithm.rsa)
+                    {
+                        signatureAndHashAlgorithm = sigAlg;
+                        break;
+                    }
+                }
+
+                if (signatureAndHashAlgorithm == null)
+                {
+                    return null;
+                }
+            }
+
             return TlsTestUtils.loadSignerCredentials(context, new String[]{"x509-server.pem", "x509-ca.pem"},
-                "x509-server-key.pem");
+                "x509-server-key.pem", signatureAndHashAlgorithm);
         }
     }
 }
