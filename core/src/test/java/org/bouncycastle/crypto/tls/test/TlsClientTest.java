9534e3892ae8dfce7c332f2c071c1a507541227d
Peter Dettman <peter.dettman@bouncycastle.org>
2013-12-06 19:06:00 +0700

    Extract common client/server code into top-level objects

--- core/src/test/java/org/bouncycastle/crypto/tls/test/TlsClientTest.java
+++ core/src/test/java/org/bouncycastle/crypto/tls/test/TlsClientTest.java
@@ -49,14 +49,14 @@ public class TlsClientTest
 
         long time1 = System.currentTimeMillis();
 
-        MyTlsClient client = new MyTlsClient(null);
+        MockTlsClient client = new MockTlsClient(null);
         TlsClientProtocol protocol = openTlsConnection(address, port, client);
         protocol.close();
 
         long time2 = System.currentTimeMillis();
         System.out.println("Elapsed 1: " + (time2 - time1) + "ms");
 
-        client = new MyTlsClient(client.getSessionToResume());
+        client = new MockTlsClient(client.getSessionToResume());
         protocol = openTlsConnection(address, port, client);
 
         long time3 = System.currentTimeMillis();
@@ -84,139 +84,5 @@ public class TlsClientTest
         TlsClientProtocol protocol = new TlsClientProtocol(s.getInputStream(), s.getOutputStream(), secureRandom);
         protocol.connect(client);
         return protocol;
-    }
-
-    static class MyTlsClient
-        extends DefaultTlsClient
-    {
-        TlsSession session;
-
-        MyTlsClient(TlsSession session)
-        {
-            this.session = session;
-        }
-
-        public TlsSession getSessionToResume()
-        {
-            return this.session;
-        }
-
-        public void notifyAlertRaised(short alertLevel, short alertDescription, String message, Exception cause)
-        {
-            PrintStream out = (alertLevel == AlertLevel.fatal) ? System.err : System.out;
-            out.println("TLS client raised alert (AlertLevel." + alertLevel + ", AlertDescription." + alertDescription
-                + ")");
-            if (message != null)
-            {
-                out.println(message);
-            }
-            if (cause != null)
-            {
-                cause.printStackTrace(out);
-            }
-        }
-
-        public void notifyAlertReceived(short alertLevel, short alertDescription)
-        {
-            PrintStream out = (alertLevel == AlertLevel.fatal) ? System.err : System.out;
-            out.println("TLS client received alert (AlertLevel." + alertLevel + ", AlertDescription."
-                + alertDescription + ")");
-        }
-
-        public Hashtable getClientExtensions() throws IOException
-        {
-            Hashtable clientExtensions = TlsExtensionsUtils.ensureExtensionsInitialised(super.getClientExtensions());
-            TlsExtensionsUtils.addMaxFragmentLengthExtension(clientExtensions, MaxFragmentLength.pow2_9);
-            TlsExtensionsUtils.addTruncatedHMacExtension(clientExtensions);
-            // For testing draft-gutmann-tls-encrypt-then-mac
-//            clientExtensions.put(Integers.valueOf(0x42), TlsExtensionsUtils.createEmptyExtensionData());
-            return clientExtensions;
-        }
-
-        public void notifyServerVersion(ProtocolVersion serverVersion) throws IOException
-        {
-            super.notifyServerVersion(serverVersion);
-
-            System.out.println("Negotiated " + serverVersion);
-        }
-
-        public TlsAuthentication getAuthentication()
-            throws IOException
-        {
-            return new TlsAuthentication()
-            {
-                public void notifyServerCertificate(org.bouncycastle.crypto.tls.Certificate serverCertificate)
-                    throws IOException
-                {
-                    Certificate[] chain = serverCertificate.getCertificateList();
-                    System.out.println("Received server certificate chain of length " + chain.length);
-                    for (int i = 0; i != chain.length; i++)
-                    {
-                        Certificate entry = chain[i];
-                        // TODO Create fingerprint based on certificate signature algorithm digest
-                        System.out.println("    fingerprint:SHA-256 " + TlsTestUtils.fingerprint(entry) + " ("
-                            + entry.getSubject() + ")");
-                    }
-                }
-
-                public TlsCredentials getClientCredentials(CertificateRequest certificateRequest)
-                    throws IOException
-                {
-                    short[] certificateTypes = certificateRequest.getCertificateTypes();
-                    if (certificateTypes == null || !Arrays.contains(certificateTypes, ClientCertificateType.rsa_sign))
-                    {
-                        return null;
-                    }
-
-                    SignatureAndHashAlgorithm signatureAndHashAlgorithm = null;
-                    Vector sigAlgs = certificateRequest.getSupportedSignatureAlgorithms();
-                    if (sigAlgs != null)
-                    {
-                        for (int i = 0; i < sigAlgs.size(); ++i)
-                        {
-                            SignatureAndHashAlgorithm sigAlg = (SignatureAndHashAlgorithm)
-                                sigAlgs.elementAt(i);
-                            if (sigAlg.getSignature() == SignatureAlgorithm.rsa)
-                            {
-                                signatureAndHashAlgorithm = sigAlg;
-                                break;
-                            }
-                        }
-
-                        if (signatureAndHashAlgorithm == null)
-                        {
-                            return null;
-                        }
-                    }
-
-                    // TODO Create a distinct client certificate for use here
-                    return TlsTestUtils.loadSignerCredentials(context, new String[] { "x509-server.pem", "x509-ca.pem" },
-                        "x509-server-key.pem", signatureAndHashAlgorithm);
-                }
-            };
-        }
-
-        public void notifyHandshakeComplete() throws IOException
-        {
-            super.notifyHandshakeComplete();
-
-            TlsSession newSession = context.getResumableSession();
-            if (newSession != null)
-            {
-                byte[] newSessionID = newSession.getSessionID();
-                String hex = Hex.toHexString(newSessionID);
-
-                if (this.session != null && Arrays.areEqual(this.session.getSessionID(), newSessionID))
-                {
-                    System.out.println("Resumed session: " + hex);
-                }
-                else
-                {
-                    System.out.println("Established session: " + hex);
-                }
-
-                this.session = newSession;
-            }
-        }
     }
 }
