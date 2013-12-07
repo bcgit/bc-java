ba79d393c9963e0250161f04144502ae3d2053b9
Peter Dettman <peter.dettman@bouncycastle.org>
2013-12-07 08:41:55 +0700

    Use named constant

--- core/src/main/jdk1.4/org/bouncycastle/util/Times.java
+++ core/src/main/jdk1.4/org/bouncycastle/util/Times.java
@@ -2,8 +2,10 @@ package org.bouncycastle.util;
 
 public final class Times
 {
+    private static long NANOS_PER_MILLI = 1000000L;
+
     public static long nanoTime()
     {
-        return 1000000L * System.currentTimeMillis();
+        return NANOS_PER_MILLI * System.currentTimeMillis();
     }
 }
