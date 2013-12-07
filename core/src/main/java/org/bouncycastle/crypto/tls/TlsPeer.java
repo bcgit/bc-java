440441709c9433f24d88bc3c3758747938979021
Peter Dettman <peter.dettman@bouncycastle.org>
2013-12-07 08:38:34 +0700

    Add shouldUseGMTUnixTime method to allow optional inclusion of gmt_unix_time in Random (defaults to false)

--- core/src/main/java/org/bouncycastle/crypto/tls/TlsPeer.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/TlsPeer.java
@@ -4,6 +4,17 @@ import java.io.IOException;
 
 public interface TlsPeer
 {
+    /**
+     * draft-mathewson-no-gmtunixtime-00 2. "If existing users of a TLS implementation may rely on
+     * gmt_unix_time containing the current time, we recommend that implementors MAY provide the
+     * ability to set gmt_unix_time as an option only, off by default."
+     * 
+     * @return <code>true</code> if the current time should be used in the gmt_unix_time field of
+     *         Random, or <code>false</code> if gmt_unix_time should contain a cryptographically
+     *         random value.
+     */
+    boolean shouldUseGMTUnixTime();
+
     void notifySecureRenegotiation(boolean secureNegotiation) throws IOException;
 
     TlsCompression getCompression() throws IOException;
