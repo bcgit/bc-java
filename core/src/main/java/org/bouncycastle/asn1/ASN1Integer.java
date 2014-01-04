edddcb766dca890eb6eb52b35fdb38e350a426f7
David Hook <dgh@cryptoworkshop.com>
2014-01-04 13:12:41 +1100

    Added constructor taking signed byte array.

--- core/src/main/java/org/bouncycastle/asn1/ASN1Integer.java
+++ core/src/main/java/org/bouncycastle/asn1/ASN1Integer.java
@@ -2,12 +2,24 @@ package org.bouncycastle.asn1;
 
 import java.math.BigInteger;
 
+import org.bouncycastle.util.Arrays;
+
 public class ASN1Integer
     extends DERInteger
 {
-    ASN1Integer(byte[] bytes)
+    ASN1Integer(byte[] bytes, boolean clone)
     {
-        super(bytes);
+        super(clone ? Arrays.clone(bytes) : bytes);
+    }
+
+    /**
+     * Constructor from a byte array containing a signed representation of the number.
+     *
+     * @param bytes a byte array containing the signed number.A copy is made of the byte array.
+     */
+    public ASN1Integer(byte[] bytes)
+    {
+        this(bytes, true);
     }
 
     public ASN1Integer(BigInteger value)
