96b4d9571c71a4471479d941b1db71347842b707
Peter Dettman <peter.dettman@bouncycastle.org>
2014-01-05 18:37:11 +0700

    Implement Integers rotate methods for J2ME and JDK 1.4

--- core/src/main/j2me/org/bouncycastle/util/Integers.java
+++ core/src/main/j2me/org/bouncycastle/util/Integers.java
@@ -2,6 +2,16 @@ package org.bouncycastle.util;
 
 public class Integers
 {
+    public static int rotateLeft(int i, int distance)
+    {
+        return (i << distance) ^ (i >>> -distance);
+    }
+
+    public static int rotateRight(int i, int distance)
+    {
+        return (i >>> distance) ^ (i << -distance);
+    }
+
     public static Integer valueOf(int value)
     {
         return new Integer(value);
