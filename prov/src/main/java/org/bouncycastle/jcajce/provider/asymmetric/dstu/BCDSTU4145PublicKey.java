438334ccef6ef7d36cda04ff1372b2a07a42bd4c
Peter Dettman <peter.dettman@bouncycastle.org>
2014-01-05 15:21:44 +0700

    More custom curve temporary fixes

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/dstu/BCDSTU4145PublicKey.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/dstu/BCDSTU4145PublicKey.java
@@ -36,6 +36,8 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 import org.bouncycastle.jce.spec.ECNamedCurveSpec;
 import org.bouncycastle.math.ec.ECCurve;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
+import org.bouncycastle.math.ec.custom.sec.SecP256R1Point;
 
 public class BCDSTU4145PublicKey
     implements ECPublicKey, org.bouncycastle.jce.interfaces.ECPublicKey, ECPointEncoder
@@ -355,6 +357,16 @@ public class BCDSTU4145PublicKey
             if (q instanceof org.bouncycastle.math.ec.ECPoint.Fp)
             {
                 return new org.bouncycastle.math.ec.ECPoint.Fp(null, q.getAffineXCoord(), q.getAffineYCoord());
+            }
+            // TODO: fix when custom curves have common interface
+            else if (q instanceof SecP256K1Point)
+            {
+                return new SecP256K1Point(null, q.getAffineXCoord(), q.getAffineYCoord());
+            }
+            // TODO: fix when custom curves have common interface
+            else if (q instanceof SecP256R1Point)
+            {
+                return new SecP256R1Point(null, q.getAffineXCoord(), q.getAffineYCoord());
             }
             else
             {
