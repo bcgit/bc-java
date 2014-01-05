09d5054567b98654949a14a4a42f3a2ddb16eff8
Peter Dettman <peter.dettman@bouncycastle.org>
2014-01-05 19:44:21 +0700

    Change exception message to reflect actual class name

--- core/src/main/java/org/bouncycastle/math/ec/custom/sec/SecP256K1FieldElement.java
+++ core/src/main/java/org/bouncycastle/math/ec/custom/sec/SecP256K1FieldElement.java
@@ -16,7 +16,7 @@ public class SecP256K1FieldElement extends ECFieldElement
     {
         if (x == null || x.signum() < 0 || x.compareTo(Q) >= 0)
         {
-            throw new IllegalArgumentException("x value invalid in FEp256k1 field element");
+            throw new IllegalArgumentException("x value invalid for SecP256K1FieldElement");
         }
 
         this.x = SecP256K1Field.fromBigInteger(x);
