62f90674d6ef103f7735f942710fdacba6314bc3
Erik Tews <erik@datenzone.de>
2014-03-22 19:53:59 +0100

    Improved the client version number check.
    If the client sends a wrong version number within the encrypted
    Pre-Master-Secret, and a version number check is required, the decoded
    Pre-Master-Secret is replaced with a random value in constant time and memory.

--- core/src/main/java/org/bouncycastle/crypto/encodings/PKCS1Encoding.java
+++ core/src/main/java/org/bouncycastle/crypto/encodings/PKCS1Encoding.java
@@ -34,7 +34,8 @@ public class PKCS1Encoding
     private boolean                 forEncryption;
     private boolean                 forPrivateKey;
     private boolean                 useStrictLength;
-    private int pLen = -1;
+    private int                     pLen = -1;
+    private byte[]                  fallback = null;
 
     /**
      * Basic constructor.
@@ -49,6 +50,7 @@ public class PKCS1Encoding
 
     /**
      * Constructor for decryption with a fixed plaintext length.
+     * 
      * @param cipher The cipher to use for cryptographic operation.
      * @param pLen Length of the expected plaintext.
      */
@@ -59,7 +61,27 @@ public class PKCS1Encoding
         this.engine = cipher;
         this.useStrictLength = useStrict();
         this.pLen = pLen;
-    }   
+    }
+
+	/**
+	 * Constructor for decryption with a fixed plaintext length and a fallback
+	 * value that is returned, if the padding is incorrect.
+	 * 
+	 * @param cipher
+	 *            The cipher to use for cryptographic operation.
+	 * @param fallback
+	 *            The fallback value, we don't to a arraycopy here.
+	 */
+	public PKCS1Encoding(
+    	AsymmetricBlockCipher   cipher,
+        byte[] fallback)
+    {
+    	this.engine = cipher;
+    	this.useStrictLength = useStrict();
+    	this.fallback = fallback;
+    	this.pLen = fallback.length;
+    }
+        
 
     
     //
@@ -257,14 +279,19 @@ public class PKCS1Encoding
      * 
      * @throws InvalidCipherTextException
      */
-	private byte[] decodeBlockOrRandom(byte[] in, int inOff, int inLen, int pLen)
+	private byte[] decodeBlockOrRandom(byte[] in, int inOff, int inLen)
 			throws InvalidCipherTextException {
 		if (!forPrivateKey) {
 			throw new InvalidCipherTextException("sorry, this method is only for decryption, not for signing");
 		}
 		byte[] block = engine.processBlock(in, inOff, inLen);
-		byte[] random = new byte[pLen];
-		this.random.nextBytes(random);
+		byte[] random = null;
+		if (this.fallback == null) {
+			random = new byte[this.pLen];
+			this.random.nextBytes(random);
+		} else {
+			random = fallback;
+		}
 		
 		/*
 		 * TODO: This is a potential dangerous side channel. However, you can
@@ -287,14 +314,14 @@ public class PKCS1Encoding
 		/*
 		 * Check the padding.
 		 */
-		int correct = PKCS1Encoding.checkPkcs1Encoding(block, pLen);
+		int correct = PKCS1Encoding.checkPkcs1Encoding(block, this.pLen);
 		
 		/*
 		 * Now, to a constant time constant memory copy of the decrypted value
 		 * or the random value, depending on the validity of the padding.
 		 */
-		byte[] result = new byte[pLen];
-		for (int i = 0; i < pLen; i++) {
+		byte[] result = new byte[this.pLen];
+		for (int i = 0; i < this.pLen; i++) {
 			result[i] = (byte)((block[i+(block.length-pLen)]&(~correct)) | (random[i]&correct));
 		}
 		
@@ -315,7 +342,7 @@ public class PKCS1Encoding
 		 * time decryption. If the decryption fails, we return a random value.
 		 */
 		if (this.pLen != -1) {
-    		return this.decodeBlockOrRandom(in, inOff, inLen, this.pLen);
+    		return this.decodeBlockOrRandom(in, inOff, inLen);
     	}
     	
         byte[]  block = engine.processBlock(in, inOff, inLen);
