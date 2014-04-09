0918e28521cdec6f57d2ad77579d395739423ee8
Rich DiCroce <rdicroce@lapis.com>
2014-04-09 13:10:59 -0400

    Make the RFC 5054 Appendix A values easily accessible

--- core/src/test/java/org/bouncycastle/crypto/tls/test/TlsProtocolTest.java
+++ core/src/test/java/org/bouncycastle/crypto/tls/test/TlsProtocolTest.java
@@ -12,6 +12,7 @@ import java.security.SecureRandom;
 
 import junit.framework.TestCase;
 
+import org.bouncycastle.crypto.agreement.srp.SRP6GroupParameters;
 import org.bouncycastle.crypto.agreement.srp.SRP6VerifierGenerator;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.tls.SRPTlsParameters;
@@ -39,11 +40,8 @@ public class TlsProtocolTest
         byte[] identity = "alice".getBytes(utf8);
         byte[] password = "password123".getBytes(utf8);
         byte[] salt = Hex.decode("BEB25379D1A8581EB5A727673A2441EE");
-        BigInteger prime = new BigInteger(1, Hex.decode(
-                "EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813"
-                + "D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885"
-                + "C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3"));
-        BigInteger generator = BigInteger.valueOf(2);
+        BigInteger prime = SRP6GroupParameters.PRIME_1024;
+        BigInteger generator = SRP6GroupParameters.GENERATOR_1024;
         
         SRP6VerifierGenerator verifierGenerator = new SRP6VerifierGenerator();
         verifierGenerator.init(prime, generator, new SHA1Digest());
