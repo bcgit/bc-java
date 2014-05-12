601934138d98368d99a28e2eb656a072ac92e14c
David Hook <dgh@cryptoworkshop.com>
2014-05-12 21:29:22 +1000

    JavaDoc additions

--- core/src/main/java/org/bouncycastle/crypto/SkippingCipher.java
+++ core/src/main/java/org/bouncycastle/crypto/SkippingCipher.java
@@ -1,16 +1,17 @@
 package org.bouncycastle.crypto;
 
 /**
- * Ciphers producing a keystream which can be moved around
+ * Ciphers producing a key stream which can be reset to particular points in the stream.
  */
 public interface SkippingCipher
 {
     /**
-     * Skip numberOfBlocks forwards, or backwards. If the cipher is a streamcipher a block
+     * Skip numberOfBlocks forwards, or backwards. If the cipher is a stream cipher a block
      * size of 1 is assumed.
      *
      * @param numberOfBlocks the number of blocks to skip (positive forward, negative backwards).
      * @return the number of blocks actually skipped.
+     * @throws java.lang.IllegalArgumentException if numberOfBlocks is an invalid value.
      */
     long skip(long numberOfBlocks);
 }
