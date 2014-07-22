29becdd4a0c9b76e1fa35a4bd82e446382a6a8df
Peter Dettman <peter.dettman@bouncycastle.org>
2014-07-22 21:41:33 +0700

    Add utility methods for classifying cipher suites

--- core/src/main/java/org/bouncycastle/crypto/tls/DTLSProtocol.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/DTLSProtocol.java
@@ -68,23 +68,11 @@ public abstract class DTLSProtocol
     protected static void validateSelectedCipherSuite(int selectedCipherSuite, short alertDescription)
         throws IOException
     {
-        switch (selectedCipherSuite)
+        switch (TlsUtils.getEncryptionAlgorithm(selectedCipherSuite))
         {
-        case CipherSuite.TLS_RSA_EXPORT_WITH_RC4_40_MD5:
-        case CipherSuite.TLS_RSA_WITH_RC4_128_MD5:
-        case CipherSuite.TLS_RSA_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_DH_anon_EXPORT_WITH_RC4_40_MD5:
-        case CipherSuite.TLS_DH_anon_WITH_RC4_128_MD5:
-        case CipherSuite.TLS_PSK_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_DHE_PSK_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_RSA_PSK_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_ECDH_ECDSA_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_ECDH_RSA_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_ECDHE_RSA_WITH_RC4_128_SHA:
-        case CipherSuite.TLS_ECDH_anon_WITH_RC4_128_SHA:
-            // TODO Alert
-            throw new IllegalStateException("RC4 MUST NOT be used with DTLS");
+        case EncryptionAlgorithm.RC4_40:
+        case EncryptionAlgorithm.RC4_128:
+            throw new TlsFatalAlert(alertDescription);
         }
     }
 }
