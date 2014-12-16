17785970abeb48ef21020cc21c6a606913438ed3
Peter Dettman <peter.dettman@bouncycastle.org>
2014-12-16 14:53:34 +0700

    Convert to ints for actual multiplication

--- core/src/main/java/org/bouncycastle/crypto/modes/gcm/BasicGCMMultiplier.java
+++ core/src/main/java/org/bouncycastle/crypto/modes/gcm/BasicGCMMultiplier.java
@@ -1,18 +1,18 @@
 package org.bouncycastle.crypto.modes.gcm;
 
-import org.bouncycastle.util.Arrays;
-
 public class BasicGCMMultiplier implements GCMMultiplier
 {
-    private byte[] H;
+    private int[] H;
 
     public void init(byte[] H)
     {
-        this.H = Arrays.clone(H);
+        this.H = GCMUtil.asInts(H);
     }
 
     public void multiplyH(byte[] x)
     {
-        GCMUtil.multiply(x, H);
+        int[] t = GCMUtil.asInts(x);
+        GCMUtil.multiply(t, H);
+        GCMUtil.asBytes(t, x);
     }
 }
