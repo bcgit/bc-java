c4fd24f8e94c3f53b2b8b903a21f703ca08b51cb
Tim Whittington <bc@whittington.net.nz>
2015-01-06 10:04:37 +1300

    Javadoc for KeyParameter.

--- core/src/main/java/org/bouncycastle/crypto/params/KeyParameter.java
+++ core/src/main/java/org/bouncycastle/crypto/params/KeyParameter.java
@@ -2,17 +2,34 @@ package org.bouncycastle.crypto.params;
 
 import org.bouncycastle.crypto.CipherParameters;
 
+/**
+ * A key parameter to a cipher.
+ */
 public class KeyParameter
     implements CipherParameters
 {
     private byte[]  key;
 
+    /**
+     * Construct a key parameter from key data.<br/>
+     * The key data is copied by this constructor.
+     *
+     * @param key the key data.
+     */
     public KeyParameter(
         byte[]  key)
     {
         this(key, 0, key.length);
     }
 
+    /**
+     * Construct a key parameter from key data.<br/>
+     * The key data is copied by this constructor.
+     *
+     * @param key the array containing the key data.
+     * @param keyOff the offset in the array where the key data begins.
+     * @param keyLen the length of the key data.
+     */
     public KeyParameter(
         byte[]  key,
         int     keyOff,
@@ -23,6 +40,9 @@ public class KeyParameter
         System.arraycopy(key, keyOff, this.key, 0, keyLen);
     }
 
+    /**
+     * Obtains a mutable reference to the key data in this parameter.
+     */
     public byte[] getKey()
     {
         return key;
