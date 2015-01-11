7b9083391bede0a0ee7f48a10c3eb174ae73b1a0
David Hook <dgh@cryptoworkshop.com>
2015-01-12 10:34:08 +1100

    added Encodable interface

--- pg/src/main/java/org/bouncycastle/bcpg/BCPGObject.java
+++ pg/src/main/java/org/bouncycastle/bcpg/BCPGObject.java
@@ -3,10 +3,13 @@ package org.bouncycastle.bcpg;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import org.bouncycastle.util.Encodable;
+
 /**
  * Base class for a PGP object.
  */
 public abstract class BCPGObject
+    implements Encodable
 {
     public byte[] getEncoded()
         throws IOException
