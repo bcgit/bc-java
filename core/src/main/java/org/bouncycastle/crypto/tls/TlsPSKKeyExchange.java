6915742533506af0f3e64d4c48c67505bbffdd32
Peter Dettman <peter.dettman@bouncycastle.org>
2015-01-13 14:41:51 +0700

    Add server-side support for PSK key exchanges. Add PSKTlsServer as a simple PSK server implementation. Add support for ECDHE-PSK key exchange in client and server. Add TLS test variants using PSK exchanges.

--- core/src/main/java/org/bouncycastle/crypto/tls/TlsPSKKeyExchange.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/TlsPSKKeyExchange.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 import java.util.Vector;
 
 import org.bouncycastle.asn1.x509.KeyUsage;
@@ -12,8 +13,13 @@ import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.DHParameters;
 import org.bouncycastle.crypto.params.DHPrivateKeyParameters;
 import org.bouncycastle.crypto.params.DHPublicKeyParameters;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
 import org.bouncycastle.crypto.params.RSAKeyParameters;
 import org.bouncycastle.crypto.util.PublicKeyFactory;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.io.Streams;
 
 /**
  * (D)TLS PSK key exchange (RFC 4279).
@@ -22,14 +28,20 @@ public class TlsPSKKeyExchange
     extends AbstractTlsKeyExchange
 {
     protected TlsPSKIdentity pskIdentity;
+    protected TlsPSKIdentityManager pskIdentityManager;
+
     protected DHParameters dhParameters;
     protected int[] namedCurves;
     protected short[] clientECPointFormats, serverECPointFormats;
 
     protected byte[] psk_identity_hint = null;
+    protected byte[] psk = null;
 
     protected DHPrivateKeyParameters dhAgreePrivateKey = null;
     protected DHPublicKeyParameters dhAgreePublicKey = null;
+
+    protected ECPrivateKeyParameters ecAgreePrivateKey = null;
+    protected ECPublicKeyParameters ecAgreePublicKey = null;
 
     protected AsymmetricKeyParameter serverPublicKey = null;
     protected RSAKeyParameters rsaServerPublicKey = null;
@@ -37,7 +49,8 @@ public class TlsPSKKeyExchange
     protected byte[] premasterSecret;
 
     public TlsPSKKeyExchange(int keyExchange, Vector supportedSignatureAlgorithms, TlsPSKIdentity pskIdentity,
-        DHParameters dhParameters, int[] namedCurves, short[] clientECPointFormats, short[] serverECPointFormats)
+        TlsPSKIdentityManager pskIdentityManager, DHParameters dhParameters, int[] namedCurves,
+        short[] clientECPointFormats, short[] serverECPointFormats)
     {
         super(keyExchange, supportedSignatureAlgorithms);
 
@@ -53,14 +66,14 @@ public class TlsPSKKeyExchange
         }
 
         this.pskIdentity = pskIdentity;
+        this.pskIdentityManager = pskIdentityManager;
         this.dhParameters = dhParameters;
         this.namedCurves = namedCurves;
         this.clientECPointFormats = clientECPointFormats;
         this.serverECPointFormats = serverECPointFormats;
     }
 
-    public void skipServerCredentials()
-        throws IOException
+    public void skipServerCredentials() throws IOException
     {
         if (keyExchange == KeyExchangeAlgorithm.RSA_PSK)
         {
@@ -68,8 +81,7 @@ public class TlsPSKKeyExchange
         }
     }
 
-    public void processServerCredentials(TlsCredentials serverCredentials)
-        throws IOException
+    public void processServerCredentials(TlsCredentials serverCredentials) throws IOException
     {
         if (!(serverCredentials instanceof TlsEncryptionCredentials))
         {
@@ -83,8 +95,7 @@ public class TlsPSKKeyExchange
 
     public byte[] generateServerKeyExchange() throws IOException
     {
-        // TODO[RFC 4279] Need a server-side PSK API to determine hint and resolve identities to keys
-        this.psk_identity_hint = null;
+        this.psk_identity_hint = pskIdentityManager.getHint();
 
         if (this.psk_identity_hint == null && !requiresServerKeyExchange())
         {
@@ -114,14 +125,14 @@ public class TlsPSKKeyExchange
         }
         else if (this.keyExchange == KeyExchangeAlgorithm.ECDHE_PSK)
         {
-            // TODO[RFC 5489]
+            this.ecAgreePrivateKey = TlsECCUtils.generateEphemeralServerKeyExchange(context.getSecureRandom(),
+                namedCurves, clientECPointFormats, buf);
         }
 
         return buf.toByteArray();
     }
 
-    public void processServerCertificate(Certificate serverCertificate)
-        throws IOException
+    public void processServerCertificate(Certificate serverCertificate) throws IOException
     {
         if (keyExchange != KeyExchangeAlgorithm.RSA_PSK)
         {
@@ -169,8 +180,7 @@ public class TlsPSKKeyExchange
         }
     }
 
-    public void processServerKeyExchange(InputStream input)
-        throws IOException
+    public void processServerKeyExchange(InputStream input) throws IOException
     {
         this.psk_identity_hint = TlsUtils.readOpaque16(input);
 
@@ -182,24 +192,26 @@ public class TlsPSKKeyExchange
         }
         else if (this.keyExchange == KeyExchangeAlgorithm.ECDHE_PSK)
         {
-            // TODO[RFC 5489]
+            ECDomainParameters ecParams = TlsECCUtils.readECParameters(namedCurves, clientECPointFormats, input);
+
+            byte[] point = TlsUtils.readOpaque8(input);
+
+            this.ecAgreePublicKey = TlsECCUtils.validateECPublicKey(TlsECCUtils.deserializeECPublicKey(
+                clientECPointFormats, ecParams, point));
         }
     }
 
-    public void validateCertificateRequest(CertificateRequest certificateRequest)
-        throws IOException
+    public void validateCertificateRequest(CertificateRequest certificateRequest) throws IOException
     {
         throw new TlsFatalAlert(AlertDescription.unexpected_message);
     }
 
-    public void processClientCredentials(TlsCredentials clientCredentials)
-        throws IOException
+    public void processClientCredentials(TlsCredentials clientCredentials) throws IOException
     {
         throw new TlsFatalAlert(AlertDescription.internal_error);
     }
 
-    public void generateClientKeyExchange(OutputStream output)
-        throws IOException
+    public void generateClientKeyExchange(OutputStream output) throws IOException
     {
         if (psk_identity_hint == null)
         {
@@ -211,6 +223,16 @@ public class TlsPSKKeyExchange
         }
 
         byte[] psk_identity = pskIdentity.getPSKIdentity();
+        if (psk_identity == null)
+        {
+            throw new TlsFatalAlert(AlertDescription.internal_error);
+        }
+
+        this.psk = pskIdentity.getPSK();
+        if (psk == null)
+        {
+            throw new TlsFatalAlert(AlertDescription.internal_error);
+        }
 
         TlsUtils.writeOpaque16(psk_identity, output);
 
@@ -221,8 +243,8 @@ public class TlsPSKKeyExchange
         }
         else if (this.keyExchange == KeyExchangeAlgorithm.ECDHE_PSK)
         {
-            // TODO[RFC 5489]
-            throw new TlsFatalAlert(AlertDescription.internal_error);
+            this.ecAgreePrivateKey = TlsECCUtils.generateEphemeralClientKeyExchange(context.getSecureRandom(),
+                serverECPointFormats, ecAgreePublicKey.getParameters(), output);
         }
         else if (this.keyExchange == KeyExchangeAlgorithm.RSA_PSK)
         {
@@ -231,15 +253,59 @@ public class TlsPSKKeyExchange
         }
     }
 
-    public byte[] generatePremasterSecret()
-        throws IOException
+    public void processClientKeyExchange(InputStream input) throws IOException
     {
-        byte[] psk = pskIdentity.getPSK();
+        byte[] psk_identity = TlsUtils.readOpaque16(input);
+
+        this.psk = pskIdentityManager.getPSK(psk_identity);
+        if (psk == null)
+        {
+            throw new TlsFatalAlert(AlertDescription.unknown_psk_identity);
+        }
+
+        if (this.keyExchange == KeyExchangeAlgorithm.DHE_PSK)
+        {
+            BigInteger Yc = TlsDHUtils.readDHParameter(input);
+
+            this.dhAgreePublicKey = TlsDHUtils.validateDHPublicKey(new DHPublicKeyParameters(Yc, dhParameters));
+        }
+        else if (this.keyExchange == KeyExchangeAlgorithm.ECDHE_PSK)
+        {
+            byte[] point = TlsUtils.readOpaque8(input);
+
+            ECDomainParameters curve_params = this.ecAgreePrivateKey.getParameters();
+
+            this.ecAgreePublicKey = TlsECCUtils.validateECPublicKey(TlsECCUtils.deserializeECPublicKey(
+                serverECPointFormats, curve_params, point));
+        }
+        else if (this.keyExchange == KeyExchangeAlgorithm.RSA_PSK)
+        {
+            byte[] encryptedPreMasterSecret;
+            if (TlsUtils.isSSL(context))
+            {
+                // TODO Do any SSLv3 clients actually include the length?
+                encryptedPreMasterSecret = Streams.readAll(input);
+            }
+            else
+            {
+                encryptedPreMasterSecret = TlsUtils.readOpaque16(input);
+            }
+
+            this.premasterSecret = serverCredentials.decryptPreMasterSecret(encryptedPreMasterSecret);
+        }
+    }
+
+    public byte[] generatePremasterSecret() throws IOException
+    {
         byte[] other_secret = generateOtherSecret(psk.length);
 
         ByteArrayOutputStream buf = new ByteArrayOutputStream(4 + other_secret.length + psk.length);
         TlsUtils.writeOpaque16(other_secret, buf);
         TlsUtils.writeOpaque16(psk, buf);
+
+        Arrays.fill(psk, (byte)0);
+        this.psk = null;
+
         return buf.toByteArray();
     }
 
@@ -257,7 +323,11 @@ public class TlsPSKKeyExchange
 
         if (this.keyExchange == KeyExchangeAlgorithm.ECDHE_PSK)
         {
-            // TODO[RFC 5489]
+            if (ecAgreePrivateKey != null)
+            {
+                return TlsECCUtils.calculateECDHBasicAgreement(ecAgreePublicKey, ecAgreePrivateKey);
+            }
+
             throw new TlsFatalAlert(AlertDescription.internal_error);
         }
 
@@ -269,8 +339,7 @@ public class TlsPSKKeyExchange
         return new byte[pskLength];
     }
 
-    protected RSAKeyParameters validateRSAPublicKey(RSAKeyParameters key)
-        throws IOException
+    protected RSAKeyParameters validateRSAPublicKey(RSAKeyParameters key) throws IOException
     {
         // TODO What is the minimum bit length required?
         // key.getModulus().bitLength();
