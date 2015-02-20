ee2b8bd7a3833fb3981e254ffe6b304301a06acd
Peter Dettman <peter.dettman@bouncycastle.org>
2015-02-20 11:33:32 +0700

    Javadoc spelling fix

--- core/src/main/java/org/bouncycastle/crypto/BufferedBlockCipher.java
+++ core/src/main/java/org/bouncycastle/crypto/BufferedBlockCipher.java
@@ -141,7 +141,7 @@ public class BufferedBlockCipher
     }
 
     /**
-     * process a single byte, producing an output block if neccessary.
+     * process a single byte, producing an output block if necessary.
      *
      * @param in the input byte.
      * @param out the space for any output that might be produced.
