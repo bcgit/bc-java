7721ff9557c2db9c70941177a94314013d7079ed
David Hook <dgh@cryptoworkshop.com>
2015-02-23 23:31:15 +1100

    Added getInstance() with flag for implicit.

--- core/src/main/java/org/bouncycastle/asn1/cms/EncryptedData.java
+++ core/src/main/java/org/bouncycastle/asn1/cms/EncryptedData.java
@@ -6,6 +6,7 @@ import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1Set;
+import org.bouncycastle.asn1.ASN1TaggedObject;
 import org.bouncycastle.asn1.BERSequence;
 import org.bouncycastle.asn1.BERTaggedObject;
 
@@ -73,7 +74,7 @@ public class EncryptedData
 
         if (seq.size() == 3)
         {
-            this.unprotectedAttrs = ASN1Set.getInstance(seq.getObjectAt(2));
+            this.unprotectedAttrs = ASN1Set.getInstance((ASN1TaggedObject)seq.getObjectAt(2), false);
         }
     }
 
