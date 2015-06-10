51aae896376c2129a9cf6c4276285aa95b3c7f16
David Hook <dgh@cryptoworkshop.com>
2015-06-11 07:09:27 +1000

    exception rationalisation

--- core/src/main/java/org/bouncycastle/asn1/x500/style/AbstractX500NameStyle.java
+++ core/src/main/java/org/bouncycastle/asn1/x500/style/AbstractX500NameStyle.java
@@ -6,6 +6,7 @@ import java.util.Hashtable;
 
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.ASN1ParsingException;
 import org.bouncycastle.asn1.DERUTF8String;
 import org.bouncycastle.asn1.x500.AttributeTypeAndValue;
 import org.bouncycastle.asn1.x500.RDN;
@@ -101,7 +102,7 @@ public abstract class AbstractX500NameStyle
             }
             catch (IOException e)
             {
-                throw new RuntimeException("can't recode value for oid " + oid.getId());
+                throw new ASN1ParsingException("can't recode value for oid " + oid.getId());
             }
         }
 
