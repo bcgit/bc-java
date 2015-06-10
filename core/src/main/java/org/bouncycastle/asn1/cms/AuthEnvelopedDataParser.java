215685d73538b08d865e314acfbf427328c9a00e
David Hook <dgh@cryptoworkshop.com>
2015-06-10 15:06:42 +1000

    fixed TODO

--- core/src/main/java/org/bouncycastle/asn1/cms/AuthEnvelopedDataParser.java
+++ core/src/main/java/org/bouncycastle/asn1/cms/AuthEnvelopedDataParser.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1ParsingException;
 import org.bouncycastle.asn1.ASN1SequenceParser;
 import org.bouncycastle.asn1.ASN1SetParser;
 import org.bouncycastle.asn1.ASN1TaggedObjectParser;
@@ -30,14 +31,18 @@ public class AuthEnvelopedDataParser
     private ASN1Integer version;
     private ASN1Encodable nextObject;
     private boolean originatorInfoCalled;
+    private EncryptedContentInfoParser authEncryptedContentInfoParser;
 
     public AuthEnvelopedDataParser(ASN1SequenceParser seq) throws IOException
     {
         this.seq = seq;
 
-        // TODO
         // "It MUST be set to 0."
         this.version = ASN1Integer.getInstance(seq.readObject());
+        if (this.version.getValue().intValue() != 0)
+        {
+            throw new ASN1ParsingException("AuthEnvelopedData version number must be 0");
+        }
     }
 
     public ASN1Integer getVersion()
@@ -83,7 +88,7 @@ public class AuthEnvelopedDataParser
         return recipientInfos;
     }
 
-    public EncryptedContentInfoParser getAuthEncryptedContentInfo() 
+    public EncryptedContentInfoParser getAuthEncryptedContentInfo()
         throws IOException
     {
         if (nextObject == null)
@@ -95,7 +100,8 @@ public class AuthEnvelopedDataParser
         {
             ASN1SequenceParser o = (ASN1SequenceParser) nextObject;
             nextObject = null;
-            return new EncryptedContentInfoParser(o);
+            authEncryptedContentInfoParser = new EncryptedContentInfoParser(o);
+            return authEncryptedContentInfoParser;
         }
 
         return null;
@@ -116,9 +122,12 @@ public class AuthEnvelopedDataParser
             return (ASN1SetParser)((ASN1TaggedObjectParser)o).getObjectParser(BERTags.SET, false);
         }
 
-        // TODO
         // "The authAttrs MUST be present if the content type carried in
         // EncryptedContentInfo is not id-data."
+        if (!authEncryptedContentInfoParser.getContentType().equals(CMSObjectIdentifiers.data))
+        {
+            throw new ASN1ParsingException("authAttrs must be present with non-data content");
+        }
 
         return null;
     }
