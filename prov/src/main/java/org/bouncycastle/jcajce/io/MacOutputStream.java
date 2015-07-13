9cf5afc1cfff4aba494c3972a70ae8f3fec6eb12
David Hook <dgh@cryptoworkshop.com>
2015-07-13 18:15:56 +1000

    JavaDoc

--- prov/src/main/java/org/bouncycastle/jcajce/io/MacOutputStream.java
+++ prov/src/main/java/org/bouncycastle/jcajce/io/MacOutputStream.java
@@ -5,32 +5,60 @@ import java.io.OutputStream;
 
 import javax.crypto.Mac;
 
-public class MacOutputStream
+/**
+ * An output stream which calculates a MAC based on the data that is written to it.
+ */
+public final class MacOutputStream
     extends OutputStream
 {
-    protected Mac mac;
+    private Mac mac;
 
+    /**
+     * Base constructor - specify the MAC algorithm to use.
+     *
+     * @param mac the MAC implementation to use as the basis of the stream.
+     */
     public MacOutputStream(
-        Mac          mac)
+        Mac mac)
     {
         this.mac = mac;
     }
 
+    /**
+     * Write a single byte to the stream.
+     *
+     * @param b the byte value to write.
+     * @throws IOException  in case of failure.
+     */
     public void write(int b)
         throws IOException
     {
         mac.update((byte)b);
     }
 
+    /**
+     * Write a block of data of length len starting at offset off in the byte array bytes to
+     * the stream.
+     *
+     * @param bytes byte array holding the data.
+     * @param off offset into bytes that the data starts at.
+     * @param len the length of the data block to write.
+     * @throws IOException in case of failure.
+     */
     public void write(
-        byte[] b,
+        byte[] bytes,
         int off,
         int len)
         throws IOException
     {
-        mac.update(b, off, len);
+        mac.update(bytes, off, len);
     }
 
+    /**
+     * Execute doFinal() and return the calculated MAC.
+     *
+     * @return the MAC calculated from the output written to the stream.
+     */
     public byte[] getMac()
     {
         return mac.doFinal();
