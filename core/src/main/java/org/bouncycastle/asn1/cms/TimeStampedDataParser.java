95c15184ae1d74a5a98ec669aa1f6e380c329431
David Hook <dgh@cryptoworkshop.com>
2015-07-13 16:49:37 +1000

    removed dangerous deprecated method...

--- core/src/main/java/org/bouncycastle/asn1/cms/TimeStampedDataParser.java
+++ core/src/main/java/org/bouncycastle/asn1/cms/TimeStampedDataParser.java
@@ -3,13 +3,10 @@ package org.bouncycastle.asn1.cms;
 import java.io.IOException;
 
 import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1OctetStringParser;
-import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1SequenceParser;
-import org.bouncycastle.asn1.BERSequence;
 import org.bouncycastle.asn1.DERIA5String;
 
 /**
@@ -98,44 +95,5 @@ public class TimeStampedDataParser
         }
 
         return temporalEvidence;
-    }
-
-    /**
-     * <pre>
-     * TimeStampedData ::= SEQUENCE {
-     *   version              INTEGER { v1(1) },
-     *   dataUri              IA5String OPTIONAL,
-     *   metaData             MetaData OPTIONAL,
-     *   content              OCTET STRING OPTIONAL,
-     *   temporalEvidence     Evidence
-     * }
-     * </pre>
-     * @return
-     * @deprecated will be removed
-     */
-    public ASN1Primitive toASN1Primitive()
-    {
-        ASN1EncodableVector v = new ASN1EncodableVector();
-
-        v.add(version);
-
-        if (dataUri != null)
-        {
-            v.add(dataUri);
-        }
-
-        if (metaData != null)
-        {
-            v.add(metaData);
-        }
-
-        if (content != null)
-        {
-            v.add(content);
-        }
-
-        v.add(temporalEvidence);
-
-        return new BERSequence(v);
     }
 }
