a094396323d8014d71d76839b75c3d768b8394f7
Peter Dettman <peter.dettman@bouncycastle.org>
2015-07-26 00:54:26 +0700

    Address several TODOs

--- core/src/main/java/org/bouncycastle/crypto/generators/DSAParametersGenerator.java
+++ core/src/main/java/org/bouncycastle/crypto/generators/DSAParametersGenerator.java
@@ -17,17 +17,17 @@ import org.bouncycastle.util.encoders.Hex;
  */
 public class DSAParametersGenerator
 {
-    private Digest          digest;
-    private int             L, N;
-    private int             certainty;
-    private SecureRandom    random;
-
     private static final BigInteger ZERO = BigInteger.valueOf(0);
     private static final BigInteger ONE = BigInteger.valueOf(1);
     private static final BigInteger TWO = BigInteger.valueOf(2);
 
-    private boolean use186_3;
-    private int usageIndex;
+    private Digest          digest;
+    private int             L, N;
+    private int             certainty;
+    private int             iterations;
+    private SecureRandom    random;
+    private boolean         use186_3;
+    private int             usageIndex;
 
     public DSAParametersGenerator()
     {
@@ -51,11 +51,13 @@ public class DSAParametersGenerator
         int             certainty,
         SecureRandom    random)
     {
-        this.use186_3 = false;
         this.L = size;
         this.N = getDefaultN(size);
         this.certainty = certainty;
+        this.iterations = Math.max(getMinimumIterations(L), (certainty + 1) / 2);
         this.random = random;
+        this.use186_3 = false;
+        this.usageIndex = -1;
     }
 
     /**
@@ -69,13 +71,7 @@ public class DSAParametersGenerator
     public void init(
         DSAParameterGenerationParameters params)
     {
-        // TODO Should we enforce the minimum 'certainty' values as per C.3 Table C.1?
-        this.use186_3 = true;
-        this.L = params.getL();
-        this.N = params.getN();
-        this.certainty = params.getCertainty();
-        this.random = params.getRandom();
-        this.usageIndex = params.getUsageIndex();
+        int L = params.getL(), N = params.getN();
 
         if ((L < 1024 || L > 3072) || L % 1024 != 0)
         {
@@ -98,6 +94,14 @@ public class DSAParametersGenerator
         {
             throw new IllegalStateException("Digest output size too small for value of N");
         }
+
+        this.L = L;
+        this.N = N;
+        this.certainty = params.getCertainty();
+        this.iterations = Math.max(getMinimumIterations(L), (certainty + 1) / 2);
+        this.random = params.getRandom();
+        this.use186_3 = true;
+        this.usageIndex = params.getUsageIndex();
     }
 
     /**
@@ -131,10 +135,10 @@ public class DSAParametersGenerator
         {
             random.nextBytes(seed);
 
-            hash(digest, seed, part1);
+            hash(digest, seed, part1, 0);
             System.arraycopy(seed, 0, part2, 0, seed.length);
             inc(part2);
-            hash(digest, part2, part2);
+            hash(digest, part2, part2, 0);
 
             for (int i = 0; i != u.length; i++)
             {
@@ -146,7 +150,7 @@ public class DSAParametersGenerator
 
             BigInteger q = new BigInteger(1, u);
 
-            if (!q.isProbablePrime(certainty))
+            if (!isProbablePrime(q))
             {
                 continue;
             }
@@ -156,18 +160,20 @@ public class DSAParametersGenerator
 
             for (int counter = 0; counter < 4096; ++counter)
             {
-                for (int k = 0; k < n; k++)
                 {
+                    for (int k = 1; k <= n; k++)
+                    {
+                        inc(offset);
+                        hash(digest, offset, w, w.length - k * part1.length);
+                    }
+
+                    int remaining = w.length - (n * part1.length);
                     inc(offset);
-                    hash(digest, offset, part1);
-                    System.arraycopy(part1, 0, w, w.length - (k + 1) * part1.length, part1.length);
+                    hash(digest, offset, part1, 0);
+                    System.arraycopy(part1, part1.length - remaining, w, 0, remaining);
+
+                    w[0] |= (byte)0x80;
                 }
-
-                inc(offset);
-                hash(digest, offset, part1);
-                System.arraycopy(part1, part1.length - ((w.length - (n) * part1.length)), w, 0, w.length - n * part1.length);
-
-                w[0] |= (byte)0x80;
 
                 BigInteger x = new BigInteger(1, w);
 
@@ -180,7 +186,7 @@ public class DSAParametersGenerator
                     continue;
                 }
 
-                if (p.isProbablePrime(certainty))
+                if (isProbablePrime(p))
                 {
                     BigInteger g = calculateGenerator_FIPS186_2(p, q, random);
 
@@ -232,6 +238,7 @@ public class DSAParametersGenerator
 // 4. b = L - 1 - (n * outlen).
         int b = (L - 1) % outlen;
 
+        byte[] w = new byte[L / 8];
         byte[] output = new byte[d.getDigestSize()];
         for (;;)
         {
@@ -239,7 +246,7 @@ public class DSAParametersGenerator
             random.nextBytes(seed);
 
 // 6. U = Hash (domain_parameter_seed) mod 2^(N–1).
-            hash(d, seed, output);
+            hash(d, seed, output, 0);
 
             BigInteger U = new BigInteger(1, output).mod(ONE.shiftLeft(N - 1));
 
@@ -247,8 +254,7 @@ public class DSAParametersGenerator
             BigInteger q = U.setBit(0).setBit(N - 1);
 
 // 8. Test whether or not q is prime as specified in Appendix C.3.
-            // TODO Review C.3 for primality checking
-            if (!q.isProbablePrime(certainty))
+            if (!isProbablePrime(q))
             {
 // 9. If q is not a prime, then go to step 5.
                 continue;
@@ -265,24 +271,23 @@ public class DSAParametersGenerator
 // 11.1 For j = 0 to n do
 //      Vj = Hash ((domain_parameter_seed + offset + j) mod 2^seedlen).
 // 11.2 W = V0 + (V1 ∗ 2^outlen) + ... + (V^(n–1) ∗ 2^((n–1) ∗ outlen)) + ((Vn mod 2^b) ∗ 2^(n ∗ outlen)).
-                // TODO Assemble w as a byte array
-                BigInteger W = ZERO;
-                for (int j = 0, exp = 0; j <= n; ++j, exp += outlen)
                 {
-                    inc(offset);
-                    hash(d, offset, output);
-
-                    BigInteger Vj = new BigInteger(1, output);
-                    if (j == n)
+                    for (int j = 1; j <= n; ++j)
                     {
-                        Vj = Vj.mod(ONE.shiftLeft(b));
+                        inc(offset);
+                        hash(d, offset, w, w.length - j * output.length);
                     }
 
-                    W = W.add(Vj.shiftLeft(exp));
+                    int remaining = w.length - (n * output.length);
+                    inc(offset);
+                    hash(d, offset, output, 0);
+                    System.arraycopy(output, output.length - remaining, w, 0, remaining);
+
+// 11.3 X = W + 2^(L–1). Comment: 0 ≤ W < 2^(L–1); hence, 2^(L–1) ≤ X < 2^L.
+                    w[0] |= (byte)0x80;
                 }
 
-// 11.3 X = W + 2^(L–1). Comment: 0 ≤ W < 2L–1; hence, 2L–1 ≤ X < 2L.
-                BigInteger X = W.add(ONE.shiftLeft(L - 1));
+                BigInteger X = new BigInteger(1, w);
  
 // 11.4 c = X mod 2q.
                 BigInteger c = X.mod(q.shiftLeft(1));
@@ -290,15 +295,14 @@ public class DSAParametersGenerator
 // 11.5 p = X - (c - 1). Comment: p ≡ 1 (mod 2q).
                 BigInteger p = X.subtract(c.subtract(ONE));
 
-// 11.6 If (p < 2^(L - 1)), then go to step 11.9
+// 11.6 If (p < 2^(L-1)), then go to step 11.9
                 if (p.bitLength() != L)
                 {
                     continue;
                 }
 
 // 11.7 Test whether or not p is prime as specified in Appendix C.3.
-                // TODO Review C.3 for primality checking
-                if (p.isProbablePrime(certainty))
+                if (isProbablePrime(p))
                 {
 // 11.8 If p is determined to be prime, then return VALID and the values of p, q and
 //      (optionally) the values of domain_parameter_seed and counter.
@@ -325,6 +329,16 @@ public class DSAParametersGenerator
         }
     }
 
+    private boolean isProbablePrime(BigInteger x)
+    {
+        /*
+         * TODO Use Primes class for FIPS 186-4 C.3 primality checking - but it breaks existing
+         * tests using FixedSecureRandom
+         */
+//        return !Primes.hasAnySmallFactors(x) && Primes.isMRProbablePrime(x, random, iterations);
+        return x.isProbablePrime(certainty);
+    }
+
     private static BigInteger calculateGenerator_FIPS186_3_Unverifiable(BigInteger p, BigInteger q,
         SecureRandom r)
     {
@@ -348,7 +362,7 @@ public class DSAParametersGenerator
         for (int count = 1; count < (1 << 16); ++count)
         {
             inc(U);
-            hash(d, U, w);
+            hash(d, U, w, 0);
             BigInteger W = new BigInteger(1, w);
             BigInteger g = W.modPow(e, p);
             if (g.compareTo(TWO) >= 0)
@@ -360,15 +374,21 @@ public class DSAParametersGenerator
         return null;
     }
 
-    private static void hash(Digest d, byte[] input, byte[] output)
+    private static void hash(Digest d, byte[] input, byte[] output, int outputPos)
     {
         d.update(input, 0, input.length);
-        d.doFinal(output, 0);
+        d.doFinal(output, outputPos);
     }
 
     private static int getDefaultN(int L)
     {
         return L > 1024 ? 256 : 160;
+    }
+
+    private static int getMinimumIterations(int L)
+    {
+        // Values based on FIPS 186-4 C.3 Table C.1
+        return L <= 1024 ? 40 : (48 + 8 * ((L - 1) / 1024)); 
     }
 
     private static void inc(byte[] buf)
