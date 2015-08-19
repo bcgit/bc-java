8db9bde1bd88a7dbf814252a02146e362b20c4e3
Peter Dettman <peter.dettman@bouncycastle.org>
2015-08-19 13:32:35 +0700

    Add test coverage of math.Primes

---
+++ core/src/test/java/org/bouncycastle/math/test/AllTests.java
@@ -0,0 +1,59 @@
+package org.bouncycastle.math.test;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests 
+{
+    public static void main (String[] args) 
+        throws Exception
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    public static Test suite() 
+        throws Exception
+    {   
+        TestSuite suite = new TestSuite("Math tests");
+
+        suite.addTest(PrimesTest.suite());
+
+        return new BCTestSetup(suite);
+    }
+
+    static List enumToList(Enumeration en)
+    {
+        List rv = new ArrayList();
+
+        while (en.hasMoreElements())
+        {
+            rv.add(en.nextElement());
+        }
+
+        return rv;
+    }
+
+    static class BCTestSetup
+        extends TestSetup
+    {
+        public BCTestSetup(Test test)
+        {
+            super(test);
+        }
+
+        protected void setUp()
+        {
+
+        }
+
+        protected void tearDown()
+        {
+
+        }
+    }
+}
