084ac499c0d4fc9dcbf4ce78f8e20d97459b4665
David Hook <dgh@cryptoworkshop.com>
2015-09-07 10:05:51 +1000

    moved UKM generator out of provider.

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/util/KeyMaterialGenerator.java
+++
@@ -1,8 +0,0 @@
-package org.bouncycastle.jcajce.provider.asymmetric.util;
-
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-
-public interface KeyMaterialGenerator
-{
-    byte[] generateKDFMaterial(ASN1ObjectIdentifier keyAlgorithm, int keySize, byte[] userKeyMaterialParameters);
-}
