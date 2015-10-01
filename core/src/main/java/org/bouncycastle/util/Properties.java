0990783d5243453c1b4aeb61b3553fa9237fbd5a
David Hook <dgh@cryptoworkshop.com>
2015-10-01 13:21:20 +1000

    added MQV property

---
+++ core/src/main/java/org/bouncycastle/util/Properties.java
@@ -0,0 +1,35 @@
+package org.bouncycastle.util;
+
+import java.security.AccessControlException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+/**
+ * Utility method for accessing system properties.
+ */
+public class Properties
+{
+    public static boolean isOverrideSet(final String propertyName)
+    {
+        try
+        {
+            return "true".equals(AccessController.doPrivileged(new PrivilegedAction<String>()
+            {
+                public String run()
+                {
+                    String value = System.getProperty(propertyName);
+                    if (value == null)
+                    {
+                        return null;
+                    }
+
+                    return Strings.toLowerCase(value);
+                }
+            }));
+        }
+        catch (AccessControlException e)
+        {
+            return false;
+        }
+    }
+}
