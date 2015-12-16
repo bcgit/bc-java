dddc76e05a722e580c4152f5e292a20c65903b84
David Hook <dgh@cryptoworkshop.com>
2015-12-17 06:16:49 +1100

    JavaDoc

--- pkix/src/main/java/org/bouncycastle/openssl/PEMDecryptor.java
+++ pkix/src/main/java/org/bouncycastle/openssl/PEMDecryptor.java
@@ -1,7 +1,18 @@
 package org.bouncycastle.openssl;
 
+/**
+ * Base interface for decryption operations.
+ */
 public interface PEMDecryptor
 {
-    byte[] decrypt(byte[] keyBytes, byte[] iv)
+    /**
+     * Decrypt the passed in data using the associated IV and the decryptor's key state.
+     *
+     * @param data the encrypted data
+     * @param iv the initialisation vector associated with the decryption.
+     * @return the decrypted data.
+     * @throws PEMException in the event of an issue.
+     */
+    byte[] decrypt(byte[] data, byte[] iv)
         throws PEMException;
 }
