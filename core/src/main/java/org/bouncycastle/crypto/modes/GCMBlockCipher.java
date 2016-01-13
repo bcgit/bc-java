2d80e6cc6f5b78e159dba3277414e3bfea511dea
David Hook <dgh@cryptoworkshop.com>
2016-01-14 04:45:47 +1100

    Set max P to 2^32 - 256

--- core/src/main/java/org/bouncycastle/crypto/modes/GCMBlockCipher.java
+++ core/src/main/java/org/bouncycastle/crypto/modes/GCMBlockCipher.java
@@ -177,7 +177,7 @@ public class GCMBlockCipher
         this.atLength = 0;
         this.atLengthPre = 0;
         this.counter = Arrays.clone(J0);
-        this.blocksRemaining = -1;
+        this.blocksRemaining = -2;      // page 8, len(P) <= 2^39 - 256
         this.bufOff = 0;
         this.totalLength = 0;
 
@@ -458,7 +458,7 @@ public class GCMBlockCipher
         atLength = 0;
         atLengthPre = 0;
         counter = Arrays.clone(J0);
-        blocksRemaining = -1;
+        blocksRemaining = -2;
         bufOff = 0;
         totalLength = 0;
 
