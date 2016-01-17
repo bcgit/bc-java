26af49cbeabee038ddef7f8cb1209cc837ece156
Peter Dettman <peter.dettman@bouncycastle.org>
2016-01-17 14:54:43 +0700

    Remove old cipher suite names from commented out code

--- core/src/test/java/org/bouncycastle/crypto/tls/test/MockTlsClient.java
+++ core/src/test/java/org/bouncycastle/crypto/tls/test/MockTlsClient.java
@@ -64,10 +64,6 @@ class MockTlsClient
 //            new int[]
 //            {
 //                CipherSuite.DRAFT_TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-//                CipherSuite.TLS_ECDHE_RSA_WITH_ESTREAM_SALSA20_SHA1,
-//                CipherSuite.TLS_ECDHE_RSA_WITH_SALSA20_SHA1,
-//                CipherSuite.TLS_RSA_WITH_ESTREAM_SALSA20_SHA1,
-//                CipherSuite.TLS_RSA_WITH_SALSA20_SHA1,
 //            });
 //    }
 
