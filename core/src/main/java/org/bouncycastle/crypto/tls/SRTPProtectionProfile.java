846c9ea51e32e479b7dc7487be2445a676e50cdb
Peter Dettman <peter.dettman@bouncycastle.org>
2016-01-18 14:58:48 +0700

    Add DTLS-SRTP protection profiles from RFC 7714

--- core/src/main/java/org/bouncycastle/crypto/tls/SRTPProtectionProfile.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/SRTPProtectionProfile.java
@@ -9,4 +9,10 @@ public class SRTPProtectionProfile
     public static final int SRTP_AES128_CM_HMAC_SHA1_32 = 0x0002;
     public static final int SRTP_NULL_HMAC_SHA1_80 = 0x0005;
     public static final int SRTP_NULL_HMAC_SHA1_32 = 0x0006;
+
+    /*
+     * RFC 7714 14.2.
+     */
+    public static final int SRTP_AEAD_AES_128_GCM = 0x0007;
+    public static final int SRTP_AEAD_AES_256_GCM = 0x0008;
 }
