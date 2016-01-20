200d01031779114b9d2db302fff7a593fcbb280c
David Hook <dgh@cryptoworkshop.com>
2016-01-20 14:29:23 +1100

    Updated DANE package to the latest draft.

--- pkix/src/test/java/org/bouncycastle/cert/test/SHA256DigestCalculator.java
+++ pkix/src/test/java/org/bouncycastle/cert/test/SHA256DigestCalculator.java
@@ -6,18 +6,18 @@ import java.io.OutputStream;
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.crypto.Digest;
-import org.bouncycastle.crypto.digests.SHA224Digest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.operator.DigestCalculator;
 
 
-class SHA224DigestCalculator
+class SHA256DigestCalculator
     implements DigestCalculator
 {
     private ByteArrayOutputStream bOut = new ByteArrayOutputStream();
 
     public AlgorithmIdentifier getAlgorithmIdentifier()
     {
-        return new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha224);
+        return new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha256);
     }
 
     public OutputStream getOutputStream()
@@ -31,13 +31,13 @@ class SHA224DigestCalculator
 
         bOut.reset();
 
-        Digest sha224 = new SHA224Digest();
+        Digest sha256 = new SHA256Digest();
 
-        sha224.update(bytes, 0, bytes.length);
+        sha256.update(bytes, 0, bytes.length);
 
-        byte[] digest = new byte[sha224.getDigestSize()];
+        byte[] digest = new byte[sha256.getDigestSize()];
 
-        sha224.doFinal(digest, 0);
+        sha256.doFinal(digest, 0);
 
         return digest;
     }
