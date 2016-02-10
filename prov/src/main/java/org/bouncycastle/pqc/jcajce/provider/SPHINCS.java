d35b267d530c4054a0775f3b107fff4cff3351bf
David Hook <dgh@cryptoworkshop.com>
2016-02-10 11:43:16 +1100

    Turfed algorithm parameters for SPHINCS-256

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/SPHINCS.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/SPHINCS.java
@@ -22,9 +22,6 @@ public class SPHINCS
             provider.addAlgorithm("KeyFactory.SPHINCS256", PREFIX + "Sphincs256KeyFactorySpi");
             provider.addAlgorithm("KeyPairGenerator.SPHINCS256", PREFIX + "Sphincs256KeyPairGeneratorSpi");
 
-            provider.addAlgorithm("AlgorithmParameters.SHA512WITHSPHINCS256", PREFIX + "AlgorithmParametersSpi");
-            provider.addAlgorithm("AlgorithmParameters.SHA3-512WITHSPHINCS256", PREFIX + "AlgorithmParametersSpi");
-
             addSignatureAlgorithm(provider, "SHA512", "SPHINCS256", PREFIX + "SignatureSpi$withSha512", PQCObjectIdentifiers.sphincs256_with_SHA512);
             addSignatureAlgorithm(provider, "SHA3-512", "SPHINCS256", PREFIX + "SignatureSpi$withSha3_512", PQCObjectIdentifiers.sphincs256_with_SHA3_512);
 
