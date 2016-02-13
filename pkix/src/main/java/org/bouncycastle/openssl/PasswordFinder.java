23dceda0764b9cb4ab5d1eae1339f2f065230954
David Hook <dgh@cryptoworkshop.com>
2016-02-13 13:14:42 +1100

    marked password finder interface as deprecated.

--- pkix/src/main/java/org/bouncycastle/openssl/PasswordFinder.java
+++ pkix/src/main/java/org/bouncycastle/openssl/PasswordFinder.java
@@ -2,6 +2,7 @@ package org.bouncycastle.openssl;
 
 /**
  * call back to allow a password to be fetched when one is requested.
+ * @deprecated no longer used.
  */
 public interface PasswordFinder
 {
