f04880e7ca83611f5142c6dfbd1b38e52b2f8fbc
David Hook <dgh@cryptoworkshop.com>
2016-03-02 03:04:56 +1100

    BJA-585 Added support for PRF's other than SHA-1 to PKCS#5 secret key factories.

--- pkix/src/main/java/org/bouncycastle/cms/PasswordRecipient.java
+++ pkix/src/main/java/org/bouncycastle/cms/PasswordRecipient.java
@@ -1,5 +1,7 @@
 package org.bouncycastle.cms;
 
+import org.bouncycastle.asn1.DERNull;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 
 public interface PasswordRecipient
@@ -7,6 +9,34 @@ public interface PasswordRecipient
 {
     public static final int PKCS5_SCHEME2 = 0;
     public static final int PKCS5_SCHEME2_UTF8 = 1;
+
+    static final class PRF
+    {
+        public static final PRF HMacSHA1 = new PRF("HMacSHA1", new AlgorithmIdentifier(PKCSObjectIdentifiers.id_hmacWithSHA1, DERNull.INSTANCE));
+        public static final PRF HMacSHA224 = new PRF("HMacSHA224", new AlgorithmIdentifier(PKCSObjectIdentifiers.id_hmacWithSHA224, DERNull.INSTANCE));
+        public static final PRF HMacSHA256 = new PRF("HMacSHA256", new AlgorithmIdentifier(PKCSObjectIdentifiers.id_hmacWithSHA256, DERNull.INSTANCE));
+        public static final PRF HMacSHA384 = new PRF("HMacSHA384", new AlgorithmIdentifier(PKCSObjectIdentifiers.id_hmacWithSHA384, DERNull.INSTANCE));
+        public static final PRF HMacSHA512 = new PRF("HMacSHA512", new AlgorithmIdentifier(PKCSObjectIdentifiers.id_hmacWithSHA512, DERNull.INSTANCE));
+
+        private final String hmac;
+        final AlgorithmIdentifier prfAlgID;
+
+        private PRF(String hmac, AlgorithmIdentifier prfAlgID)
+        {
+            this.hmac = hmac;
+            this.prfAlgID = prfAlgID;
+        }
+
+        public String getName()
+        {
+            return hmac;
+        }
+
+        public AlgorithmIdentifier getAlgorithmID()
+        {
+            return prfAlgID;
+        }
+    }
 
     byte[] calculateDerivedKey(int schemeID, AlgorithmIdentifier derivationAlgorithm, int keySize)
         throws CMSException;
