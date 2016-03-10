589bdd14ac90f17c0476ce57a051ac7fe160b73c
Peter Dettman <peter.dettman@bouncycastle.org>
2016-03-10 13:43:17 +0700

    Implement unique name_type restriction from RFC 6066

--- core/src/main/java/org/bouncycastle/crypto/tls/NameType.java
+++ core/src/main/java/org/bouncycastle/crypto/tls/NameType.java
@@ -6,4 +6,9 @@ public class NameType
      * RFC 3546 3.1.
      */
     public static final short host_name = 0;
+
+    public static boolean isValid(short nameType)
+    {
+        return nameType == host_name;
+    }
 }
