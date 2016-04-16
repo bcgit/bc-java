32ac93dd7ce5ccc3a01d4bba237e3101093ecb14
Peter Dettman <peter.dettman@bouncycastle.org>
2016-04-16 15:30:45 +0700

    Update NewHope implementation
    - follows C ref. impl. updates, March 28, 2016
    - includes a new interop test

--- core/src/main/java/org/bouncycastle/pqc/crypto/newhope/Poly.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/newhope/Poly.java
@@ -1,6 +1,7 @@
 package org.bouncycastle.pqc.crypto.newhope;
 
 import org.bouncycastle.crypto.digests.SHAKEDigest;
+import org.bouncycastle.util.Pack;
 
 class Poly
 {
@@ -12,24 +13,25 @@ class Poly
         }
     }
 
-    static void bitReverse(short[] r)
-    {
-        NTT.bitReverse(r);
-    }
-
     static void fromBytes(short[] r, byte[] a)
     {
-        for (int i = 0; i < Params.N; ++i)
+        for (int i = 0; i < Params.N / 4; ++i)
         {
-            byte lo = a[2 * i];
-            byte hi = a[2 * i + 1];
-            int x = (lo & 0xFF) | ((hi & 0x3F) << 8);
-            r[i] = (short)x;
+            int j = 7 * i;
+            int a0 = a[j + 0] & 0xFF, a1 = a[j + 1] & 0xFF, a2 = a[j + 2] & 0xFF, a3 = a[j + 3] & 0xFF,
+                a4 = a[j + 4] & 0xFF, a5 = a[j + 5] & 0xFF, a6 = a[j + 6] & 0xFF;
+
+            int k = 4 * i;
+            r[k + 0] = (short)( a0                    | ((a1 & 0x3F) <<  8));
+            r[k + 1] = (short)((a1 >>> 6) | (a2 << 2) | ((a3 & 0x0F) << 10));
+            r[k + 2] = (short)((a3 >>> 4) | (a4 << 4) | ((a5 & 0x03) << 12));
+            r[k + 3] = (short)((a5 >>> 2) | (a6 << 6));
         }
     }
 
     static void fromNTT(short[] r)
     {
+        NTT.bitReverse(r);
         NTT.core(r, Precomp.OMEGAS_INV_MONTGOMERY);
         NTT.mulCoefficients(r, Precomp.PSIS_INV_MONTGOMERY);
     }
@@ -39,18 +41,22 @@ class Poly
         byte[] iv = new byte[8];
         iv[0] = nonce;
 
-        byte[] buf = new byte[3 * r.length];
+        byte[] buf = new byte[4 * Params.N];
         ChaCha20.process(seed, iv, buf, 0, buf.length);
 
-        int j = 0;
-        for (int i = 0; i < r.length; ++i)
+        for (int i = 0; i < Params.N; ++i)
         {
-            int f = buf[j++] & 0xFF; 
-            int g = buf[j++] & 0xFF; 
-            int h = buf[j++] & 0xFF;
+            int t = Pack.bigEndianToInt(buf, i * 4);
+            //r[i] = (short)(bitCount(t) + Params.Q - Params.K);
 
-            int t = (f << 16) | (g << 8) | h;
-            r[i] = (short)(bitCount(t) + Params.Q - 12);
+            int d = 0;
+            for (int j = 0; j < 8; ++j)
+            {
+                d += (t >> j) & 0x01010101;
+            }
+            int a = ((d >>> 24) + (d >>> 0)) & 0xFF;
+            int b = ((d >>> 16) + (d >>> 8)) & 0xFF;
+            r[i] = (short)(a + Params.Q - b);
         }
     }
 
@@ -66,14 +72,24 @@ class Poly
 
     static void toBytes(byte[] r, short[] p)
     {
-        for (int i = 0; i < Params.N; ++i)
+        for (int i = 0; i < Params.N / 4; ++i)
         {
-            int t = Reduce.barrett(p[i]);   // Make sure that coefficients have only 14 bits
-            int m = t - Params.Q;
-            int c = m >> 31;
-            t = m ^ ((t ^ m) & c);          // Make sure that coefficients are in [0,q]
-            r[2 * i    ] = (byte)t;
-            r[2 * i + 1] = (byte)(t >>> 8);
+            int j = 4 * i;
+
+            // Make sure that coefficients are in [0,q]
+            short t0 = normalize(p[j + 0]);
+            short t1 = normalize(p[j + 1]);
+            short t2 = normalize(p[j + 2]);
+            short t3 = normalize(p[j + 3]);
+
+            int k = 7 * i;
+            r[k + 0] = (byte)t0;
+            r[k + 1] = (byte)((t0 >> 8) | (t1 << 6));
+            r[k + 2] = (byte)(t1 >> 2);
+            r[k + 3] = (byte)((t1 >> 10) | (t2 << 4));
+            r[k + 4] = (byte)(t2 >> 4);
+            r[k + 5] = (byte)((t2 >> 12) | (t3 << 2));
+            r[k + 6] = (byte)(t3 >> 6);
         }
     }
 
@@ -110,11 +126,20 @@ class Poly
         }
     }
 
-    private static int bitCount(int n)
+//    private static int bitCount(int n)
+//    {
+////        return Integer.bitCount(n);
+//        n = n - ((n >>> 1) & 0x55555555);
+//        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+//        return ((n + (n >>> 4) & 0x0F0F0F0F) * 0x01010101) >>> 24;
+//    }
+
+    private static short normalize(short x)
     {
-//        return Integer.bitCount(n);
-        n = n - ((n >>> 1) & 0x55555555);
-        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
-        return ((n + (n >>> 4) & 0x0F0F0F0F) * 0x01010101) >>> 24;
+        int t = Reduce.barrett(x);
+        int m = t - Params.Q;
+        int c = m >> 31;
+        t = m ^ ((t ^ m) & c);
+        return (short)t;
     }
 }
