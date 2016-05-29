7b6d6c95ee9013c863244436c4221422f8ca7ba5
Peter Dettman <peter.dettman@bouncycastle.org>
2016-05-29 23:32:30 +0700

    Add more TlsCertificate methods

--- tls/src/main/java/org/bouncycastle/tls/crypto/TlsCertificate.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/TlsCertificate.java
@@ -1,6 +1,12 @@
 package org.bouncycastle.tls.crypto;
 
+import java.io.IOException;
+
 public interface TlsCertificate
 {
+    short getCertificateType();
+
+    byte[] getEncoded() throws IOException;
+
     boolean hasKeyUsage(int keyUsage);
 }
--- tls/src/main/java/org/bouncycastle/tls/crypto/bc/BcTlsCertificate.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/bc/BcTlsCertificate.java
@@ -1,12 +1,27 @@
 package org.bouncycastle.tls.crypto.bc;
 
+import java.io.IOException;
+
+import org.bouncycastle.asn1.ASN1Encoding;
 import org.bouncycastle.asn1.x509.Certificate;
 import org.bouncycastle.asn1.x509.Extensions;
 import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.tls.CertificateType;
+import org.bouncycastle.tls.crypto.TlsCertificate;
 
-public class BcTlsCertificate
+public class BcTlsCertificate implements TlsCertificate
 {
     protected Certificate certificate;
+
+    public short getCertificateType()
+    {
+        return CertificateType.X509;
+    }
+
+    public byte[] getEncoded() throws IOException
+    {
+        return certificate.getEncoded(ASN1Encoding.DER);
+    }
 
     public boolean hasKeyUsage(int keyUsageBits)
     {
