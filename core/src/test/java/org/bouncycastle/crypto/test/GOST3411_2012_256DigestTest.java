a3fcb9638fb6619395f50e22da915e60c743dc1e
David Hook <dgh@cryptoworkshop.com>
2016-07-25 11:07:29 +1000

    added GMAC test, reversed output, minor edits

--- core/src/test/java/org/bouncycastle/crypto/test/GOST3411_2012_256DigestTest.java
+++ core/src/test/java/org/bouncycastle/crypto/test/GOST3411_2012_256DigestTest.java
@@ -1,47 +1,90 @@
 package org.bouncycastle.crypto.test;
 
+import java.util.ArrayList;
+
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.GOST3411_2012_256Digest;
-
-import java.util.ArrayList;
+import org.bouncycastle.crypto.digests.GOST3411_2012_512Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.Strings;
+import org.bouncycastle.util.encoders.Hex;
 
 public class GOST3411_2012_256DigestTest
     extends DigestTest
 {
     private static final String[] messages;
-    static {
-        ArrayList<String> strList = new ArrayList<String>();
-        strList.add("012345678901234567890123456789012345678901234567890123456789012");
 
-        char[] M2 = {0xD1, 0xE5, 0x20, 0xE2, 0xE5, 0xF2, 0xF0, 0xE8, 0x2C, 0x20, 0xD1, 0xF2,
-                     0xF0, 0xE8, 0xE1, 0xEE, 0xE6, 0xE8, 0x20, 0xE2, 0xED, 0xF3, 0xF6, 0xE8,
-                     0x2C, 0x20, 0xE2, 0xE5, 0xFE, 0xF2, 0xFA, 0x20, 0xF1, 0x20, 0xEC, 0xEE,
-                     0xF0, 0xFF, 0x20, 0xF1, 0xF2, 0xF0, 0xE5, 0xEB, 0xE0, 0xEC, 0xE8, 0x20,
-                     0xED, 0xE0, 0x20, 0xF5, 0xF0, 0xE0, 0xE1, 0xF0, 0xFB, 0xFF, 0x20, 0xEF,
-                     0xEB, 0xFA, 0xEA, 0xFB, 0x20, 0xC8, 0xE3, 0xEE, 0xF0, 0xE5, 0xE2, 0xFB};
+    private static char[] M1 =
+        {
+            0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+            0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+            0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+            0x30, 0x31, 0x32
+        };
+
+    private static char[] M2=
+        {
+            0xd1,0xe5,0x20,0xe2,0xe5,0xf2,0xf0,0xe8,0x2c,0x20,0xd1,0xf2,0xf0,0xe8,0xe1,0xee,0xe6,0xe8,0x20,0xe2,
+            0xed,0xf3,0xf6,0xe8,0x2c,0x20,0xe2,0xe5,0xfe,0xf2,0xfa,0x20,0xf1,0x20,0xec,0xee,0xf0,0xff,0x20,0xf1,
+            0xf2,0xf0,0xe5,0xeb,0xe0,0xec,0xe8,0x20,0xed,0xe0,0x20,0xf5,0xf0,0xe0,0xe1,0xf0,0xfb,0xff,0x20,0xef,
+            0xeb,0xfa,0xea,0xfb,0x20,0xc8,0xe3,0xee,0xf0,0xe5,0xe2,0xfb
+        };
+
+    static
+    {
+
+        ArrayList<String> strList = new ArrayList<String>();
+
+        strList.add(new String(M1));
         strList.add(new String(M2));
 
         messages = new String[strList.size()];
-        for (int i = 0; i < strList.size(); i++) {
+        for (int i = 0; i < strList.size(); i++)
+        {
             messages[i] = strList.get(i);
         }
-    };
+    }
 
     private static final String[] digests = {
-        "00557be5e584fd52a449b16b0251d05d27f94ab76cbaa6da890b59d8ef1e159d",
-        "508f7e553c06501d749a66fc28c6cac0b005746d97537fa85d9e40904efed29d"
+        "9d151eefd8590b89daa6ba6cb74af9275dd051026bb149a452fd84e5e57b5500",
+        "9dd2fe4e90409e5da87f53976d7405b0c0cac628fc669a741d50063c557e8f50"
     };
 
-    GOST3411_2012_256DigestTest() {
+    GOST3411_2012_256DigestTest()
+    {
         super(new GOST3411_2012_256Digest(), messages, digests);
     }
 
-    @Override
-    protected Digest cloneDigest(Digest digest) {
+    public void performTest()
+    {
+        super.performTest();
+
+        HMac gMac = new HMac(new GOST3411_2012_256Digest());
+
+        gMac.init(new KeyParameter(Hex.decode("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f")));
+
+        byte[] data = Hex.decode("0126bdb87800af214341456563780100");
+
+        gMac.update(data, 0, data.length);
+        byte[] mac = new byte[gMac.getMacSize()];
+
+        gMac.doFinal(mac, 0);
+
+        if (!Arrays.areEqual(Hex.decode("a1aa5f7de402d7b3d323f2991c8d4534013137010a83754fd0af6d7cd4922ed9"), mac))
+        {
+            fail("mac calculation failed.");
+        }
+    }
+
+    protected Digest cloneDigest(Digest digest)
+    {
         return new GOST3411_2012_256Digest((GOST3411_2012_256Digest)digest);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         runTest(new GOST3411_2012_256DigestTest());
     }
 }
