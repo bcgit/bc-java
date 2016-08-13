ff34e120de7a496b34c4a6f4668d42182ec80f70
David Hook <dgh@cryptoworkshop.com>
2016-08-13 15:07:01 +1000

    jdk 1.3 compatibility files.

--- prov/src/main/jdk1.3/org/bouncycastle/jcajce/provider/asymmetric/rsa/PSSSignatureSpi.java
+++ prov/src/main/jdk1.3/org/bouncycastle/jcajce/provider/asymmetric/rsa/PSSSignatureSpi.java
@@ -23,6 +23,7 @@ import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.digests.SHA384Digest;
 import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.bouncycastle.crypto.digests.SHA512tDigest;
+import org.bouncycastle.crypto.digests.SHA3Digest;
 import org.bouncycastle.crypto.engines.RSABlindedEngine;
 import org.bouncycastle.crypto.params.ParametersWithRandom;
 
@@ -379,6 +380,41 @@ public class PSSSignatureSpi
         }
     }
 
+    static public class SHA3_224withRSA
+        extends PSSSignatureSpi
+    {
+        public SHA3_224withRSA()
+        {
+            super("SHA3-224withRSAandMGF1", new RSABlindedEngine(), new SHA3Digest(224));
+        }
+    }
+
+    static public class SHA3_256withRSA
+        extends PSSSignatureSpi
+    {
+        public SHA3_256withRSA()
+        {
+            super("SHA3-256withRSAandMGF1", new RSABlindedEngine(), new SHA3Digest(256));
+        }
+    }
+
+    static public class SHA3_384withRSA
+        extends PSSSignatureSpi
+    {
+        public SHA3_384withRSA()
+        {
+            super("SHA3-384withRSAandMGF1", new RSABlindedEngine(), new SHA3Digest(384));
+        }
+    }
+
+    static public class SHA3_512withRSA
+        extends PSSSignatureSpi
+    {
+        public SHA3_512withRSA()
+        {
+            super("SHA3-512withRSAandMGF1", new RSABlindedEngine(), new SHA3Digest(512));
+        }
+    }
     private class NullPssDigest
         implements Digest
     {
