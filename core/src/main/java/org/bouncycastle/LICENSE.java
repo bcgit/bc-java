169c7764cfcaa391bba7da6b93d0f8d8564a9fc9
David Hook <dgh@cryptoworkshop.com>
2016-08-18 14:35:15 +1000

    updated license year.

--- core/src/main/java/org/bouncycastle/LICENSE.java
+++ core/src/main/java/org/bouncycastle/LICENSE.java
@@ -5,7 +5,7 @@ import org.bouncycastle.util.Strings;
 /**
  * The Bouncy Castle License
  *
- * Copyright (c) 2000-2015 The Legion Of The Bouncy Castle Inc. (http://www.bouncycastle.org)
+ * Copyright (c) 2000-2016 The Legion Of The Bouncy Castle Inc. (http://www.bouncycastle.org)
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
  * and associated documentation files (the "Software"), to deal in the Software without restriction, 
@@ -26,7 +26,7 @@ import org.bouncycastle.util.Strings;
 public class LICENSE
 {
     public static String licenseText =
-      "Copyright (c) 2000-2015 The Legion of the Bouncy Castle Inc. (http://www.bouncycastle.org) "
+      "Copyright (c) 2000-2016 The Legion of the Bouncy Castle Inc. (http://www.bouncycastle.org) "
       + Strings.lineSeparator()
       + Strings.lineSeparator()
       + "Permission is hereby granted, free of charge, to any person obtaining a copy of this software "
