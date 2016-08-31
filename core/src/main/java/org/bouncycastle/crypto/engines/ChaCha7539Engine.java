3b978063371fb225dce2d84820436dc00019549d
David Hook <dgh@cryptoworkshop.com>
2016-08-31 10:49:51 +1000

    added provider support for ChaCha7539

--- core/src/main/java/org/bouncycastle/crypto/engines/ChaCha7539Engine.java
+++ core/src/main/java/org/bouncycastle/crypto/engines/ChaCha7539Engine.java
@@ -17,7 +17,7 @@ public class ChaCha7539Engine extends Salsa20Engine
 
     public String getAlgorithmName()
     {
-        return "ChaCha" + rounds;
+        return "ChaCha7539-" + rounds;
     }
 
     protected int getNonceSize()
