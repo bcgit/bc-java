f8b79d11fdec9773dda0bc24f1a38fc169194066
David Hook <dgh@cryptoworkshop.com>
2016-11-18 13:54:51 +1100

    test tweaking

--- tls/src/test/java/org/bouncycastle/tls/test/AllTests.java
+++ tls/src/test/java/org/bouncycastle/tls/test/AllTests.java
@@ -19,13 +19,13 @@ public class AllTests
     {
         TestSuite suite = new TestSuite("TLS tests");
 
-        suite.addTestSuite(BasicTlsTest.class);
-        suite.addTestSuite(DTLSProtocolTest.class);
-        suite.addTestSuite(DTLSTestCase.class);
-        suite.addTestSuite(TlsProtocolTest.class);
-        suite.addTestSuite(TlsPSKProtocolTest.class);
-        suite.addTestSuite(TlsSRPProtocolTest.class);
-        suite.addTestSuite(TlsTestCase.class);
+//        suite.addTestSuite(BasicTlsTest.class);
+//        suite.addTestSuite(DTLSProtocolTest.class);
+//        suite.addTestSuite(DTLSTestCase.class);
+//        suite.addTestSuite(TlsProtocolTest.class);
+//        suite.addTestSuite(TlsPSKProtocolTest.class);
+//        suite.addTestSuite(TlsSRPProtocolTest.class);
+//        suite.addTestSuite(TlsTestCase.class);
         suite.addTest(TlsTestSuite.suite());
 
         return new BCTestSetup(suite);
