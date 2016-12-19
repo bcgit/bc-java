fd4e92b738f9bd718abd78e9179e6678016deb63
David Hook <dgh@cryptoworkshop.com>
2016-12-20 07:56:35 +1100

    added lookup for base SecureRandom generation

--- prov/src/main/java/org/bouncycastle/jcajce/provider/drbg/DRBG.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/drbg/DRBG.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.jcajce.provider.drbg;
 
+import java.security.Provider;
 import java.security.SecureRandom;
 import java.security.SecureRandomSpi;
 
@@ -15,10 +16,67 @@ public class DRBG
 {
     private static final String PREFIX = DRBG.class.getName();
 
+    private static final String[][] initialEntropySourceNames = new String[][]{
+        // Normal JVM
+        {"sun.security.provider.Sun", "sun.security.provider.SecureRandom"},
+        // Apache harmony
+        {"org.apache.harmony.security.provider.crypto.CryptoProvider", "org.apache.harmony.security.provider.crypto.SHA1PRNG_SecureRandomImpl"},
+        // {"Provider class name","SecureRandomSpi class name"}
+        //            // Android.
+        {"com.android.org.conscrypt.OpenSSLProvider", "com.android.org.conscrypt.OpenSSLRandom"},
+        {"org.conscrypt.OpenSSLProvider", "org.conscrypt.OpenSSLRandom"},
+    };
+
+    private static final Object[] initialEntropySourceAndSpi = findSource();
+
+    // Cascade through providers looking for match.
+    private final static Object[] findSource()
+    {
+        for (int t = 0; t < initialEntropySourceNames.length; t++)
+        {
+            String[] pair = initialEntropySourceNames[t];
+            try
+            {
+                Object[] r = new Object[]{Class.forName(pair[0]).newInstance(), Class.forName(pair[1]).newInstance()};
+
+                return r;
+            }
+            catch (Throwable ex)
+            {
+                continue;
+            }
+        }
+
+        return null;
+    }
+
+    private static class CoreSecureRandom
+        extends SecureRandom
+    {
+        CoreSecureRandom()
+        {
+            super((SecureRandomSpi)initialEntropySourceAndSpi[1], (Provider)initialEntropySourceAndSpi[0]);
+        }
+    }
+
+    // unfortunately new SecureRandom() can cause a regress and it's the only reliable way of getting access
+    // to the JVM's seed generator.
+    private static SecureRandom createInitialEntropySource()
+    {
+        if (initialEntropySourceAndSpi != null)
+        {
+            return new CoreSecureRandom();
+        }
+        else
+        {
+            return new SecureRandom();  // we're desperate, it's worth a try.
+        }
+    }
+
     public static class Default
         extends SecureRandomSpi
     {
-        private SecureRandom randomSource = new SecureRandom();   // needs to be done late, can't use static
+        private SecureRandom randomSource = createInitialEntropySource();   // needs to be done late, can't use static
         private SecureRandom random = new SP800SecureRandomBuilder(randomSource, true)
             .setPersonalizationString(generateDefaultPersonalizationString(randomSource))
             .buildHash(new SHA512Digest(), randomSource.generateSeed(32), true);
@@ -42,7 +100,7 @@ public class DRBG
     public static class NonceAndIV
         extends SecureRandomSpi
     {
-        private SecureRandom randomSource = new SecureRandom();  // needs to be done late, can't use static
+        private SecureRandom randomSource = createInitialEntropySource();
         private SecureRandom random = new SP800SecureRandomBuilder(randomSource, true)
             .setPersonalizationString(generateNonceIVPersonalizationString(randomSource))
             .buildHash(new SHA512Digest(), randomSource.generateSeed(32), false);
