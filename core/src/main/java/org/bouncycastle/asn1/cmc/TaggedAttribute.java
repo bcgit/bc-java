2ebb3f5fbece233c6d3fea2f8e91743628a4b5a9
David Hook <dgh@cryptoworkshop.com>
2017-01-02 10:43:54 +1100

    rework of tagged attribute test

--- core/src/main/java/org/bouncycastle/asn1/cmc/TaggedAttribute.java
+++ core/src/main/java/org/bouncycastle/asn1/cmc/TaggedAttribute.java
@@ -1,93 +1,65 @@
 package org.bouncycastle.asn1.cmc;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
 import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1Set;
 import org.bouncycastle.asn1.DERSequence;
-import org.bouncycastle.asn1.DERSet;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Iterator;
-
-
-import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1EncodableVector;
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1Integer;
-import org.bouncycastle.asn1.ASN1Object;
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.ASN1Primitive;
-import org.bouncycastle.asn1.ASN1Sequence;
-import org.bouncycastle.asn1.ASN1Set;
-import org.bouncycastle.asn1.DERSequence;
-import org.bouncycastle.asn1.DERSet;
-import org.bouncycastle.asn1.DLSequence;
-import org.bouncycastle.asn1.DLSet;
-import org.bouncycastle.pqc.math.linearalgebra.Vector;
 
 /**
  * TaggedAttribute from RFC5272
- * <p>
+ * <pre>
  * TaggedAttribute ::= SEQUENCE {
  * bodyPartID         BodyPartID,
  * attrType           OBJECT IDENTIFIER,
  * attrValues         SET OF AttributeValue
  * }
+ * </pre>
  */
 public class TaggedAttribute
     extends ASN1Object
 {
-    private ASN1Integer bodyPartID;
-    private ASN1ObjectIdentifier attrType;
-    private ASN1Set attrValues = new DERSet();
+    private final BodyPartID bodyPartID;
+    private final ASN1ObjectIdentifier attrType;
+    private final ASN1Set attrValues;
 
-
-    public ASN1Primitive toASN1Primitive()
-    {
-        return new DERSequence(new ASN1Encodable[]{bodyPartID, attrType, attrValues});
-    }
-
-    public static TaggedAttribute getInstance(Object src)
+    public static TaggedAttribute getInstance(Object o)
         throws Exception
     {
+        if (o instanceof TaggedAttribute)
+        {
+            return (TaggedAttribute)o;
+        }
 
-        if (src instanceof ASN1InputStream)
+        if (o != null)
         {
-            ASN1Sequence seq =(ASN1Sequence)((ASN1InputStream)src).readObject();
+            return new TaggedAttribute(ASN1Sequence.getInstance(o));
+        }
 
-            return new TaggedAttribute(
-                (ASN1Integer)seq.getObjectAt(0),
-                (ASN1ObjectIdentifier)seq.getObjectAt(1),
-                (ASN1Set)seq.getObjectAt(2));
-        }
-        else if (src instanceof byte[])
-        {
-            return getInstance(new ASN1InputStream(new ByteArrayInputStream((byte[])src)));
-        }
-        else if (src instanceof InputStream)
-        {
-            return getInstance(new ASN1InputStream((InputStream)src));
-        }
-        throw new IllegalArgumentException("src not byte[], ASN1InputStream or InputStream");
+        return null;
     }
 
-    public TaggedAttribute(ASN1Integer bodyPartID, ASN1ObjectIdentifier attrType, ASN1Set attrValues)
+    private TaggedAttribute(ASN1Sequence seq)
+    {
+        if (seq.size() != 3)
+        {
+            throw new IllegalArgumentException("incorrect sequence size");
+        }
+        this.bodyPartID = BodyPartID.getInstance(seq.getObjectAt(0));
+        this.attrType = ASN1ObjectIdentifier.getInstance(seq.getObjectAt(1));
+        this.attrValues = ASN1Set.getInstance(seq.getObjectAt(2));
+    }
+
+    public TaggedAttribute(BodyPartID bodyPartID, ASN1ObjectIdentifier attrType, ASN1Set attrValues)
     {
         this.bodyPartID = bodyPartID;
         this.attrType = attrType;
         this.attrValues = attrValues;
     }
 
-    public ASN1Integer getBodyPartID()
+    public BodyPartID getBodyPartID()
     {
         return bodyPartID;
     }
@@ -102,67 +74,9 @@ public class TaggedAttribute
         return attrValues;
     }
 
-    public void setBodyPartID(ASN1Integer bodyPartID)
+    public ASN1Primitive toASN1Primitive()
     {
-        this.bodyPartID = bodyPartID;
-    }
-
-    public void setAttrType(ASN1ObjectIdentifier attrType)
-    {
-        this.attrType = attrType;
-    }
-
-    public void setAttrValues(ASN1Set attrValues)
-    {
-        this.attrValues = attrValues;
-    }
-
-    @Override
-    public boolean equals(Object o)
-    {
-        if (this == o)
-        {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass())
-        {
-            return false;
-        }
-
-        TaggedAttribute that = (TaggedAttribute)o;
-
-        if (!bodyPartID.equals(that.bodyPartID))
-        {
-            return false;
-        }
-        if (!attrType.equals(that.attrType))
-        {
-            return false;
-        }
-
-        if (attrValues.size() != ((TaggedAttribute)o).attrValues.size()) {
-            return false;
-        }
-
-        for (int t=0; t<attrValues.size(); t++)
-        {
-            if (attrValues.getObjectAt(t).equals(((TaggedAttribute)o).attrValues.getObjectAt(t))) {
-                return false;
-            }
-        }
-
-        return true;
-
-    }
-
-    @Override
-    public int hashCode()
-    {
-        int result = super.hashCode();
-        result = 31 * result + bodyPartID.hashCode();
-        result = 31 * result + attrType.hashCode();
-        result = 31 * result + attrValues.hashCode();
-        return result;
+        return new DERSequence(new ASN1Encodable[]{bodyPartID, attrType, attrValues});
     }
 }
 
--- core/src/test/java/org/bouncycastle/asn1/test/TaggedAttributeTest.java
+++ core/src/test/java/org/bouncycastle/asn1/test/TaggedAttributeTest.java
@@ -1,21 +1,17 @@
 package org.bouncycastle.asn1.test;
 
 
-import java.io.ByteArrayInputStream;
-
-import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1Integer;
-import org.bouncycastle.asn1.ASN1String;
 import org.bouncycastle.asn1.DERIA5String;
 import org.bouncycastle.asn1.DERSet;
-
+import org.bouncycastle.asn1.cmc.BodyPartID;
+import org.bouncycastle.asn1.cmc.CMCObjectIdentifiers;
 import org.bouncycastle.asn1.cmc.TaggedAttribute;
-import org.bouncycastle.asn1.dvcs.DVCSObjectIdentifiers;
+import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.test.SimpleTest;
 
 
-public class TaggedAttributeTest extends SimpleTest
+public class TaggedAttributeTest
+    extends SimpleTest
 {
     public String getName()
     {
@@ -28,38 +24,25 @@ public class TaggedAttributeTest extends SimpleTest
         //
         // This creates and tests the various get instance  methods.
         //
-        TaggedAttribute ta = new TaggedAttribute(new ASN1Integer(10L), DVCSObjectIdentifiers.id_cct_PKIData,new DERSet(new ASN1Encodable[]{new DERIA5String("Cats")}));
-        byte[] d = ta.getEncoded();
+        TaggedAttribute ta = new TaggedAttribute(
+            new BodyPartID(10L),
+            CMCObjectIdentifiers.id_cct_PKIData,
+            new DERSet(new DERIA5String("Cats")));
 
+        byte[] d = ta.getEncoded();
 
         {
             TaggedAttribute res1 = TaggedAttribute.getInstance(d);
             isEquals(ta.getBodyPartID(), res1.getBodyPartID());
             isEquals(ta.getAttrType(), res1.getAttrType());
             isEquals(ta.getAttrValues().getObjectAt(0), res1.getAttrValues().getObjectAt(0));
+            isTrue(Arrays.areEqual(res1.getEncoded(), d));
         }
-
-
-        {
-            TaggedAttribute res1 = TaggedAttribute.getInstance(new ByteArrayInputStream(d));
-            isEquals(ta.getBodyPartID(), res1.getBodyPartID());
-            isEquals(ta.getAttrType(), res1.getAttrType());
-            isEquals(ta.getAttrValues().getObjectAt(0), res1.getAttrValues().getObjectAt(0));
-        }
-
-
-        {
-            TaggedAttribute res1 = TaggedAttribute.getInstance(new ASN1InputStream(new ByteArrayInputStream(d)));
-            isEquals(ta.getBodyPartID(), res1.getBodyPartID());
-            isEquals(ta.getAttrType(), res1.getAttrType());
-            isEquals(ta.getAttrValues().getObjectAt(0), res1.getAttrValues().getObjectAt(0));
-        }
-
-
-
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)
+        throws Exception
+    {
         runTest(new TaggedAttributeTest());
     }
 }
