40d349c3a7d8870fcecb6d067b55d3f16ff9dae3
David Hook <dgh@cryptoworkshop.com>
2017-01-03 15:33:50 +1100

    further work on CMCStatusInfo

--- core/src/main/java/org/bouncycastle/asn1/cmc/CMCStatusInfo.java
+++ core/src/main/java/org/bouncycastle/asn1/cmc/CMCStatusInfo.java
@@ -1,8 +1,7 @@
 package org.bouncycastle.asn1.cmc;
 
-import java.io.IOException;
-
 import org.bouncycastle.asn1.ASN1Choice;
+import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1Object;
@@ -35,15 +34,7 @@ public class CMCStatusInfo
     private final DERUTF8String statusString;
     private final OtherInfo otherInfo;
 
-    public CMCStatusInfo(CMCStatus cMCStatus, ASN1Sequence bodyList, DERUTF8String statusString)
-    {
-        this.cMCStatus = cMCStatus;
-        this.bodyList = bodyList;
-        this.statusString = statusString;
-        this.otherInfo = null;
-    }
-
-    public CMCStatusInfo(CMCStatus cMCStatus, ASN1Sequence bodyList, DERUTF8String statusString, OtherInfo otherInfo)
+    CMCStatusInfo(CMCStatus cMCStatus, ASN1Sequence bodyList, DERUTF8String statusString, OtherInfo otherInfo)
     {
         this.cMCStatus = cMCStatus;
         this.bodyList = bodyList;
@@ -51,19 +42,37 @@ public class CMCStatusInfo
         this.otherInfo = otherInfo;
     }
 
-    /**
-     * Create from ASN1Sequence.
-     * @param seq The source sequence.
-     */
-    public CMCStatusInfo(ASN1Sequence seq)
+    private CMCStatusInfo(ASN1Sequence seq)
     {
+        if (seq.size() < 2 || seq.size() > 4)
+        {
+            throw new IllegalArgumentException("incorrect sequence size");
+        }
         this.cMCStatus = CMCStatus.getInstance(seq.getObjectAt(0));
         this.bodyList = ASN1Sequence.getInstance(seq.getObjectAt(1));
-        this.statusString = DERUTF8String.getInstance(seq.getObjectAt(2));
-        if (seq.size()>3) {
-            otherInfo = OtherInfo.getInstance(seq.getObjectAt(3));
-        } else {
-            otherInfo = null;
+
+        if (seq.size() > 3)
+        {
+            this.statusString = DERUTF8String.getInstance(seq.getObjectAt(2));
+            this.otherInfo = OtherInfo.getInstance(seq.getObjectAt(3));
+        }
+        else if (seq.size() > 2)
+        {
+            if (seq.getObjectAt(2) instanceof  DERUTF8String)
+            {
+                this.statusString = DERUTF8String.getInstance(seq.getObjectAt(2));
+                this.otherInfo = null;
+            }
+            else
+            {
+                this.statusString = null;
+                this.otherInfo = OtherInfo.getInstance(seq.getObjectAt(2));
+            }
+        }
+        else
+        {
+            this.statusString = null;
+            this.otherInfo = null;
         }
     }
 
@@ -74,7 +83,8 @@ public class CMCStatusInfo
             return (CMCStatusInfo)o;
         }
 
-        if (o != null) {
+        if (o != null)
+        {
             return new CMCStatusInfo(ASN1Sequence.getInstance(o));
         }
 
@@ -86,30 +96,35 @@ public class CMCStatusInfo
         ASN1EncodableVector v = new ASN1EncodableVector();
         v.add(cMCStatus);
         v.add(bodyList);
-        v.add(statusString);
-        if (otherInfo != null) {
+        if (statusString != null)
+        {
+            v.add(statusString);
+        }
+        if (otherInfo != null)
+        {
             v.add(otherInfo);
         }
         return new DERSequence(v);
     }
 
-    public boolean hasOtherInfo() {
-        return otherInfo != null;
-    }
-
-    public CMCStatus getcMCStatus()
+    public CMCStatus getCMCStatus()
     {
         return cMCStatus;
     }
 
-    public ASN1Sequence getBodyList()
+    public BodyPartID[] getBodyList()
     {
-        return bodyList;
+        return Utils.toBodyPartIDArray(bodyList);
     }
 
     public DERUTF8String getStatusString()
     {
         return statusString;
+    }
+
+    public boolean hasOtherInfo()
+    {
+        return otherInfo != null;
     }
 
     public OtherInfo getOtherInfo()
@@ -124,46 +139,40 @@ public class CMCStatusInfo
         extends ASN1Object
         implements ASN1Choice
     {
-
         private final CMCFailInfo failInfo;
         private final PendInfo pendInfo;
 
-
-        public static OtherInfo getInstance(Object obj)
+        private static OtherInfo getInstance(Object obj)
         {
             if (obj instanceof OtherInfo)
             {
                 return (OtherInfo)obj;
             }
 
-            if (obj instanceof CMCFailInfo)
+            if (obj instanceof ASN1Encodable)
             {
-                return new OtherInfo((CMCFailInfo)obj, null);
-            }
-            else if (obj instanceof PendInfo)
-            {
-                return new OtherInfo(null, (PendInfo)obj);
-            }
-            else if (obj instanceof ASN1Integer) // CMCFail info is an asn1 integer.
-            {
-                return getInstance(CMCFailInfo.getInstance(obj));
-            }
-            else if (obj instanceof ASN1Sequence) // PendInfo is a sequence.
-            {
-                return getInstance(PendInfo.getInstance(obj));
-            }
-            else if (obj instanceof byte[])
-            {
-                try
+                ASN1Encodable asn1Value = ((ASN1Encodable)obj).toASN1Primitive();
+
+                if (asn1Value instanceof ASN1Integer) // CMCFail info is an asn1 integer.
                 {
-                    return getInstance(ASN1Primitive.fromByteArray((byte[])obj));
+                    return new OtherInfo(CMCFailInfo.getInstance(asn1Value));
                 }
-                catch (IOException e)
+                else if (asn1Value instanceof ASN1Sequence) // PendInfo is a sequence.
                 {
-                    throw new IllegalArgumentException("unknown encoding in getInstance()");
+                    return new OtherInfo(PendInfo.getInstance(asn1Value));
                 }
             }
             throw new IllegalArgumentException("unknown object in getInstance(): " + obj.getClass().getName());
+        }
+
+        OtherInfo(CMCFailInfo failInfo)
+        {
+            this(failInfo, null);
+        }
+
+        OtherInfo(PendInfo pendInfo)
+        {
+            this(null, pendInfo);
         }
 
         private OtherInfo(CMCFailInfo failInfo, PendInfo pendInfo)
@@ -172,17 +181,9 @@ public class CMCStatusInfo
             this.pendInfo = pendInfo;
         }
 
-        public boolean isFailInfo() {
-            return failInfo != null;
-        }
-
-        public byte[] getEncoded()
-            throws IOException
+        public boolean isFailInfo()
         {
-            if (pendInfo!= null) {
-                return pendInfo.getEncoded();
-            }
-            return failInfo.getEncoded();
+            return failInfo != null;
         }
 
         public ASN1Primitive toASN1Primitive()
@@ -194,6 +195,4 @@ public class CMCStatusInfo
             return failInfo.toASN1Primitive();
         }
     }
-
-
 }
--- core/src/main/java/org/bouncycastle/asn1/cmc/Utils.java
+++ core/src/main/java/org/bouncycastle/asn1/cmc/Utils.java
@@ -1,9 +1,22 @@
 package org.bouncycastle.asn1.cmc;
 
+import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.x509.Extension;
 
 class Utils
 {
+    static BodyPartID[] toBodyPartIDArray(ASN1Sequence bodyPartIDs)
+    {
+        BodyPartID[] ids = new BodyPartID[bodyPartIDs.size()];
+
+        for (int i = 0; i != bodyPartIDs.size(); i++)
+        {
+            ids[i] = BodyPartID.getInstance(bodyPartIDs.getObjectAt(i));
+        }
+
+        return ids;
+    }
+
     static BodyPartID[] clone(BodyPartID[] ids)
     {
         BodyPartID[] tmp = new BodyPartID[ids.length];
