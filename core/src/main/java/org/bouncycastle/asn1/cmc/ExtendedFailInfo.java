d216f867bda501ad093ccc51bcc2f15dba0d9814
Megan Woods <megan@kllective.com>
2017-01-04 16:25:57 +1100

    ExtendedFailInfoTest, getters on ExtendedFailInfo and getInstance that accepts a byte[]

--- core/src/main/java/org/bouncycastle/asn1/cmc/ExtendedFailInfo.java
+++ core/src/main/java/org/bouncycastle/asn1/cmc/ExtendedFailInfo.java
@@ -26,6 +26,17 @@ public class ExtendedFailInfo
         this.failInfoValue = failInfoValue;
     }
 
+    private ExtendedFailInfo(ASN1Sequence s)
+    {
+        if (s.size() != 2)
+        {
+            throw new IllegalArgumentException("Sequence must be 2 elements.");
+        }
+
+        failInfoOID = ASN1ObjectIdentifier.getInstance(s.getObjectAt(0));
+        failInfoValue = s.getObjectAt(1);
+    }
+
     public static ExtendedFailInfo getInstance(Object obj)
     {
         if (obj instanceof ExtendedFailInfo)
@@ -39,16 +50,30 @@ public class ExtendedFailInfo
             if (asn1Value instanceof ASN1Sequence)
             {
                 return new ExtendedFailInfo(
-                    (ASN1ObjectIdentifier)((ASN1Sequence)asn1Value).getObjectAt(0),
-                    ((ASN1Sequence)asn1Value).getObjectAt(1)
+                    (ASN1Sequence)asn1Value
                 );
             }
         }
+        else if (obj instanceof byte[])
+        {
+            return getInstance(ASN1Sequence.getInstance(obj));
+        }
+
         throw new IllegalArgumentException("unknown object in getInstance(): " + obj.getClass().getName());
     }
 
     public ASN1Primitive toASN1Primitive()
     {
         return new DERSequence(new ASN1Encodable[]{failInfoOID, failInfoValue});
+    }
+
+    public ASN1ObjectIdentifier getFailInfoOID()
+    {
+        return failInfoOID;
+    }
+
+    public ASN1Encodable getFailInfoValue()
+    {
+        return failInfoValue;
     }
 }
