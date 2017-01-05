2707f3cc966a9d0751a95d230c1cdeec3d2ed4bc
Megan Woods <megan@kllective.com>
2017-01-05 11:33:31 +1100

    Added null testing in isEquals of SimpleTest. Changed cRLName option instanceof in GetCRL(ASN1Sequence) from ASN1Sequence to ASN1TaggedObject. Added test GetCRLTest.

--- core/src/main/java/org/bouncycastle/asn1/cmc/GetCRL.java
+++ core/src/main/java/org/bouncycastle/asn1/cmc/GetCRL.java
@@ -5,6 +5,7 @@ import org.bouncycastle.asn1.ASN1GeneralizedTime;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.ASN1TaggedObject;
 import org.bouncycastle.asn1.DERBitString;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.x500.X500Name;
@@ -26,10 +27,18 @@ public class GetCRL
     extends ASN1Object
 {
     private final X500Name issuerName;
-
     private GeneralName cRLName;
     private ASN1GeneralizedTime time;
     private ReasonFlags reasons;
+
+    public GetCRL(X500Name issuerName, GeneralName cRLName, ASN1GeneralizedTime time, ReasonFlags reasons)
+    {
+        this.issuerName = issuerName;
+        this.cRLName = cRLName;
+        this.time = time;
+        this.reasons = reasons;
+    }
+
 
     private GetCRL(ASN1Sequence seq)
     {
@@ -40,7 +49,7 @@ public class GetCRL
         this.issuerName = X500Name.getInstance(seq.getObjectAt(0));
 
         int index = 1;
-        if (seq.size() > index && seq.getObjectAt(index).toASN1Primitive() instanceof ASN1Sequence)
+        if (seq.size() > index && seq.getObjectAt(index).toASN1Primitive() instanceof ASN1TaggedObject)
         {
             this.cRLName = GeneralName.getInstance(seq.getObjectAt(index++));
         }
@@ -72,6 +81,21 @@ public class GetCRL
     public X500Name getIssuerName()
     {
         return issuerName;
+    }
+
+    public GeneralName getcRLName()
+    {
+        return cRLName;
+    }
+
+    public ASN1GeneralizedTime getTime()
+    {
+        return time;
+    }
+
+    public ReasonFlags getReasons()
+    {
+        return reasons;
     }
 
     public ASN1Primitive toASN1Primitive()
--- core/src/main/java/org/bouncycastle/util/test/SimpleTest.java
+++ core/src/main/java/org/bouncycastle/util/test/SimpleTest.java
@@ -13,7 +13,7 @@ public abstract class SimpleTest
     {
         return SimpleTestResult.successful(this, "Okay");
     }
-    
+
     protected void fail(
         String message)
     {
@@ -54,6 +54,19 @@ public abstract class SimpleTest
         Object a,
         Object b)
     {
+        if (a == null && b == null)
+        {
+            return;
+        }
+        else if (a == null)
+        {
+            throw new TestFailedException(SimpleTestResult.failed(this, message));
+        }
+        else if (b == null)
+        {
+            throw new TestFailedException(SimpleTestResult.failed(this, message));
+        }
+
         if (!a.equals(b))
         {
             throw new TestFailedException(SimpleTestResult.failed(this, message));
@@ -61,12 +74,12 @@ public abstract class SimpleTest
     }
 
     protected void fail(
-        String    message,
+        String message,
         Throwable throwable)
     {
         throw new TestFailedException(SimpleTestResult.failed(this, message, throwable));
     }
-    
+
     protected void fail(
         String message,
         Object expected,
@@ -74,20 +87,19 @@ public abstract class SimpleTest
     {
         throw new TestFailedException(SimpleTestResult.failed(this, message, expected, found));
     }
-        
+
     protected boolean areEqual(
         byte[] a,
         byte[] b)
     {
         return Arrays.areEqual(a, b);
     }
-    
+
     public TestResult perform()
     {
         try
         {
             performTest();
-            
             return success();
         }
         catch (TestFailedException e)
@@ -96,21 +108,21 @@ public abstract class SimpleTest
         }
         catch (Exception e)
         {
-            return SimpleTestResult.failed(this, "Exception: " +  e, e);
+            return SimpleTestResult.failed(this, "Exception: " + e, e);
         }
     }
-    
+
     protected static void runTest(
-        Test        test)
+        Test test)
     {
         runTest(test, System.out);
     }
-    
+
     protected static void runTest(
-        Test        test,
+        Test test,
         PrintStream out)
     {
-        TestResult      result = test.perform();
+        TestResult result = test.perform();
 
         out.println(result.toString());
         if (result.getException() != null)
