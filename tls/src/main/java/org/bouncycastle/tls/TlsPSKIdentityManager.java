ed5c30f7b4e57a5487664d2b13fecc2554e3062e
David Hook <dgh@cryptoworkshop.com>
2017-02-04 18:18:07 +1100

    JavaDoc

--- tls/src/main/java/org/bouncycastle/tls/TlsPSKIdentityManager.java
+++ tls/src/main/java/org/bouncycastle/tls/TlsPSKIdentityManager.java
@@ -1,5 +1,8 @@
 package org.bouncycastle.tls;
 
+/**
+ * Base interface for an object that can process a PSK identity.
+ */
 public interface TlsPSKIdentityManager
 {
     byte[] getHint();
--- tls/src/main/java/org/bouncycastle/tls/TlsSession.java
+++ tls/src/main/java/org/bouncycastle/tls/TlsSession.java
@@ -1,5 +1,8 @@
 package org.bouncycastle.tls;
 
+/**
+ * Base interface for a carrier object for a TLS session.
+ */
 public interface TlsSession
 {
     SessionParameters exportSessionParameters();
