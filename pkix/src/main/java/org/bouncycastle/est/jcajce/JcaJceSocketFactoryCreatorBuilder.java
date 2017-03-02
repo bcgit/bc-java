095b55e86c0aab32c8cab62c96a4dde27da4fd9c
Megan Woods <megan@kllective.com>
2017-03-02 17:28:12 +1100

    Updated example clients to use new EST code.

--- pkix/src/main/java/org/bouncycastle/est/jcajce/JcaJceSocketFactoryCreatorBuilder.java
+++ pkix/src/main/java/org/bouncycastle/est/jcajce/JcaJceSocketFactoryCreatorBuilder.java
@@ -1,7 +1,6 @@
 package org.bouncycastle.est.jcajce;
 
 
-import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -9,42 +8,29 @@ import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CRL;
-import java.security.cert.CertPathBuilder;
-import java.security.cert.CertStore;
-import java.security.cert.CertificateException;
-import java.security.cert.CollectionCertStoreParameters;
-import java.security.cert.PKIXBuilderParameters;
-import java.security.cert.PKIXCertPathValidatorResult;
-import java.security.cert.TrustAnchor;
-import java.security.cert.X509CertSelector;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Set;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 public class JcaJceSocketFactoryCreatorBuilder
 {
-    protected String tlsVersion;
+    protected String tlsVersion = "TLS";
     protected String tlsProvider;
     protected KeyManagerFactory keyManagerFactory;
-    protected JcaJceAuthorizer estAuthorizer;
-    protected final Set<TrustAnchor> tlsTrustAnchors;
+    protected X509TrustManager[] trustManagers;
     protected CRL[] revocationLists;
 
-    public JcaJceSocketFactoryCreatorBuilder()
-    {
-        this.tlsTrustAnchors = null;
-    }
 
-    public JcaJceSocketFactoryCreatorBuilder(Set<TrustAnchor> trustAnchors)
+    public JcaJceSocketFactoryCreatorBuilder(X509TrustManager trustManager)
     {
-        this.tlsTrustAnchors = trustAnchors;
+        if (trustManager == null)
+        {
+            this.trustManagers = null;
+            return;
+        }
+        this.trustManagers = new X509TrustManager[]{trustManager};
     }
 
 
@@ -66,34 +52,8 @@ public class JcaJceSocketFactoryCreatorBuilder
         return this;
     }
 
-    public JcaJceSocketFactoryCreatorBuilder withKeyManagerFactory(
-        String type,
-        String provider,
-        KeyStore clientKeyStore,
-        char[] clientKeyStorePass)
-        throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, NoSuchProviderException
-    {
-        if (type == null && provider == null)
-        {
-            this.keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        }
-        else if (provider == null)
-        {
-            this.keyManagerFactory = KeyManagerFactory.getInstance(type);
-        }
-        else
-        {
-            this.keyManagerFactory = KeyManagerFactory.getInstance(type, provider);
-        }
-        this.keyManagerFactory.init(clientKeyStore, clientKeyStorePass);
-        return this;
-    }
 
-    public JcaJceSocketFactoryCreatorBuilder setEstAuthorizer(JcaJceAuthorizer estAuthorizer)
-    {
-        this.estAuthorizer = estAuthorizer;
-        return this;
-    }
+
 
     public JcaJceSocketFactoryCreatorBuilder setRevocationLists(CRL[] revocationLists)
     {
@@ -101,116 +61,11 @@ public class JcaJceSocketFactoryCreatorBuilder
         return this;
     }
 
-    /**
-     * Makes an authorizer that will accept any certificate tendered by the server.
-     *
-     * @return
-     */
-    protected JcaJceAuthorizer makeAuthorizerWithoutTrustAnchors()
-    {
-        return new JcaJceAuthorizer()
-        {
-            public void authorize(
-                X509Certificate[] chain,
-                String authType, Set<TrustAnchor> tlsTrustAnchors, CRL[] revocationLists)
-                throws CertificateException
-            {
-                // Does nothing, will accept any and all tendered certificates from the server.
-            }
-        };
-    }
-
-    /**
-     * Returns a default CertPath Authorizer.
-     *
-     * @return
-     */
-    protected JcaJceAuthorizer makeAuthorizerWithTrustAnchors()
-    {
-
-        return new JcaJceAuthorizer()
-        {
-            public void authorize(X509Certificate[] chain, String authType, Set<TrustAnchor> tlsTrustAnchors, CRL[] revocationLists)
-                throws CertificateException
-            {
-                try
-                {
-                    CertStore certStore = CertStore.getInstance("Collection",
-                        new CollectionCertStoreParameters(Arrays.asList(chain)), "BC");
-
-                    CertPathBuilder pathBuilder = CertPathBuilder.getInstance("PKIX", "BC");
-
-                    X509CertSelector constraints = new X509CertSelector();
-
-                    constraints.setCertificate(chain[0]);
-
-
-                    PKIXBuilderParameters param = new PKIXBuilderParameters(tlsTrustAnchors, constraints);
-                    param.addCertStore(certStore);
-                    if (revocationLists != null)
-                    {
-                        param.setRevocationEnabled(true);
-                        param.addCertStore(
-                            CertStore.getInstance(
-                                "Collection",
-                                new CollectionCertStoreParameters(Arrays.asList(revocationLists)
-                                )));
-                    }
-                    else
-                    {
-                        param.setRevocationEnabled(false);
-                    }
-
-                    PKIXCertPathValidatorResult result = (PKIXCertPathValidatorResult)pathBuilder.build(param);
-                }
-                catch (GeneralSecurityException e)
-                {
-                    throw new CertificateException("unable to process certificates: " + e.getMessage(), e);
-                }
-            }
-        };
-    }
-
-
     public SocketFactoryCreator build()
     {
-        if (tlsTrustAnchors != null && tlsTrustAnchors.isEmpty())
+        if (trustManagers == null)
         {
-            throw new IllegalStateException("Empty TrustAnchors, must be null or contain one or more TrustAnchors.");
-        }
-
-
-        final X509Certificate[] acceptedIssuers;
-
-        if (tlsVersion == null)
-        {
-            tlsVersion = "TLS";
-        }
-
-        if (estAuthorizer == null)
-        {
-            if (tlsTrustAnchors == null || tlsTrustAnchors.isEmpty())
-            {
-                estAuthorizer = makeAuthorizerWithoutTrustAnchors();
-            }
-            else
-            {
-                estAuthorizer = makeAuthorizerWithTrustAnchors();
-            }
-        }
-
-        if (tlsTrustAnchors != null)
-        {
-            acceptedIssuers = new X509Certificate[tlsTrustAnchors.size()];
-            int i = 0;
-            for (Iterator<TrustAnchor> it = tlsTrustAnchors.iterator(); it.hasNext(); )
-            {
-                acceptedIssuers[i++] = it.next().getTrustedCert();
-            }
-        }
-        else
-        {
-            acceptedIssuers = new X509Certificate[0];
+            throw new IllegalStateException("Trust managers can not be null");
         }
 
 
@@ -219,7 +74,15 @@ public class JcaJceSocketFactoryCreatorBuilder
 
             public boolean isTrusted()
             {
-                return tlsTrustAnchors != null && !tlsTrustAnchors.isEmpty();
+                for (X509TrustManager tm : trustManagers)
+                {
+                    if (tm.getAcceptedIssuers().length > 0)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
             }
 
             public SSLSocketFactory createFactory()
@@ -236,41 +99,7 @@ public class JcaJceSocketFactoryCreatorBuilder
                 }
 
 
-                if (keyManagerFactory == null && estAuthorizer == null)
-                {
-                    ctx.init(null, null, new SecureRandom());
-                    return ctx.getSocketFactory();
-                }
-
-
-                X509TrustManager tm = new X509TrustManager()
-                {
-                    public void checkClientTrusted(X509Certificate[] x509Certificates, String authType)
-                        throws CertificateException
-                    {
-                        // For clients.
-                    }
-
-                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s)
-                        throws CertificateException
-                    {
-                        if (estAuthorizer == null)
-                        {
-                            throw new CertificateException(
-                                "No serverTLSAuthorizer specified, if you wish to have no validation then you must supply an instance that does nothing."
-                            );
-                        }
-
-                        estAuthorizer.authorize(x509Certificates, s, tlsTrustAnchors, revocationLists);
-                    }
-
-                    public X509Certificate[] getAcceptedIssuers()
-                    {
-                        return acceptedIssuers;
-                    }
-                };
-
-                ctx.init((keyManagerFactory != null) ? keyManagerFactory.getKeyManagers() : null, new TrustManager[]{tm}, new SecureRandom());
+                ctx.init((keyManagerFactory != null) ? keyManagerFactory.getKeyManagers() : null, trustManagers, new SecureRandom());
                 return ctx.getSocketFactory();
 
             }
