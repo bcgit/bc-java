fdf1b8988423fdfc229e58582d731108b8bed005
David Hook <dgh@cryptoworkshop.com>
2017-03-28 12:41:11 +1100

    removed dependency on utils class

--- tls/src/main/java/org/bouncycastle/tls/crypto/impl/jcajce/SignatureOutputStream.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/impl/jcajce/SignatureOutputStream.java
@@ -1,20 +1,34 @@
 package org.bouncycastle.tls.crypto.impl.jcajce;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.security.Signature;
 import java.security.SignatureException;
 
 import org.bouncycastle.tls.AlertDescription;
 import org.bouncycastle.tls.TlsFatalAlert;
-import org.bouncycastle.util.io.SimpleOutputStream;
 
-class SignatureOutputStream extends SimpleOutputStream
+class SignatureOutputStream extends OutputStream
 {
     protected final Signature s;
 
     SignatureOutputStream(Signature s)
     {
         this.s = s;
+    }
+
+    public void close()
+    {
+    }
+
+    public void flush()
+    {
+    }
+
+    public void write(int b) throws IOException
+    {
+        byte[] buf = new byte[]{ (byte)b };
+        write(buf, 0, 1);
     }
 
     public void write(byte[] buf, int off, int len) throws IOException
