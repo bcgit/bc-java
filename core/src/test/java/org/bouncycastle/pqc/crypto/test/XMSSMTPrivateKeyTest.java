30a3b423634297f05259ac711c88707d5dc86e37
sebastian roland <seroland86@gmail.com>
2017-04-09 10:52:25 +0200

    XMSS/XMSS^MT implementation according to draft-irtf-cfrg-xmss-hash-based-signatures-09 including BDS algorithm for efficient auth path computation

---
+++ core/src/test/java/org/bouncycastle/pqc/crypto/test/XMSSMTPrivateKeyTest.java
@@ -0,0 +1,35 @@
+package org.bouncycastle.pqc.crypto.test;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.pqc.crypto.xmss.NullPRNG;
+import org.bouncycastle.pqc.crypto.xmss.XMSSMT;
+import org.bouncycastle.pqc.crypto.xmss.XMSSMTParameters;
+import org.bouncycastle.pqc.crypto.xmss.XMSSUtil;
+
+import junit.framework.TestCase;
+
+/**
+ * Test cases for XMSSMTPrivateKey class.
+ * 
+ * @author Sebastian Roland <seroland86@gmail.com>
+ */
+public class XMSSMTPrivateKeyTest extends TestCase {
+
+	public void testPrivateKeyParsingSHA256() throws IOException, ClassNotFoundException {
+		XMSSMTParameters params = new XMSSMTParameters(20, 10, new SHA256Digest(), new NullPRNG());
+		XMSSMT mt = new XMSSMT(params);
+		mt.generateKeys();
+		byte[] privateKey = mt.exportPrivateKey();
+		byte[] publicKey = mt.exportPublicKey();
+		byte[] bdsStates = mt.exportBDSState();
+		try {
+			mt.importState(privateKey, publicKey, bdsStates);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		assertTrue(XMSSUtil.compareByteArray(privateKey, mt.exportPrivateKey()));
+	}
+}
