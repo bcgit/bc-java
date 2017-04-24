401d641b9f6d1b797f00869048a2388c589f2322
Sebastian Roland <seroland86@gmail.com>
2017-04-24 20:25:15 +0200

    reduce tests

--- core/src/test/java/org/bouncycastle/pqc/crypto/test/AllTests.java
+++ core/src/test/java/org/bouncycastle/pqc/crypto/test/AllTests.java
@@ -29,13 +29,13 @@ public class AllTests
         suite.addTestSuite(XMSSMTPrivateKeyTest.class);
         suite.addTestSuite(XMSSMTPublicKeyTest.class);
         suite.addTestSuite(XMSSMTSignatureTest.class);
-        //suite.addTestSuite(XMSSMTTest.class);
+        suite.addTestSuite(XMSSMTTest.class);
         suite.addTestSuite(XMSSOidTest.class);
         suite.addTestSuite(XMSSPrivateKeyTest.class);
         suite.addTestSuite(XMSSPublicKeyTest.class);
         suite.addTestSuite(XMSSReducedSignatureTest.class);
         suite.addTestSuite(XMSSSignatureTest.class);
-        //suite.addTestSuite(XMSSTest.class);
+        suite.addTestSuite(XMSSTest.class);
         suite.addTestSuite(XMSSUtilTest.class);
         suite.addTestSuite(AllTests.SimpleTestTest.class);
 
