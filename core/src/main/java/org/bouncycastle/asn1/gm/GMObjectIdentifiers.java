8fac36bbc4cea658b2c2b405b8ed2d51f5611ca5
David Hook <dgh@cryptoworkshop.com>
2017-04-29 16:39:52 +1000

    further work on SM2

---
+++ core/src/main/java/org/bouncycastle/asn1/gm/GMObjectIdentifiers.java
@@ -0,0 +1,10 @@
+package org.bouncycastle.asn1.gm;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+
+public interface GMObjectIdentifiers
+{
+    ASN1ObjectIdentifier SM2_Elliptic_Curve_Cryptography  = new ASN1ObjectIdentifier("1.2.156.10197.1.301");
+
+    ASN1ObjectIdentifier SM2_Signing_with_SM3 = new ASN1ObjectIdentifier("1.2.156.10197.1.501");
+}
