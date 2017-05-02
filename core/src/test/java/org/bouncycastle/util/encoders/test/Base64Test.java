2f6db8b327500e7ff4ab2f67ab06d0cd43858ee7
David Hook <dgh@cryptoworkshop.com>
2017-05-02 12:49:14 +1000

    fixed last character check on padded Base64 data.

--- core/src/test/java/org/bouncycastle/util/encoders/test/Base64Test.java
+++ core/src/test/java/org/bouncycastle/util/encoders/test/Base64Test.java
@@ -31,6 +31,7 @@ public class Base64Test extends AbstractCoderTest
     private static final String invalidb = "UJmEdJYodqHJmd7Rtv6/OP29/jU%Fc==";
     private static final String invalidc = "mO4TyLWG7vjFWdKT8IJcVbZ/%wc=";
     private static final String invalidd = "F4I4p8Vf/mS+Kxvri3FPoMcqm%1c";
+    private static final String invalide = "UJmEdJYodqHJmd7Rtv6/OP29/jUEFw=1";
 
 
     public Base64Test(
@@ -59,7 +60,7 @@ public class Base64Test extends AbstractCoderTest
     public void testInvalidInput()
         throws IOException
     {
-        String[] invalid = new String[] { invalid1, invalid2, invalid3, invalid4, invalid5, invalid6, invalid7, invalid8, invalid9, invalida, invalidb, invalidc, invalidd };
+        String[] invalid = new String[] { invalid1, invalid2, invalid3, invalid4, invalid5, invalid6, invalid7, invalid8, invalid9, invalida, invalidb, invalidc, invalidd, invalide };
 
         for (int i = 0; i != invalid.length; i++)
         {
