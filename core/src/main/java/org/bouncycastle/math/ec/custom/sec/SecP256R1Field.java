eb70f29122c4ff67fb89c0da1533045cf7d527d9
Peter Dettman <peter.dettman@bouncycastle.org>
2017-06-03 19:03:07 +0700

    Fix reductions for custom secp128r1 field

--- core/src/main/java/org/bouncycastle/math/ec/custom/sec/SecP256R1Field.java
+++ core/src/main/java/org/bouncycastle/math/ec/custom/sec/SecP256R1Field.java
@@ -16,7 +16,7 @@ public class SecP256R1Field
         0xFFFFFFFF, 0xFFFFFFFE, 0x00000001, 0xFFFFFFFE, 0x00000001, 0xFFFFFFFE, 0x00000001, 0x00000001, 0xFFFFFFFE,
         0x00000002, 0xFFFFFFFE };
     private static final int P7 = 0xFFFFFFFF;
-    private static final int PExt15 = 0xFFFFFFFF;
+    private static final int PExt15s1 = 0xFFFFFFFE >>> 1;
 
     public static void add(int[] x, int[] y, int[] z)
     {
@@ -30,7 +30,7 @@ public class SecP256R1Field
     public static void addExt(int[] xx, int[] yy, int[] zz)
     {
         int c = Nat.add(16, xx, yy, zz);
-        if (c != 0 || (zz[15] == PExt15 && Nat.gte(16, zz, PExt)))
+        if (c != 0 || ((zz[15] >>> 1) >= PExt15s1 && Nat.gte(16, zz, PExt)))
         {
             Nat.subFrom(16, PExt, zz);
         }
@@ -78,7 +78,7 @@ public class SecP256R1Field
     public static void multiplyAddToExt(int[] x, int[] y, int[] zz)
     {
         int c = Nat256.mulAddTo(x, y, zz);
-        if (c != 0 || (zz[15] == PExt15 && Nat.gte(16, zz, PExt)))
+        if (c != 0 || ((zz[15] >>> 1) >= PExt15s1 && Nat.gte(16, zz, PExt)))
         {
             Nat.subFrom(16, PExt, zz);
         }
