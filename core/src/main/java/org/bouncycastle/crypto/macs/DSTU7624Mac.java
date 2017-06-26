6f39fdaa7da50c0c0da80f4eba4de514f5a476a5
David Hook <dgh@cryptoworkshop.com>
2017-06-26 19:29:01 +1000

    enabled both update methods

--- core/src/main/java/org/bouncycastle/crypto/macs/DSTU7624Mac.java
+++ core/src/main/java/org/bouncycastle/crypto/macs/DSTU7624Mac.java
@@ -6,7 +6,6 @@ import org.bouncycastle.crypto.Mac;
 import org.bouncycastle.crypto.engines.DSTU7624Engine;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.util.Arrays;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Implementation of DSTU7624 MAC mode
@@ -14,8 +13,10 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class DSTU7624Mac
     implements Mac
 {
-
     private final static int BITS_IN_BYTE = 8;
+
+    private byte[]              buf;
+    private int                 bufOff;
 
     private int macSize;
     private int blockSize;
@@ -25,13 +26,13 @@ public class DSTU7624Mac
 
     public DSTU7624Mac(int blockBitLength, int keyBitLength, int q)
     {
-
         this.engine = new DSTU7624Engine(blockBitLength, keyBitLength);
         this.blockSize = blockBitLength / BITS_IN_BYTE;
         this.macSize = q / BITS_IN_BYTE;
         this.c = new byte[blockSize];
         this.kDelta = new byte[blockSize];
         this.cTemp = new byte[blockSize];
+        this.buf = new byte[blockSize];
     }
 
     public void init(CipherParameters params)
@@ -59,53 +60,74 @@ public class DSTU7624Mac
     }
 
     public void update(byte in)
-        throws IllegalStateException
     {
-        throw new NotImplementedException();
+        if (bufOff == buf.length)
+        {
+            processBlock(buf, 0);
+            bufOff = 0;
+        }
+
+        buf[bufOff++] = in;
     }
 
     public void update(byte[] in, int inOff, int len)
-        throws DataLengthException, IllegalStateException
     {
         if (len < 0)
         {
-            throw new IllegalArgumentException("Can't have negative input length!");
+            throw new IllegalArgumentException(
+                "can't have a negative input length!");
         }
 
-        if ((inOff + len) % blockSize != 0)
+        int blockSize = engine.getBlockSize();
+        int gapLen = blockSize - bufOff;
+
+        if (len > gapLen)
         {
-            //Partial blocks not supported
-            throw new NotImplementedException();
+            System.arraycopy(in, inOff, buf, bufOff, gapLen);
+
+            processBlock(buf, 0);
+
+            bufOff = 0;
+            len -= gapLen;
+            inOff += gapLen;
+
+            while (len > blockSize)
+            {
+                processBlock(in, inOff);
+
+                len -= blockSize;
+                inOff += blockSize;
+            }
         }
 
-        if (inOff + len > in.length)
-        {
-            throw new DataLengthException("Input buffer too short");
-        }
+        System.arraycopy(in, inOff, buf, bufOff, len);
 
-        while (len > blockSize)
-        {
-            xor(c, 0, in, inOff, cTemp);
+        bufOff += len;
+    }
 
-            engine.processBlock(cTemp, 0, c, 0);
-
-            len -= blockSize;
-            inOff += blockSize;
-        }
-
-        //Last block
+    private void processBlock(byte[] in, int inOff)
+    {
         xor(c, 0, in, inOff, cTemp);
-        xor(cTemp, 0, kDelta, 0, c);
-        engine.processBlock(c, 0, c, 0);
+
+        engine.processBlock(cTemp, 0, c, 0);
     }
 
     public int doFinal(byte[] out, int outOff)
         throws DataLengthException, IllegalStateException
     {
+        if (bufOff % buf.length != 0)
+        {
+            throw new DataLengthException("input must be a multiple of blocksize");
+        }
+
+        //Last block
+        xor(c, 0, buf, 0, cTemp);
+        xor(cTemp, 0, kDelta, 0, c);
+        engine.processBlock(c, 0, c, 0);
 
         if (macSize + outOff > out.length)
         {
-            throw new DataLengthException("Output buffer too short");
+            throw new DataLengthException("output buffer too short");
         }
 
         System.arraycopy(c, 0, out, outOff, macSize);
@@ -118,8 +140,10 @@ public class DSTU7624Mac
         Arrays.fill(c, (byte)0x00);
         Arrays.fill(cTemp, (byte)0x00);
         Arrays.fill(kDelta, (byte)0x00);
+        Arrays.fill(buf, (byte)0x00);
         engine.reset();
         engine.processBlock(kDelta, 0, kDelta, 0);
+        bufOff = 0;
     }
 
     private void xor(byte[] x, int xOff, byte[] y, int yOff, byte[] x_xor_y)
@@ -127,7 +151,7 @@ public class DSTU7624Mac
 
         if (x.length - xOff < blockSize || y.length - yOff < blockSize || x_xor_y.length < blockSize)
         {
-            throw new IllegalArgumentException("Some of input buffers too short");
+            throw new IllegalArgumentException("some of input buffers too short");
         }
         for (int byteIndex = 0; byteIndex < blockSize; byteIndex++)
         {
