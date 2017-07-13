3e83171839d24ff84f61bc4ee2a25a9aad939da7
Peter Dettman <peter.dettman@bouncycastle.org>
2017-07-13 12:26:22 +0700

    Add TlsObjectIdentifiers class
    - initially with id-pe-tlsfeature from RFC 7633

---
+++ tls/src/main/java/org/bouncycastle/tls/TlsObjectIdentifiers.java
@@ -0,0 +1,12 @@
+package org.bouncycastle.tls;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
+
+public interface TlsObjectIdentifiers
+{
+    /**
+     * RFC 7633
+     */
+    static final ASN1ObjectIdentifier id_pe_tlsfeature = X509ObjectIdentifiers.id_pe.branch("24");
+}
