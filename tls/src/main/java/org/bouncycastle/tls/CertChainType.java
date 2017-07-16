6cb82afe1c3d2381a24dac58da2a6e64f113d1d9
Peter Dettman <peter.dettman@bouncycastle.org>
2017-07-16 11:13:31 +0700

    More work on client_certificate_url extension (RFC 6066)

--- tls/src/main/java/org/bouncycastle/tls/CertChainType.java
+++ tls/src/main/java/org/bouncycastle/tls/CertChainType.java
@@ -8,6 +8,24 @@ public class CertChainType
     public static final short individual_certs = 0;
     public static final short pkipath = 1;
 
+    public static String getName(short certChainType)
+    {
+        switch (certChainType)
+        {
+        case individual_certs:
+            return "individual_certs";
+        case pkipath:
+            return "pkipath";
+        default:
+            return "UNKNOWN";
+        }
+    }
+
+    public static String getText(short certChainType)
+    {
+        return getName(certChainType) + "(" + certChainType + ")";
+    }
+
     public static boolean isValid(short certChainType)
     {
         return certChainType >= individual_certs && certChainType <= pkipath;
