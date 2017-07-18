c62bf1029d18ca6d74c01f571a682f616ea43260
David Hook <dgh@cryptoworkshop.com>
2017-07-18 10:06:20 +1000

    minor tweaks, fixed name of rotate///

--- core/src/main/java/org/bouncycastle/crypto/digests/KeccakDigest.java
+++ core/src/main/java/org/bouncycastle/crypto/digests/KeccakDigest.java
@@ -459,7 +459,7 @@ public class KeccakDigest
         }
     }
 
-    private long rRotate(long v, int r)
+    private long leftRotate(long v, int r)
     {
         return (v << r) | (v >>> -r);
     }
@@ -472,15 +472,15 @@ public class KeccakDigest
         long C3 = A[3 + 0] ^ A[3 + 5] ^ A[3 + 10] ^ A[3 + 15] ^ A[3 + 20];
         long C4 = A[4 + 0] ^ A[4 + 5] ^ A[4 + 10] ^ A[4 + 15] ^ A[4 + 20];
 
-        long dX = rRotate(C1, 1) ^ C4;
-
+        long dX = leftRotate(C1, 1) ^ C4;
+                       
         A[0] ^= dX;
         A[5] ^= dX;
         A[10] ^= dX;
         A[15] ^= dX;
         A[20] ^= dX;
 
-        dX = rRotate(C2, 1) ^ C0;
+        dX = leftRotate(C2, 1) ^ C0;
 
         A[1] ^= dX;
         A[6] ^= dX;
@@ -488,7 +488,7 @@ public class KeccakDigest
         A[16] ^= dX;
         A[21] ^= dX;
 
-        dX = rRotate(C3, 1) ^ C1;
+        dX = leftRotate(C3, 1) ^ C1;
 
         A[2] ^= dX;
         A[7] ^= dX;
@@ -496,7 +496,7 @@ public class KeccakDigest
         A[17] ^= dX;
         A[22] ^= dX;
 
-        dX = rRotate(C4, 1) ^ C2;
+        dX = leftRotate(C4, 1) ^ C2;
 
         A[3] ^= dX;
         A[8] ^= dX;
@@ -504,7 +504,7 @@ public class KeccakDigest
         A[18] ^= dX;
         A[23] ^= dX;
 
-        dX = rRotate(C0, 1) ^ C3;
+        dX = leftRotate(C0, 1) ^ C3;
 
         A[4] ^= dX;
         A[9] ^= dX;
@@ -518,7 +518,7 @@ public class KeccakDigest
         // KeccakRhoOffsets[0] == 0
         for (int x = 1; x < 25; x++)
         {
-            A[x] = rRotate(A[x], KeccakRhoOffsets[x]);
+            A[x] = leftRotate(A[x], KeccakRhoOffsets[x]);
         }
     }
 
@@ -559,21 +559,23 @@ public class KeccakDigest
         A[4 + 5 * ((2 * 4 + 3 * 4) % 5)] = tempA[4 + 5 * 4];
     }
 
-    private final long[] chiC = new long[5];
-
     private void chi(long[] A)
     {
-        for (int y = 0; y < 5; y++)
+        long chiC0, chiC1, chiC2, chiC3, chiC4;
+
+        for (int yBy5 = 0; yBy5 < 25; yBy5 += 5)
         {
-            chiC[0] = A[0 + 5 * y] ^ ((~A[(((0 + 1) % 5) + 5 * y)]) & A[(((0 + 2) % 5) + 5 * y)]);
-            chiC[1] = A[1 + 5 * y] ^ ((~A[(((1 + 1) % 5) + 5 * y)]) & A[(((1 + 2) % 5) + 5 * y)]);
-            chiC[2] = A[2 + 5 * y] ^ ((~A[(((2 + 1) % 5) + 5 * y)]) & A[(((2 + 2) % 5) + 5 * y)]);
-            chiC[3] = A[3 + 5 * y] ^ ((~A[(((3 + 1) % 5) + 5 * y)]) & A[(((3 + 2) % 5) + 5 * y)]);
-            chiC[4] = A[4 + 5 * y] ^ ((~A[(((4 + 1) % 5) + 5 * y)]) & A[(((4 + 2) % 5) + 5 * y)]);
-            for (int x = 0; x < 5; x++)
-            {
-                A[x + 5 * y] = chiC[x];
-            }
+            chiC0 = A[0 + yBy5] ^ ((~A[(((0 + 1) % 5) + yBy5)]) & A[(((0 + 2) % 5) + yBy5)]);
+            chiC1 = A[1 + yBy5] ^ ((~A[(((1 + 1) % 5) + yBy5)]) & A[(((1 + 2) % 5) + yBy5)]);
+            chiC2 = A[2 + yBy5] ^ ((~A[(((2 + 1) % 5) + yBy5)]) & A[(((2 + 2) % 5) + yBy5)]);
+            chiC3 = A[3 + yBy5] ^ ((~A[(((3 + 1) % 5) + yBy5)]) & A[(((3 + 2) % 5) + yBy5)]);
+            chiC4 = A[4 + yBy5] ^ ((~A[(((4 + 1) % 5) + yBy5)]) & A[(((4 + 2) % 5) + yBy5)]);
+
+            A[0 + yBy5] = chiC0;
+            A[1 + yBy5] = chiC1;
+            A[2 + yBy5] = chiC2;
+            A[3 + yBy5] = chiC3;
+            A[4 + yBy5] = chiC4;
         }
     }
 
