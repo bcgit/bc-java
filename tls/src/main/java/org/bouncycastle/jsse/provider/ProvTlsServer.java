f96e671b0e351b4661c6641732451af9dce4349b
Peter Dettman <peter.dettman@bouncycastle.org>
2017-09-05 12:32:34 +0700

    Support system property sun.security.ssl.allowLegacyHelloMessages
    - allows interop with peers not implementing RFC 5746
    - defaults to 'true' ("Interoperable")

--- tls/src/main/java/org/bouncycastle/jsse/provider/ProvTlsServer.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/ProvTlsServer.java
@@ -388,6 +388,23 @@ class ProvTlsServer
     }
 
     @Override
+    public void notifySecureRenegotiation(boolean secureRenegotiation) throws IOException
+    {
+        if (!secureRenegotiation)
+        {
+            boolean allowLegacyHelloMessages = PropertyUtils.getBooleanSystemProperty("sun.security.ssl.allowLegacyHelloMessages", true);
+            if (!allowLegacyHelloMessages)
+            {
+                /*
+                 * RFC 5746 3.4/3.6. In this case, some clients/servers may want to terminate the handshake instead
+                 * of continuing; see Section 4.1/4.3 for discussion.
+                 */
+                throw new TlsFatalAlert(AlertDescription.handshake_failure);
+            }
+        }
+    }
+
+    @Override
     public void processClientExtensions(Hashtable clientExtensions) throws IOException
     {
         super.processClientExtensions(clientExtensions);
