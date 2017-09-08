804819bced4f8046a638f1081cf75ee09b0b64bb
Peter Dettman <peter.dettman@bouncycastle.org>
2017-09-08 11:42:16 +0700

    Add javadoc to make clear this is for testing only

--- core/src/main/java/org/bouncycastle/util/test/UncloseableOutputStream.java
+++ core/src/main/java/org/bouncycastle/util/test/UncloseableOutputStream.java
@@ -4,6 +4,11 @@ import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * This is a testing utility class to check the property that an {@link OutputStream} is never
+ * closed in some particular context - typically when wrapped by another {@link OutputStream} that
+ * should not be forwarding its {@link OutputStream#close()} calls. Not needed in production code.
+ */
 public class UncloseableOutputStream extends FilterOutputStream
 {
     public UncloseableOutputStream(OutputStream s)
