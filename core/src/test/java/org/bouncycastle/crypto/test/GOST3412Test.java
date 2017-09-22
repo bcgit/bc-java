12a24f863ec5a7cb00ba6d7389f3428fee20bfe2
msafonov <msafonovmail@gmail.com>
2017-09-22 11:03:08 +0300

    GOST 34.12 2015 modes ECB, CFB, OFB

--- core/src/test/java/org/bouncycastle/crypto/test/GOST3412Test.java
+++ core/src/test/java/org/bouncycastle/crypto/test/GOST3412Test.java
@@ -1,19 +1,14 @@
 package org.bouncycastle.crypto.test;
 
-import org.bouncycastle.crypto.BlockCipher;
-import org.bouncycastle.crypto.BufferedBlockCipher;
-import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.engines.GOST3412_2015Engine;
 import org.bouncycastle.crypto.modes.CBCBlockCipher;
-import org.bouncycastle.crypto.modes.GOFBBlockCipher;
+import org.bouncycastle.crypto.modes.CFBBlockCipher;
+import org.bouncycastle.crypto.modes.GCTRBlockCipher;
+import org.bouncycastle.crypto.modes.OFBBlockCipher;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
-import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.util.test.SimpleTest;
-
-import java.math.BigInteger;
-import java.security.SecureRandom;
 
 /**
  * @author MikeSafonov
@@ -21,19 +16,29 @@ import java.security.SecureRandom;
 public class GOST3412Test extends CipherTest {
 
     static SimpleTest[] tests = {
-//        new BlockCipherVectorTest(1, new GOST3412_2015Engine(),
-//            new KeyParameter(Hex.decode("8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef")),
-//            "1122334455667700ffeeddccbbaa9988", "7f679d90bebc24305a468d42b9d4edcd"),
 
-//        new BlockCipherVectorTest(2, new CBCBlockCipher(new GOST3412_2015Engine()),
-//            new ParametersWithIV(new KeyParameter(Hex.decode("8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef")),
-//                Hex.decode("1234567890abcef0a1b2c3d4e5f0011223344556677889901213141516171819")),
-//            "1122334455667700ffeeddccbbaa9988", "00112233445566778899aabbcceeff0a"),
+        new BlockCipherVectorTest(1, new GOST3412_2015Engine(),
+            new KeyParameter(Hex.decode("8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef")),
+            "1122334455667700ffeeddccbbaa9988", "7f679d90bebc24305a468d42b9d4edcd"),
 
-        new BlockCipherVectorTest(2, new GOFBBlockCipher(new GOST3412_2015Engine()),
+
+        new BlockCipherVectorTest(2, new CFBBlockCipher(new GOST3412_2015Engine(), 128),
             new ParametersWithIV(new KeyParameter(Hex.decode("8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef")),
-                Hex.decode("1234567890abcef0")),
-            "1122334455667700ffeeddccbbaa9988", "00112233445566778899aabbcceeff0a")
+                Hex.decode("1234567890abcef0a1b2c3d4e5f0011223344556677889901213141516171819")), "1122334455667700ffeeddccbbaa9988", "81800a59b1842b24ff1f795e897abd95"),
+
+        new BlockCipherVectorTest(3, new OFBBlockCipher(new GOST3412_2015Engine(), 128),
+            new ParametersWithIV(new KeyParameter(Hex.decode("8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef")),
+                Hex.decode("1234567890abcef0a1b2c3d4e5f0011223344556677889901213141516171819")),
+            "1122334455667700ffeeddccbbaa9988", "81800a59b1842b24ff1f795e897abd95"),
+
+
+//                new BlockCipherVectorTest(2, new GCTRBlockCipher(new GOST3412_2015Engine(), 16),
+//                    new ParametersWithIV(new KeyParameter(Hex.decode("8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef")),
+//                        Hex.decode("1234567890abcef0")),
+//                    "1122334455667700ffeeddccbbaa9988", "f195d8bec10ed1dbd57b5fa240bda1b8"),
+
+        // ==============
+
     };
 
 
--- prov/src/main/java/org/bouncycastle/jcajce/provider/symmetric/GOST3412_2015.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/symmetric/GOST3412_2015.java
@@ -1,57 +1,58 @@
 package org.bouncycastle.jcajce.provider.symmetric;
 
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.cryptopro.CryptoProObjectIdentifiers;
 import org.bouncycastle.crypto.BufferedBlockCipher;
 import org.bouncycastle.crypto.CipherKeyGenerator;
-import org.bouncycastle.crypto.engines.CryptoProWrapEngine;
 import org.bouncycastle.crypto.engines.GOST28147Engine;
-import org.bouncycastle.crypto.engines.GOST28147WrapEngine;
 import org.bouncycastle.crypto.engines.GOST3412_2015Engine;
-import org.bouncycastle.crypto.macs.GOST3412_2015Mac;
+import org.bouncycastle.crypto.macs.CMac;
 import org.bouncycastle.crypto.modes.CBCBlockCipher;
-import org.bouncycastle.crypto.modes.GCFBBlockCipher;
+import org.bouncycastle.crypto.modes.CFBBlockCipher;
+import org.bouncycastle.crypto.modes.GCTRBlockCipher;
+import org.bouncycastle.crypto.modes.OFBBlockCipher;
 import org.bouncycastle.jcajce.provider.config.ConfigurableProvider;
-import org.bouncycastle.jcajce.provider.symmetric.util.*;
+import org.bouncycastle.jcajce.provider.symmetric.util.BaseBlockCipher;
+import org.bouncycastle.jcajce.provider.symmetric.util.BaseKeyGenerator;
 import org.bouncycastle.jcajce.provider.util.AlgorithmProvider;
-
-import java.security.AlgorithmParameters;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.SecureRandom;
-import java.security.spec.AlgorithmParameterSpec;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class GOST3412_2015 {
 
 
     public static class ECB
-        extends BaseBlockCipher
-    {
-        public ECB()
-        {
+        extends BaseBlockCipher {
+        public ECB() {
             super(new GOST3412_2015Engine());
         }
     }
 
     public static class CBC
-        extends BaseBlockCipher
-    {
-        public CBC()
-        {
+        extends BaseBlockCipher {
+        public CBC() {
             super(new CBCBlockCipher(new GOST3412_2015Engine()), 128);
         }
     }
 
     public static class GCFB
-        extends BaseBlockCipher
-    {
-        public GCFB()
-        {
-            super(new BufferedBlockCipher(new GCFBBlockCipher(new GOST28147Engine())), 128);
+        extends BaseBlockCipher {
+        public GCFB() {
+            super(new BufferedBlockCipher(new CFBBlockCipher(new GOST3412_2015Engine(), 128)), 128);
         }
     }
+
+    public static class OFB extends BaseBlockCipher {
+        public OFB() {
+            super(new BufferedBlockCipher(new OFBBlockCipher(new GOST3412_2015Engine(), 128)), 128);
+        }
+
+    }
+
+    public static class CTR extends BaseBlockCipher {
+        public CTR() {
+            super(new BufferedBlockCipher(new GCTRBlockCipher(new GOST3412_2015Engine(), 128)), 128);
+        }
+
+    }
+
 
 //    public static class GostWrap
 //        extends BaseWrapCipher
@@ -63,78 +64,26 @@ public class GOST3412_2015 {
 //    }
 
     /**
-     * GOST3412 2015
+     * GOST3412 2015 CMAC( OMAC1)
      */
     public static class Mac
-        extends BaseMac
-    {
-        public Mac()
-        {
-            super(new GOST3412_2015Mac());
+        extends CMac {
+        public Mac() {
+            super(new GOST3412_2015Engine());
         }
     }
 
 
     public static class KeyGen
-        extends BaseKeyGenerator
-    {
-        public KeyGen()
-        {
+        extends BaseKeyGenerator {
+        public KeyGen() {
             this(256);
         }
 
-        public KeyGen(int keySize)
-        {
+        public KeyGen(int keySize) {
             super("GOST3412_2015", keySize, new CipherKeyGenerator());
         }
     }
-
-//    public static class AlgParamGen
-//        extends BaseAlgorithmParameterGenerator
-//    {
-//        byte[] iv = new byte[8];
-//        byte[] sBox = GOST28147Engine.getSBox("E-A");
-//
-//        protected void engineInit(
-//            AlgorithmParameterSpec genParamSpec,
-//            SecureRandom random)
-//            throws InvalidAlgorithmParameterException
-//        {
-//            if (genParamSpec instanceof GOST28147ParameterSpec)
-//            {
-//                this.sBox = ((GOST28147ParameterSpec)genParamSpec).getSBox();
-//            }
-//            else
-//            {
-//                throw new InvalidAlgorithmParameterException("parameter spec not supported");
-//            }
-//        }
-//
-//        protected AlgorithmParameters engineGenerateParameters()
-//        {
-//            if (random == null)
-//            {
-//                random = new SecureRandom();
-//            }
-//
-//            random.nextBytes(iv);
-//
-//            AlgorithmParameters params;
-//
-//            try
-//            {
-//                params = createParametersInstance("GOST28147");
-//                params.init(new GOST28147ParameterSpec(sBox, iv));
-//            }
-//            catch (Exception e)
-//            {
-//                throw new RuntimeException(e.getMessage());
-//            }
-//
-//            return params;
-//        }
-//    }
-//
 
     public static class Mappings
         extends AlgorithmProvider {
@@ -145,23 +94,20 @@ public class GOST3412_2015 {
 
         public void configure(ConfigurableProvider provider) {
             provider.addAlgorithm("Cipher.GOST3412_2015", PREFIX + "$ECB");
-            provider.addAlgorithm("Alg.Alias.Cipher.GOST", "GOST3412_2015");
             provider.addAlgorithm("Alg.Alias.Cipher.GOST3412_2015", "GOST3412_2015");
-            provider.addAlgorithm("Cipher." + CryptoProObjectIdentifiers.gostR28147_gcfb, PREFIX + "$GCFB");
+            provider.addAlgorithm("Alg.Alias.Cipher.GOST3412_2015_CFB", PREFIX + "$GCFB");
+            provider.addAlgorithm("Alg.Alias.Cipher.GOST3412_2015_OFB", PREFIX + "$OFB");
+            provider.addAlgorithm("Alg.Alias.Cipher.GOST3412_2015_CTR", PREFIX + "$CTR");
+
+//            provider.addAlgorithm("Cipher." + CryptoProObjectIdentifiers.gostR28147_gcfb, PREFIX + "$GCFB");
 
             provider.addAlgorithm("KeyGenerator.GOST3412_2015", PREFIX + "$KeyGen");
             provider.addAlgorithm("Alg.Alias.KeyGenerator.GOST", "GOST3412_2015");
             provider.addAlgorithm("Alg.Alias.KeyGenerator.GOST3412_2015", "GOST3412_2015");
-            provider.addAlgorithm("Alg.Alias.KeyGenerator." + CryptoProObjectIdentifiers.gostR28147_gcfb, "GOST3412_2015");
 
-            provider.addAlgorithm("AlgorithmParameters." + "GOST3412_2015", PREFIX + "$AlgParams");
-            provider.addAlgorithm("AlgorithmParameterGenerator." + "GOST3412_2015", PREFIX + "$AlgParamGen");
 
-            provider.addAlgorithm("Alg.Alias.AlgorithmParameters." + CryptoProObjectIdentifiers.gostR28147_gcfb, "GOST3412_2015");
-            provider.addAlgorithm("Alg.Alias.AlgorithmParameterGenerator." + CryptoProObjectIdentifiers.gostR28147_gcfb, "GOST3412_2015");
-
-            provider.addAlgorithm("Cipher." + CryptoProObjectIdentifiers.id_Gost28147_89_CryptoPro_KeyWrap, PREFIX + "$CryptoProWrap");
-            provider.addAlgorithm("Cipher." + CryptoProObjectIdentifiers.id_Gost28147_89_None_KeyWrap, PREFIX + "$GostWrap");
+//            provider.addAlgorithm("Cipher." + CryptoProObjectIdentifiers.id_Gost28147_89_CryptoPro_KeyWrap, PREFIX + "$CryptoProWrap");
+//            provider.addAlgorithm("Cipher." + CryptoProObjectIdentifiers.id_Gost28147_89_None_KeyWrap, PREFIX + "$GostWrap");
 
             provider.addAlgorithm("Mac.GOST3412MAC", PREFIX + "$Mac");
             provider.addAlgorithm("Alg.Alias.Mac.GOST3412_2015", "GOST3412MAC");
