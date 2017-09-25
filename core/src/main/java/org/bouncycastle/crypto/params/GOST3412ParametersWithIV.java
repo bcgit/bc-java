f3d0751d03fe8b3a03db7852927c95a4fb98e2ab
msafonov <msafonovmail@gmail.com>
2017-09-25 14:16:30 +0300

    added two modes: CFB (block/stream) and OFB(stream)

---
+++ core/src/main/java/org/bouncycastle/crypto/params/GOST3412ParametersWithIV.java
@@ -0,0 +1,44 @@
+package org.bouncycastle.crypto.params;
+
+import org.bouncycastle.crypto.CipherParameters;
+
+/**
+ * Parameters for GOST 3412 2015 cipher algorithm.
+ */
+public class GOST3412ParametersWithIV implements CipherParameters {
+
+    private byte[]              iv;
+    private CipherParameters    parameters;
+    private int s;
+    private int m;
+
+    public GOST3412ParametersWithIV(byte[] iv, CipherParameters parameters, int s, int m) {
+        this.iv = iv;
+        this.parameters = parameters;
+        this.s = s;
+        this.m = m;
+    }
+
+    public GOST3412ParametersWithIV(byte[] iv, CipherParameters parameters, int s) {
+        this.iv = iv;
+        this.parameters = parameters;
+        this.s = s;
+        this.m = 2 * s;
+    }
+
+    public byte[] getIV() {
+        return iv;
+    }
+
+    public CipherParameters getParameters() {
+        return parameters;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public int getM() {
+        return m;
+    }
+}
