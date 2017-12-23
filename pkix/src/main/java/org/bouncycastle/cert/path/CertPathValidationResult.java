febfa7d258a349a90cc364cb9981269fa3929d4d
David Hook <dgh@cryptoworkshop.com>
2017-12-23 12:23:40 +1100

    github #264: added failure details for evaluate on CertPath.

--- pkix/src/main/java/org/bouncycastle/cert/path/CertPathValidationResult.java
+++ pkix/src/main/java/org/bouncycastle/cert/path/CertPathValidationResult.java
@@ -3,18 +3,26 @@ package org.bouncycastle.cert.path;
 import java.util.Collections;
 import java.util.Set;
 
+import org.bouncycastle.util.Arrays;
+
 public class CertPathValidationResult
 {
     private final boolean isValid;
     private final CertPathValidationException cause;
     private final Set unhandledCriticalExtensionOIDs;
+    private final int certIndex;
+    private final int ruleIndex;
 
+    private CertPathValidationException[] causes;
     private int[] certIndexes;
+    private int[] ruleIndexes;
 
     public CertPathValidationResult(CertPathValidationContext context)
     {
         this.unhandledCriticalExtensionOIDs = Collections.unmodifiableSet(context.getUnhandledCriticalExtensionOIDs());
         this.isValid = this.unhandledCriticalExtensionOIDs.isEmpty();
+        this.certIndex = -1;
+        this.ruleIndex = -1;
         cause = null;
     }
 
@@ -22,16 +30,21 @@ public class CertPathValidationResult
     {
         this.unhandledCriticalExtensionOIDs = Collections.unmodifiableSet(context.getUnhandledCriticalExtensionOIDs());
         this.isValid = false;
+        this.certIndex = certIndex;
+        this.ruleIndex = ruleIndex;
         this.cause = cause;
     }
 
-    public CertPathValidationResult(CertPathValidationContext context, int[] certIndexes, int[] ruleIndexes, CertPathValidationException[] cause)
+    public CertPathValidationResult(CertPathValidationContext context, int[] certIndexes, int[] ruleIndexes, CertPathValidationException[] causes)
     {
-        // TODO
         this.unhandledCriticalExtensionOIDs = Collections.unmodifiableSet(context.getUnhandledCriticalExtensionOIDs());
         this.isValid = false;
-        this.cause = cause[0];
+        this.cause = causes[0];
+        this.certIndex = certIndexes[0];
+        this.ruleIndex = ruleIndexes[0];
+        this.causes = causes;
         this.certIndexes = certIndexes;
+        this.ruleIndexes = ruleIndexes;
     }
 
     public boolean isValid()
@@ -39,7 +52,7 @@ public class CertPathValidationResult
         return isValid;
     }
 
-    public Exception getCause()
+    public CertPathValidationException getCause()
     {
         if (cause != null)
         {
@@ -54,6 +67,16 @@ public class CertPathValidationResult
         return null;
     }
 
+    public int getFailingCertIndex()
+    {
+        return certIndex;
+    }
+
+    public int getFailingRuleIndex()
+    {
+        return ruleIndex;
+    }
+
     public Set getUnhandledCriticalExtensionOIDs()
     {
         return unhandledCriticalExtensionOIDs;
@@ -62,5 +85,35 @@ public class CertPathValidationResult
     public boolean isDetailed()
     {
         return this.certIndexes != null;
+    }
+
+    public CertPathValidationException[] getCauses()
+    {
+        if (causes != null)
+        {
+            CertPathValidationException[] rv = new CertPathValidationException[causes.length];
+
+            System.arraycopy(causes, 0, rv, 0, causes.length);
+
+            return rv;
+        }
+
+        if (!unhandledCriticalExtensionOIDs.isEmpty())
+        {
+            return new CertPathValidationException[]
+                { new CertPathValidationException("Unhandled Critical Extensions") };
+        }
+
+        return null;
+    }
+
+    public int[] getFailingCertIndexes()
+    {
+        return Arrays.clone(certIndexes);
+    }
+
+    public int[] getFailingRuleIndexes()
+    {
+        return Arrays.clone(ruleIndexes);
     }
 }
