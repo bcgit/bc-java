ef0bd5d1a9c9ccd55df6eaf0289f9f349c0b184b
David Hook <dgh@cryptoworkshop.com>
2018-02-11 17:14:19 +1100

    JavaDoc

--- pkix/src/main/java/org/bouncycastle/operator/DigestCalculatorProvider.java
+++ pkix/src/main/java/org/bouncycastle/operator/DigestCalculatorProvider.java
@@ -2,6 +2,9 @@ package org.bouncycastle.operator;
 
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 
+/**
+ * The base interface for a provider of DigestCalculator implementations.
+ */
 public interface DigestCalculatorProvider
 {
     DigestCalculator get(AlgorithmIdentifier digestAlgorithmIdentifier)
