404b9d47e73b4e2433248dc784c7aa57955d3b58
David Hook <dgh@cryptoworkshop.com>
2018-06-24 11:40:48 +1000

    JavaDoc

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/interfaces/StateAwareSignature.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/interfaces/StateAwareSignature.java
@@ -64,7 +64,7 @@ public interface StateAwareSignature
      * Return the current version of the private key with the updated state.
      * <p>
      * <b>Note:</b> calling this method will effectively disable the Signature object from being used for further
-     *  signature generation without another call to init().
+     *  signature generation without another call to initSign().
      * </p>
      * @return an updated private key object, which can be used for later signature generation.
      */
