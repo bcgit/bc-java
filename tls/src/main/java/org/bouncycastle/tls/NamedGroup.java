bab8bc9f224ed9c63d801719de29b01ee504074d
super-horst <karsten.rost@RDSPC469.robotron.de>
2018-07-04 10:39:41 +0200

    Converted brainpool curve names to lowercase Curve-resolution via NamedGroup.getByName() is always performed with the lowercase name

--- tls/src/main/java/org/bouncycastle/tls/NamedGroup.java
+++ tls/src/main/java/org/bouncycastle/tls/NamedGroup.java
@@ -73,7 +73,7 @@ public class NamedGroup
         "sect193r2", "sect233k1", "sect233r1", "sect239k1", "sect283k1", "sect283r1", "sect409k1", "sect409r1",
         "sect571k1", "sect571r1", "secp160k1", "secp160r1", "secp160r2", "secp192k1", "secp192r1", "secp224k1",
         "secp224r1", "secp256k1", "secp256r1", "secp384r1", "secp521r1",
-        "brainpoolP256r1", "brainpoolP384r1", "brainpoolP512r1"};
+        "brainpoolp256r1", "brainpoolp384r1", "brainpoolp512r1"};
 
     private static final String[] FINITE_FIELD_NAMES = new String[] { "ffdhe2048", "ffdhe3072", "ffdhe4096", "ffdhe6144", "ffdhe8192" };
 
