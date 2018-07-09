c29436405d5f96183e64bae087e8e05ed45527a5
David Hook <dgh@cryptoworkshop.com>
2018-07-09 11:34:26 +1000

    fixed impossible equals method.

--- core/src/main/java/org/bouncycastle/crypto/params/CramerShoupParameters.java
+++ core/src/main/java/org/bouncycastle/crypto/params/CramerShoupParameters.java
@@ -5,49 +5,59 @@ import java.math.BigInteger;
 import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.Digest;
 
-public class CramerShoupParameters implements CipherParameters {
+public class CramerShoupParameters
+    implements CipherParameters
+{
 
-	private BigInteger p; // prime order of G
-	private BigInteger g1, g2; // generate G
-	
-	private Digest H; // hash function
+    private BigInteger p; // prime order of G
+    private BigInteger g1, g2; // generate G
 
-	public CramerShoupParameters(BigInteger p, BigInteger g1, BigInteger g2, Digest H) {
-		this.p = p;
-		this.g1 = g1;
-		this.g2 = g2;
-		this.H = H;
-	}
+    private Digest H; // hash function
 
-	public boolean equals(Object obj) {
-		if (!(obj instanceof DSAParameters)) {
-			return false;
-		}
+    public CramerShoupParameters(BigInteger p, BigInteger g1, BigInteger g2, Digest H)
+    {
+        this.p = p;
+        this.g1 = g1;
+        this.g2 = g2;
+        this.H = H;
+    }
 
-		CramerShoupParameters pm = (CramerShoupParameters) obj;
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof CramerShoupParameters))
+        {
+            return false;
+        }
 
-		return (pm.getP().equals(p) && pm.getG1().equals(g1) && pm.getG2().equals(g2));
-	}
+        CramerShoupParameters pm = (CramerShoupParameters)obj;
 
-	public int hashCode() {
-		return getP().hashCode() ^ getG1().hashCode() ^ getG2().hashCode();
-	}
-	
-	public BigInteger getG1() {
-		return g1;
-	}
-	
-	public BigInteger getG2() {
-		return g2;
-	}
-	
-	public BigInteger getP() {
-		return p;
-	}
-	
-	public Digest getH() {
-		H.reset();
-		return H;
-	}
-	
+        return (pm.getP().equals(p) && pm.getG1().equals(g1) && pm.getG2().equals(g2));
+    }
+
+    public int hashCode()
+    {
+        return getP().hashCode() ^ getG1().hashCode() ^ getG2().hashCode();
+    }
+
+    public BigInteger getG1()
+    {
+        return g1;
+    }
+
+    public BigInteger getG2()
+    {
+        return g2;
+    }
+
+    public BigInteger getP()
+    {
+        return p;
+    }
+
+    public Digest getH()
+    {
+        H.reset();
+        return H;
+    }
+
 }
