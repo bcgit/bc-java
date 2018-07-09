c9b996dcb3443a2722cf33886f2d28d0ae061c79
mw <megan@cryptoworkshop.com>
2018-07-09 10:29:46 +1000

    Updated tests to test BA and JCA variants.

--- pg/src/main/java/org/bouncycastle/gpg/keybox/BlobVerifier.java
+++ pg/src/main/java/org/bouncycastle/gpg/keybox/BlobVerifier.java
@@ -8,7 +8,7 @@ public interface BlobVerifier
     /**
      * Return true if the passed in blobData calculates to the expected digest.
      *
-     * @param blobData bytes making up the blob.
+     * @param blobData   bytes making up the blob.
      * @param blobDigest the expected digest.
      * @return true on a match, false otherwise.
      */
