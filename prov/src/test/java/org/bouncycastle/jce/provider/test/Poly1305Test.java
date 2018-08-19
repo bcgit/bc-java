81d707b08419495ef3ce4c6765faa4eefc43be2e
David Hook <dgh@cryptoworkshop.com>
2018-08-19 13:52:33 +1000

    corrected Poly1305 serpent.

--- prov/src/test/java/org/bouncycastle/jce/provider/test/Poly1305Test.java
+++ prov/src/test/java/org/bouncycastle/jce/provider/test/Poly1305Test.java
@@ -50,7 +50,7 @@ public class Poly1305Test
             "211195296d9afc7b35a1223a79487c87",
             "f328857a1b653684e73760c804c55b1d",
             "21cd8adb23ca84eb4dbb12780595bf28",
-            "211195296d9afc7b35a1223a79487c87",
+            "c218102702d8a2ee5c9ef9000e91454d",
             "9bb04be6a1c314a9054ae3c94d3c941b",
             "db86de7b1fcae429753d68b1263d7ca0",
             "11918174f33a2f278fb86554da094112"};
