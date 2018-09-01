53ac95896a591860bfc0e0b55acd688cccd56af6
David Hook <dgh@cryptoworkshop.com>
2018-09-01 11:26:02 +1000

    First cut of KeyFactory, Keys for EdDSA, XDH. Fixed issue with constructor for PrivateKeyInfo not taking byte[] for private key. Added EdECObjectIdentifiers.

---
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/edec/BCEdDSAPrivateKey.java
@@ -0,0 +1,78 @@
+package org.bouncycastle.jcajce.provider.asymmetric.edec;
+
+import java.io.IOException;
+import java.security.PrivateKey;
+import java.security.spec.AlgorithmParameterSpec;
+
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1Set;
+import org.bouncycastle.asn1.edec.EdECObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
+import org.bouncycastle.crypto.params.Ed448PrivateKeyParameters;
+import org.bouncycastle.crypto.util.PrivateKeyInfoFactory;
+import org.bouncycastle.jcajce.interfaces.EdDSAKey;
+
+public class BCEdDSAPrivateKey
+    implements EdDSAKey, PrivateKey
+{
+    private transient AsymmetricKeyParameter eddsaPrivateKey;
+
+    private final boolean hasPublicKey;
+    private final byte[] attributes;
+
+    BCEdDSAPrivateKey(PrivateKeyInfo keyInfo)
+        throws IOException
+    {
+        this.hasPublicKey = keyInfo.hasPublicKey();
+        this.attributes = (keyInfo.getAttributes() != null) ? keyInfo.getAttributes().getEncoded() : null;
+
+        ASN1Encodable keyOcts = keyInfo.parsePrivateKey();
+        if (EdECObjectIdentifiers.id_Ed448.equals(keyInfo.getPrivateKeyAlgorithm().getAlgorithm()))
+        {
+            eddsaPrivateKey = new Ed448PrivateKeyParameters(ASN1OctetString.getInstance(keyOcts).getOctets(), 0);
+        }
+        else
+        {
+            eddsaPrivateKey = new Ed25519PrivateKeyParameters(ASN1OctetString.getInstance(keyOcts).getOctets(), 0);
+        }
+    }
+
+    public String getAlgorithm()
+    {
+        return (eddsaPrivateKey instanceof Ed448PrivateKeyParameters) ? "Ed448" : "Ed25519";
+    }
+
+    public String getFormat()
+    {
+        return "PKCS#8";
+    }
+
+    public byte[] getEncoded()
+    {
+        try
+        {
+            PrivateKeyInfo privInfo = PrivateKeyInfoFactory.createPrivateKeyInfo(eddsaPrivateKey, ASN1Set.getInstance(attributes));
+
+            if (hasPublicKey)
+            {
+                return privInfo.getEncoded();
+            }
+            else
+            {
+                return new PrivateKeyInfo(privInfo.getPrivateKeyAlgorithm(), privInfo.parsePrivateKey(), ASN1Set.getInstance(attributes)).getEncoded();
+            }
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+    public AlgorithmParameterSpec getParams()
+    {
+        return null;
+    }
+}
