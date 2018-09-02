d0114b105468a80ac40334637d9b0d279284989f
David Hook <dgh@cryptoworkshop.com>
2018-09-02 14:26:01 +1000

    Added unified agreement for XDH. Added KDF support for XDH.

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/edec/KeyAgreementSpi.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/edec/KeyAgreementSpi.java
@@ -6,23 +6,37 @@ import java.security.Key;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 
+import org.bouncycastle.crypto.DerivationFunction;
 import org.bouncycastle.crypto.RawAgreement;
 import org.bouncycastle.crypto.agreement.X25519Agreement;
 import org.bouncycastle.crypto.agreement.X448Agreement;
+import org.bouncycastle.crypto.agreement.XDHUnifiedAgreement;
+import org.bouncycastle.crypto.agreement.kdf.ConcatenationKDFGenerator;
+import org.bouncycastle.crypto.generators.KDF2BytesGenerator;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.X448PrivateKeyParameters;
+import org.bouncycastle.crypto.params.XDHUPrivateParameters;
+import org.bouncycastle.crypto.params.XDHUPublicParameters;
+import org.bouncycastle.crypto.util.DigestFactory;
 import org.bouncycastle.jcajce.provider.asymmetric.util.BaseAgreementSpi;
+import org.bouncycastle.jcajce.spec.DHUParameterSpec;
+import org.bouncycastle.jcajce.spec.UserKeyingMaterialSpec;
 
 public class KeyAgreementSpi
     extends BaseAgreementSpi
 {
     private RawAgreement agreement;
-
+    private DHUParameterSpec dhuSpec;
     private byte[] result;
 
     KeyAgreementSpi(String algorithm)
     {
         super(algorithm, null);
+    }
+
+    KeyAgreementSpi(String algorithm, DerivationFunction kdf)
+    {
+        super(algorithm, kdf);
     }
 
     protected byte[] calcSecret()
@@ -52,14 +66,25 @@ public class KeyAgreementSpi
         {
             throw new InvalidKeyException("cannot identify XDH private key");
         }
+
+        if (kdf != null)
+        {
+            ukmParameters = new byte[0];
+        }
+        else
+        {
+            ukmParameters = null;
+        }
     }
 
-    protected void engineInit(Key key, AlgorithmParameterSpec algorithmParameterSpec, SecureRandom secureRandom)
+    protected void engineInit(Key key, AlgorithmParameterSpec params, SecureRandom secureRandom)
         throws InvalidKeyException, InvalidAlgorithmParameterException
     {
+        AsymmetricKeyParameter priv;
+
         if (key instanceof BCXDHPrivateKey)
         {
-            AsymmetricKeyParameter priv = ((BCXDHPrivateKey)key).engineGetKeyParameters();
+            priv = ((BCXDHPrivateKey)key).engineGetKeyParameters();
 
             if (priv instanceof X448PrivateKeyParameters)
             {
@@ -69,12 +94,49 @@ public class KeyAgreementSpi
             {
                 agreement = getAgreement("X25519");
             }
-
-            agreement.init(priv);
         }
         else
         {
             throw new InvalidKeyException("cannot identify XDH private key");
+        }
+
+        ukmParameters = null;
+        if (params instanceof DHUParameterSpec)
+        {
+            if (kaAlgorithm.indexOf('U') < 0)
+            {
+                throw new InvalidAlgorithmParameterException("agreement algorithm not DHU based");
+            }
+
+            dhuSpec = (DHUParameterSpec)params;
+
+            ukmParameters = dhuSpec.getUserKeyingMaterial();
+            
+            agreement.init(new XDHUPrivateParameters(
+                priv, ((BCXDHPrivateKey)dhuSpec.getEphemeralPrivateKey()).engineGetKeyParameters(),
+                ((BCXDHPublicKey)dhuSpec.getEphemeralPublicKey()).engineGetKeyParameters()));
+        }
+        else
+        {
+            agreement.init(priv);
+
+            if (params instanceof UserKeyingMaterialSpec)
+            {
+                if (kdf == null)
+                {
+                    throw new InvalidAlgorithmParameterException("no KDF specified for UserKeyingMaterialSpec");
+                }
+                this.ukmParameters = ((UserKeyingMaterialSpec)params).getUserKeyingMaterial();
+            }
+            else
+            {
+                throw new InvalidAlgorithmParameterException("unknown ParameterSpec");
+            }
+        }
+
+        if (kdf != null && ukmParameters == null)
+        {
+            ukmParameters = new byte[0];
         }
     }
 
@@ -100,7 +162,14 @@ public class KeyAgreementSpi
 
         result = new byte[agreement.getAgreementSize()];
 
-        agreement.calculateAgreement(pub, result, 0);
+        if (dhuSpec != null)
+        {
+            agreement.calculateAgreement(new XDHUPublicParameters(pub, ((BCXDHPublicKey)dhuSpec.getOtherPartyEphemeralKey()).engineGetKeyParameters()), result, 0);
+        }
+        else
+        {
+            agreement.calculateAgreement(pub, result, 0);
+        }
 
         return null;
     }
@@ -108,18 +177,32 @@ public class KeyAgreementSpi
     private RawAgreement getAgreement(String alg)
         throws InvalidKeyException
     {
-        if (!(kaAlgorithm.equals("XDH") || alg.equals(kaAlgorithm)))
+        if (!(kaAlgorithm.equals("XDH") || kaAlgorithm.startsWith(alg)))
         {
             throw new InvalidKeyException("inappropriate key for " + kaAlgorithm);
         }
 
-        if (alg.equals("X448"))
+        if (kaAlgorithm.indexOf('U') > 0)
         {
-            return new X448Agreement();
+            if (alg.startsWith("X448"))
+            {
+                return new XDHUnifiedAgreement(new X448Agreement());
+            }
+            else
+            {
+                return new XDHUnifiedAgreement(new X25519Agreement());
+            }
         }
         else
         {
-            return new X25519Agreement();
+            if (alg.startsWith("X448"))
+            {
+                return new X448Agreement();
+            }
+            else
+            {
+                return new X25519Agreement();
+            }
         }
     }
 
@@ -147,6 +230,78 @@ public class KeyAgreementSpi
         public X25519()
         {
             super("X25519");
+        }
+    }
+
+    public final static class X25519withSHA256CKDF
+        extends KeyAgreementSpi
+    {
+        public X25519withSHA256CKDF()
+        {
+            super("X25519withSHA256CKDF", new ConcatenationKDFGenerator(DigestFactory.createSHA256()));
+        }
+    }
+
+    public final static class X448withSHA512CKDF
+        extends KeyAgreementSpi
+    {
+        public X448withSHA512CKDF()
+        {
+            super("X448withSHA512CKDF", new ConcatenationKDFGenerator(DigestFactory.createSHA512()));
+        }
+    }
+
+    public final static class X25519withSHA256KDF
+        extends KeyAgreementSpi
+    {
+        public X25519withSHA256KDF()
+        {
+            super("X25519withSHA256KDF", new KDF2BytesGenerator(DigestFactory.createSHA256()));
+        }
+    }
+
+    public final static class X448withSHA512KDF
+        extends KeyAgreementSpi
+    {
+        public X448withSHA512KDF()
+        {
+            super("X448withSHA512KDF", new KDF2BytesGenerator(DigestFactory.createSHA512()));
+        }
+    }
+
+    public static class X25519UwithSHA256CKDF
+        extends KeyAgreementSpi
+    {
+        public X25519UwithSHA256CKDF()
+        {
+            super("X25519UwithSHA256CKDF", new ConcatenationKDFGenerator(DigestFactory.createSHA256()));
+        }
+    }
+
+    public static class X448UwithSHA512CKDF
+        extends KeyAgreementSpi
+    {
+        public X448UwithSHA512CKDF()
+        {
+            super("X448UwithSHA512CKDF", new ConcatenationKDFGenerator(DigestFactory.createSHA512()));
+        }
+    }
+
+    public static class X25519UwithSHA256KDF
+        extends KeyAgreementSpi
+    {
+        public X25519UwithSHA256KDF()
+        {
+            super("X25519UwithSHA256KDF", new KDF2BytesGenerator(DigestFactory.createSHA256()));
+        }
+    }
+
+    public static class X448UwithSHA512KDF
+        extends KeyAgreementSpi
+    {
+        public X448UwithSHA512KDF()
+        {
+            super("X448UwithSHA512KDF", new KDF2BytesGenerator(DigestFactory.createSHA512()));
         }
     }
 }
