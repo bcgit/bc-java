86d7163aa288b6872daa3c9dd368183ceb6b2015
Peter Dettman <peter.dettman@bouncycastle.org>
2018-09-04 15:23:13 +0700

    Java 1.4 updates

--- core/src/main/java/org/bouncycastle/crypto/signers/Ed25519Signer.java
+++ core/src/main/java/org/bouncycastle/crypto/signers/Ed25519Signer.java
@@ -1,13 +1,13 @@
 package org.bouncycastle.crypto.signers;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
 
 import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.Signer;
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
 import org.bouncycastle.math.ec.rfc8032.Ed25519;
+import org.bouncycastle.util.Arrays;
 
 public class Ed25519Signer
     implements Signer
