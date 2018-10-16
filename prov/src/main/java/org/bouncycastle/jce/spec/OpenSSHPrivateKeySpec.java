ce9338d782bc41a5251fa7bc2848e88de9abdb6f
mwcw <megan@cryptoworkshop.com>
2018-10-16 14:51:00 +1100

    capitalized OpenSSH

--- prov/src/main/java/org/bouncycastle/jce/spec/OpenSSHPrivateKeySpec.java
+++ prov/src/main/java/org/bouncycastle/jce/spec/OpenSSHPrivateKeySpec.java
@@ -11,7 +11,7 @@ public class OpenSSHPrivateKeySpec
     public OpenSSHPrivateKeySpec(byte[] encodedKey)
     {
         super(encodedKey);
-        this.format = "openssh";
+        this.format = "OpenSSH";
     }
 
 
