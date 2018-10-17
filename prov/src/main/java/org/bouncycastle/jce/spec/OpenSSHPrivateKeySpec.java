7a19f8cee89498cd4764cd61df2ee08fad09bd86
mwcw <megan@cryptoworkshop.com>
2018-10-17 14:05:57 +1100

    Added format detection to OpenSSHPrivateKeySpec updated tests.

--- prov/src/main/java/org/bouncycastle/jce/spec/OpenSSHPrivateKeySpec.java
+++ prov/src/main/java/org/bouncycastle/jce/spec/OpenSSHPrivateKeySpec.java
@@ -2,17 +2,36 @@ package org.bouncycastle.jce.spec;
 
 import java.security.spec.EncodedKeySpec;
 
+import org.bouncycastle.crypto.util.OpenSSHPrivateKeyUtil;
+
 public class OpenSSHPrivateKeySpec
     extends EncodedKeySpec
 
 {
+    private final String format;
+
     public OpenSSHPrivateKeySpec(byte[] encodedKey)
     {
+
         super(encodedKey);
+
+        boolean openssh = true;
+
+        for (int t = 0; t < OpenSSHPrivateKeyUtil.AUTH_MAGIC.length; t++)
+        {
+            if (encodedKey[t] != OpenSSHPrivateKeyUtil.AUTH_MAGIC[t])
+            {
+                openssh = false;
+                break;
+            }
+        }
+
+        format = openssh ? "OpenSSH" : "ASN.1";
+
     }
 
     public String getFormat()
     {
-        return "OpenSSH";
+        return format;
     }
 }
