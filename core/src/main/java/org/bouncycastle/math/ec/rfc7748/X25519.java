76360bbd8d4a2ab63e6842458e4eafa26dd2451f
Peter Dettman <peter.dettman@bouncycastle.org>
2018-10-21 18:19:36 +0700

    Use Edwards internals for X25519/X448 public key calculations

--- core/src/main/java/org/bouncycastle/math/ec/rfc7748/X25519.java
+++ core/src/main/java/org/bouncycastle/math/ec/rfc7748/X25519.java
@@ -2,25 +2,25 @@ package org.bouncycastle.math.ec.rfc7748;
 
 import java.security.SecureRandom;
 
+import org.bouncycastle.math.ec.rfc8032.Ed25519;
 import org.bouncycastle.util.Arrays;
 
 public abstract class X25519
 {
+    public static class Friend
+    {
+        private static final Friend INSTANCE = new Friend();
+        private Friend() {}
+    }
+
     public static final int POINT_SIZE = 32;
     public static final int SCALAR_SIZE = 32;
 
     private static final int C_A = 486662;
     private static final int C_A24 = (C_A + 2)/4;
 
-    // 0x1
-//    private static final int[] S_x = new int[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-    // 0x215132111D8354CB52385F46DCA2B71D440F6A51EB4D1207816B1E0137D48290
-    private static final int[] PsubS_x = new int[]{ 0x03D48290, 0x02C7804D, 0x01207816, 0x028F5A68, 0x00881ED4, 0x00A2B71D,
-        0x0217D1B7, 0x014CB523, 0x0088EC1A, 0x0042A264 };
-
-    private static Object precompLock = new Object();
-    private static int[] precompBase = null;
+//    private static final int[] SQRT_NEG_486664 = { 0x03457E06, 0x03812ABF, 0x01A82CC6, 0x028A5BE8, 0x018B43A7,
+//        0x03FC4F7E, 0x02C23700, 0x006BBD27, 0x03A30500, 0x001E4DDB };
 
     public static boolean calculateAgreement(byte[] k, int kOff, byte[] u, int uOff, byte[] r, int rOff)
     {
@@ -80,70 +80,7 @@ public abstract class X25519
 
     public static void precompute()
     {
-        synchronized (precompLock)
-        {
-            if (precompBase != null)
-            {
-                return;
-            }
-
-            precompBase = new int[X25519Field.SIZE * 252];
-
-            int[] xs = precompBase;
-            int[] zs = new int[X25519Field.SIZE * 251];
-
-            int[] x = X25519Field.create();     x[0] = 9;          
-            int[] z = X25519Field.create();     z[0] = 1;
-
-            int[] n = X25519Field.create();
-            int[] d = X25519Field.create();
-
-            X25519Field.apm(x, z, n, d);
-
-            int[] c = X25519Field.create();     X25519Field.copy(d, 0, c, 0);
-
-            int off = 0;
-            for (;;)
-            {
-                X25519Field.copy(n, 0, xs, off);
-
-                if (off == (X25519Field.SIZE * 251))
-                {
-                    break;
-                }
-
-                pointDouble(x, z);
-
-                X25519Field.apm(x, z, n, d);
-                X25519Field.mul(n, c, n);
-                X25519Field.mul(c, d, c);
-
-                X25519Field.copy(d, 0, zs, off);
-
-                off += X25519Field.SIZE;
-            }
-
-            int[] u = X25519Field.create();
-            X25519Field.inv(c, u);
-
-            for (;;)
-            {
-                X25519Field.copy(xs, off, x, 0);
-
-                X25519Field.mul(x, u, x);
-//                X25519Field.normalize(x);
-                X25519Field.copy(x, 0, precompBase, off);
-
-                if (off == 0)
-                {
-                    break;
-                }
-
-                off -= X25519Field.SIZE;
-                X25519Field.copy(zs, off, z, 0);
-                X25519Field.mul(u, z, u);
-            }
-        }
+        Ed25519.precompute();
     }
 
     public static void scalarMult(byte[] k, int kOff, byte[] u, int uOff, byte[] r, int rOff)
@@ -209,60 +146,17 @@ public abstract class X25519
 
     public static void scalarMultBase(byte[] k, int kOff, byte[] r, int rOff)
     {
-        precompute();
+        int[] y = X25519Field.create();
+        int[] z = X25519Field.create();
 
-        int[] n = new int[8];   decodeScalar(k, kOff, n);
+        Ed25519.scalarMultBaseYZ(Friend.INSTANCE, k, kOff, y, z);
 
-        int[] x0 = X25519Field.create();
-//        int[] x1 = X25519Field.create();        X25519Field.copy(S_x, 0, x1, 0);
-        int[] x1 = X25519Field.create();        x1[0] = 1;
-        int[] z1 = X25519Field.create();        z1[0] = 1;        
-        int[] x2 = X25519Field.create();        X25519Field.copy(PsubS_x, 0, x2, 0);
-        int[] z2 = X25519Field.create();        z2[0] = 1;        
+        X25519Field.apm(z, y, y, z);
 
-        int[] A = x1;
-        int[] B = z1;
-        int[] C = x0;
-        int[] D = A;
-        int[] E = B;
+        X25519Field.inv(z, z);
+        X25519Field.mul(y, z, y);
 
-//      assert n[7] >>> 30 == 1;
-
-        int off = 0, bit = 3, swap = 1;
-        do
-        {
-            X25519Field.copy(precompBase, off, x0, 0);
-            off += X25519Field.SIZE;
-
-            int word = bit >>> 5, shift = bit & 0x1F;
-            int kt = (n[word] >>> shift) & 1;
-            swap ^= kt;
-            X25519Field.cswap(swap, x1, x2);
-            X25519Field.cswap(swap, z1, z2);
-            swap = kt;
-
-            X25519Field.apm(x1, z1, A, B);
-            X25519Field.mul(x0, B, C);
-            X25519Field.carry(A);
-            X25519Field.apm(A, C, D, E);
-            X25519Field.sqr(D, D);
-            X25519Field.sqr(E, E);
-            X25519Field.mul(z2, D, x1);
-            X25519Field.mul(x2, E, z1);
-        }
-        while (++bit < 255);
-
-//        assert swap == 1;
-
-        for (int i = 0; i < 3; ++i)
-        {
-            pointDouble(x1, z1);
-        }
-
-        X25519Field.inv(z1, z1);
-        X25519Field.mul(x1, z1, x1);
-
-        X25519Field.normalize(x1);
-        X25519Field.encode(x1, r, rOff);
+        X25519Field.normalize(y);
+        X25519Field.encode(y, r, rOff);
     }
 }
