89da6e1f78683cc3b1df1047b322e69a3260ee28
Peter Dettman <peter.dettman@bouncycastle.org>
2018-10-25 14:14:23 +0700

    Check OID for RSASSA-PKCS1-v1_5 certificates before using

--- core/src/main/java/org/bouncycastle/crypto/util/PrivateKeyFactory.java
+++ core/src/main/java/org/bouncycastle/crypto/util/PrivateKeyFactory.java
@@ -20,6 +20,7 @@ import org.bouncycastle.asn1.pkcs.RSAPrivateKey;
 import org.bouncycastle.asn1.sec.ECPrivateKey;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.DSAParameter;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
 import org.bouncycastle.asn1.x9.ECNamedCurveTable;
 import org.bouncycastle.asn1.x9.X962Parameters;
 import org.bouncycastle.asn1.x9.X9ECParameters;
@@ -84,7 +85,8 @@ public class PrivateKeyFactory
         ASN1ObjectIdentifier algOID = algId.getAlgorithm();
 
         if (algOID.equals(PKCSObjectIdentifiers.rsaEncryption)
-            || algOID.equals(PKCSObjectIdentifiers.id_RSASSA_PSS))
+            || algOID.equals(PKCSObjectIdentifiers.id_RSASSA_PSS)
+            || algOID.equals(X509ObjectIdentifiers.id_ea_rsa))
         {
             RSAPrivateKey keyStructure = RSAPrivateKey.getInstance(keyInfo.parsePrivateKey());
 
