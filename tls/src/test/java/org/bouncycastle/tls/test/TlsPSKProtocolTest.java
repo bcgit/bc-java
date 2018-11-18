9b8c608e6d369b8f47c08aec9e115d63094aef4f
David Hook <dgh@cryptoworkshop.com>
2018-11-18 21:22:58 +1100

    changed use of PipedInputStream (backwards compatability)

--- tls/src/test/java/org/bouncycastle/tls/test/TlsPSKProtocolTest.java
+++ tls/src/test/java/org/bouncycastle/tls/test/TlsPSKProtocolTest.java
@@ -18,8 +18,8 @@ public class TlsPSKProtocolTest
     {
         SecureRandom secureRandom = new SecureRandom();
 
-        PipedInputStream clientRead = new PipedInputStream(16384);
-        PipedInputStream serverRead = new PipedInputStream(16384);
+        PipedInputStream clientRead = TlsTestUtils.createPipedInputStream();
+        PipedInputStream serverRead = TlsTestUtils.createPipedInputStream();
         PipedOutputStream clientWrite = new PipedOutputStream(serverRead);
         PipedOutputStream serverWrite = new PipedOutputStream(clientRead);
 
