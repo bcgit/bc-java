42618780439f3945a0301d79ae454a3ebda7e691
David Hook <dgh@cryptoworkshop.com>
2018-12-20 11:45:06 +1100

    added some extra vector tests, so people can find them more easily.

--- core/src/test/java/org/bouncycastle/crypto/test/Ed25519Test.java
+++ core/src/test/java/org/bouncycastle/crypto/test/Ed25519Test.java
@@ -12,6 +12,7 @@ import org.bouncycastle.crypto.signers.Ed25519Signer;
 import org.bouncycastle.crypto.signers.Ed25519ctxSigner;
 import org.bouncycastle.crypto.signers.Ed25519phSigner;
 import org.bouncycastle.math.ec.rfc8032.Ed25519;
+import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.util.test.SimpleTest;
 
 public class Ed25519Test
@@ -39,6 +40,29 @@ public class Ed25519Test
             testConsistency(Ed25519.Algorithm.Ed25519ctx, context);
             testConsistency(Ed25519.Algorithm.Ed25519ph, context);
         }
+
+        basicSigTest();
+    }
+
+    private void basicSigTest()
+        throws Exception
+    {
+        Ed25519PrivateKeyParameters privateKey = new Ed25519PrivateKeyParameters(
+            Hex.decode("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"), 0);
+        Ed25519PublicKeyParameters publicKey = new Ed25519PublicKeyParameters(
+            Hex.decode("d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"), 0);
+
+        byte[] sig = Hex.decode("e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b");
+
+        Signer signer = new Ed25519Signer();
+
+        signer.init(true, privateKey);
+
+        areEqual(sig, signer.generateSignature());
+
+        signer.init(false, publicKey);
+
+        isTrue(signer.verifySignature(sig));
     }
 
     private Signer createSigner(int algorithm, byte[] context)
