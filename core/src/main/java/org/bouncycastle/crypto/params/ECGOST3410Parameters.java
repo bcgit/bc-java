f44dbcd4559d360f8699d86a954933b8f0c78773
David Hook <dgh@cryptoworkshop.com>
2019-01-04 13:43:31 +1100

    added support for explicit GOST 3410 parameters

---
+++ core/src/main/java/org/bouncycastle/crypto/params/ECGOST3410Parameters.java
@@ -0,0 +1,40 @@
+package org.bouncycastle.crypto.params;
+
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+
+public class ECGOST3410Parameters
+    extends ECDomainParameters
+{
+    private final ASN1ObjectIdentifier  publicKeyParamSet;
+    private final ASN1ObjectIdentifier  digestParamSet;
+    private final ASN1ObjectIdentifier  encryptionParamSet;
+
+    public ECGOST3410Parameters(ECDomainParameters ecParameters, ASN1ObjectIdentifier publicKeyParamSet, ASN1ObjectIdentifier digestParamSet)
+    {
+        this(ecParameters, publicKeyParamSet, digestParamSet, null);
+    }
+
+    public ECGOST3410Parameters(ECDomainParameters ecParameters, ASN1ObjectIdentifier publicKeyParamSet, ASN1ObjectIdentifier digestParamSet, ASN1ObjectIdentifier  encryptionParamSet)
+    {
+        super(ecParameters.getCurve(), ecParameters.getG(), ecParameters.getN(), ecParameters.getH(), ecParameters.getSeed());
+
+        this.publicKeyParamSet = publicKeyParamSet;
+        this.digestParamSet = digestParamSet;
+        this.encryptionParamSet = encryptionParamSet;
+    }
+
+    public ASN1ObjectIdentifier getPublicKeyParamSet()
+    {
+        return publicKeyParamSet;
+    }
+
+    public ASN1ObjectIdentifier getDigestParamSet()
+    {
+        return digestParamSet;
+    }
+
+    public ASN1ObjectIdentifier getEncryptionParamSet()
+    {
+        return encryptionParamSet;
+    }
+}
