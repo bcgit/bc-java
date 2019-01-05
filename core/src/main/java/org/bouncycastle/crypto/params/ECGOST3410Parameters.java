d3489eacc8915f14378cc9256e4121371227ef47
mw <megan@cryptoworkshop.com>
2019-01-05 15:52:56 +1100

    LW api for ECGOST Encoding and decoding.

--- core/src/main/java/org/bouncycastle/crypto/params/ECGOST3410Parameters.java
+++ core/src/main/java/org/bouncycastle/crypto/params/ECGOST3410Parameters.java
@@ -3,20 +3,20 @@ package org.bouncycastle.crypto.params;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 
 public class ECGOST3410Parameters
-    extends ECDomainParameters
+    extends ECNamedDomainParameters
 {
     private final ASN1ObjectIdentifier  publicKeyParamSet;
     private final ASN1ObjectIdentifier  digestParamSet;
     private final ASN1ObjectIdentifier  encryptionParamSet;
 
-    public ECGOST3410Parameters(ECDomainParameters ecParameters, ASN1ObjectIdentifier publicKeyParamSet, ASN1ObjectIdentifier digestParamSet)
+    public ECGOST3410Parameters(ECNamedDomainParameters ecParameters, ASN1ObjectIdentifier publicKeyParamSet, ASN1ObjectIdentifier digestParamSet)
     {
         this(ecParameters, publicKeyParamSet, digestParamSet, null);
     }
 
-    public ECGOST3410Parameters(ECDomainParameters ecParameters, ASN1ObjectIdentifier publicKeyParamSet, ASN1ObjectIdentifier digestParamSet, ASN1ObjectIdentifier  encryptionParamSet)
+    public ECGOST3410Parameters(ECNamedDomainParameters ecParameters, ASN1ObjectIdentifier publicKeyParamSet, ASN1ObjectIdentifier digestParamSet, ASN1ObjectIdentifier encryptionParamSet)
     {
-        super(ecParameters.getCurve(), ecParameters.getG(), ecParameters.getN(), ecParameters.getH(), ecParameters.getSeed());
+        super(ecParameters.getName(), ecParameters.getCurve(), ecParameters.getG(), ecParameters.getN(), ecParameters.getH(), ecParameters.getSeed());
 
         this.publicKeyParamSet = publicKeyParamSet;
         this.digestParamSet = digestParamSet;
