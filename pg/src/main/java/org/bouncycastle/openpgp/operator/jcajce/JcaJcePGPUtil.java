7cbe9f1ec3aa06254a7b15a6d88bc98d4d21244c
David Hook <dgh@cryptoworkshop.com>
2019-01-28 16:15:45 +1100

    added support for Curve25519 keys and messages to OpenPGP.

--- pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JcaJcePGPUtil.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JcaJcePGPUtil.java
@@ -9,6 +9,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.x9.ECNamedCurveTable;
 import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.ec.CustomNamedCurves;
 import org.bouncycastle.math.ec.ECCurve;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.openpgp.PGPException;
@@ -44,6 +45,13 @@ class JcaJcePGPUtil
 
     static X9ECParameters getX9Parameters(ASN1ObjectIdentifier curveOID)
     {
-        return ECNamedCurveTable.getByOID(curveOID);
+        X9ECParameters x9Params = CustomNamedCurves.getByOID(curveOID);
+
+        if (x9Params == null)
+        {
+            return ECNamedCurveTable.getByOID(curveOID);
+        }
+
+        return x9Params;
     }
 }
