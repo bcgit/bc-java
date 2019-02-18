93b6ffea73892a4bcefaebe96fe0f5834d597eff
Peter Dettman <peter.dettman@bouncycastle.org>
2019-02-18 14:31:37 +0700

    DTLS: Initial implementation of HelloVerifyRequest support

--- tls/src/main/java/org/bouncycastle/tls/DatagramTransport.java
+++ tls/src/main/java/org/bouncycastle/tls/DatagramTransport.java
@@ -5,18 +5,12 @@ import java.io.IOException;
 /**
  * Base interface for an object sending and receiving DTLS data.
  */
-public interface DatagramTransport
+public interface DatagramTransport extends DatagramSender
 {
     int getReceiveLimit()
         throws IOException;
 
-    int getSendLimit()
-        throws IOException;
-
     int receive(byte[] buf, int off, int len, int waitMillis)
-        throws IOException;
-
-    void send(byte[] buf, int off, int len)
         throws IOException;
 
     void close()
