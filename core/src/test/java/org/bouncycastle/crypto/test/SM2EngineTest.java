bda04fc807b206bd3162e78fb1027e8d077301ef
David Hook <dgh@cryptoworkshop.com>
2019-04-05 11:55:26 +1100

    github #478 added support for C1C3C2 mode in SM2Engine

--- core/src/test/java/org/bouncycastle/crypto/test/SM2EngineTest.java
+++ core/src/test/java/org/bouncycastle/crypto/test/SM2EngineTest.java
@@ -107,6 +107,85 @@ public class SM2EngineTest
         isTrue("dec wrong", Arrays.areEqual(m, dec));
     }
 
+    private void doEngineTestFpC1C3C2()
+        throws Exception
+    {
+        BigInteger SM2_ECC_P = new BigInteger("8542D69E4C044F18E8B92435BF6FF7DE457283915C45517D722EDB8B08F1DFC3", 16);
+        BigInteger SM2_ECC_A = new BigInteger("787968B4FA32C3FD2417842E73BBFEFF2F3C848B6831D7E0EC65228B3937E498", 16);
+        BigInteger SM2_ECC_B = new BigInteger("63E4C6D3B23B0C849CF84241484BFE48F61D59A5B16BA06E6E12D1DA27C5249A", 16);
+        BigInteger SM2_ECC_N = new BigInteger("8542D69E4C044F18E8B92435BF6FF7DD297720630485628D5AE74EE7C32E79B7", 16);
+        BigInteger SM2_ECC_H = ECConstants.ONE;
+        BigInteger SM2_ECC_GX = new BigInteger("421DEBD61B62EAB6746434EBC3CC315E32220B3BADD50BDC4C4E6C147FEDD43D", 16);
+        BigInteger SM2_ECC_GY = new BigInteger("0680512BCBB42C07D47349D2153B70C4E5D7FDFCBFA36EA1A85841B9E46E09A2", 16);
+
+        ECCurve curve = new ECCurve.Fp(SM2_ECC_P, SM2_ECC_A, SM2_ECC_B, SM2_ECC_N, SM2_ECC_H);
+
+        ECPoint g = curve.createPoint(SM2_ECC_GX, SM2_ECC_GY);
+        ECDomainParameters domainParams = new ECDomainParameters(curve, g, SM2_ECC_N);
+
+        ECKeyPairGenerator keyPairGenerator = new ECKeyPairGenerator();
+
+        ECKeyGenerationParameters aKeyGenParams = new ECKeyGenerationParameters(domainParams, new TestRandomBigInteger("1649AB77A00637BD5E2EFE283FBF353534AA7F7CB89463F208DDBC2920BB0DA0", 16));
+
+        keyPairGenerator.init(aKeyGenParams);
+
+        AsymmetricCipherKeyPair aKp = keyPairGenerator.generateKeyPair();
+
+        ECPublicKeyParameters aPub = (ECPublicKeyParameters)aKp.getPublic();
+        ECPrivateKeyParameters aPriv = (ECPrivateKeyParameters)aKp.getPrivate();
+
+        SM2Engine sm2Engine = new SM2Engine(SM2Engine.Mode.C1C3C2);
+
+        byte[] m = Strings.toByteArray("encryption standard");
+
+        sm2Engine.init(true, new ParametersWithRandom(aPub, new TestRandomBigInteger("4C62EEFD6ECFC2B95B92FD6C3D9575148AFA17425546D49018E5388D49DD7B4F", 16)));
+
+        byte[] enc = sm2Engine.processBlock(m, 0, m.length);
+
+        isTrue("enc wrong", Arrays.areEqual(Hex.decode(
+            "04245C26 FB68B1DD DDB12C4B 6BF9F2B6 D5FE60A3 83B0D18D 1C4144AB F17F6252" +
+            "E776CB92 64C2A7E8 8E52B199 03FDC473 78F605E3 6811F5C0 7423A24B 84400F01" +
+            "B8 9C3D7360 C30156FA B7C80A02" +
+            "76712DA9 D8094A63 4B766D3A 285E0748 0653426D 650053 A89B41C4 18B0C3AA D00D886C 00286467"), enc));
+
+        sm2Engine.init(false, aPriv);
+
+        byte[] dec = sm2Engine.processBlock(enc, 0, enc.length);
+
+        isTrue("dec wrong", Arrays.areEqual(m, dec));
+
+        enc[80] = (byte)(enc[80] + 1);
+
+        try
+        {
+            sm2Engine.processBlock(enc, 0, enc.length);
+            fail("no exception");
+        }
+        catch (InvalidCipherTextException e)
+        {
+            isTrue("wrong exception", "invalid cipher text".equals(e.getMessage()));
+        }
+
+        // long message
+        sm2Engine = new SM2Engine(SM2Engine.Mode.C1C3C2);
+
+        m = new byte[4097];
+        for (int i = 0; i != m.length; i++)
+        {
+            m[i] = (byte)i;
+        }
+
+        sm2Engine.init(true, new ParametersWithRandom(aPub, new TestRandomBigInteger("4C62EEFD6ECFC2B95B92FD6C3D9575148AFA17425546D49018E5388D49DD7B4F", 16)));
+
+        enc = sm2Engine.processBlock(m, 0, m.length);
+
+        sm2Engine.init(false, aPriv);
+
+        dec = sm2Engine.processBlock(enc, 0, enc.length);
+
+        isTrue("dec wrong", Arrays.areEqual(m, dec));
+    }
+
     private void doEngineTestF2m()
         throws Exception
     {
@@ -159,6 +238,7 @@ public class SM2EngineTest
     {
         doEngineTestFp();
         doEngineTestF2m();
+        doEngineTestFpC1C3C2();
     }
 
     public static void main(String[] args)
