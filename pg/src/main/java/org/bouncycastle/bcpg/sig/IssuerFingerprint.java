929db001e2d04f17b59fb3ded1bbff122509b7dc
David Hook <dgh@cryptoworkshop.com>
2019-04-05 11:07:24 +1100

    added new signature sub-packet types. minor formatting changes in SExprParser

---
+++ pg/src/main/java/org/bouncycastle/bcpg/sig/IssuerFingerprint.java
@@ -0,0 +1,39 @@
+package org.bouncycastle.bcpg.sig;
+
+import org.bouncycastle.bcpg.SignatureSubpacket;
+import org.bouncycastle.bcpg.SignatureSubpacketTags;
+import org.bouncycastle.util.Arrays;
+
+/**
+ * packet giving the issuer key ID.
+ */
+public class IssuerFingerprint
+    extends SignatureSubpacket
+{
+    public IssuerFingerprint(
+        boolean    critical,
+        boolean    isLongLength,
+        byte[]     data)
+    {
+        super(SignatureSubpacketTags.ISSUER_FINGERPRINT, critical, isLongLength, data);
+    }
+
+    public IssuerFingerprint(
+        boolean    critical,
+        int        keyVersion,
+        byte[]     fingerprint)
+    {
+        super(SignatureSubpacketTags.ISSUER_FINGERPRINT, critical, false,
+                    Arrays.concatenate(new byte[] { (byte)keyVersion }, fingerprint));
+    }
+
+    public int getKeyVersion()
+    {
+        return data[0] & 0xff;
+    }
+
+    public byte[] getFingerprint()
+    {
+        return Arrays.copyOfRange(data, 1, data.length);
+    }
+}
