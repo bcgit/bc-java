52a1088bbd2181783f5d41162270c1d2a87d25fd
权观宇 <quanguanyu@qq.com>
2019-05-20 11:21:39 +0800

    change sytle

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/ec/GMSignatureSpi.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/ec/GMSignatureSpi.java
@@ -21,7 +21,8 @@ import org.bouncycastle.jcajce.util.BCJcaJceHelper;
 import org.bouncycastle.jcajce.util.JcaJceHelper;
 
 public class GMSignatureSpi
-        extends java.security.SignatureSpi {
+    extends java.security.SignatureSpi
+{
     private final JcaJceHelper helper = new BCJcaJceHelper();
 
     private AlgorithmParameters engineParams;
@@ -29,15 +30,18 @@ public class GMSignatureSpi
 
     private final SM2Signer signer;
 
-    GMSignatureSpi(SM2Signer signer) {
+    GMSignatureSpi(SM2Signer signer)
+    {
         this.signer = signer;
     }
 
     protected void engineInitVerify(PublicKey publicKey)
-            throws InvalidKeyException {
+        throws InvalidKeyException
+    {
         CipherParameters param = ECUtils.generatePublicKeyParameter(publicKey);
 
-        if (paramSpec != null) {
+        if (paramSpec != null)
+        {
             param = new ParametersWithID(param, paramSpec.getID());
         }
 
@@ -45,62 +49,84 @@ public class GMSignatureSpi
     }
 
     protected void engineInitSign(
-            PrivateKey privateKey)
-            throws InvalidKeyException {
+        PrivateKey privateKey)
+        throws InvalidKeyException
+    {
         CipherParameters param = ECUtil.generatePrivateKeyParameter(privateKey);
 
-        if (appRandom != null) {
+        if (appRandom != null)
+        {
             param = new ParametersWithRandom(param, appRandom);
         }
 
-        if (paramSpec != null) {
+        if (paramSpec != null)
+        {
             signer.init(true, new ParametersWithID(param, paramSpec.getID()));
-        } else {
+        }
+        else
+        {
             signer.init(true, param);
         }
     }
 
     protected void engineUpdate(byte b)
-            throws SignatureException {
+        throws SignatureException
+    {
         signer.update(b);
     }
 
     protected void engineUpdate(byte[] bytes, int off, int length)
-            throws SignatureException {
+        throws SignatureException
+    {
         signer.update(bytes, off, length);
     }
 
     protected byte[] engineSign()
-            throws SignatureException {
-        try {
+        throws SignatureException
+    {
+        try
+        {
             return signer.generateSignature();
-        } catch (CryptoException e) {
+        }
+        catch (CryptoException e)
+        {
             throw new SignatureException("unable to create signature: " + e.getMessage());
         }
     }
 
     protected boolean engineVerify(byte[] bytes)
-            throws SignatureException {
+        throws SignatureException
+    {
         return signer.verifySignature(bytes);
     }
 
     protected void engineSetParameter(
-            AlgorithmParameterSpec params)
-            throws InvalidAlgorithmParameterException {
-        if (params instanceof SM2ParameterSpec) {
-            paramSpec = (SM2ParameterSpec) params;
-        } else {
+        AlgorithmParameterSpec params)
+        throws InvalidAlgorithmParameterException
+    {
+        if (params instanceof SM2ParameterSpec)
+        {
+            paramSpec = (SM2ParameterSpec)params;
+        }
+        else
+        {
             throw new InvalidAlgorithmParameterException("only SM2ParameterSpec supported");
         }
     }
 
-    protected AlgorithmParameters engineGetParameters() {
-        if (engineParams == null) {
-            if (paramSpec != null) {
-                try {
+    protected AlgorithmParameters engineGetParameters()
+    {
+        if (engineParams == null)
+        {
+            if (paramSpec != null)
+            {
+                try
+                {
                     engineParams = helper.createAlgorithmParameters("PSS");
                     engineParams.init(paramSpec);
-                } catch (Exception e) {
+                }
+                catch (Exception e)
+                {
                     throw new RuntimeException(e.toString());
                 }
             }
@@ -110,35 +136,33 @@ public class GMSignatureSpi
     }
 
     protected void engineSetParameter(
-            String param,
-            Object value) {
+        String param,
+        Object value)
+    {
         throw new UnsupportedOperationException("engineSetParameter unsupported");
     }
 
     protected Object engineGetParameter(
-            String param) {
+        String param)
+    {
         throw new UnsupportedOperationException("engineGetParameter unsupported");
     }
 
     static public class sm3WithSM2
-            extends GMSignatureSpi {
-        public sm3WithSM2() {
+        extends GMSignatureSpi
+    {
+        public sm3WithSM2()
+        {
             super(new SM2Signer());
         }
     }
 
-
     static public class sha1WithSM2
-            extends GMSignatureSpi {
-        public sha1WithSM2() {
+            extends GMSignatureSpi
+    {
+        public sha1WithSM2()
+        {
             super(new SM2Signer(new SHA1Digest()));
-        }
-    }
-
-    static public class sha256WithSM2
-            extends GMSignatureSpi {
-        public sha256WithSM2() {
-            super(new SM2Signer(new SHA256Digest()));
         }
     }
 
