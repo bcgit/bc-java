d4775f7d6cd978904f86470ac79a53b0f6733da1
权观宇 <quanguanyu@qq.com>
2019-05-21 15:04:23 +0800

    develop remain bug

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/GM.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/GM.java
@@ -31,10 +31,10 @@ public class GM
             provider.addAlgorithm("Signature.SM3WITHSM2", PREFIX + "GMSignatureSpi$sm3WithSM2");
             provider.addAlgorithm("Alg.Alias.Signature." + GMObjectIdentifiers.sm2sign_with_sm3, "SM3WITHSM2");
             // SHA1 with SM2
-            provider.addAlgorithm("Signature.SHA1WithSM2", PREFIX + "GMSignatureFullSpi$sha1WithSM2");
+            provider.addAlgorithm("Signature.SHA1WithSM2", PREFIX + "GMSignatureSpi$sha1WithSM2");
             provider.addAlgorithm("Alg.Alias.Signature." + GMObjectIdentifiers.sm2sign_with_sha1, "SHA1WithSM2");
             // SHA256 with SM2
-            provider.addAlgorithm("Signature.SHA256WithSM2", PREFIX + "GMSignatureFullSpi$sha256WithSM2");
+            provider.addAlgorithm("Signature.SHA256WithSM2", PREFIX + "GMSignatureSpi$sha256WithSM2");
             provider.addAlgorithm("Alg.Alias.Signature." + GMObjectIdentifiers.sm2sign_with_sha256, "SHA256WithSM2");
 
             provider.addAlgorithm("Cipher.SM2", PREFIX + "GMCipherSpi$SM2");
