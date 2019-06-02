7d776837688405883b5b046ecc168aaee3a74abd
Peter Dettman <peter.dettman@bouncycastle.org>
2019-06-02 16:08:17 +0700

    Use RSABlindedEngine

--- pkix/src/main/java/org/bouncycastle/operator/bc/BcRSAAsymmetricKeyUnwrapper.java
+++ pkix/src/main/java/org/bouncycastle/operator/bc/BcRSAAsymmetricKeyUnwrapper.java
@@ -4,7 +4,7 @@ import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.crypto.AsymmetricBlockCipher;
 import org.bouncycastle.crypto.encodings.PKCS1Encoding;
-import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.engines.RSABlindedEngine;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 
 public class BcRSAAsymmetricKeyUnwrapper
@@ -17,6 +17,6 @@ public class BcRSAAsymmetricKeyUnwrapper
 
     protected AsymmetricBlockCipher createAsymmetricUnwrapper(ASN1ObjectIdentifier algorithm)
     {
-        return new PKCS1Encoding(new RSAEngine());
+        return new PKCS1Encoding(new RSABlindedEngine());
     }
 }
