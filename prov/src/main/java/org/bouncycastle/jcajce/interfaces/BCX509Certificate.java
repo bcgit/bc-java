9a6f4de166a817f5771c6d3639e6b84aae6f2ad3
Peter Dettman <peter.dettman@bouncycastle.org>
2019-07-12 14:20:26 +0700

    Refactored PrincipalUtils
    - added issuer/subject methods to BCX509Certificate

--- prov/src/main/java/org/bouncycastle/jcajce/interfaces/BCX509Certificate.java
+++ prov/src/main/java/org/bouncycastle/jcajce/interfaces/BCX509Certificate.java
@@ -1,8 +1,11 @@
 package org.bouncycastle.jcajce.interfaces;
 
+import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.TBSCertificate;
 
 public interface BCX509Certificate
 {
+    X500Name getIssuerX500Name();
     TBSCertificate getTBSCertificateNative();
+    X500Name getSubjectX500Name();
 }
