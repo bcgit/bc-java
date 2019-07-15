e997cf51e8e8b9abd1fe3ac6ef14814e5cc8f39c
Peter Dettman <peter.dettman@bouncycastle.org>
2019-07-15 10:52:54 +0700

    Migrate DER sequence/set parsers to "DL" naming
    - Return DL for loaded objects

---
+++ core/src/main/java/org/bouncycastle/asn1/DLSetParser.java
@@ -0,0 +1,12 @@
+package org.bouncycastle.asn1;
+
+/**
+ * Parser class for DL SETs.
+ */
+public class DLSetParser extends DERSetParser
+{
+    DLSetParser(ASN1StreamParser parser)
+    {
+        super(parser);
+    }
+}
