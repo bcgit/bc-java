74bde5f4df65ebec3d07bde679984f2f640ffca5
Matthias Edelhoff <matthias.edelhoff@cryptovision.com>
2019-07-25 00:00:38 +0200

    Basic AuthEnveloped Content Encryptor, inlcuding attributes. For now AES/GCM only

--- pkix/src/main/java/org/bouncycastle/cms/bc/BcCMSContentEncryptorBuilder.java
+++ pkix/src/main/java/org/bouncycastle/cms/bc/BcCMSContentEncryptorBuilder.java
@@ -1,20 +1,25 @@
 package org.bouncycastle.cms.bc;
 
-import java.io.OutputStream;
-import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.DERSet;
+import org.bouncycastle.asn1.cms.AttributeTable;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.cms.CMSAlgorithm;
+import org.bouncycastle.cms.CMSAttributeTableGenerator;
 import org.bouncycastle.cms.CMSException;
 import org.bouncycastle.crypto.CipherKeyGenerator;
+import org.bouncycastle.crypto.modes.AEADBlockCipher;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.util.CipherFactory;
 import org.bouncycastle.operator.GenericKey;
 import org.bouncycastle.operator.OutputEncryptor;
 import org.bouncycastle.util.Integers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BcCMSContentEncryptorBuilder
 {
@@ -25,6 +30,10 @@ public class BcCMSContentEncryptorBuilder
         keySizes.put(CMSAlgorithm.AES128_CBC, Integers.valueOf(128));
         keySizes.put(CMSAlgorithm.AES192_CBC, Integers.valueOf(192));
         keySizes.put(CMSAlgorithm.AES256_CBC, Integers.valueOf(256));
+
+        keySizes.put(CMSAlgorithm.AES128_GCM, Integers.valueOf(128));
+        keySizes.put(CMSAlgorithm.AES192_GCM, Integers.valueOf(192));
+        keySizes.put(CMSAlgorithm.AES256_GCM, Integers.valueOf(256));
 
         keySizes.put(CMSAlgorithm.CAMELLIA128_CBC, Integers.valueOf(128));
         keySizes.put(CMSAlgorithm.CAMELLIA192_CBC, Integers.valueOf(192));
@@ -70,6 +79,9 @@ public class BcCMSContentEncryptorBuilder
     public OutputEncryptor build()
         throws CMSException
     {
+        if(helper.isAuthEnveloped(encryptionOID)){
+            return new CMSAuthOutputEncryptor(encryptionOID, keySize, random);
+        }
         return new CMSOutputEncryptor(encryptionOID, keySize, random);
     }
 
@@ -78,7 +90,7 @@ public class BcCMSContentEncryptorBuilder
     {
         private KeyParameter encKey;
         private AlgorithmIdentifier algorithmIdentifier;
-        private Object             cipher;
+        protected Object             cipher;
 
         CMSOutputEncryptor(ASN1ObjectIdentifier encryptionOID, int keySize, SecureRandom random)
             throws CMSException
@@ -110,6 +122,44 @@ public class BcCMSContentEncryptorBuilder
         public GenericKey getKey()
         {
             return new GenericKey(algorithmIdentifier, encKey.getKey());
+        }
+    }
+
+    public interface MacProvider{
+        byte[] addAuthenticatedAttributes(AttributeTable authAttributes) throws IOException;
+        byte[] getMac();
+    }
+    private class CMSAuthOutputEncryptor extends CMSOutputEncryptor implements MacProvider {
+        CMSAttributeTableGenerator authenticatedAttributeGenerator;
+
+        CMSAuthOutputEncryptor(ASN1ObjectIdentifier encryptionOID, int keySize, SecureRandom random)
+                throws CMSException
+        {
+            super(encryptionOID, keySize, random);
+            //check the cipher is able to produce authenticated results.
+            getCipher();
+        }
+
+        private AEADBlockCipher getCipher()
+        {
+            if(!(cipher instanceof AEADBlockCipher)){
+                throw new IllegalArgumentException("Unable to create Authenticated Output Encryptor without Authenticaed Data cipher!") ;
+            }
+            return (AEADBlockCipher)cipher;
+        }
+
+        byte[] aad = null;
+        public byte[] addAuthenticatedAttributes(AttributeTable authAttributes) throws IOException {
+            aad = new DERSet(authAttributes.toASN1EncodableVector())
+                        .toASN1Primitive().getEncoded();
+            return aad;
+        }
+
+        public byte[] getMac() {
+            if(aad != null){
+                getCipher().processAADBytes(aad, 0, aad.length);
+            }
+            return getCipher().getMac();
         }
     }
 }
