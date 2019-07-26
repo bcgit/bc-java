b3d4461f03f54752b5e0595247a06a424ff9f906
Peter Dettman <peter.dettman@bouncycastle.org>
2019-07-26 13:06:03 +0700

    RegressionTest classes now show failure summary at end

--- core/src/test/j2me/java/math/test/RegressionTest.java
+++ core/src/test/j2me/java/math/test/RegressionTest.java
@@ -1,7 +1,7 @@
 package java.math.test;
 
+import org.bouncycastle.util.test.SimpleTest;
 import org.bouncycastle.util.test.Test;
-import org.bouncycastle.util.test.TestResult;
 
 public class RegressionTest
 {
@@ -9,13 +9,8 @@ public class RegressionTest
         new BigIntegerTest()
     };
 
-    public static void main(
-        String[]    args)
+    public static void main(String[] args)
     {
-        for (int i = 0; i != tests.length; i++)
-        {
-            TestResult  result = tests[i].perform();
-            System.out.println(result);
-        }
+        SimpleTest.runTests(tests);
     }
 }
--- pg/src/main/j2me/org/bouncycastle/openpgp/test/RegressionTest.java
+++ pg/src/main/j2me/org/bouncycastle/openpgp/test/RegressionTest.java
@@ -1,7 +1,7 @@
 package org.bouncycastle.openpgp.test;
 
+import org.bouncycastle.util.test.SimpleTest;
 import org.bouncycastle.util.test.Test;
-import org.bouncycastle.util.test.TestResult;
 
 public class RegressionTest
 {
@@ -13,20 +13,8 @@ public class RegressionTest
         new BcPGPRSATest()
     };
 
-    public static void main(
-        String[]    args)
+    public static void main(String[] args)
     {
-        for (int i = 0; i != tests.length; i++)
-        {
-            TestResult  result = tests[i].perform();
-            
-            if (result.getException() != null)
-            {
-                result.getException().printStackTrace();
-            }
-            
-            System.out.println(result);
-        }
+        SimpleTest.runTests(tests);
     }
 }
-
