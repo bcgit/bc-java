572d56e7f7cca3dcd554c6dd3bf835e9d1e2744b
David Hook <dgh@cryptoworkshop.com>
2019-08-18 10:16:55 +1000

    github #574 added getEncoded() with encoding style method.

--- pkix/src/main/java/org/bouncycastle/tsp/TimeStampResponse.java
+++ pkix/src/main/java/org/bouncycastle/tsp/TimeStampResponse.java
@@ -4,8 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1Encoding;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.DLSequence;
 import org.bouncycastle.asn1.cmp.PKIFailureInfo;
@@ -214,15 +212,6 @@ public class TimeStampResponse
      */
     public byte[] getEncoded(String encoding) throws IOException
     {
-        if (ASN1Encoding.DL.equals(encoding))
-        {
-            final ASN1Encodable[] encodable =
-                timeStampToken == null ?
-                new ASN1Encodable[] { resp.getStatus() } :
-                new ASN1Encodable[] { resp.getStatus(),
-                                      timeStampToken.toCMSSignedData().toASN1Structure() };
-            return new DLSequence(encodable).getEncoded(encoding);
-        }
         return resp.getEncoded(encoding);
     }
 }
