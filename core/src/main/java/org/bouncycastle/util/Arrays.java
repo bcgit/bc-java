06833774ee1478d03fa26f73d5f7f11f468720f1
Peter Dettman <peter.dettman@bouncycastle.org>
2019-09-03 23:42:27 +0700

    RFC 7539: Initial implementation of ChaCha20Poly1305 AEAD cipher

--- core/src/main/java/org/bouncycastle/util/Arrays.java
+++ core/src/main/java/org/bouncycastle/util/Arrays.java
@@ -97,6 +97,37 @@ public final class Arrays
         return nonEqual == 0;
     }
 
+    public static boolean constantTimeAreEqual(int len, byte[] a, int aOff, byte[] b, int bOff)
+    {
+        if (null == a)
+        {
+            throw new NullPointerException("'a' cannot be null");
+        }
+        if (null == b)
+        {
+            throw new NullPointerException("'b' cannot be null");
+        }
+        if (len < 0)
+        {
+            throw new IllegalArgumentException("'len' cannot be negative");
+        }
+        if (aOff > (a.length - len))
+        {
+            throw new IndexOutOfBoundsException("'aOff' value invalid for specified length");
+        }
+        if (bOff > (b.length - len))
+        {
+            throw new IndexOutOfBoundsException("'bOff' value invalid for specified length");
+        }
+
+        int d = 0;
+        for (int i = 0; i < len; ++i)
+        {
+            d |= (a[aOff + i] ^ b[bOff + i]);
+        }
+        return 0 == d;
+    }
+
     public static int compareUnsigned(byte[] a, byte[] b)
     {
         if (a == b)
