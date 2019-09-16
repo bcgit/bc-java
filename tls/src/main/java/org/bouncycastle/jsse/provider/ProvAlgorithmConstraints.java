cf2a4a52f12cd8136a0ad8e5dc06f1702c6bec23
Peter Dettman <peter.dettman@bouncycastle.org>
2019-09-17 01:52:46 +0700

    Initial outline of disabled-algorithms support

--- tls/src/main/java/org/bouncycastle/jsse/provider/ProvAlgorithmConstraints.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/ProvAlgorithmConstraints.java
@@ -10,6 +10,11 @@ import org.bouncycastle.jsse.java.security.BCCryptoPrimitive;
 public class ProvAlgorithmConstraints
     implements BCAlgorithmConstraints
 {
+    private static final String PROPERTY_DISABLED_ALGORITHMS = "jdk.tls.disabledAlgorithms";
+
+    private static final DisabledAlgorithmConstraints provTlsDisabledAlgorithms =
+        DisabledAlgorithmConstraints.create(ProvAlgorithmDecomposer.INSTANCE, PROPERTY_DISABLED_ALGORITHMS);
+
     static final ProvAlgorithmConstraints DEFAULT = new ProvAlgorithmConstraints();
 
     public ProvAlgorithmConstraints()
@@ -19,18 +24,33 @@ public class ProvAlgorithmConstraints
 
     public boolean permits(Set<BCCryptoPrimitive> primitives, String algorithm, AlgorithmParameters parameters)
     {
+        if (null != provTlsDisabledAlgorithms && !provTlsDisabledAlgorithms.permits(primitives, algorithm, parameters))
+        {
+            return false;
+        }
+
         // TODO[jsse]
         return true;
     }
 
     public boolean permits(Set<BCCryptoPrimitive> primitives, Key key)
     {
+        if (null != provTlsDisabledAlgorithms && !provTlsDisabledAlgorithms.permits(primitives, key))
+        {
+            return false;
+        }
+
         // TODO[jsse]
         return true;
     }
 
     public boolean permits(Set<BCCryptoPrimitive> primitives, String algorithm, Key key, AlgorithmParameters parameters)
     {
+        if (null != provTlsDisabledAlgorithms && !provTlsDisabledAlgorithms.permits(primitives, algorithm, key, parameters))
+        {
+            return false;
+        }
+
         // TODO[jsse]
         return true;
     }
