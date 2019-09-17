46ed07076d93692ee3016370608cd706c5bc349d
Peter Dettman <peter.dettman@bouncycastle.org>
2019-09-17 23:06:35 +0700

    Work on AlgorithmConstraints support

--- tls/src/main/java/org/bouncycastle/jsse/provider/AbstractAlgorithmConstraints.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/AbstractAlgorithmConstraints.java
@@ -1,8 +1,12 @@
 package org.bouncycastle.jsse.provider;
 
+import java.security.Key;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.bouncycastle.jsse.java.security.BCAlgorithmConstraints;
+import org.bouncycastle.jsse.java.security.BCCryptoPrimitive;
 
 abstract class AbstractAlgorithmConstraints implements BCAlgorithmConstraints
 {
@@ -13,6 +17,30 @@ abstract class AbstractAlgorithmConstraints implements BCAlgorithmConstraints
         this.decomposer = decomposer;
     }
 
+    protected void checkAlgorithmName(String algorithm)
+    {
+        if (!isAlgorithmSpecified(algorithm))
+        {
+            throw new IllegalArgumentException("No algorithm name specified");
+        }
+    }
+
+    protected void checkKey(Key key)
+    {
+        if (null == key)
+        {
+            throw new NullPointerException("'key' cannot be null");
+        }
+    }
+
+    protected void checkPrimitives(Set<BCCryptoPrimitive> primitives)
+    {
+        if (!isPrimitivesSpecified(primitives))
+        {
+            throw new IllegalArgumentException("No cryptographic primitive specified");
+        }
+    }
+
     protected boolean containsAnyPartIgnoreCase(Set<String> elements, String algorithm)
     {
         if (containsIgnoreCase(elements, algorithm))
@@ -20,11 +48,14 @@ abstract class AbstractAlgorithmConstraints implements BCAlgorithmConstraints
             return true;
         }
 
-        for (String part : decomposer.decompose(algorithm))
+        if (null != decomposer)
         {
-            if (containsIgnoreCase(elements, part))
+            for (String part : decomposer.decompose(algorithm))
             {
-                return true;
+                if (containsIgnoreCase(elements, part))
+                {
+                    return true;
+                }
             }
         }
 
@@ -41,5 +72,36 @@ abstract class AbstractAlgorithmConstraints implements BCAlgorithmConstraints
             }
         }
         return false;
+    }
+
+    protected boolean isAlgorithmSpecified(String algorithm)
+    {
+        return null != algorithm && algorithm.length() > 0;
+    }
+
+    protected boolean isPrimitivesSpecified(Set<BCCryptoPrimitive> primitives)
+    {
+        return null != primitives && !primitives.isEmpty();
+    }
+
+    protected static Set<String> asUnmodifiableSet(String[] algorithms)
+    {
+        return null == algorithms || algorithms.length < 1
+            ? Collections.<String> emptySet()
+            : Collections.unmodifiableSet(asSet(algorithms));
+    }
+
+    protected static Set<String> asSet(String[] algorithms)
+    {
+        Set<String> result = new HashSet<String>();
+        if (null != algorithms)
+        {
+            for (String algorithm : algorithms)
+            {
+                // TODO[jsse] toLowerCase?
+                result.add(algorithm);
+            }
+        }
+        return result;
     }
 }
