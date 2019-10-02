4f713cde1cd42948f17bed6faf600080829a1e91
David Hook <dgh@cryptoworkshop.com>
2019-10-02 20:11:05 +1000

    removed packaged protected test

--- pg/src/test/java/org/bouncycastle/gpg/test/RegressionTest.java
+++ pg/src/test/java/org/bouncycastle/gpg/test/RegressionTest.java
@@ -2,15 +2,13 @@ package org.bouncycastle.gpg.test;
 
 import java.security.Security;
 
-import org.bouncycastle.gpg.keybox.KeyBoxByteBufferTest;
 import org.bouncycastle.util.test.SimpleTest;
 import org.bouncycastle.util.test.Test;
 
 public class RegressionTest
 {
     public static Test[] tests = {
-        new KeyBoxTest(),
-        new KeyBoxByteBufferTest()
+        new KeyBoxTest()
     };
 
     public static void main(String[] args)
