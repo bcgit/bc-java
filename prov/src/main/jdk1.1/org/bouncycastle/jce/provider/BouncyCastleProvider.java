6f8e3b850d6fc4307f15e991a2f0f9b6c05af089
David Hook <dgh@cryptoworkshop.com>
2019-10-05 15:21:38 +1000

    updated version numbers

--- prov/src/main/jdk1.1/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/jdk1.1/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -43,7 +43,7 @@ import org.bouncycastle.jcajce.provider.util.AsymmetricKeyInfoConverter;
 public final class BouncyCastleProvider extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Security Provider v1.63";
+    private static String info = "BouncyCastle Security Provider v1.64";
 
     public static final String PROVIDER_NAME = "BC";
 
@@ -98,7 +98,7 @@ public final class BouncyCastleProvider extends Provider
     private static final String[] DIGESTS =
     {
         "GOST3411", "Keccak", "MD2", "MD4", "MD5", "SHA1", "RIPEMD128", "RIPEMD160", "RIPEMD256", "RIPEMD320", "SHA224",
-        "SHA256", "SHA384", "SHA512", "SHA3", "Skein", "SM3", "Tiger", "Whirlpool", "Blake2b", "Blake2s", "DSTU7564"
+        "SHA256", "SHA384", "SHA512", "SHA3", "Skein", "SM3", "Tiger", "Whirlpool", "Blake2b", "Blake2s", "DSTU7564", "Haraka"
     };
 
     /*
@@ -117,7 +117,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.63, info);
+        super(PROVIDER_NAME, 1.64, info);
 
         setup();
     }
