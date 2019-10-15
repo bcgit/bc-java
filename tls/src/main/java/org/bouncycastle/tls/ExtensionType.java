29705d969379d77d74647bc338fd1c20927b8150
Peter Dettman <peter.dettman@bouncycastle.org>
2019-10-15 18:58:01 +0700

    Add utility methods to ExtensionType
    - also, token_binding is out of draft (RFC 8472)

--- tls/src/main/java/org/bouncycastle/tls/ExtensionType.java
+++ tls/src/main/java/org/bouncycastle/tls/ExtensionType.java
@@ -95,9 +95,9 @@ public class ExtensionType
     public static final int extended_master_secret = 23;
 
     /*
-     * draft-ietf-tokbind-negotiation-08
+     * RFC 8472
      */
-    public static final int DRAFT_token_binding = 24;
+    public static final int token_binding = 24;
 
     /*
      * RFC 7924
@@ -132,4 +132,145 @@ public class ExtensionType
      * RFC 5746 3.2.
      */
     public static final int renegotiation_info = 0xff01;
+
+    public static String getName(int extensionType)
+    {
+        switch (extensionType)
+        {
+        case server_name:
+            return "server_name";
+        case max_fragment_length:
+            return "max_fragment_length";
+        case client_certificate_url:
+            return "client_certificate_url";
+        case trusted_ca_keys:
+            return "trusted_ca_keys";
+        case truncated_hmac:
+            return "truncated_hmac";
+        case status_request:
+            return "status_request";
+        case user_mapping:
+            return "user_mapping";
+        case client_authz:
+            return "client_authz";
+        case server_authz:
+            return "server_authz";
+        case cert_type:
+            return "cert_type";
+        case supported_groups:
+            return "supported_groups";
+        case ec_point_formats:
+            return "ec_point_formats";
+        case srp:
+            return "srp";
+        case signature_algorithms:
+            return "signature_algorithms";
+        case use_srtp:
+            return "use_srtp";
+        case heartbeat:
+            return "heartbeat";
+        case application_layer_protocol_negotiation:
+            return "application_layer_protocol_negotiation";
+        case status_request_v2:
+            return "status_request_v2";
+        case signed_certificate_timestamp:
+            return "signed_certificate_timestamp";
+        case client_certificate_type:
+            return "client_certificate_type";
+        case server_certificate_type:
+            return "server_certificate_type";
+        case padding:
+            return "padding";
+        case encrypt_then_mac:
+            return "encrypt_then_mac";
+        case extended_master_secret:
+            return "extended_master_secret";
+        case token_binding:
+            return "token_binding";
+        case cached_info:
+            return "cached_info";
+        case record_size_limit:
+            return "record_size_limit";
+        case session_ticket:
+            return "session_ticket";
+        case pre_shared_key:
+            return "pre_shared_key";
+        case early_data:
+            return "early_data";
+        case supported_versions:
+            return "supported_versions";
+        case cookie:
+            return "cookie";
+        case psk_key_exchange_modes:
+            return "psk_key_exchange_modes";
+        case certificate_authorities:
+            return "certificate_authorities";
+        case oid_filters:
+            return "oid_filters";
+        case post_handshake_auth:
+            return "post_handshake_auth";
+        case signature_algorithms_cert:
+            return "signature_algorithms_cert";
+        case key_share:
+            return "key_share";
+        case renegotiation_info:
+            return "renegotiation_info";
+        default:
+            return "UNKNOWN";
+        }
+    }
+
+    public static String getText(short extensionType)
+    {
+        return getName(extensionType) + "(" + extensionType + ")";
+    }
+
+    public static boolean isRecognized(int extensionType)
+    {
+        switch (extensionType)
+        {
+        case server_name:
+        case max_fragment_length:
+        case client_certificate_url:
+        case trusted_ca_keys:
+        case truncated_hmac:
+        case status_request:
+        case user_mapping:
+        case client_authz:
+        case server_authz:
+        case cert_type:
+        case supported_groups:
+        case ec_point_formats:
+        case srp:
+        case signature_algorithms:
+        case use_srtp:
+        case heartbeat:
+        case application_layer_protocol_negotiation:
+        case status_request_v2:
+        case signed_certificate_timestamp:
+        case client_certificate_type:
+        case server_certificate_type:
+        case padding:
+        case encrypt_then_mac:
+        case extended_master_secret:
+        case token_binding:
+        case cached_info:
+        case record_size_limit:
+        case session_ticket:
+        case pre_shared_key:
+        case early_data:
+        case supported_versions:
+        case cookie:
+        case psk_key_exchange_modes:
+        case certificate_authorities:
+        case oid_filters:
+        case post_handshake_auth:
+        case signature_algorithms_cert:
+        case key_share:
+        case renegotiation_info:
+            return true;
+        default:
+            return false;
+        }
+    }
 }
