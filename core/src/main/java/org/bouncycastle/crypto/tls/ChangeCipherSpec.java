37128d478453eb8340c6213b9d7fe52560f334b2
Peter Dettman <peter.dettman@bouncycastle.org>
2019-10-16 15:12:48 +0700

    Remove deprecated legacy TLS API

--- core/src/main/java/org/bouncycastle/crypto/tls/ChangeCipherSpec.java
+++
@@ -1,9 +0,0 @@
-package org.bouncycastle.crypto.tls;
-
-/**
- * @deprecated Migrate to the (D)TLS API in org.bouncycastle.tls (bctls jar).
- */
-public class ChangeCipherSpec
-{
-    public static final short change_cipher_spec = 1;
-}
--- core/src/main/java/org/bouncycastle/crypto/tls/TlsCredentials.java
+++
@@ -1,9 +0,0 @@
-package org.bouncycastle.crypto.tls;
-
-/**
- * @deprecated Migrate to the (D)TLS API in org.bouncycastle.tls (bctls jar).
- */
-public interface TlsCredentials
-{
-    Certificate getCertificate();
-}
