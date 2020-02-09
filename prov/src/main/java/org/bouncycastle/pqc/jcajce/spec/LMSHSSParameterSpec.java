02fa3457b2bf66ce5ac1673a7a7a6c72605aa7c1
David Hook <dgh@cryptoworkshop.com>
2020-02-09 12:25:19 +1100

    JavaDoc

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/spec/LMSHSSParameterSpec.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/spec/LMSHSSParameterSpec.java
@@ -2,16 +2,29 @@ package org.bouncycastle.pqc.jcajce.spec;
 
 import java.security.spec.AlgorithmParameterSpec;
 
+/**
+ * ParameterSpec for keys using the LMS Hierarchical Signature System (HSS).
+ */
 public class LMSHSSParameterSpec
     implements AlgorithmParameterSpec
 {
     private final LMSParameterSpec[] specs;
 
+    /**
+     * Base constructor, specify the LMS parameters at each level of the hierarchy.
+     *
+     * @param specs the LMS parameter specs for each level of the hierarchy.
+     */
     public LMSHSSParameterSpec(LMSParameterSpec[] specs)
     {
         this.specs = specs.clone();
     }
 
+    /**
+     * Return the LMS parameters for the HSS hierarchy.
+     *
+     * @return the HSS component LMS parameter specs.
+     */
     public LMSParameterSpec[] getLMSSpecs()
     {
         return specs.clone();
