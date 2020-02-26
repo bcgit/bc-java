1a92b501009de4250146b233e46eb3a5852fbe1a
David Hook <dgh@cryptoworkshop.com>
2020-02-27 06:32:11 +1100

    github #661 - fixed accidental inclusion of test class in provider jar.

---
+++ core/src/test/java/org/bouncycastle/pqc/crypto/lms/AllTests.java
@@ -0,0 +1,46 @@
+package org.bouncycastle.pqc.crypto.lms;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllTests
+    extends TestCase
+{
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("Lightweight LMS PQ Crypto Tests");
+
+        suite.addTestSuite(HSSTests.class);
+        suite.addTestSuite(LMSKeyGenTests.class);
+        suite.addTestSuite(LMSTests.class);
+        suite.addTestSuite(TypeTests.class);
+
+        return new BCTestSetup(suite);
+    }
+    
+    static class BCTestSetup
+        extends TestSetup
+    {
+        public BCTestSetup(Test test)
+        {
+            super(test);
+        }
+
+        protected void setUp()
+        {
+
+        }
+
+        protected void tearDown()
+        {
+
+        }
+    }
+}
