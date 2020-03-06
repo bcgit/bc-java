c2bbb5d1e0fcf139705eb6de20c9ea1414d4644d
Peter Dettman <peter.dettman@bouncycastle.org>
2020-03-06 16:48:42 +0700

    Remove the PUBLIC_KEY_ENCRYPTION sets

--- tls/src/main/java/org/bouncycastle/jsse/provider/JsseUtils.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/JsseUtils.java
@@ -57,8 +57,6 @@ abstract class JsseUtils
         Collections.unmodifiableSet(EnumSet.of(BCCryptoPrimitive.KEY_AGREEMENT));
     static final Set<BCCryptoPrimitive> KEY_ENCAPSULATION_CRYPTO_PRIMITIVES_BC =
         Collections.unmodifiableSet(EnumSet.of(BCCryptoPrimitive.KEY_ENCAPSULATION));
-    static final Set<BCCryptoPrimitive> PUBLIC_KEY_ENCRYPTION_CRYPTO_PRIMITIVES_BC =
-        Collections.unmodifiableSet(EnumSet.of(BCCryptoPrimitive.PUBLIC_KEY_ENCRYPTION));
     static final Set<BCCryptoPrimitive> SIGNATURE_CRYPTO_PRIMITIVES_BC =
         Collections.unmodifiableSet(EnumSet.of(BCCryptoPrimitive.SIGNATURE));
 
