9d887549bc368b65f0cae934660135d280de7ecc
Peter Dettman <peter.dettman@bouncycastle.org>
2020-03-16 12:02:43 +0700

    Special handling for NULL parameters
    - keep a table of affected alg OIDs

--- tls/src/main/java/org/bouncycastle/jsse/provider/ProvAlgorithmChecker.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/ProvAlgorithmChecker.java
@@ -10,6 +10,7 @@ import java.security.cert.X509Certificate;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,6 +22,7 @@ import org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
 import org.bouncycastle.jcajce.util.JcaJceHelper;
 import org.bouncycastle.jsse.java.security.BCAlgorithmConstraints;
 import org.bouncycastle.jsse.java.security.BCCryptoPrimitive;
+import org.bouncycastle.util.Arrays;
 
 class ProvAlgorithmChecker
     extends PKIXCertPathChecker
@@ -30,6 +32,9 @@ class ProvAlgorithmChecker
     static final int KU_KEY_AGREEMENT = 4;
 
     private static final Map<String, String> sigAlgNames = createSigAlgNames();
+    private static final Set<String> sigAlgNoParams = createSigAlgNoParams();
+
+    private static final byte[] DER_NULL_ENCODING = new byte[]{ 0x05, 0x00 };
 
     private static Map<String, String> createSigAlgNames()
     {
@@ -42,6 +47,16 @@ class ProvAlgorithmChecker
         names.put(X9ObjectIdentifiers.id_dsa_with_sha1.getId(), "SHA1withDSA");
 
         return Collections.unmodifiableMap(names);
+    }
+
+    private static Set<String> createSigAlgNoParams()
+    {
+        Set<String> noParams = new HashSet<String>();
+
+        noParams.add(OIWObjectIdentifiers.dsaWithSHA1.getId());
+        noParams.add(X9ObjectIdentifiers.id_dsa_with_sha1.getId());
+
+        return Collections.unmodifiableSet(noParams);
     }
 
     private final JcaJceHelper helper;
@@ -278,10 +293,16 @@ class ProvAlgorithmChecker
             return null;
         }
 
+        String sigAlgOID = cert.getSigAlgOID();
+        if (sigAlgNoParams.contains(sigAlgOID) && Arrays.areEqual(DER_NULL_ENCODING, encoded))
+        {
+            return null;
+        }
+
         AlgorithmParameters sigAlgParams;
         try
         {
-            sigAlgParams = helper.createAlgorithmParameters(cert.getSigAlgOID());
+            sigAlgParams = helper.createAlgorithmParameters(sigAlgOID);
         }
         catch (GeneralSecurityException e)
         {
