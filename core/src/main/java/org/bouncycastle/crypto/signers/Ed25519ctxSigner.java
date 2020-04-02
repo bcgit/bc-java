cd1f5cea60683f9548193900e63e246f541e90f1
Peter Dettman <peter.dettman@bouncycastle.org>
2020-04-02 11:47:54 +0700

    Reset after wrong-length failure

--- core/src/main/java/org/bouncycastle/crypto/signers/Ed25519ctxSigner.java
+++ core/src/main/java/org/bouncycastle/crypto/signers/Ed25519ctxSigner.java
@@ -91,6 +91,7 @@ public class Ed25519ctxSigner
         {
             if (Ed25519.SIGNATURE_SIZE != signature.length)
             {
+                reset();
                 return false;
             }
 
