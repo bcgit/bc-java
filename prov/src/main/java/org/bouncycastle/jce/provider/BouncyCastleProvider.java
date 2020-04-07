134f15e2e744e30091d7878bb850a30ce79ad635
David Hook <dgh@cryptoworkshop.com>
2020-04-07 17:11:51 +1000

    updated provider to beta

--- prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -58,7 +58,7 @@ import org.bouncycastle.pqc.jcajce.provider.xmss.XMSSMTKeyFactorySpi;
 public final class BouncyCastleProvider extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Security Provider v1.65";
+    private static String info = "BouncyCastle Security Provider v1.66b";
 
     public static final String PROVIDER_NAME = "BC";
 
@@ -142,7 +142,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.65, info);
+        super(PROVIDER_NAME, 1.655, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
