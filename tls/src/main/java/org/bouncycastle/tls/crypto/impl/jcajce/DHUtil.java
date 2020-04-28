3d940364e5830ca0bf723ddddc46b9d5d3073af0
Peter Dettman <peter.dettman@bouncycastle.org>
2020-04-28 21:08:36 +0700

    Partially address DH Q parameter issue

--- tls/src/main/java/org/bouncycastle/tls/crypto/impl/jcajce/DHUtil.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/impl/jcajce/DHUtil.java
@@ -1,18 +1,33 @@
 package org.bouncycastle.tls.crypto.impl.jcajce;
 
+import java.math.BigInteger;
 import java.security.AlgorithmParameters;
 import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.KeySpec;
 
 import javax.crypto.spec.DHParameterSpec;
+import javax.crypto.spec.DHPublicKeySpec;
 
+import org.bouncycastle.jcajce.spec.DHDomainParameterSpec;
 import org.bouncycastle.tls.crypto.DHGroup;
 
 class DHUtil
 {
     static AlgorithmParameterSpec createInitSpec(DHGroup dhGroup)
     {
-        // NOTE: dhGroup.getQ() is ignored here
-        return new DHParameterSpec(dhGroup.getP(), dhGroup.getG(), dhGroup.getL());
+        // NOTE: A BC-specific spec, so other providers probably won't see Q 
+        return new DHDomainParameterSpec(dhGroup.getP(), dhGroup.getQ(), dhGroup.getG(), dhGroup.getL());
+    }
+
+    static KeySpec createPublicKeySpec(BigInteger y, DHParameterSpec dhSpec)
+    {
+        BigInteger P = dhSpec.getP(), G = dhSpec.getG();
+
+        @SuppressWarnings("unused")
+        BigInteger Q = getQ(dhSpec);
+
+        // TODO Need a way to pass Q here (when available)
+        return new DHPublicKeySpec(y, P, G);
     }
 
     static AlgorithmParameters getAlgorithmParameters(JcaTlsCrypto crypto, DHGroup dhGroup)
@@ -63,5 +78,10 @@ class DHUtil
         }
 
         return null;
+    }
+
+    static BigInteger getQ(DHParameterSpec dhSpec)
+    {
+        return dhSpec instanceof DHDomainParameterSpec ? ((DHDomainParameterSpec)dhSpec).getQ() : null;
     }
 }
