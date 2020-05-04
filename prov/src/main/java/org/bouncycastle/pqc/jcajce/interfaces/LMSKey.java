74a0fb3bc55533744fb40e96399cc9da2808b4d7
David Hook <dgh@cryptoworkshop.com>
2020-05-04 14:27:22 +1000

    JavaDoc

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/interfaces/LMSKey.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/interfaces/LMSKey.java
@@ -2,8 +2,16 @@ package org.bouncycastle.pqc.jcajce.interfaces;
 
 import java.security.Key;
 
+/**
+ * Base interface for Leighton-Micali Hash-Based Signatures (LMS) keys.
+ */
 public interface LMSKey
     extends Key
 {
+    /**
+     * Return the number of levels (L) associated with the key.
+     *
+     * @return L.
+     */
     int getLevels();
 }
