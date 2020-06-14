dc4c606b03bfc1f5e50ce73e07d20c3969e50435
Peter Dettman <peter.dettman@bouncycastle.org>
2020-06-14 20:14:16 +0700

    Use X.509v3 certs, remove old test KeyStore's

--- tls/src/test/java/org/bouncycastle/tls/test/HTTPSServerThread.java
+++ tls/src/test/java/org/bouncycastle/tls/test/HTTPSServerThread.java
@@ -1,27 +1,30 @@
 package org.bouncycastle.tls.test;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
-import javax.net.ssl.TrustManagerFactory;
+
+import org.bouncycastle.tls.crypto.impl.jcajce.JcaTlsCertificate;
+import org.bouncycastle.tls.crypto.impl.jcajce.JcaTlsCrypto;
+import org.bouncycastle.tls.crypto.impl.jcajce.JcaTlsCryptoProvider;
 
 public class HTTPSServerThread
     extends Thread
 {
     private static final int PORT_NO = 12001;
     private static final char[] SERVER_PASSWORD = "serverPassword".toCharArray();
-    private static final char[] TRUST_STORE_PASSWORD = "trustPassword".toCharArray();
 
     /**
      * Read a HTTP request
@@ -62,26 +65,20 @@ public class HTTPSServerThread
     SSLContext createSSLContext()
         throws Exception
     {
-        KeyManagerFactory mgrFact = TlsTestUtils.getSunX509KeyManagerFactory();
+        JcaTlsCrypto crypto = new JcaTlsCryptoProvider().create(new SecureRandom());
+        X509Certificate certificate = ((JcaTlsCertificate)TlsTestUtils.loadCertificateResource(crypto,
+            "x509-server-rsa-sign.pem")).getX509Certificate();
+        PrivateKey privateKey = TlsTestUtils.loadJcaPrivateKeyResource(crypto, "x509-server-key-rsa-sign.pem");
+
         KeyStore serverStore = KeyStore.getInstance("JKS");
+        serverStore.load(null, null);
+        serverStore.setKeyEntry("server", privateKey, SERVER_PASSWORD, new X509Certificate[]{ certificate });
 
-        serverStore.load(new ByteArrayInputStream(KeyStores.server), SERVER_PASSWORD);
-
+        KeyManagerFactory mgrFact = TlsTestUtils.getSunX509KeyManagerFactory();
         mgrFact.init(serverStore, SERVER_PASSWORD);
 
-        // set up a trust manager so we can recognize the server
-        TrustManagerFactory trustFact = TlsTestUtils.getSunX509TrustManagerFactory();
-        KeyStore trustStore = KeyStore.getInstance("JKS");
-
-        trustStore.load(new ByteArrayInputStream(KeyStores.trustStore), TRUST_STORE_PASSWORD);
-
-        trustFact.init(trustStore);
-
-        // create a context and set up a socket factory
         SSLContext sslContext = SSLContext.getInstance("TLS");
-
-        sslContext.init(mgrFact.getKeyManagers(), trustFact.getTrustManagers(), null);
-
+        sslContext.init(mgrFact.getKeyManagers(), null, null);
         return sslContext;
     }
 
@@ -98,7 +95,8 @@ public class HTTPSServerThread
 
             readRequest(sslSock.getInputStream());
 
-            SSLSession session = sslSock.getSession();
+//            SSLSession session =
+            sslSock.getSession();
 
             sendResponse(sslSock.getOutputStream());
 
