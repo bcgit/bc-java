80d44295685f7dfdbfa787f26c9ca4120fe408ef
Peter Dettman <peter.dettman@bouncycastle.org>
2020-06-21 14:07:07 +0700

    BCJSSE: Only select X.509v3 credentials

--- tls/src/main/java/org/bouncycastle/jsse/provider/ProvX509KeyManager.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/ProvX509KeyManager.java
@@ -42,6 +42,8 @@ class ProvX509KeyManager
 {
     private static final Logger LOG = Logger.getLogger(ProvX509KeyManager.class.getName());
 
+    private static final int X509V3_VERSION = 3;
+
     private final JcaJceHelper helper;
     private final List<KeyStore.Builder> builders;
 
@@ -356,6 +358,7 @@ class ProvX509KeyManager
         BCAlgorithmConstraints algorithmConstraints, boolean forServer)
     {
         if (null == chain || chain.length < 1
+            || !isSuitableChainForTLS(chain)
             || !isSuitableChainForIssuers(chain, uniqueIssuers)
             || !isSuitableEECert(chain[0], keyTypes, algorithmConstraints, forServer))
         {
@@ -568,6 +571,18 @@ class ProvX509KeyManager
         X509Certificate eeCert = chain[0];
         return eeCert.getBasicConstraints() >= 0
             && uniqueIssuers.contains(eeCert.getSubjectX500Principal());
+    }
+
+    private static boolean isSuitableChainForTLS(X509Certificate[] chain)
+    {
+        for (int i = 0, count = chain.length; i < count; ++i)
+        {
+            if (X509V3_VERSION != chain[i].getVersion())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static boolean isSuitableEECert(X509Certificate eeCert, List<String> keyTypes,
