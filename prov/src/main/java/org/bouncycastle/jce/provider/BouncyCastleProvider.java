ad519302044532abdb0ee6e41157030a8283c32c
David Hook <dgh@cryptoworkshop.com>
2020-06-27 16:00:24 +1000

    1.4 update moved provider version to 1.66 tweak test classes and build files for Java 11 gradle build

--- prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -58,7 +58,7 @@ import org.bouncycastle.pqc.jcajce.provider.xmss.XMSSMTKeyFactorySpi;
 public final class BouncyCastleProvider extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Security Provider v1.66b";
+    private static String info = "BouncyCastle Security Provider v1.66";
 
     public static final String PROVIDER_NAME = "BC";
 
@@ -144,7 +144,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.655, info);
+        super(PROVIDER_NAME, 1.66, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
--- prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/BouncyCastlePQCProvider.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/BouncyCastlePQCProvider.java
@@ -22,7 +22,7 @@ public class BouncyCastlePQCProvider
     extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Post-Quantum Security Provider v1.65";
+    private static String info = "BouncyCastle Post-Quantum Security Provider v1.66";
 
     public static String PROVIDER_NAME = "BCPQC";
 
@@ -47,7 +47,7 @@ public class BouncyCastlePQCProvider
      */
     public BouncyCastlePQCProvider()
     {
-        super(PROVIDER_NAME, 1.65, info);
+        super(PROVIDER_NAME, 1.66, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
