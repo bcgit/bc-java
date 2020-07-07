285cb6b0e57e5c9dba7a12e9b6b5d5e9f56624b8
Peter Dettman <peter.dettman@bouncycastle.org>
2020-07-07 15:18:58 +0700

    Noekeon performance improvements

--- core/src/main/java/org/bouncycastle/crypto/engines/NoekeonEngine.java
+++ core/src/main/java/org/bouncycastle/crypto/engines/NoekeonEngine.java
@@ -5,259 +5,253 @@ import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.DataLengthException;
 import org.bouncycastle.crypto.OutputLengthException;
 import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.util.Integers;
+import org.bouncycastle.util.Pack;
 
 /**
  * A Noekeon engine, using direct-key mode.
  */
-
 public class NoekeonEngine
     implements BlockCipher
 {
-    private static final int genericSize = 16; // Block and key size, as well as the amount of rounds.
-    
-    private static final int[] nullVector = 
-                               {
-                                    0x00, 0x00, 0x00, 0x00 // Used in decryption
-                               },
-        
-                               roundConstants = 
-                               {
-                                    0x80, 0x1b, 0x36, 0x6c,
-                                    0xd8, 0xab, 0x4d, 0x9a,
-                                    0x2f, 0x5e, 0xbc, 0x63,
-                                    0xc6, 0x97, 0x35, 0x6a,
-                                    0xd4
-                               };
-    
-    private int[] state   = new int[4], // a
-                  subKeys = new int[4], // k
-                  decryptKeys = new int[4];
-    
-    private boolean _initialised,
-                    _forEncryption;
-    
+    // Block and key size, as well as the amount of rounds.
+    private static final int SIZE = 16;
+
+    // Used in decryption
+    private static final byte[] roundConstants = { (byte)0x80, 0x1b, 0x36, 0x6c, (byte)0xd8, (byte)0xab, 0x4d,
+        (byte)0x9a, 0x2f, 0x5e, (byte)0xbc, 0x63, (byte)0xc6, (byte)0x97, 0x35, 0x6a, (byte)0xd4 };
+
+    private final int[] k = new int[4];
+
+    private boolean _initialised, _forEncryption;
+
     /**
-     * Create an instance of the Noekeon encryption algorithm
-     * and set some defaults
+     * Create an instance of the Noekeon encryption algorithm and set some defaults
      */
     public NoekeonEngine()
     {
         _initialised = false;
     }
-    
+
     public String getAlgorithmName()
     {
         return "Noekeon";
     }
-    
+
     public int getBlockSize()
     {
-        return genericSize;
+        return SIZE;
     }
-    
+
     /**
      * initialise
      *
-     * @param forEncryption whether or not we are for encryption.
-     * @param params the parameters required to set up the cipher.
-     * @exception IllegalArgumentException if the params argument is
-     * inappropriate.
+     * @param forEncryption
+     *            whether or not we are for encryption.
+     * @param params
+     *            the parameters required to set up the cipher.
+     * @exception IllegalArgumentException
+     *                if the params argument is inappropriate.
      */
-    public void init(
-                     boolean             forEncryption,
-                     CipherParameters    params)
+    public void init(boolean forEncryption, CipherParameters params)
     {
         if (!(params instanceof KeyParameter))
         {
-            throw new IllegalArgumentException("invalid parameter passed to Noekeon init - " + params.getClass().getName());
+            throw new IllegalArgumentException(
+                "invalid parameter passed to Noekeon init - " + params.getClass().getName());
         }
-        
-        _forEncryption = forEncryption;
-        _initialised = true;
-        
-        KeyParameter       p = (KeyParameter)params;
-        
-        setKey(p.getKey());
+
+        this._forEncryption = forEncryption;
+        this._initialised = true;
+
+        KeyParameter p = (KeyParameter)params;
+
+        Pack.bigEndianToInt(p.getKey(), 0, k, 0, 4);
+
+        if (!forEncryption)
+        {
+            // theta(k, new int[]{ 0x00, 0x00, 0x00, 0x00 });
+            {
+                int a0 = k[0], a1 = k[1], a2 = k[2], a3 = k[3];
+
+                int t = a0 ^ a2;
+                t ^= Integers.rotateLeft(t, 8) ^ Integers.rotateLeft(t, 24);
+                a1 ^= t;
+                a3 ^= t;
+
+                t = a1 ^ a3;
+                t ^= Integers.rotateLeft(t, 8) ^ Integers.rotateLeft(t, 24);
+                a0 ^= t;
+                a2 ^= t;
+
+                k[0] = a0; k[1] = a1; k[2] = a2; k[3] = a3;
+            }
+        }
     }
-    
-    public int processBlock(
-                            byte[]  in,
-                            int     inOff,
-                            byte[]  out,
-                            int     outOff)
+
+    public int processBlock(byte[] in, int inOff, byte[] out, int outOff)
     {
         if (!_initialised)
         {
-            throw new IllegalStateException(getAlgorithmName()+" not initialised");
+            throw new IllegalStateException(getAlgorithmName() + " not initialised");
         }
-        
-        if ((inOff + genericSize) > in.length)
+        if (inOff > in.length - SIZE)
         {
             throw new DataLengthException("input buffer too short");
         }
-        
-        if ((outOff + genericSize) > out.length)
+        if (outOff > out.length - SIZE)
         {
             throw new OutputLengthException("output buffer too short");
         }
-        
-        return (_forEncryption) ? encryptBlock(in, inOff, out, outOff)
-                                : decryptBlock(in, inOff, out, outOff);
+
+        return _forEncryption ? encryptBlock(in, inOff, out, outOff) : decryptBlock(in, inOff, out, outOff);
     }
-    
+
     public void reset()
     {
     }
-    
-    /**
-     * Re-key the cipher.
-     * <p>
-     * @param  key  the key to be used
-     */
-    private void setKey(
-                        byte[]      key)
+
+    private int encryptBlock(byte[] in, int inOff, byte[] out, int outOff)
     {
-        subKeys[0] = bytesToIntBig(key, 0);
-        subKeys[1] = bytesToIntBig(key, 4);
-        subKeys[2] = bytesToIntBig(key, 8);
-        subKeys[3] = bytesToIntBig(key, 12);
-    }
-    
-    private int encryptBlock(
-                             byte[]  in,
-                             int     inOff,
-                             byte[]  out,
-                             int     outOff)
-    {
-        state[0] = bytesToIntBig(in, inOff);
-        state[1] = bytesToIntBig(in, inOff+4);
-        state[2] = bytesToIntBig(in, inOff+8);
-        state[3] = bytesToIntBig(in, inOff+12);
-        
-        int i;
-        for (i = 0; i < genericSize; i++)
+        int a0 = Pack.bigEndianToInt(in, inOff);
+        int a1 = Pack.bigEndianToInt(in, inOff + 4);
+        int a2 = Pack.bigEndianToInt(in, inOff + 8);
+        int a3 = Pack.bigEndianToInt(in, inOff + 12);
+
+        int k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];
+
+        int round = 0, t;
+        for (;;)
         {
-            state[0] ^= roundConstants[i];
-            theta(state, subKeys);
-            pi1(state);
-            gamma(state);
-            pi2(state);            
+            a0 ^= roundConstants[round] & 0xFF;
+
+            // theta(a, k);
+            {
+                t = a0 ^ a2;
+                t ^= Integers.rotateLeft(t, 8) ^ Integers.rotateLeft(t, 24);
+                a1 ^= t;
+                a3 ^= t;
+
+                a0 ^= k0;
+                a1 ^= k1;
+                a2 ^= k2;
+                a3 ^= k3;
+
+                t = a1 ^ a3;
+                t ^= Integers.rotateLeft(t, 8) ^ Integers.rotateLeft(t, 24);
+                a0 ^= t;
+                a2 ^= t;
+            }
+
+            if (++round > SIZE)
+            {
+                break;
+            }
+
+            // pi1(a);
+            {
+                a1 = Integers.rotateLeft(a1, 1);
+                a2 = Integers.rotateLeft(a2, 5);
+                a3 = Integers.rotateLeft(a3, 2);
+            }
+
+            // gamma(a);
+            {
+                a1 ^= ~a3 & ~a2;
+                a0 ^= a2 & a1;
+
+                t = a3; a3 = a0; a0 = t;
+                a2 ^= a0 ^ a1 ^ a3;
+
+                a1 ^= ~a3 & ~a2;
+                a0 ^= a2 & a1;
+            }
+
+            // pi2(a);
+            {
+                a1 = Integers.rotateLeft(a1, 31);
+                a2 = Integers.rotateLeft(a2, 27);
+                a3 = Integers.rotateLeft(a3, 30);
+            }
         }
-        
-        state[0] ^= roundConstants[i];
-        theta(state, subKeys);
-        
-        intToBytesBig(state[0], out, outOff);
-        intToBytesBig(state[1], out, outOff+4);
-        intToBytesBig(state[2], out, outOff+8);
-        intToBytesBig(state[3], out, outOff+12);
-        
-        return genericSize;
+
+        Pack.intToBigEndian(a0, out, outOff);
+        Pack.intToBigEndian(a1, out, outOff + 4);
+        Pack.intToBigEndian(a2, out, outOff + 8);
+        Pack.intToBigEndian(a3, out, outOff + 12);
+
+        return SIZE;
     }
-    
-    private int decryptBlock(
-                             byte[]  in,
-                             int     inOff,
-                             byte[]  out,
-                             int     outOff)
+
+    private int decryptBlock(byte[] in, int inOff, byte[] out, int outOff)
     {
-        state[0] = bytesToIntBig(in, inOff);
-        state[1] = bytesToIntBig(in, inOff+4);
-        state[2] = bytesToIntBig(in, inOff+8);
-        state[3] = bytesToIntBig(in, inOff+12);
-        
-        System.arraycopy(subKeys, 0, decryptKeys, 0, subKeys.length);
-        theta(decryptKeys, nullVector);
-        
-        int i;
-        for (i = genericSize; i > 0; i--)
+        int a0 = Pack.bigEndianToInt(in, inOff);
+        int a1 = Pack.bigEndianToInt(in, inOff + 4);
+        int a2 = Pack.bigEndianToInt(in, inOff + 8);
+        int a3 = Pack.bigEndianToInt(in, inOff + 12);
+
+        int k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];
+
+        int round = SIZE, t;
+        for (;;)
         {
-            theta(state, decryptKeys);
-            state[0] ^= roundConstants[i];
-            pi1(state);
-            gamma(state);
-            pi2(state);
+            // theta(a, k);
+            {
+                t = a0 ^ a2;
+                t ^= Integers.rotateLeft(t, 8) ^ Integers.rotateLeft(t, 24);
+                a1 ^= t;
+                a3 ^= t;
+
+                a0 ^= k0;
+                a1 ^= k1;
+                a2 ^= k2;
+                a3 ^= k3;
+
+                t = a1 ^ a3;
+                t ^= Integers.rotateLeft(t, 8) ^ Integers.rotateLeft(t, 24);
+                a0 ^= t;
+                a2 ^= t;
+            }
+
+            a0 ^= roundConstants[round] & 0xFF;
+
+            if (--round < 0)
+            {
+                break;
+            }
+
+            // pi1(a);
+            {
+                a1 = Integers.rotateLeft(a1, 1);
+                a2 = Integers.rotateLeft(a2, 5);
+                a3 = Integers.rotateLeft(a3, 2);
+            }
+
+            // gamma(a);
+            {
+                a1 ^= ~a3 & ~a2;
+                a0 ^= a2 & a1;
+
+                t = a3; a3 = a0; a0 = t;
+                a2 ^= a0 ^ a1 ^ a3;
+
+                a1 ^= ~a3 & ~a2;
+                a0 ^= a2 & a1;
+            }
+
+            // pi2(a);
+            {
+                a1 = Integers.rotateLeft(a1, 31);
+                a2 = Integers.rotateLeft(a2, 27);
+                a3 = Integers.rotateLeft(a3, 30);
+            }
         }
-        
-        theta(state, decryptKeys);
-        state[0] ^= roundConstants[i];
-        
-        intToBytesBig(state[0], out, outOff);
-        intToBytesBig(state[1], out, outOff+4);
-        intToBytesBig(state[2], out, outOff+8);
-        intToBytesBig(state[3], out, outOff+12);
-        
-        return genericSize;
-    }
-        
-    private void gamma(int[] a)
-    {
-        a[1] ^= ~a[3] & ~a[2];
-        a[0] ^= a[2] & a[1];
-        
-        int tmp = a[3];
-        a[3]  = a[0];
-        a[0]  = tmp;
-        a[2] ^= a[0]^a[1]^a[3];
-        
-        a[1] ^= ~a[3] & ~a[2];
-        a[0] ^= a[2] & a[1];
-    }
-    
-    private void theta(int[] a, int[] k)
-    {
-        int tmp;
-        
-        tmp   = a[0]^a[2]; 
-        tmp  ^= rotl(tmp,8)^rotl(tmp,24); 
-        a[1] ^= tmp; 
-        a[3] ^= tmp; 
-        
-        for (int i = 0; i < 4; i++)
-        {
-            a[i] ^= k[i];
-        }
-        
-        tmp   = a[1]^a[3]; 
-        tmp  ^= rotl(tmp,8)^rotl(tmp,24); 
-        a[0] ^= tmp; 
-        a[2] ^= tmp;
-    }
-    
-    private void pi1(int[] a)
-    {
-        a[1] = rotl(a[1], 1);
-        a[2] = rotl(a[2], 5);
-        a[3] = rotl(a[3], 2);
-    }
-    
-    private void pi2(int[] a)
-    {
-        a[1] = rotl(a[1], 31);
-        a[2] = rotl(a[2], 27);
-        a[3] = rotl(a[3], 30);
-    }
-    
-    // Helpers
-    
-    private int bytesToIntBig(byte[] in, int off)
-    {
-        return ((in[off++]) << 24) |
-        ((in[off++] & 0xff) << 16) |
-        ((in[off++] & 0xff) <<  8) |
-         (in[off  ] & 0xff);
-    }
-    
-    private void intToBytesBig(int x, byte[] out, int off)
-    {
-        out[off++] = (byte)(x >>> 24);
-        out[off++] = (byte)(x >>> 16);
-        out[off++] = (byte)(x >>>  8);
-        out[off  ] = (byte)x;
-    }
-    
-    private int rotl(int x, int y)
-    {
-        return (x << y) | (x >>> (32-y));
+
+        Pack.intToBigEndian(a0, out, outOff);
+        Pack.intToBigEndian(a1, out, outOff + 4);
+        Pack.intToBigEndian(a2, out, outOff + 8);
+        Pack.intToBigEndian(a3, out, outOff + 12);
+
+        return SIZE;
     }
 }
