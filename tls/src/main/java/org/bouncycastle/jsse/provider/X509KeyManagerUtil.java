c1444ab821fa0465782ce23a0046f60710f5641a
Peter Dettman <peter.dettman@bouncycastle.org>
2020-10-14 22:04:13 +0700

    BCJSSE: Fix key manager import

--- tls/src/main/java/org/bouncycastle/jsse/provider/X509KeyManagerUtil.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/X509KeyManagerUtil.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.jsse.provider;
 
+import javax.net.ssl.X509ExtendedKeyManager;
 import javax.net.ssl.X509KeyManager;
 
 import org.bouncycastle.jcajce.util.JcaJceHelper;
@@ -7,6 +8,16 @@ import org.bouncycastle.jsse.BCX509ExtendedKeyManager;
 
 abstract class X509KeyManagerUtil
 {
+    static X509KeyManager exportX509KeyManager(BCX509ExtendedKeyManager x509KeyManager)
+    {
+        if (x509KeyManager instanceof ImportX509KeyManager)
+        {
+            return ((ImportX509KeyManager)x509KeyManager).unwrap();
+        }
+
+        return x509KeyManager;
+    }
+
     static BCX509ExtendedKeyManager importX509KeyManager(JcaJceHelper helper, X509KeyManager x509KeyManager)
     {
         if (x509KeyManager instanceof BCX509ExtendedKeyManager)
@@ -14,6 +25,11 @@ abstract class X509KeyManagerUtil
             return (BCX509ExtendedKeyManager)x509KeyManager;
         }
 
-        return new ImportX509KeyManager_5(helper, x509KeyManager);
+        if (x509KeyManager instanceof X509ExtendedKeyManager)
+        {
+            return new ImportX509KeyManager_5((X509ExtendedKeyManager)x509KeyManager);
+        }
+
+        return new ImportX509KeyManager_4(x509KeyManager);
     }
 }
