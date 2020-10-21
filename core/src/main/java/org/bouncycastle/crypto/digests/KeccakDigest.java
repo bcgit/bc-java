67e3bfa1f594a455e2346a0b1ee5b80d87ba67af
mw <megan@cryptoworkshop.com>
2020-10-21 16:26:30 +1100

    Ongoing implementation of ETSI.

--- core/src/main/java/org/bouncycastle/crypto/digests/KeccakDigest.java
+++ core/src/main/java/org/bouncycastle/crypto/digests/KeccakDigest.java
@@ -3,6 +3,7 @@ package org.bouncycastle.crypto.digests;
 import org.bouncycastle.crypto.ExtendedDigest;
 import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.Pack;
+import org.bouncycastle.util.encoders.Hex;
 
 /**
  * implementation of Keccak based on following KeccakNISTInterface.c from http://keccak.noekeon.org/
@@ -12,12 +13,12 @@ import org.bouncycastle.util.Pack;
 public class KeccakDigest
     implements ExtendedDigest
 {
-    private static long[] KeccakRoundConstants = new long[]{ 0x0000000000000001L, 0x0000000000008082L,
+    private static long[] KeccakRoundConstants = new long[]{0x0000000000000001L, 0x0000000000008082L,
         0x800000000000808aL, 0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L, 0x8000000080008081L,
         0x8000000000008009L, 0x000000000000008aL, 0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL,
         0x000000008000808bL, 0x800000000000008bL, 0x8000000000008089L, 0x8000000000008003L, 0x8000000000008002L,
         0x8000000000000080L, 0x000000000000800aL, 0x800000008000000aL, 0x8000000080008081L, 0x8000000000008080L,
-        0x0000000080000001L, 0x8000000080008008L };
+        0x0000000080000001L, 0x8000000080008008L};
 
     protected long[] state = new long[25];
     protected byte[] dataQueue = new byte[192];
@@ -35,6 +36,26 @@ public class KeccakDigest
     {
         init(bitLength);
     }
+
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        KeccakDigest dig = new KeccakDigest(256);
+        byte[] res = new byte[dig.getDigestSize()];
+
+        byte[] m = new byte[2041];
+        for (int t=0; t<m.length;t++) {
+            m[t] = (byte)t;
+        }
+        dig.update(m, 0, m.length);
+
+        dig.doFinal(res, 0);
+
+        String r = Hex.toHexString(res);
+        System.out.println(r);
+    }
+
 
     public KeccakDigest(KeccakDigest source)
     {
@@ -224,6 +245,21 @@ public class KeccakDigest
         bitsInQueue += bits;
     }
 
+
+    protected byte[] dumpState()
+    {
+        byte[] out = new byte[state.length * 8];
+        int p = 0;
+        for (long item : state)
+        {
+            Pack.longToLittleEndian(item, out, p);
+            p += 8;
+        }
+
+        return out;
+    }
+
+
     private void padAndSwitchToSqueezingPhase()
     {
         dataQueue[bitsInQueue >>> 3] |= (byte)(1 << (bitsInQueue & 7));
@@ -241,6 +277,9 @@ public class KeccakDigest
                 state[i] ^= Pack.littleEndianToLong(dataQueue, off);
                 off += 8;
             }
+
+            byte[] z = dumpState();
+
             if (partial > 0)
             {
                 long mask = (1L << partial) - 1L;
@@ -260,6 +299,9 @@ public class KeccakDigest
         {
             padAndSwitchToSqueezingPhase();
         }
+
+        byte[] z = dumpState();
+
         if ((outputLength % 8) != 0)
         {
             throw new IllegalStateException("outputLength not a multiple of 8");
@@ -277,6 +319,9 @@ public class KeccakDigest
             bitsInQueue -= partialBlock;
             i += partialBlock;
         }
+
+        z = dumpState();
+
     }
 
     private void KeccakAbsorb(byte[] data, int off)
@@ -289,7 +334,7 @@ public class KeccakDigest
             state[i] ^= Pack.littleEndianToLong(data, off);
             off += 8;
         }
-
+        String z = Hex.toHexString(dumpState()).toLowerCase();
         KeccakPermutation();
     }
 
@@ -298,6 +343,8 @@ public class KeccakDigest
 //        assert 0 == bitsInQueue;
 
         KeccakPermutation();
+
+        byte[] z = dumpState();
 
         Pack.longToLittleEndian(state, 0, rate >>> 6, dataQueue, 0);
 
@@ -308,8 +355,8 @@ public class KeccakDigest
     {
         long[] A = state;
 
-        long a00 = A[ 0], a01 = A[ 1], a02 = A[ 2], a03 = A[ 3], a04 = A[ 4];
-        long a05 = A[ 5], a06 = A[ 6], a07 = A[ 7], a08 = A[ 8], a09 = A[ 9];
+        long a00 = A[0], a01 = A[1], a02 = A[2], a03 = A[3], a04 = A[4];
+        long a05 = A[5], a06 = A[6], a07 = A[7], a08 = A[8], a09 = A[9];
         long a10 = A[10], a11 = A[11], a12 = A[12], a13 = A[13], a14 = A[14];
         long a15 = A[15], a16 = A[16], a17 = A[17], a18 = A[18], a19 = A[19];
         long a20 = A[20], a21 = A[21], a22 = A[22], a23 = A[23], a24 = A[24];
@@ -329,37 +376,57 @@ public class KeccakDigest
             long d4 = (c4 << 1 | c4 >>> -1) ^ c2;
             long d0 = (c0 << 1 | c0 >>> -1) ^ c3;
 
-            a00 ^= d1; a05 ^= d1; a10 ^= d1; a15 ^= d1; a20 ^= d1;
-            a01 ^= d2; a06 ^= d2; a11 ^= d2; a16 ^= d2; a21 ^= d2;
-            a02 ^= d3; a07 ^= d3; a12 ^= d3; a17 ^= d3; a22 ^= d3;
-            a03 ^= d4; a08 ^= d4; a13 ^= d4; a18 ^= d4; a23 ^= d4;
-            a04 ^= d0; a09 ^= d0; a14 ^= d0; a19 ^= d0; a24 ^= d0;
+            a00 ^= d1;
+            a05 ^= d1;
+            a10 ^= d1;
+            a15 ^= d1;
+            a20 ^= d1;
+            a01 ^= d2;
+            a06 ^= d2;
+            a11 ^= d2;
+            a16 ^= d2;
+            a21 ^= d2;
+            a02 ^= d3;
+            a07 ^= d3;
+            a12 ^= d3;
+            a17 ^= d3;
+            a22 ^= d3;
+            a03 ^= d4;
+            a08 ^= d4;
+            a13 ^= d4;
+            a18 ^= d4;
+            a23 ^= d4;
+            a04 ^= d0;
+            a09 ^= d0;
+            a14 ^= d0;
+            a19 ^= d0;
+            a24 ^= d0;
 
             // rho/pi
-            c1  = a01 <<  1 | a01 >>> 63;
+            c1 = a01 << 1 | a01 >>> 63;
             a01 = a06 << 44 | a06 >>> 20;
             a06 = a09 << 20 | a09 >>> 44;
-            a09 = a22 << 61 | a22 >>>  3;
+            a09 = a22 << 61 | a22 >>> 3;
             a22 = a14 << 39 | a14 >>> 25;
             a14 = a20 << 18 | a20 >>> 46;
-            a20 = a02 << 62 | a02 >>>  2;
+            a20 = a02 << 62 | a02 >>> 2;
             a02 = a12 << 43 | a12 >>> 21;
             a12 = a13 << 25 | a13 >>> 39;
-            a13 = a19 <<  8 | a19 >>> 56;
-            a19 = a23 << 56 | a23 >>>  8;
+            a13 = a19 << 8 | a19 >>> 56;
+            a19 = a23 << 56 | a23 >>> 8;
             a23 = a15 << 41 | a15 >>> 23;
             a15 = a04 << 27 | a04 >>> 37;
             a04 = a24 << 14 | a24 >>> 50;
-            a24 = a21 <<  2 | a21 >>> 62;
-            a21 = a08 << 55 | a08 >>>  9;
+            a24 = a21 << 2 | a21 >>> 62;
+            a21 = a08 << 55 | a08 >>> 9;
             a08 = a16 << 45 | a16 >>> 19;
             a16 = a05 << 36 | a05 >>> 28;
             a05 = a03 << 28 | a03 >>> 36;
             a03 = a18 << 21 | a18 >>> 43;
             a18 = a17 << 15 | a17 >>> 49;
             a17 = a11 << 10 | a11 >>> 54;
-            a11 = a07 <<  6 | a07 >>> 58;
-            a07 = a10 <<  3 | a10 >>> 61;
+            a11 = a07 << 6 | a07 >>> 58;
+            a07 = a10 << 3 | a10 >>> 61;
             a10 = c1;
 
             // chi
@@ -406,11 +473,31 @@ public class KeccakDigest
             // iota
             a00 ^= KeccakRoundConstants[i];
         }
-        
-        A[ 0] = a00; A[ 1] = a01; A[ 2] = a02; A[ 3] = a03; A[ 4] = a04;
-        A[ 5] = a05; A[ 6] = a06; A[ 7] = a07; A[ 8] = a08; A[ 9] = a09;
-        A[10] = a10; A[11] = a11; A[12] = a12; A[13] = a13; A[14] = a14;
-        A[15] = a15; A[16] = a16; A[17] = a17; A[18] = a18; A[19] = a19;
-        A[20] = a20; A[21] = a21; A[22] = a22; A[23] = a23; A[24] = a24;
+
+        A[0] = a00;
+        A[1] = a01;
+        A[2] = a02;
+        A[3] = a03;
+        A[4] = a04;
+        A[5] = a05;
+        A[6] = a06;
+        A[7] = a07;
+        A[8] = a08;
+        A[9] = a09;
+        A[10] = a10;
+        A[11] = a11;
+        A[12] = a12;
+        A[13] = a13;
+        A[14] = a14;
+        A[15] = a15;
+        A[16] = a16;
+        A[17] = a17;
+        A[18] = a18;
+        A[19] = a19;
+        A[20] = a20;
+        A[21] = a21;
+        A[22] = a22;
+        A[23] = a23;
+        A[24] = a24;
     }
 }
---
+++ pkix/src/main/java/org/bouncycastle/its/asn1/SspRange.java
@@ -0,0 +1,169 @@
+package org.bouncycastle.its.asn1;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Null;
+import org.bouncycastle.asn1.ASN1Object;
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DERNull;
+
+public class SspRange extends ASN1Object
+{
+    private ASN1Sequence opaque;
+    private ASN1Null all;
+    private BitmapSspRange bitmapSspRange;
+
+    public SspRange getInstance(Object src)
+    {
+        if (src == null)
+        {
+            return null;
+        }
+        else if (src instanceof SspRange)
+        {
+            return (SspRange)src;
+        }
+        else if (src instanceof ASN1InputStream)
+        {
+            try
+            {
+                SspRange range = new SspRange();
+                Object o = ((ASN1InputStream)src).readObject();
+                if (o instanceof ASN1Null)
+                {
+                    range.setAll();
+                }
+                else if (o instanceof ASN1Sequence)
+                {
+                    if (((ASN1Sequence)o).size() == 2)
+                    {
+
+                        /*
+                        There is ambiguity here:
+
+                        SspRange ::= CHOICE {
+                            opaque SequenceOfOctetString,
+                            all NULL,
+                            ... ,
+                        bitmapSspRange BitmapSspRange
+                        }
+
+                        BitmapSspRange ::= SEQUENCE {
+                            sspValue OCTET STRING (SIZE(1..32)),
+                            sspBitmask OCTET STRING (SIZE(1..32))
+                        }
+                        SequenceOfOctetString ::= SEQUENCE (SIZE (0..MAX)) OF
+                         OCTET STRING (SIZE(0..MAX))
+
+                       SequenceOfOctetString could be confused with BitmapSspRange so if there are two
+                       octet strings and they are less than 32 elements we set both "opaque" and BitMapSspRange.
+
+                         */
+
+                        for (int t = 0; t < ((ASN1Sequence)o).size(); t++)
+                        {
+                            ASN1Encodable item = ((ASN1Sequence)o).getObjectAt(t);
+                            if (item instanceof ASN1OctetString)
+                            {
+                                if (((ASN1OctetString)item).getOctets().length > 32)
+                                {
+                                    // Outside of range for BitmapSspRange so assume opaque
+                                    range.setOpaque((ASN1Sequence)o);
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                throw new IllegalArgumentException("inner sequence does not contain octet string");
+                            }
+                        }
+
+                        //
+                        // Opaque was not set so assume both.
+                        //
+                        if (opaque == null)
+                        {
+                            range.opaque = (ASN1Sequence)o;
+                            range.bitmapSspRange = BitmapSspRange.getInstance(o);
+                        }
+
+                    }
+                    else
+                    {
+                        range.setOpaque((ASN1Sequence)o);
+                    }
+                }
+                return range;
+            }
+            catch (IOException e)
+            {
+                throw new IllegalStateException(e.getMessage(), e);
+            }
+
+        } else if (src instanceof byte[]) {
+            return getInstance(new ByteArrayInputStream((byte[])src));
+        } else if (src instanceof InputStream) {
+            return getInstance(new ASN1InputStream((InputStream)src));
+        }
+
+        throw new IllegalStateException("Unable to parse SspRange");
+    }
+
+    public ASN1Sequence getOpaque()
+    {
+        return opaque;
+    }
+
+    public void setOpaque(ASN1Sequence opaque)
+    {
+        this.opaque = opaque;
+        this.all = null;
+        this.bitmapSspRange = null;
+    }
+
+    public ASN1Null getAll()
+    {
+        return all;
+    }
+
+    public void setAll()
+    {
+        this.all = DERNull.INSTANCE;
+        this.opaque = null;
+        this.bitmapSspRange = null;
+    }
+
+
+    public BitmapSspRange getBitmapSspRange()
+    {
+        return bitmapSspRange;
+    }
+
+    public void setBitmapSspRange(BitmapSspRange bitmapSspRange)
+    {
+        this.bitmapSspRange = bitmapSspRange;
+        this.all = null;
+        this.opaque = null;
+    }
+
+    public ASN1Primitive toASN1Primitive()
+    {
+        if (opaque != null) {
+            return opaque;
+        }
+        if (all != null) {
+            return all;
+        }
+
+        if (bitmapSspRange != null) {
+            return bitmapSspRange.toASN1Primitive();
+        }
+        throw new IllegalStateException("SspRange has no value");
+    }
+}
