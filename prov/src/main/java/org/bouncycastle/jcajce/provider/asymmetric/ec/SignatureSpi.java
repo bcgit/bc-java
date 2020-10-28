7b823088fbfcab867be6749e84682b3556d15aab
David Hook <dgh@cryptoworkshop.com>
2020-10-28 17:07:03 +1100

    github #770 added null return for engineGetParameters()

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/ec/SignatureSpi.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/ec/SignatureSpi.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.jcajce.provider.asymmetric.ec;
 
+import java.security.AlgorithmParameters;
 import java.security.InvalidKeyException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -55,6 +56,11 @@ public class SignatureSpi
         }
     }
 
+    protected AlgorithmParameters engineGetParameters()
+    {
+        return null;
+    }
+    
     static public class ecDSA
         extends SignatureSpi
     {
