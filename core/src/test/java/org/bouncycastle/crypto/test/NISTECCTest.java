aceecf06e1976f2abcc5cf8527b1923979662d97
Peter Dettman <peter.dettman@bouncycastle.org>
2020-10-28 14:16:46 +0700

    Tidy up NIST ECC tests PR:
    - formatting
    - support early Java versions
    - check custom curves too

--- core/src/test/java/org/bouncycastle/crypto/test/NISTECCTest.java
+++ core/src/test/java/org/bouncycastle/crypto/test/NISTECCTest.java
@@ -2,6 +2,7 @@ package org.bouncycastle.crypto.test;
 
 import org.bouncycastle.asn1.nist.NISTNamedCurves;
 import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.ec.CustomNamedCurves;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.util.test.SimpleTest;
 
@@ -13,69 +14,95 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NISTECCTest
-        extends SimpleTest
+    extends SimpleTest
 {
-
     public String getName()
     {
         return "NISTECC";
     }
 
-    public void performTest()
-            throws Exception
+    public void performTest() throws Exception
     {
         testVectors();
     }
 
-    public void testVectors()
+    public void testVectors() throws IOException
     {
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(getClass().getResourceAsStream("nist_ecc.txt")))) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("nist_ecc.txt")));
+        try
+        {
             String line = br.readLine();
             X9ECParameters curve = null;
+            X9ECParameters curveCustom = null;
             BigInteger k = null;
             BigInteger x = null;
             BigInteger y = null;
 
-            while (line != null) {
-                while (null != (line = br.readLine())) {
+            while (line != null)
+            {
+                while (null != (line = br.readLine()))
+                {
                     Matcher matcher = Pattern.compile("^ ?(\\w+):? =? ?(\\w+)").matcher(line);
-                    if (!matcher.matches()) continue;
+                    if (!matcher.matches())
+                    {
+                        continue;
+                    }
 
                     String nistKey = matcher.group(1);
                     String nistValue = matcher.group(2);
 
-                    switch (nistKey) {
-                        case "Curve":
-                            // Change curve name from LNNN to L-NNN ie: P256 to P-256
-                            String curveName = nistValue.charAt(0) + "-" + nistValue.substring(1);
-                            curve = NISTNamedCurves.getByName(curveName);
-                            break;
-                        case "k":
-                            k = new BigInteger(nistValue, 10);
-                            break;
-                        case "x":
-                            x = new BigInteger(nistValue, 16);
-                            break;
-                        case "y":
-                            y = new BigInteger(nistValue,  16);
-                            break;
+                    if ("Curve".equals(nistKey))
+                    {
+                        // Change curve name from LNNN to L-NNN ie: P256 to P-256
+                        String curveName = nistValue.charAt(0) + "-" + nistValue.substring(1);
+                        curve = NISTNamedCurves.getByName(curveName);
+                        curveCustom = CustomNamedCurves.getByName(curveName);
+                    }
+                    else if ("k".equals(nistKey))
+                    {
+                        k = new BigInteger(nistValue, 10);
+                    }
+                    else if ("x".equals(nistKey))
+                    {
+                        x = new BigInteger(nistValue, 16);
+                    }
+                    else if ("y".equals(nistKey))
+                    {
+                        y = new BigInteger(nistValue, 16);
                     }
 
-                    if (curve == null || k == null || x == null || y == null) continue;
+                    if (k == null || x == null || y == null)
+                    {
+                        continue;
+                    }
 
-                    TestMultiply(curve, k, x, y);
+                    if (curve != null)
+                    {
+                        implTestMultiply(curve, k, x, y);
+                    }
+                    if (curveCustom != null)
+                    {
+                        implTestMultiply(curveCustom, k, x, y);
+                    }
+
                     k = null;
                     x = null;
                     y = null;
                 }
             }
-        } catch (IOException exception) {
+        }
+        catch (IOException exception)
+        {
             fail("Failed to load resources.", exception);
+        }
+        finally
+        {
+            br.close();
         }
     }
 
-    private void TestMultiply(X9ECParameters curve, BigInteger k, BigInteger x, BigInteger y) {
+    private void implTestMultiply(X9ECParameters curve, BigInteger k, BigInteger x, BigInteger y)
+    {
         // Act
         ECPoint ecPoint = curve.getG().multiply(k).normalize();
         BigInteger affineXCoord = ecPoint.getAffineXCoord().toBigInteger();
