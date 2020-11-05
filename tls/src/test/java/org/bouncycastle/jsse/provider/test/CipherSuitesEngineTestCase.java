781a647d64eb0fc7d8f422e733201fe504f25b42
Peter Dettman <peter.dettman@bouncycastle.org>
2020-11-05 13:43:10 +0700

    BCJSSE: SSLEngine-based handshake tests

---
+++ tls/src/test/java/org/bouncycastle/jsse/provider/test/CipherSuitesEngineTestCase.java
@@ -0,0 +1,192 @@
+package org.bouncycastle.jsse.provider.test;
+
+import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+import javax.net.ssl.SSLEngineResult.HandshakeStatus;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManagerFactory;
+
+import junit.framework.TestCase;
+
+public class CipherSuitesEngineTestCase extends TestCase
+{
+    private static String getName(CipherSuitesTestConfig config)
+    {
+        String category = config.category;
+        String prefix = (null == category || category.length() < 1)
+            ?   ""
+            :   (category + " ");
+
+        return prefix + config.protocol + " : " + config.cipherSuite;
+    }
+
+    protected final CipherSuitesTestConfig config;
+
+    public CipherSuitesEngineTestCase(String name)
+    {
+        super(name);
+
+        this.config = null;
+    }
+
+    public CipherSuitesEngineTestCase(CipherSuitesTestConfig config)
+    {
+        super(getName(config));
+
+        this.config = config;
+    }
+
+    protected void setUp()
+    {
+        if (config != null)
+        {
+            ProviderUtils.setupHighPriority(config.fips);
+        }
+    }
+
+    public void testDummy()
+    {
+        // Avoid "No tests found" warning from junit
+    }
+
+    protected void runTest() throws Throwable
+    {
+        // Disable the test if it is not being run via CipherSuitesTestSuite
+        if (config == null)
+        {
+            return;
+        }
+
+        SSLContext clientContext = createSSLContextClient();
+        SSLContext serverContext = createSSLContextServer();
+
+        SSLEngine clientEngine = clientContext.createSSLEngine();
+        clientEngine.setEnabledCipherSuites(new String[]{ config.cipherSuite });
+        clientEngine.setEnabledProtocols(new String[]{ config.protocol });
+        clientEngine.setUseClientMode(true);
+
+        SSLEngine serverEngine = serverContext.createSSLEngine();
+        serverEngine.setEnabledCipherSuites(new String[]{ config.cipherSuite });
+        serverEngine.setEnabledProtocols(new String[]{ config.protocol });
+        serverEngine.setUseClientMode(false);
+        serverEngine.setWantClientAuth(false);
+
+        SSLSession clientSession = clientEngine.getSession();
+        SSLSession serverSession = serverEngine.getSession();
+
+        final int clientAppBufSize = clientSession.getApplicationBufferSize();
+        final int serverAppBufSize = serverSession.getApplicationBufferSize();
+
+        final int clientNetBufSize = clientSession.getPacketBufferSize();
+        final int serverNetBufSize = serverSession.getPacketBufferSize();
+
+        ByteBuffer clientIn = ByteBuffer.allocate(clientAppBufSize + 64);
+        ByteBuffer serverIn = ByteBuffer.allocate(serverAppBufSize + 64);
+
+        ByteBuffer clientToServer = ByteBuffer.allocate(clientNetBufSize);
+        ByteBuffer serverToClient = ByteBuffer.allocate(serverNetBufSize);
+
+        ByteBuffer clientOut = ByteBuffer.wrap("Dear Prudence, won't you come out to play?".getBytes("UTF-8"));
+        ByteBuffer serverOut = ByteBuffer.wrap("Impudence! I won't come out to today.".getBytes("UTF-8"));
+
+        SSLEngineResult clientResult;
+        SSLEngineResult serverResult;
+
+        boolean dataDone = false;
+        while (!isEngineClosed(clientEngine) || !isEngineClosed(serverEngine))
+        {
+            clientResult = clientEngine.wrap(clientOut, clientToServer);
+            runDelegatedTasks(clientEngine, clientResult);
+
+            serverResult = serverEngine.wrap(serverOut, serverToClient);
+            runDelegatedTasks(serverEngine, serverResult);
+
+            clientToServer.flip();
+            serverToClient.flip();
+
+            clientResult = clientEngine.unwrap(serverToClient, clientIn);
+            runDelegatedTasks(clientEngine, clientResult);
+
+            serverResult = serverEngine.unwrap(clientToServer, serverIn);
+            runDelegatedTasks(serverEngine, serverResult);
+
+            clientToServer.compact();
+            serverToClient.compact();
+
+            if (!dataDone && (clientOut.limit() == serverIn.position()) && (serverOut.limit() == clientIn.position()))
+            {
+                checkData(clientOut, serverIn);
+                checkData(serverOut, clientIn);
+
+                clientEngine.closeOutbound();
+                // engineServer.closeOutbound();
+
+                dataDone = true;
+            }
+        }
+    }
+
+    private static void checkData(ByteBuffer a, ByteBuffer b) throws Exception
+    {
+        a.flip();
+        b.flip();
+
+        assertEquals(a, b);
+
+        a.position(a.limit());
+        b.position(b.limit());
+        a.limit(a.capacity());
+        b.limit(b.capacity());
+    }
+
+    private SSLContext createSSLContextClient() throws GeneralSecurityException
+    {
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX", ProviderUtils.PROVIDER_NAME_BCJSSE);
+        tmf.init(config.clientTrustStore);
+
+        SecureRandom random = SecureRandom.getInstance("DEFAULT", ProviderUtils.PROVIDER_NAME_BC);
+
+        SSLContext clientContext = SSLContext.getInstance("TLS", ProviderUtils.PROVIDER_NAME_BCJSSE);
+        clientContext.init(null, tmf.getTrustManagers(), random);
+        return clientContext;
+    }
+
+    private SSLContext createSSLContextServer() throws GeneralSecurityException
+    {
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("PKIX", ProviderUtils.PROVIDER_NAME_BCJSSE);
+        kmf.init(config.serverKeyStore, config.serverPassword);
+
+        SecureRandom random = SecureRandom.getInstance("DEFAULT", ProviderUtils.PROVIDER_NAME_BC);
+
+        SSLContext serverContext = SSLContext.getInstance("TLS", ProviderUtils.PROVIDER_NAME_BCJSSE);
+        serverContext.init(kmf.getKeyManagers(), null, random);
+        return serverContext;
+    }
+
+    private static boolean isEngineClosed(SSLEngine engine)
+    {
+        return (engine.isOutboundDone() && engine.isInboundDone());
+    }
+
+    private static void runDelegatedTasks(SSLEngine engine, SSLEngineResult result)
+    {
+        if (HandshakeStatus.NEED_TASK != result.getHandshakeStatus())
+        {
+            return;
+        }
+
+        Runnable runnable;
+        while ((runnable = engine.getDelegatedTask()) != null)
+        {
+            runnable.run();
+        }
+
+        assertTrue(HandshakeStatus.NEED_TASK != engine.getHandshakeStatus());
+    }
+}
---
+++ tls/src/test/java/org/bouncycastle/jsse/provider/test/CipherSuitesEngineTestSuite.java
@@ -0,0 +1,152 @@
+package org.bouncycastle.jsse.provider.test;
+
+import static org.junit.Assert.fail;
+
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLContext;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class CipherSuitesEngineTestSuite
+    extends TestSuite
+{
+    public CipherSuitesEngineTestSuite()
+    {
+        super("CipherSuites : SSLEngine");
+    }
+
+    public static Test suite()
+        throws Exception
+    {
+        return createSuite(new CipherSuitesEngineTestSuite(), null, false, new CipherSuitesFilter()
+        {
+            public boolean isIgnored(String cipherSuite)
+            {
+                /*
+                 * TODO[jsse] jdk.tls.disabledAlgorithms default value doesn't permit these. Perhaps
+                 * we could modify that security property when running this test suite.
+                 */
+                return cipherSuite.contains("_WITH_NULL_") || cipherSuite.contains("_WITH_3DES_EDE_CBC_");
+            }
+
+            public boolean isPermitted(String cipherSuite)
+            {
+                return true;
+            }
+        });
+    }
+
+    static Test createSuite(TestSuite testSuite, String category, boolean fips, CipherSuitesFilter filter)
+        throws Exception
+    {
+        // TODO Consider configuring BCJSSE with explicit crypto provider (maybe only when in fips mode?)
+        ProviderUtils.setupHighPriority(fips);
+
+        char[] serverPassword = "serverPassword".toCharArray();
+
+        KeyPair caKeyPairDSA = TestUtils.generateDSAKeyPair();
+        KeyPair caKeyPairEC = TestUtils.generateECKeyPair();
+        KeyPair caKeyPairRSA = TestUtils.generateRSAKeyPair();
+
+        X509Certificate caCertDSA = TestUtils.generateRootCert(caKeyPairDSA);
+        X509Certificate caCertEC = TestUtils.generateRootCert(caKeyPairEC);
+        X509Certificate caCertRSA = TestUtils.generateRootCert(caKeyPairRSA);
+
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(null, null);
+        ks.setKeyEntry("serverDSA", caKeyPairDSA.getPrivate(), serverPassword, new X509Certificate[]{ caCertDSA });
+        ks.setKeyEntry("serverEC", caKeyPairEC.getPrivate(), serverPassword, new X509Certificate[]{ caCertEC });
+        ks.setKeyEntry("serverRSA", caKeyPairRSA.getPrivate(), serverPassword, new X509Certificate[]{ caCertRSA });
+
+        KeyStore ts = KeyStore.getInstance("JKS");
+        ts.load(null, null);
+        ts.setCertificateEntry("caDSA", caCertDSA);
+        ts.setCertificateEntry("caEC", caCertEC);
+        ts.setCertificateEntry("caRSA", caCertRSA);
+
+        SSLContext defaultSSLContext = SSLContext.getInstance("Default", ProviderUtils.PROVIDER_NAME_BCJSSE);
+
+        String[] cipherSuites = defaultSSLContext.getSocketFactory().getSupportedCipherSuites();
+        for (int i = 0; i < cipherSuites.length; ++i)
+        {
+            if (!filter.isPermitted(cipherSuites[i]))
+            {
+                fail("Cipher suite not permitted in supported cipher suites: " + cipherSuites[i]);
+            }
+        }
+        Arrays.sort(cipherSuites);
+
+        /*
+         * TODO[jsse] jdk.tls.disabledAlgorithms default value doesn't permit SSLv3. Perhaps we
+         * could modify that security property when running this test suite.
+         */
+        // NOTE: Avoid defaultSSLContext.getSupportedSSLParameters() for 1.5 compatibility
+        String[] protocols = new String[]{
+            "TLSv1",
+            "TLSv1.1",
+            "TLSv1.2",
+            "TLSv1.3",
+        };
+
+        for (int p = 0; p < protocols.length; ++p)
+        {
+            String protocol = protocols[p];
+
+            boolean isTLSv13Protocol = "TLSv1.3".equals(protocol);
+            boolean isTLSv12Protocol = "TLSv1.2".equals(protocol);
+
+            for (int t = 0; t < cipherSuites.length; t++)
+            {
+                String cipherSuite = cipherSuites[t];
+                if (filter.isIgnored(cipherSuite))
+                {
+                    continue;
+                }
+
+                boolean isTLSv13CipherSuite = !cipherSuite.contains("_WITH_");
+                if (isTLSv13CipherSuite != isTLSv13Protocol)
+                {
+                    // TLS 1.3 uses a distinct set of cipher suites that don't specify a key exchange
+                    continue;
+                }
+
+                boolean isTLSv12CipherSuite = !isTLSv13CipherSuite
+                    && (cipherSuite.contains("_CHACHA20_POLY1305_") ||
+                        cipherSuite.contains("_GCM_") ||
+                        cipherSuite.endsWith("_CBC_SHA256") ||
+                        cipherSuite.endsWith("_CBC_SHA384") ||
+                        cipherSuite.endsWith("_CCM") ||
+                        cipherSuite.endsWith("_CCM_8"));
+                if (isTLSv12CipherSuite && !isTLSv12Protocol)
+                {
+                    //  AEAD ciphers and configurable CBC PRFs are both 1.2 features
+                    continue;
+                }
+
+                /*
+                 * TODO[jsse] Note that there may be failures for cipher suites that are listed as supported
+                 * even though the TlsCrypto instance doesn't implement them (JcaTlsCrypto is dependent on the
+                 * configured crypto providers).
+                 */
+
+                CipherSuitesTestConfig config = new CipherSuitesTestConfig();
+                config.category = category;
+                config.cipherSuite = cipherSuite;
+                config.clientTrustStore = ts;
+                config.fips = fips;
+                config.protocol = protocol;
+                config.serverKeyStore = ks;
+                config.serverPassword = serverPassword;
+
+                testSuite.addTest(new CipherSuitesEngineTestCase(config));
+            }
+        }
+
+        return testSuite;
+    }
+}
