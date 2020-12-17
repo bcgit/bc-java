3848c7b11fec65cdbf423b443c27d508d05c9289
David Hook <dgh@cryptoworkshop.com>
2020-12-18 08:10:33 +1100

    tweaked use of DER set in signer collection

--- pkix/src/main/java/org/bouncycastle/cms/CMSSignedData.java
+++ pkix/src/main/java/org/bouncycastle/cms/CMSSignedData.java
@@ -22,6 +22,7 @@ import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1Set;
 import org.bouncycastle.asn1.BERSequence;
 import org.bouncycastle.asn1.DERSet;
+import org.bouncycastle.asn1.DLSet;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.asn1.cms.SignedData;
 import org.bouncycastle.asn1.cms.SignerInfo;
@@ -518,7 +519,7 @@ public class CMSSignedData
         }
 
         ASN1Set             digests = new DERSet(digestAlgs);
-        ASN1Set             signers = new DERSet(vec);
+        ASN1Set             signers = new DLSet(vec);
         ASN1Sequence        sD = (ASN1Sequence)signedData.signedData.toASN1Primitive();
 
         vec = new ASN1EncodableVector();
