c2dbef0d3aa355c792147e8729815ef258b851e2
David Hook <dgh@cryptoworkshop.com>
2021-02-14 15:04:06 +1100

    JavaDoc, added constants to Shorts.

--- core/src/main/java/org/bouncycastle/util/Shorts.java
+++ core/src/main/java/org/bouncycastle/util/Shorts.java
@@ -1,7 +1,13 @@
 package org.bouncycastle.util;
 
+/**
+ * Utility methods and constants for shorts.
+ */
 public class Shorts
 {
+    public static final int BYTES = Short.BYTES;
+    public static final int SIZE = Short.SIZE;
+
     public static Short valueOf(short value)
     {
         return Short.valueOf(value);
