b646b32894568fb562e446dc56156bfd377ecf5f
David Hook <dgh@cryptoworkshop.com>
2021-02-14 14:52:08 +1100

    fixed typo!

--- core/src/main/java/org/bouncycastle/util/Bytes.java
+++ core/src/main/java/org/bouncycastle/util/Bytes.java
@@ -5,6 +5,6 @@ package org.bouncycastle.util;
  */
 public class Bytes
 {
-    public static final int BYTES = 4;
-    public static final int SIZE = 8;
+    public static final int BYTES = Byte.BYTES;
+    public static final int SIZE = Byte.SIZE;
 }
