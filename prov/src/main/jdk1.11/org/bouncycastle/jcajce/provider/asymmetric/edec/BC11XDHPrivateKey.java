3641da173450297c1ad6cc9f9305f2e297656ac0
Peter Dettman <peter.dettman@bouncycastle.org>
2021-02-14 15:37:34 +0700

    Return correct public key type

--- prov/src/main/jdk1.11/org/bouncycastle/jcajce/provider/asymmetric/edec/BC11XDHPrivateKey.java
+++ prov/src/main/jdk1.11/org/bouncycastle/jcajce/provider/asymmetric/edec/BC11XDHPrivateKey.java
@@ -1,25 +1,16 @@
 package org.bouncycastle.jcajce.provider.asymmetric.edec;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.security.interfaces.XECPrivateKey;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.NamedParameterSpec;
 import java.util.Optional;
 
-import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1OctetString;
-import org.bouncycastle.asn1.ASN1Set;
-import org.bouncycastle.asn1.edec.EdECObjectIdentifiers;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.X25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.X448PrivateKeyParameters;
-import org.bouncycastle.crypto.util.PrivateKeyInfoFactory;
-import org.bouncycastle.jcajce.interfaces.XDHPrivateKey;
 import org.bouncycastle.jcajce.interfaces.XDHPublicKey;
-import org.bouncycastle.util.Arrays;
 
 class BC11XDHPrivateKey
     extends BCXDHPrivateKey
@@ -45,6 +36,19 @@ class BC11XDHPrivateKey
         else
         {
             return NamedParameterSpec.X25519;
+        }
+    }
+
+    @Override
+    public XDHPublicKey getPublicKey()
+    {
+        if (xdhPrivateKey instanceof X448PrivateKeyParameters)
+        {
+            return new BC11XDHPublicKey(((X448PrivateKeyParameters)xdhPrivateKey).generatePublicKey());
+        }
+        else
+        {
+            return new BC11XDHPublicKey(((X25519PrivateKeyParameters)xdhPrivateKey).generatePublicKey());
         }
     }
 
