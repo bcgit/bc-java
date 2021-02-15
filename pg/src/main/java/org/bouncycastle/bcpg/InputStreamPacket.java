96df988f25ebdc869867f351f38c0d507ab03ea7
Paul Schaub <vanitasvitae@fsfe.org>
2021-02-15 21:03:08 +0100

    Properly rewind streams for messages with multiple SKESK packets

--- pg/src/main/java/org/bouncycastle/bcpg/InputStreamPacket.java
+++ pg/src/main/java/org/bouncycastle/bcpg/InputStreamPacket.java
@@ -1,27 +1,27 @@
 package org.bouncycastle.bcpg;
 
+import java.io.BufferedInputStream;
+
 /**
  * A block of data associated with other packets in a PGP object stream.
  */
 public class InputStreamPacket
     extends Packet
 {
-    private BCPGInputStream        in;
+    private BufferedInputStream        in;
 
     public InputStreamPacket(
         BCPGInputStream  in)
     {
-        this.in = in;
+        this.in = new BufferedInputStream(in);
     }
 
     /**
      * Obtains an input stream to read the contents of the packet.
-     * <p>
-     * Note: you can only read from this once...
-     * </p>
+     *
      * @return the data in this packet.
      */
-    public BCPGInputStream getInputStream()
+    public BufferedInputStream getInputStream()
     {
         return in;
     }
