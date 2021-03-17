7b66244e14488c5c981415b02ff71837ff3d2d50
David Hook <dgh@cryptoworkshop.com>
2021-03-18 02:36:18 +1100

    introduced property guard around BKS version 1.

--- prov/src/main/java/org/bouncycastle/jcajce/provider/keystore/BC.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/keystore/BC.java
@@ -2,6 +2,7 @@ package org.bouncycastle.jcajce.provider.keystore;
 
 import org.bouncycastle.jcajce.provider.config.ConfigurableProvider;
 import org.bouncycastle.jcajce.provider.util.AsymmetricAlgorithmProvider;
+import org.bouncycastle.util.Properties;
 
 public class BC
 {
@@ -17,7 +18,12 @@ public class BC
         public void configure(ConfigurableProvider provider)
         {
             provider.addAlgorithm("KeyStore.BKS", PREFIX + "BcKeyStoreSpi$Std");
-            provider.addAlgorithm("KeyStore.BKS-V1", PREFIX + "BcKeyStoreSpi$Version1");
+
+            if (Properties.isOverrideSet("org.bouncycastle.bks.enable_v1"))
+            {
+                provider.addAlgorithm("KeyStore.BKS-V1", PREFIX + "BcKeyStoreSpi$Version1");
+            }
+            
             provider.addAlgorithm("KeyStore.BouncyCastle", PREFIX + "BcKeyStoreSpi$BouncyCastleStore");
             provider.addAlgorithm("Alg.Alias.KeyStore.UBER", "BouncyCastle");
             provider.addAlgorithm("Alg.Alias.KeyStore.BOUNCYCASTLE", "BouncyCastle");
