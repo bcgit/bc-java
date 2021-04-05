96767fa3b2a0bf9bb134c934afb2563a0adafc76
David Hook <dgh@cryptoworkshop.com>
2021-04-05 18:22:11 +1000

    added bcutil

--- add_module.sh
+++ add_module.sh
@@ -1,4 +1,4 @@
-export JAVA_9=/usr/lib/jvm/java-9-oracle
+export JAVA_9=/usr/lib/jvm/java-9.0.4-oracle-x64
 export JAVA_11=/usr/lib/jvm/java-11-openjdk-amd64
 export JAVA_15=/opt/jdk-15
 
@@ -7,7 +7,7 @@ export PATH=$JAVA_HOME/bin:$PATH
 
 if [ $# -ne 1 ]
 then
-   echo "usage: add_module.sh prov|prov-ext|tls|pg|pkix|mail"
+   echo "usage: add_module.sh prov|prov-ext|util|tls|pg|pkix|mail"
    exit 0
 fi 
 
@@ -27,20 +27,27 @@ rm -rf module.tmp
     ( cd module.tmp/v5; jar xf ../../build/artifacts/jdk1.5/jars/$jarName )
 
     provJar=`echo build/artifacts/jdk1.5/jars/bcprov-jdk15on*.jar`
+    utilJar=`echo build/artifacts/jdk1.5/jars/bcutil-jdk15on*.jar`
     pkixJar=`echo build/artifacts/jdk1.5/jars/bcpkix-jdk15on*.jar`
     if [ $1 = "prov" ]
     then
 	javac -target 1.9 -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java:core/src/main/java
 	$JAVA_11/bin/javac -classpath module.tmp/v5 -d module.tmp/v11 `find $1/src/main/jdk1.11 -name "*.java"` -sourcepath $1/src/main/jdk1.11:$1/src/main/java:core/src/main/java
-	$JAVA_15/bin/javac -classpath module.tmp/v5:mofule.tmp/v11 -d module.tmp/v15 `find prov/src/main/jdk1.15 -name "*.java"` -sourcepath prov/src/main/jdk1.15:prov/src/main/jdk1.11:prov/src/main/java:core/src/main/java
+	$JAVA_15/bin/javac -classpath module.tmp/v5:module.tmp/v11 -d module.tmp/v15 `find prov/src/main/jdk1.15 -name "*.java"` -sourcepath prov/src/main/jdk1.15:prov/src/main/jdk1.11:prov/src/main/java:core/src/main/java
     elif [ $1 = "prov-ext" ]
     then
 	javac -target 1.9 -classpath module.tmp/v5 -d module.tmp/v9 `find prov/src/main/ext-jdk1.9 -name "*.java"` -sourcepath prov/src/main/ext-jdk1.9:prov/src/main/java:core/src/main/java
 	$JAVA_11/bin/javac -classpath module.tmp/v5 -d module.tmp/v11 `find prov/src/main/jdk1.11 -name "*.java"` -sourcepath prov/src/main/jdk1.11:prov/src/main/java:core/src/main/java
-	$JAVA_15/bin/javac -classpath module.tmp/v5:mofule.tmp/v11 -d module.tmp/v15 `find prov/src/main/jdk1.15 -name "*.java"` -sourcepath prov/src/main/jdk1.15:prov/src/main/jdk1.11:prov/src/main/java:core/src/main/java
+	$JAVA_15/bin/javac -classpath module.tmp/v5:module.tmp/v11 -d module.tmp/v15 `find prov/src/main/jdk1.15 -name "*.java"` -sourcepath prov/src/main/jdk1.15:prov/src/main/jdk1.11:prov/src/main/java:core/src/main/java
     elif [ $1 = "mail" ]
     then
-	javac  -target 1.9 --module-path ${provJar}:$pkixJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
+	javac  -target 1.9 --module-path ${provJar}:$utilJar:$pkixJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
+    elif [ $1 = "pkix" ]
+    then
+	javac  -target 1.9 --module-path ${provJar}:$utilJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
+    elif [ $1 = "tls" ]
+    then
+	javac  -target 1.9 --module-path ${provJar}:$utilJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
     else
 	javac  -target 1.9 --module-path $provJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
     fi
