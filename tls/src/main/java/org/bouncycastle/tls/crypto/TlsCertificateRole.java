64b41b5c5314690a8364ccd712dc71dd2698a776
Peter Dettman <peter.dettman@bouncycastle.org>
2021-05-25 01:31:50 +0700

    Add missing abstract modifiers

--- tls/src/main/java/org/bouncycastle/tls/crypto/TlsCertificateRole.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/TlsCertificateRole.java
@@ -1,6 +1,6 @@
 package org.bouncycastle.tls.crypto;
 
-public class TlsCertificateRole
+public abstract class TlsCertificateRole
 {
     public static final int DH = 1;
     public static final int ECDH = 2;
