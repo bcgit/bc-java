80afd26c489b560886cca8badaddbc6d66d269d7
Peter Dettman <peter.dettman@bouncycastle.org>
2021-06-01 16:19:54 +0700

    Support getU(Encoding) on XDHPublicKey

--- prov/src/main/java/org/bouncycastle/jcajce/interfaces/XDHPublicKey.java
+++ prov/src/main/java/org/bouncycastle/jcajce/interfaces/XDHPublicKey.java
@@ -1,8 +1,12 @@
 package org.bouncycastle.jcajce.interfaces;
 
+import java.math.BigInteger;
 import java.security.PublicKey;
 
 public interface XDHPublicKey
     extends XDHKey, PublicKey
 {
+    public BigInteger getU();
+
+    public byte[] getUEncoding();
 }
