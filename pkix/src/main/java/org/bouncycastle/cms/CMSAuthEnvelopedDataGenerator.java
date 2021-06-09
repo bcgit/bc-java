421d3413a0159d0b07f9e9f57b5fda7be7147f48
Peter Dettman <peter.dettman@bouncycastle.org>
2021-06-09 19:56:42 +0700

    Cleanup around AttributeTable

--- pkix/src/main/java/org/bouncycastle/cms/CMSAuthEnvelopedDataGenerator.java
+++ pkix/src/main/java/org/bouncycastle/cms/CMSAuthEnvelopedDataGenerator.java
@@ -3,7 +3,7 @@ package org.bouncycastle.cms;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Iterator;
 
 import org.bouncycastle.asn1.ASN1EncodableVector;
@@ -52,7 +52,7 @@ public class CMSAuthEnvelopedDataGenerator
             
             if (authAttrsGenerator != null)
             {
-                AttributeTable attrTable = authAttrsGenerator.getAttributes(new HashMap());
+                AttributeTable attrTable = authAttrsGenerator.getAttributes(Collections.emptyMap());
 
                 authenticatedAttrSet = new DERSet(attrTable.toASN1EncodableVector());
 
@@ -90,7 +90,7 @@ public class CMSAuthEnvelopedDataGenerator
         ASN1Set unprotectedAttrSet = null;
         if (unauthAttrsGenerator != null)
         {
-            AttributeTable attrTable = unauthAttrsGenerator.getAttributes(new HashMap());
+            AttributeTable attrTable = unauthAttrsGenerator.getAttributes(Collections.emptyMap());
 
             unprotectedAttrSet = new DLSet(attrTable.toASN1EncodableVector());
         }
