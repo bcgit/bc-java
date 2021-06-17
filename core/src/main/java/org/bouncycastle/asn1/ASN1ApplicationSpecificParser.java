089b5115631bd4247bb5284aade0e7725149a813
Peter Dettman <peter.dettman@bouncycastle.org>
2021-06-17 15:01:38 +0700

    More tagged object refactoring

--- core/src/main/java/org/bouncycastle/asn1/ASN1ApplicationSpecificParser.java
+++ core/src/main/java/org/bouncycastle/asn1/ASN1ApplicationSpecificParser.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 /**
  * Interface to parse ASN.1 ApplicationSpecific objects.
  * 
- * @deprecated Will be removed with {@link BERApplicationSpecificParser).
+ * @deprecated Test for {@link ASN1TaggedObjectParser} with
+ *             {@link ASN1TaggedObjectParser#getTagClass() tag class} of
+ *             {@link BERTags#APPLICATION} instead.
  */
 public interface ASN1ApplicationSpecificParser
-    extends ASN1Encodable, InMemoryRepresentable
+    extends ASN1TaggedObjectParser
 {
     /**
      * Read the next object in the parser.
