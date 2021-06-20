0db1a8af0dd18e1f7cdb615decd551137968070d
David Hook <dgh@cryptoworkshop.com>
2021-06-20 12:55:56 +1000

    added AllTests, updated ant build to include ITS.

---
+++ pkix/src/test/java/org/bouncycastle/its/test/ITSTestSetup.java
@@ -0,0 +1,28 @@
+
+package org.bouncycastle.its.test;
+
+import java.security.Security;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+class ITSTestSetup
+    extends TestSetup
+{
+    public ITSTestSetup(Test test)
+    {
+        super(test);
+    }
+
+    protected void setUp()
+    {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    protected void tearDown()
+    {
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+    }
+
+}
