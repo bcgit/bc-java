e9699970dcbc034e35b7f448c563d1530d166c28
David Hook <dgh@cryptoworkshop.com>
2021-06-22 12:41:16 +1000

    added self-signature generation, removed use of ContentSigner.

--- pkix/src/main/java/org/bouncycastle/its/operator/ITSContentSigner.java
+++ pkix/src/main/java/org/bouncycastle/its/operator/ITSContentSigner.java
@@ -1,15 +1,39 @@
 package org.bouncycastle.its.operator;
 
+import java.io.OutputStream;
+
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.its.ITSCertificate;
-import org.bouncycastle.operator.ContentSigner;
 
 public interface ITSContentSigner
-    extends ContentSigner
 {
+    /**
+     * Returns a stream that will accept data for the purpose of calculating
+     * a signature. Use org.bouncycastle.util.io.TeeOutputStream if you want to accumulate
+     * the data on the fly as well.
+     *
+     * @return an OutputStream
+     */
+    OutputStream getOutputStream();
+
+    /**
+     * Returns a signature based on the current data written to the stream, since the
+     * start or the last call to getSignature().
+     *
+     * @return bytes representing the signature.
+     */
+    byte[] getSignature();
+
     ITSCertificate getAssociatedCertificate();
 
     byte[] getAssociatedCertificateDigest();
 
     AlgorithmIdentifier getDigestAlgorithm();
+
+    /**
+     * Return true if this ContentSigner is for self signing. False otherwise.
+     *
+     * @return true if for self-signing.
+     */
+    boolean isForSelfSigning();
 }
