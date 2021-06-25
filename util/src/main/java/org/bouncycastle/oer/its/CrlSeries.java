009bba3c41a8490805b8b3d880f96c6612783836
David Hook <dgh@cryptoworkshop.com>
2021-06-25 18:25:41 +1000

    minor refactoring. Added setValidityPeriod method to ITSCertificateBuilder

--- util/src/main/java/org/bouncycastle/oer/its/CrlSeries.java
+++ util/src/main/java/org/bouncycastle/oer/its/CrlSeries.java
@@ -10,32 +10,15 @@ import org.bouncycastle.asn1.ASN1Integer;
 public class CrlSeries
     extends Uint16
 {
-    public CrlSeries(long value)
+    public CrlSeries(int value)
     {
         super(value);
-        assertRange();
     }
 
     public CrlSeries(BigInteger value)
     {
         super(value);
-        assertRange();
     }
-
-    public CrlSeries(byte[] bytes)
-    {
-        super(bytes);
-        assertRange();
-    }
-
-    private void assertRange()
-    {
-        if (BigInteger.ZERO.compareTo(getValue()) > 0 || BigInteger.valueOf(0xFFFF).compareTo(getValue()) < 0)
-        {
-            throw new IllegalStateException("value must be >=0 && < 0xFFFF");
-        }
-    }
-
 
     public static CrlSeries getInstance(Object o)
     {
@@ -44,7 +27,5 @@ public class CrlSeries
             return (CrlSeries)o;
         }
         return new CrlSeries(ASN1Integer.getInstance(o).getValue());
-
     }
-
 }
