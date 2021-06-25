f9e66874fef41f4d6164e7f93a3db00c019f00f9
Megan Woods <megan.woods@woods-gebler.com>
2021-06-25 16:08:38 +1000

    Initial implementation of implicit ITS certificates.

---
+++ pkix/src/main/java/org/bouncycastle/its/ITSImplicitCertificateBuilder.java
@@ -0,0 +1,113 @@
+package org.bouncycastle.its;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.oer.its.Certificate;
+import org.bouncycastle.oer.its.CertificateBase;
+import org.bouncycastle.oer.its.CertificateType;
+import org.bouncycastle.oer.its.EccP256CurvePoint;
+import org.bouncycastle.oer.its.HashedId;
+import org.bouncycastle.oer.its.IssuerIdentifier;
+import org.bouncycastle.oer.its.ToBeSignedCertificate;
+import org.bouncycastle.oer.its.VerificationKeyIndicator;
+import org.bouncycastle.operator.DigestCalculator;
+import org.bouncycastle.operator.DigestCalculatorProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.util.Arrays;
+
+public class ITSImplicitCertificateBuilder
+    extends ITSCertificateBuilder
+{
+
+    private ITSCertificate issuer;
+    private ASN1Integer version = new ASN1Integer(3);
+
+    public ITSImplicitCertificateBuilder(ToBeSignedCertificate.Builder tbsCertificate)
+    {
+        super(tbsCertificate);
+    }
+
+    public ITSCertificate build(BigInteger x, BigInteger y, DigestCalculatorProvider digestCalculatorProvider)
+    {
+        EccP256CurvePoint reconstructionValue = EccP256CurvePoint.builder()
+            .uncompressedP256(x, y).createEccP256CurvePoint();
+
+        // TODO is this always true
+        AlgorithmIdentifier digestAlgId = new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha256);
+        ASN1ObjectIdentifier digestAlg = digestAlgId.getAlgorithm();
+        DigestCalculator calculator;
+        try
+        {
+            calculator = digestCalculatorProvider.get(digestAlgId);
+        }
+        catch (OperatorCreationException e)
+        {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+
+        try
+        {
+            OutputStream os = calculator.getOutputStream();
+            os.write(issuer.getEncoded());
+            os.close();
+        }
+        catch (IOException ioex)
+        {
+            throw new IllegalStateException(ioex.getMessage(), ioex);
+        }
+
+        byte[] parentDigest = calculator.getDigest();
+
+        IssuerIdentifier.Builder issuerIdentifierBuilder = IssuerIdentifier.builder();
+
+        HashedId.HashedId8 hashedID = new HashedId.HashedId8(Arrays.copyOfRange(parentDigest, parentDigest.length - 8, parentDigest.length));
+        if (digestAlg.equals(NISTObjectIdentifiers.id_sha256))
+        {
+            issuerIdentifierBuilder.sha256AndDigest(hashedID);
+        }
+        else if (digestAlg.equals(NISTObjectIdentifiers.id_sha384))
+        {
+            issuerIdentifierBuilder.sha384AndDigest(hashedID);
+        }
+        else
+        {
+            throw new IllegalStateException("unknown digest");
+        }
+
+
+        tbsCertificateBuilder.setVerificationKeyIndicator(VerificationKeyIndicator.builder()
+            .reconstructionValue(reconstructionValue)
+            .createVerificationKeyIndicator());
+
+        CertificateBase.Builder baseBldr = new CertificateBase.Builder();
+
+        baseBldr.setVersion(version);
+        baseBldr.setType(CertificateType.Implicit);
+        baseBldr.setIssuer(issuerIdentifierBuilder.createIssuerIdentifier());
+
+        baseBldr.setToBeSignedCertificate(tbsCertificateBuilder.createToBeSignedCertificate());
+
+        Certificate.Builder bldr = new Certificate.Builder();
+
+        bldr.setCertificateBase(baseBldr.createCertificateBase());
+
+        return new ITSCertificate(bldr.createCertificate());
+    }
+
+
+    public ITSCertificate getIssuer()
+    {
+        return issuer;
+    }
+
+    public void setIssuer(ITSCertificate issuer)
+    {
+        this.issuer = issuer;
+    }
+}
