95dc0e642fdb43ef56ae7797877906b717588ec9
Megan Woods <megan.woods@woods-gebler.com>
2021-06-25 16:36:20 +1000

    Copy constructor for Builder

--- util/src/main/java/org/bouncycastle/oer/its/ToBeSignedCertificate.java
+++ util/src/main/java/org/bouncycastle/oer/its/ToBeSignedCertificate.java
@@ -216,6 +216,28 @@ public class ToBeSignedCertificate
         private PublicEncryptionKey encryptionKey;
         private VerificationKeyIndicator verificationKeyIndicator;
 
+
+        public Builder()
+        {
+        }
+
+        public Builder(ToBeSignedCertificate o)
+        {
+            this.certificateId = o.certificateId;
+            this.cracaId = o.cracaId;
+            this.crlSeries = o.crlSeries;
+            this.validityPeriod = o.validityPeriod;
+            this.geographicRegion = o.geographicRegion;
+            this.assuranceLevel = o.assuranceLevel;
+            this.appPermissions = o.appPermissions;
+            this.certIssuePermissions = o.certIssuePermissions;
+            this.certRequestPermissions = o.certRequestPermissions;
+            this.canRequestRollover = o.canRequestRollover;
+            this.encryptionKey = o.encryptionKey;
+            this.verificationKeyIndicator = o.verificationKeyIndicator;
+        }
+
+
         public Builder setCertificateId(CertificateId certificateId)
         {
             this.certificateId = certificateId;
