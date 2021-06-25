6494e444cf4b7e73885350137cd250c47a6dc908
Peter Dettman <peter.dettman@bouncycastle.org>
2021-06-25 23:39:12 +0700

    Check key sizes strictly
    - see https://github.com/bcgit/bc-csharp/issues/303
    - also refactor to use Pack and Integers

--- core/src/main/java/org/bouncycastle/crypto/engines/TwofishEngine.java
+++ core/src/main/java/org/bouncycastle/crypto/engines/TwofishEngine.java
@@ -5,6 +5,8 @@ import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.DataLengthException;
 import org.bouncycastle.crypto.OutputLengthException;
 import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.util.Integers;
+import org.bouncycastle.util.Pack;
 
 /**
  * A class that provides Twofish encryption operations.
@@ -272,7 +274,19 @@ public final class TwofishEngine
         {
             this.encrypting = encrypting;
             this.workingKey = ((KeyParameter)params).getKey();
-            this.k64Cnt = (this.workingKey.length / 8); // pre-padded ?
+
+            int keyBits = this.workingKey.length * 8;
+            switch (keyBits)
+            {
+            case 128:
+            case 192:
+            case 256:
+                break;
+            default:
+                throw new IllegalArgumentException("Key length not 128/192/256 bits.");
+            }
+
+            this.k64Cnt = this.workingKey.length / 8;
             setKey(this.workingKey);
 
             return;
@@ -344,28 +358,16 @@ public final class TwofishEngine
         int[] sBoxKeys = new int[MAX_KEY_BITS/64]; // 4 
         gSubKeys = new int[TOTAL_SUBKEYS];
 
-        if (k64Cnt < 1) 
-        {
-            throw new IllegalArgumentException("Key size less than 64 bits");
-        }
-        
-        if (k64Cnt > 4)
-        {
-            throw new IllegalArgumentException("Key size larger than 256 bits");
-        }
-
         /*
-         * k64Cnt is the number of 8 byte blocks (64 chunks)
-         * that are in the input key.  The input key is a
-         * maximum of 32 bytes (256 bits), so the range
-         * for k64Cnt is 1..4
+         * k64Cnt is the number of 8 byte blocks (64 chunks) that are in the input key.
+         * The input key is 16, 24 or 32 bytes, so the range for k64Cnt is 2..4
          */
         for (int i=0; i<k64Cnt ; i++)
         {
             int p = i* 8;
 
-            k32e[i] = BytesTo32Bits(key, p);
-            k32o[i] = BytesTo32Bits(key, p+4);
+            k32e[i] = Pack.littleEndianToInt(key, p);
+            k32o[i] = Pack.littleEndianToInt(key, p + 4);
 
             sBoxKeys[k64Cnt-1-i] = RS_MDS_Encode(k32e[i], k32o[i]);
         }
@@ -376,7 +378,7 @@ public final class TwofishEngine
             q = i*SK_STEP;
             A = F32(q,         k32e);
             B = F32(q+SK_BUMP, k32o);
-            B = B << 8 | B >>> 24;
+            B = Integers.rotateLeft(B, 8);
             A += B;
             gSubKeys[i*2] = A;
             A += B;
@@ -448,10 +450,10 @@ public final class TwofishEngine
         byte[] dst,
         int dstIndex)
     {
-        int x0 = BytesTo32Bits(src, srcIndex) ^ gSubKeys[INPUT_WHITEN];
-        int x1 = BytesTo32Bits(src, srcIndex + 4) ^ gSubKeys[INPUT_WHITEN + 1];
-        int x2 = BytesTo32Bits(src, srcIndex + 8) ^ gSubKeys[INPUT_WHITEN + 2];
-        int x3 = BytesTo32Bits(src, srcIndex + 12) ^ gSubKeys[INPUT_WHITEN + 3];
+        int x0 = Pack.littleEndianToInt(src, srcIndex) ^ gSubKeys[INPUT_WHITEN];
+        int x1 = Pack.littleEndianToInt(src, srcIndex + 4) ^ gSubKeys[INPUT_WHITEN + 1];
+        int x2 = Pack.littleEndianToInt(src, srcIndex + 8) ^ gSubKeys[INPUT_WHITEN + 2];
+        int x3 = Pack.littleEndianToInt(src, srcIndex + 12) ^ gSubKeys[INPUT_WHITEN + 3];
 
         int k = ROUND_SUBKEYS;
         int t0, t1;
@@ -460,20 +462,20 @@ public final class TwofishEngine
             t0 = Fe32_0(x0);
             t1 = Fe32_3(x1);
             x2 ^= t0 + t1 + gSubKeys[k++];
-            x2 = x2 >>>1 | x2 << 31;
-            x3 = (x3 << 1 | x3 >>> 31) ^ (t0 + 2*t1 + gSubKeys[k++]);
+            x2 = Integers.rotateRight(x2, 1);
+            x3 = Integers.rotateLeft(x3, 1) ^ (t0 + 2*t1 + gSubKeys[k++]);
 
             t0 = Fe32_0(x2);
             t1 = Fe32_3(x3);
             x0 ^= t0 + t1 + gSubKeys[k++];
-            x0 = x0 >>>1 | x0 << 31;
-            x1 = (x1 << 1 | x1 >>> 31) ^ (t0 + 2*t1 + gSubKeys[k++]);
+            x0 = Integers.rotateRight(x0, 1);
+            x1 = Integers.rotateLeft(x1, 1) ^ (t0 + 2*t1 + gSubKeys[k++]);
         }
 
-        Bits32ToBytes(x2 ^ gSubKeys[OUTPUT_WHITEN], dst, dstIndex);
-        Bits32ToBytes(x3 ^ gSubKeys[OUTPUT_WHITEN + 1], dst, dstIndex + 4);
-        Bits32ToBytes(x0 ^ gSubKeys[OUTPUT_WHITEN + 2], dst, dstIndex + 8);
-        Bits32ToBytes(x1 ^ gSubKeys[OUTPUT_WHITEN + 3], dst, dstIndex + 12);
+        Pack.intToLittleEndian(x2 ^ gSubKeys[OUTPUT_WHITEN], dst, dstIndex);
+        Pack.intToLittleEndian(x3 ^ gSubKeys[OUTPUT_WHITEN + 1], dst, dstIndex + 4);
+        Pack.intToLittleEndian(x0 ^ gSubKeys[OUTPUT_WHITEN + 2], dst, dstIndex + 8);
+        Pack.intToLittleEndian(x1 ^ gSubKeys[OUTPUT_WHITEN + 3], dst, dstIndex + 12);
     }
 
     /**
@@ -487,10 +489,10 @@ public final class TwofishEngine
         byte[] dst,
         int dstIndex)
     {
-        int x2 = BytesTo32Bits(src, srcIndex) ^ gSubKeys[OUTPUT_WHITEN];
-        int x3 = BytesTo32Bits(src, srcIndex+4) ^ gSubKeys[OUTPUT_WHITEN + 1];
-        int x0 = BytesTo32Bits(src, srcIndex+8) ^ gSubKeys[OUTPUT_WHITEN + 2];
-        int x1 = BytesTo32Bits(src, srcIndex+12) ^ gSubKeys[OUTPUT_WHITEN + 3];
+        int x2 = Pack.littleEndianToInt(src, srcIndex) ^ gSubKeys[OUTPUT_WHITEN];
+        int x3 = Pack.littleEndianToInt(src, srcIndex + 4) ^ gSubKeys[OUTPUT_WHITEN + 1];
+        int x0 = Pack.littleEndianToInt(src, srcIndex + 8) ^ gSubKeys[OUTPUT_WHITEN + 2];
+        int x1 = Pack.littleEndianToInt(src, srcIndex + 12) ^ gSubKeys[OUTPUT_WHITEN + 3];
 
         int k = ROUND_SUBKEYS + 2 * ROUNDS -1 ;
         int t0, t1;
@@ -499,20 +501,20 @@ public final class TwofishEngine
             t0 = Fe32_0(x2);
             t1 = Fe32_3(x3);
             x1 ^= t0 + 2*t1 + gSubKeys[k--];
-            x0 = (x0 << 1 | x0 >>> 31) ^ (t0 + t1 + gSubKeys[k--]);
-            x1 = x1 >>>1 | x1 << 31;
+            x0 = Integers.rotateLeft(x0, 1) ^ (t0 + t1 + gSubKeys[k--]);
+            x1 = Integers.rotateRight(x1, 1);
 
             t0 = Fe32_0(x0);
             t1 = Fe32_3(x1);
             x3 ^= t0 + 2*t1 + gSubKeys[k--];
-            x2 = (x2 << 1 | x2 >>> 31) ^ (t0 + t1 + gSubKeys[k--]);
-            x3 = x3 >>>1 | x3 << 31;
+            x2 = Integers.rotateLeft(x2, 1) ^ (t0 + t1 + gSubKeys[k--]);
+            x3 = Integers.rotateRight(x3, 1);
         }
 
-        Bits32ToBytes(x0 ^ gSubKeys[INPUT_WHITEN], dst, dstIndex);
-        Bits32ToBytes(x1 ^ gSubKeys[INPUT_WHITEN + 1], dst, dstIndex + 4);
-        Bits32ToBytes(x2 ^ gSubKeys[INPUT_WHITEN + 2], dst, dstIndex + 8);
-        Bits32ToBytes(x3 ^ gSubKeys[INPUT_WHITEN + 3], dst, dstIndex + 12);
+        Pack.intToLittleEndian(x0 ^ gSubKeys[INPUT_WHITEN], dst, dstIndex);
+        Pack.intToLittleEndian(x1 ^ gSubKeys[INPUT_WHITEN + 1], dst, dstIndex + 4);
+        Pack.intToLittleEndian(x2 ^ gSubKeys[INPUT_WHITEN + 2], dst, dstIndex + 8);
+        Pack.intToLittleEndian(x3 ^ gSubKeys[INPUT_WHITEN + 3], dst, dstIndex + 12);
     }
 
     /* 
@@ -660,21 +662,5 @@ public final class TwofishEngine
                gSBox[ 0x001 + 2*(x & 0xff) ] ^
                gSBox[ 0x200 + 2*((x >>> 8) & 0xff) ] ^
                gSBox[ 0x201 + 2*((x >>> 16) & 0xff) ];
-    }
-    
-    private int BytesTo32Bits(byte[] b, int p)
-    {
-        return ((b[p] & 0xff)) | 
-             ((b[p+1] & 0xff) << 8) |
-             ((b[p+2] & 0xff) << 16) |
-             ((b[p+3] & 0xff) << 24);
-    }
-
-    private void Bits32ToBytes(int in,  byte[] b, int offset)
-    {
-        b[offset] = (byte)in;
-        b[offset + 1] = (byte)(in >> 8);
-        b[offset + 2] = (byte)(in >> 16);
-        b[offset + 3] = (byte)(in >> 24);
     }
 }
