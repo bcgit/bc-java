2d5753d02ce7a478c41dcbab377616cd7d35539d
Peter Dettman <peter.dettman@bouncycastle.org>
2021-06-26 17:03:34 +0700

    Add TYPE for ASN1BitString

--- core/src/main/java/org/bouncycastle/asn1/DLBitString.java
+++ core/src/main/java/org/bouncycastle/asn1/DLBitString.java
@@ -8,96 +8,27 @@ import java.io.IOException;
 public class DLBitString
     extends ASN1BitString
 {
-    /**
-     * return a Bit String that can be definite-length encoded from the passed in object.
-     *
-     * @param obj a DL or DER BitString or an object that can be converted into one.
-     * @exception IllegalArgumentException if the object cannot be converted.
-     * @return an ASN1BitString instance, or null.
-     */
-    public static ASN1BitString getInstance(
-        Object  obj)
-    {
-        if (obj == null || obj instanceof DLBitString)
-        {
-            return (DLBitString)obj;
-        }
-        if (obj instanceof DERBitString)
-        {
-            return (DERBitString)obj;
-        }
-        if (obj instanceof byte[])
-        {
-            try
-            {
-                return (ASN1BitString)fromByteArray((byte[])obj);
-            }
-            catch (Exception e)
-            {
-                throw new IllegalArgumentException("encoding error in getInstance: " + e.toString());
-            }
-        }
-
-        throw new IllegalArgumentException("illegal object in getInstance: " + obj.getClass().getName());
-    }
-
-    /**
-     * return a Bit String from a tagged object.
-     *
-     * @param obj the tagged object holding the object we want
-     * @param explicit true if the object is meant to be explicitly
-     *              tagged false otherwise.
-     * @exception IllegalArgumentException if the tagged object cannot
-     *               be converted.
-     * @return an ASN1BitString instance, or null.
-     */
-    public static ASN1BitString getInstance(
-        ASN1TaggedObject obj,
-        boolean          explicit)
-    {
-        ASN1Primitive o = obj.getObject();
-
-        if (explicit || o instanceof DLBitString)
-        {
-            return getInstance(o);
-        }
-        else
-        {
-            return fromOctetString(ASN1OctetString.getInstance(o));
-        }
-    }
-
-    protected DLBitString(byte data, int padBits)
-    {
-        super(data, padBits);
-    }
-
-    /**
-     * @param data the octets making up the bit string.
-     * @param padBits the number of extra bits at the end of the string.
-     */
-    public DLBitString(
-        byte[]  data,
-        int     padBits)
-    {
-        super(data, padBits);
-    }
-
-    public DLBitString(
-        byte[]  data)
+    public DLBitString(byte[] data)
     {
         this(data, 0);
     }
 
-    public DLBitString(
-        int value)
+    public DLBitString(byte data, int padBits)
+    {
+        super(data, padBits);
+    }
+
+    public DLBitString(byte[] data, int padBits)
+    {
+        super(data, padBits);
+    }
+
+    public DLBitString(int value)
     {
         super(getBytes(value), getPadBits(value));
     }
 
-    public DLBitString(
-        ASN1Encodable obj)
-        throws IOException
+    public DLBitString(ASN1Encodable obj) throws IOException
     {
         super(obj.toASN1Primitive().getEncoded(ASN1Encoding.DER), 0);
     }
