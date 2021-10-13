c21ceebd5b29597f6157b6feb3a7700ef1c54258
David Hook <dgh@cryptoworkshop.com>
2021-10-13 13:18:37 +1100

    relates to github #1035/#1040 added canonicalizing literal data generator.

--- pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
@@ -39,7 +39,8 @@ public class RegressionTest
         new ArmoredInputStreamBackslashTRVFTest(),
         new ArmoredInputStreamCRCErrorGetsThrownTest(),
         new ArmoredInputStreamIngoreMissingCRCSum(),
-        new PGPSessionKeyTest()
+        new PGPSessionKeyTest(),
+        new PGPCanonicalizedDataGeneratorTest()
     };
 
     public static void main(String[] args)
