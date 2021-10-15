cae2dc995db746903ced65b231cb42c2c2d3a83d
Paul Schaub <vanitasvitae@fsfe.org>
2021-10-15 13:11:01 +0200

    Make StreamGenerator interface public

--- pg/src/main/java/org/bouncycastle/openpgp/StreamGenerator.java
+++ pg/src/main/java/org/bouncycastle/openpgp/StreamGenerator.java
@@ -6,7 +6,7 @@ import java.io.IOException;
  * Callback interface for generators that produce a stream to be informed when the stream has been
  * closed by the client.
  */
-interface StreamGenerator
+public interface StreamGenerator
 {
     /**
      * Signal that the stream has been closed.
