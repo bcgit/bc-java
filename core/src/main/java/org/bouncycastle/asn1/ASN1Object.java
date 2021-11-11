3ab44bfba94ab7fcf2d96f0a0a1403a5334ed75c
Peter Dettman <peter.dettman@bouncycastle.org>
2021-11-11 20:53:08 +0700

    Trim stack trace

--- core/src/main/java/org/bouncycastle/asn1/ASN1Object.java
+++ core/src/main/java/org/bouncycastle/asn1/ASN1Object.java
@@ -14,12 +14,12 @@ public abstract class ASN1Object
 {
     public void encodeTo(OutputStream output) throws IOException
     {
-        ASN1OutputStream.create(output).writeObject(this);
+        toASN1Primitive().encodeTo(output);
     }
 
     public void encodeTo(OutputStream output, String encoding) throws IOException
     {
-        ASN1OutputStream.create(output, encoding).writeObject(this);
+        toASN1Primitive().encodeTo(output, encoding);
     }
 
     /**
@@ -31,7 +31,7 @@ public abstract class ASN1Object
     public byte[] getEncoded() throws IOException
     {
         ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-        encodeTo(bOut);
+        toASN1Primitive().encodeTo(bOut);
         return bOut.toByteArray();
     }
 
@@ -45,7 +45,7 @@ public abstract class ASN1Object
     public byte[] getEncoded(String encoding) throws IOException
     {
         ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-        encodeTo(bOut, encoding);
+        toASN1Primitive().encodeTo(bOut, encoding);
         return bOut.toByteArray();
     }
 
