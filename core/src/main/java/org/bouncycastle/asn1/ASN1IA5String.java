0a24b1408003fa267a3e33219d6e770e3536135b
David Hook <dgh@cryptoworkshop.com>
2021-11-24 15:33:37 +1100

    JavaDoc.

--- core/src/main/java/org/bouncycastle/asn1/ASN1IA5String.java
+++ core/src/main/java/org/bouncycastle/asn1/ASN1IA5String.java
@@ -62,7 +62,7 @@ public abstract class ASN1IA5String
     /**
      * Return an IA5 String from a tagged object.
      *
-     * @param obj the tagged object holding the object we want
+     * @param taggedObject the tagged object holding the object we want
      * @param explicit true if the object is meant to be explicitly
      *              tagged false otherwise.
      * @exception IllegalArgumentException if the tagged object cannot
