9710991d3ff9b6aed7b3a0b1a2c525be6d6d879c
David Hook <dgh@cryptoworkshop.com>
2021-11-26 14:09:22 +1100

    compatibility updates.

--- core/src/main/java/org/bouncycastle/util/Shorts.java
+++ core/src/main/java/org/bouncycastle/util/Shorts.java
@@ -5,7 +5,7 @@ package org.bouncycastle.util;
  */
 public class Shorts
 {
-    public static final int BYTES = Short.BYTES;
+    public static final int BYTES = 2;
     public static final int SIZE = Short.SIZE;
 
     public static Short valueOf(short value)
