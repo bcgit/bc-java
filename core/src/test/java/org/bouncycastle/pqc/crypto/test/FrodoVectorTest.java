b426a77292c44736357030a4e0de69b7d7569ac3
royb <roy.basmacier@primekey.com>
2022-01-06 12:27:32 -0500

    got rid of static field, added final, changed parameters and test vector

--- core/src/test/java/org/bouncycastle/pqc/crypto/test/FrodoVectorTest.java
+++ core/src/test/java/org/bouncycastle/pqc/crypto/test/FrodoVectorTest.java
@@ -4,51 +4,70 @@ import junit.framework.TestCase;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.SecretWithEncapsulation;
 import org.bouncycastle.pqc.crypto.frodo.*;
-import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
 import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
-import org.bouncycastle.util.test.FixedSecureRandom;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
 import java.util.HashMap;
 
 public class FrodoVectorTest
     extends TestCase
 {
-    public static void main(String[] args) throws IOException
+    public void testParameters()
+        throws Exception
     {
-        String files = "PQCkemKAT_19888.rsp PQCkemKAT_31296.rsp PQCkemKAT_43088.rsp PQCkemKAT_19888_shake.rsp PQCkemKAT_31296_shake.rsp PQCkemKAT_43088_shake.rsp";
-        for (String name : files.split(" "))
+        FrodoParameters[] params = new FrodoParameters[]{
+                FrodoParameters.frodokem19888r3,
+                FrodoParameters.frodokem19888shaker3,
+                FrodoParameters.frodokem31296r3,
+                FrodoParameters.frodokem31296shaker3,
+                FrodoParameters.frodokem43088r3,
+                FrodoParameters.frodokem43088shaker3
+        };
+
+        assertEquals(64, FrodoParameters.frodokem19888r3.getDefaultKeySize());
+        assertEquals(64, FrodoParameters.frodokem19888shaker3.getDefaultKeySize());
+        assertEquals(96, FrodoParameters.frodokem31296r3.getDefaultKeySize());
+        assertEquals(96, FrodoParameters.frodokem31296shaker3.getDefaultKeySize());
+        assertEquals(128, FrodoParameters.frodokem43088r3.getDefaultKeySize());
+        assertEquals(128, FrodoParameters.frodokem43088shaker3.getDefaultKeySize());
+    }
+    public void testVectors()
+        throws Exception
+    {
+        String[] files = new String[]{
+            "PQCkemKAT_19888.rsp",
+            "PQCkemKAT_31296.rsp",
+            "PQCkemKAT_43088.rsp",
+            "PQCkemKAT_19888_shake.rsp",
+            "PQCkemKAT_31296_shake.rsp",
+            "PQCkemKAT_43088_shake.rsp"
+        };
+
+        FrodoParameters[] params = new FrodoParameters[]{
+            FrodoParameters.frodokem19888r3,
+            FrodoParameters.frodokem31296r3,
+            FrodoParameters.frodokem43088r3,
+            FrodoParameters.frodokem19888shaker3,
+            FrodoParameters.frodokem31296shaker3,
+            FrodoParameters.frodokem43088shaker3
+        };
+        for (int fileIndex = 0; fileIndex != files.length; fileIndex++)
         {
+            String name = files[fileIndex];
             System.out.println("testing: " + name);
             InputStream src = SphincsPlusTest.class.getResourceAsStream("/org/bouncycastle/pqc/crypto/test/frodo/" + name);
             BufferedReader bin = new BufferedReader(new InputStreamReader(src));
 
             String line = null;
             HashMap<String, String> buf = new HashMap<String, String>();
-
-            int n = -1;
-            String mode = "";
-
             while ((line = bin.readLine()) != null)
             {
                 line = line.trim();
 
                 if (line.startsWith("#"))
                 {
-                    String[] nameParts = line.split("-");
-                    n = Integer.parseInt(nameParts[1]);
-                    mode = nameParts[2];
                     continue;
                 }
                 if (line.length() == 0)
@@ -58,20 +77,20 @@ public class FrodoVectorTest
                         String count = buf.get("count");
                         System.out.println("test case: " + count);
 
-                        byte[] seed = Hex.decode(buf.get("seed")); // seed for cmce secure random
+                        byte[] seed = Hex.decode(buf.get("seed")); // seed for nist secure random
                         byte[] pk = Hex.decode(buf.get("pk"));     // public key
                         byte[] sk = Hex.decode(buf.get("sk"));     // private key
                         byte[] ct = Hex.decode(buf.get("ct"));     // ciphertext
                         byte[] ss = Hex.decode(buf.get("ss"));     // session key
 
-                        String[] nameParts = name.split("-");
-                        // TESTS HERE
-                        System.out.println("Testing: Frodo" + n + mode + ".");
                         NISTSecureRandom random = new NISTSecureRandom(seed, null);
-                        FrodoParameters parameters = new FrodoParameters(n, mode.equals("AES"));
+                        FrodoParameters parameters = params[fileIndex];
 
                         FrodoKeyPairGenerator kpGen = new FrodoKeyPairGenerator();
                         FrodoKeyGenerationParameters genParams = new FrodoKeyGenerationParameters(random, parameters);
+                        //
+                        // Generate keys and test.
+                        //
                         kpGen.init(genParams);
                         AsymmetricCipherKeyPair kp = kpGen.generateKeyPair();
 
@@ -82,15 +101,12 @@ public class FrodoVectorTest
                         assertTrue(name + " " + count + ": secret key", Arrays.areEqual(sk, privParams.getPrivateKey()));
 
                         // kem_enc
-
                         FrodoKEMGenerator frodoEncCipher = new FrodoKEMGenerator(random);
                         SecretWithEncapsulation secWenc = frodoEncCipher.generateEncapsulated(pubParams);
                         byte[] generated_cipher_text = secWenc.getEncapsulation();
-
                         assertTrue(name + " " + count + ": kem_enc cipher text", Arrays.areEqual(ct, generated_cipher_text));
-                        assertTrue(name + " " + count + ": kem_enc key", Arrays.areEqual(ss, secWenc.getSecret()));
-
-                        // assert ct and ss
+                        byte[] secret = secWenc.getSecret();
+                        assertTrue(name + " " + count + ": kem_enc key", Arrays.areEqual(ss, secret));
 
                         // kem_dec
                         FrodoKEMExtractor frodoDecCipher = new FrodoKEMExtractor(privParams);
@@ -98,11 +114,7 @@ public class FrodoVectorTest
                         byte[] dec_key = frodoDecCipher.extractSecret(generated_cipher_text);
 
                         assertTrue(name + " " + count + ": kem_dec ss", Arrays.areEqual(dec_key, ss));
-                        assertTrue(name + " " + count + ": kem_dec key", Arrays.areEqual(dec_key, secWenc.getSecret()));
-
-                        // assert dec ss
-
-
+                        assertTrue(name + " " + count + ": kem_dec key", Arrays.areEqual(dec_key, secret));
                     }
                     buf.clear();
 
@@ -114,19 +126,8 @@ public class FrodoVectorTest
                 {
                     buf.put(line.substring(0, a).trim(), line.substring(a + 1).trim());
                 }
-
-
             }
             System.out.println("testing successful!");
         }
-        // init
-
-        // keygen
-        // Frodo640SHAKE
-
-
-
-
-
     }
 }
