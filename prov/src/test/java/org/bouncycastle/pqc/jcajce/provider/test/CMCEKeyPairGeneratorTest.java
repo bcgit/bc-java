6be2f995bbb434fed172613870e79d8dad1bcea3
David Hook <dgh@cryptoworkshop.com>
2022-01-08 16:07:02 +1100

    added KeySpec tests for SPHINCS+ and CMCE

--- prov/src/test/java/org/bouncycastle/pqc/jcajce/provider/test/CMCEKeyPairGeneratorTest.java
+++ prov/src/test/java/org/bouncycastle/pqc/jcajce/provider/test/CMCEKeyPairGeneratorTest.java
@@ -1,11 +1,15 @@
 package org.bouncycastle.pqc.jcajce.provider.test;
 
 import java.security.KeyFactory;
+import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 import org.bouncycastle.asn1.bc.BCObjectIdentifiers;
 import org.bouncycastle.pqc.jcajce.spec.CMCEParameterSpec;
+import org.bouncycastle.util.Arrays;
 
 /**
  * KeyFactory/KeyPairGenerator tests for CMCE with the BCPQC provider.
@@ -23,6 +27,22 @@ public class CMCEKeyPairGeneratorTest
     {
         kf = KeyFactory.getInstance("CMCE", "BCPQC");
         kf = KeyFactory.getInstance(BCObjectIdentifiers.pqc_kem_mceliece.getId(), "BCPQC");
+    }
+
+    public void testKeySpecs()
+        throws Exception
+    {
+        kf = KeyFactory.getInstance("CMCE", "BCPQC");
+        kpg = KeyPairGenerator.getInstance("CMCE", "BCPQC");
+        KeyPair kp = kpg.generateKeyPair();
+
+        PKCS8EncodedKeySpec privSpec = kf.getKeySpec(kp.getPrivate(), PKCS8EncodedKeySpec.class);
+
+        assertTrue(Arrays.areEqual(kp.getPrivate().getEncoded(), privSpec.getEncoded()));
+
+        X509EncodedKeySpec pubSpec = kf.getKeySpec(kp.getPublic(), X509EncodedKeySpec.class);
+
+        assertTrue(Arrays.areEqual(kp.getPublic().getEncoded(), pubSpec.getEncoded()));
     }
 
     public void testKeyPairEncoding()
