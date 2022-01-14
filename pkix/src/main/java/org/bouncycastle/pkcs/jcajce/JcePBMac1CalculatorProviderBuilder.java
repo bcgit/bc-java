f658831465fac27eb7a600ce4a3056dc648bbf97
David Hook <dgh@cryptoworkshop.com>
2022-01-14 16:23:02 +1100

    refactored so CMP PBE implemented same interface.

--- pkix/src/main/java/org/bouncycastle/pkcs/jcajce/JcePBMac1CalculatorProviderBuilder.java
+++ pkix/src/main/java/org/bouncycastle/pkcs/jcajce/JcePBMac1CalculatorProviderBuilder.java
@@ -3,6 +3,7 @@ package org.bouncycastle.pkcs.jcajce;
 import java.security.Provider;
 
 import org.bouncycastle.asn1.pkcs.PBMAC1Params;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.jcajce.util.DefaultJcaJceHelper;
 import org.bouncycastle.jcajce.util.JcaJceHelper;
@@ -41,6 +42,11 @@ public class JcePBMac1CalculatorProviderBuilder
             public MacCalculator get(AlgorithmIdentifier algorithm, char[] password)
                 throws OperatorCreationException
             {
+                if (!PKCSObjectIdentifiers.id_PBMAC1.equals(algorithm.getAlgorithm()))
+                {
+                    throw new OperatorCreationException("protection algorithm not PB mac based");
+                }
+
                 JcePBMac1CalculatorBuilder bldr
                     = new JcePBMac1CalculatorBuilder(PBMAC1Params.getInstance(algorithm.getParameters())).setHelper(helper);
 
