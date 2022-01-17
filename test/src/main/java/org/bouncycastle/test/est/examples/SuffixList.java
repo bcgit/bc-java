086120661d569e2dfe9958389c80e502131c4182
David Hook <dgh@cryptoworkshop.com>
2022-01-17 18:47:28 +1100

    added finally to ensure stream closure - relates to github #768.

--- test/src/main/java/org/bouncycastle/test/est/examples/SuffixList.java
+++ test/src/main/java/org/bouncycastle/test/est/examples/SuffixList.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,35 +14,40 @@ class SuffixList
         throws Exception
     {
         FileInputStream fin = new FileInputStream(file);
-        String line = null;
-        BufferedReader bin = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        BufferedReader bin = new BufferedReader(new InputStreamReader(fin));
         ArrayList<String> suffixes = new ArrayList<String>();
 
-        while ((line = bin.readLine()) != null)
+        try
         {
-            if (line.length() == 0 || (line.startsWith("//") && !line.startsWith("// xn--")))
+            String line = null;
+            while ((line = bin.readLine()) != null)
             {
-                continue;
-            }
+                if (line.length() == 0 || (line.startsWith("//") && !line.startsWith("// xn--")))
+                {
+                    continue;
+                }
 
-            if (line.startsWith("!"))
-            {
-                continue;
-            }
+                if (line.startsWith("!"))
+                {
+                    continue;
+                }
 
-            line = line.trim();
-            if (line.startsWith("// xn--"))
-            {
-                String[] j = line.split(" ");
-                suffixes.add(j[1]);
-            }
-            else
-            {
-                suffixes.add(line);
+                line = line.trim();
+                if (line.startsWith("// xn--"))
+                {
+                    String[] j = line.split(" ");
+                    suffixes.add(j[1]);
+                }
+                else
+                {
+                    suffixes.add(line);
+                }
             }
         }
-
-        bin.close();
+        finally
+        {
+            bin.close();
+        }
 
         for (int t = 0; t < suffixes.size(); t++)
         {
