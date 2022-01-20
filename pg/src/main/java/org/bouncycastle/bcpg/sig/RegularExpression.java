4735a544ce4155d4661f79e39518527c31e3118b
Paul Schaub <vanitasvitae@fsfe.org>
2022-01-20 13:41:34 +0100

    Add support for Regex subpackets

---
+++ pg/src/main/java/org/bouncycastle/bcpg/sig/RegularExpression.java
@@ -0,0 +1,40 @@
+package org.bouncycastle.bcpg.sig;
+
+import org.bouncycastle.bcpg.SignatureSubpacket;
+import org.bouncycastle.bcpg.SignatureSubpacketTags;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.Strings;
+
+public class RegularExpression
+        extends SignatureSubpacket
+{
+
+    public RegularExpression(
+            boolean critical,
+            boolean isLongLength,
+            byte[] data)
+    {
+        super(SignatureSubpacketTags.REG_EXP, critical, isLongLength, data);
+    }
+
+    public RegularExpression(
+            boolean critical,
+            String regex)
+    {
+        super(
+                SignatureSubpacketTags.REG_EXP,
+                critical,
+                false,
+                Strings.toNullTerminatedUTF8ByteArray(regex));
+    }
+
+    public String getRegex()
+    {
+        return Strings.fromNullTerminatedUTF8ByteArray(data);
+    }
+
+    public byte[] getRawRegex()
+    {
+        return Arrays.clone(data);
+    }
+}
