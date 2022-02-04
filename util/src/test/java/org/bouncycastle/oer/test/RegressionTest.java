e8aaf082463a690e6015f2160f60e338a8ae2ea0
David Hook <dgh@cryptoworkshop.com>
2022-02-04 15:17:49 +1100

    Migrated OER tests into new regression test.

---
+++ util/src/test/java/org/bouncycastle/oer/test/RegressionTest.java
@@ -0,0 +1,17 @@
+package org.bouncycastle.oer.test;
+
+import org.bouncycastle.util.test.SimpleTest;
+import org.bouncycastle.util.test.Test;
+
+public class RegressionTest
+{
+    public static Test[] tests = {
+        new ExpansionTest(),
+        new ExtensionTest(),
+    };
+
+    public static void main(String[] args)
+    {
+        SimpleTest.runTests(tests);
+    }
+}
