21e28ef19c3b20c6db62c7ee5ab1569ac94f43c7
David Hook <dgh@cryptoworkshop.com>
2022-02-17 12:11:43 +1100

    added key recover to data decryptor.

--- pkix/src/main/java/org/bouncycastle/its/operator/ETSIDataDecryptor.java
+++ pkix/src/main/java/org/bouncycastle/its/operator/ETSIDataDecryptor.java
@@ -3,4 +3,10 @@ package org.bouncycastle.its.operator;
 public interface ETSIDataDecryptor
 {
     byte[] decrypt(byte[] wrappedKey, byte[] content, byte[] nonce);
+
+    /**
+     * return the unwrapped key found in the data. Call after decrypt.
+     * @return the unwrapped key.
+     */
+    byte[] getKey();
 }
