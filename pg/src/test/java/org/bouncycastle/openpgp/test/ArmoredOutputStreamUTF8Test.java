270680f0cdc45b1dd0a88f034d9326dacdc55baf
David Hook <dgh@cryptoworkshop.com>
2022-03-17 14:45:21 +1100

    cleanup

--- pg/src/test/java/org/bouncycastle/openpgp/test/ArmoredOutputStreamUTF8Test.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/ArmoredOutputStreamUTF8Test.java
@@ -1,28 +1,31 @@
 package org.bouncycastle.openpgp.test;
 
-import org.bouncycastle.bcpg.ArmoredInputStream;
-import org.bouncycastle.bcpg.ArmoredOutputStream;
-import org.bouncycastle.util.test.SimpleTest;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.Charset;
 
-public class ArmoredOutputStreamUTF8Test extends SimpleTest {
+import org.bouncycastle.bcpg.ArmoredInputStream;
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.util.test.SimpleTest;
 
+public class ArmoredOutputStreamUTF8Test
+    extends SimpleTest
+{
     private static final Charset utf8 = Charset.forName("UTF8");
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         runTest(new ArmoredOutputStreamUTF8Test());
     }
 
-    @Override
-    public String getName() {
+    public String getName()
+    {
         return ArmoredOutputStreamUTF8Test.class.getSimpleName();
     }
 
-    @Override
-    public void performTest() throws Exception {
+    public void performTest()
+        throws Exception
+    {
         String utf8WithUmlauts = "ÄÖÜß&Umlauts";
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -40,15 +43,17 @@ public class ArmoredOutputStreamUTF8Test extends SimpleTest {
 
         String[] lines = armoredOutput.split("\n");
         String comment = null;
-        for (String line : lines) {
-            if (line.startsWith("Comment: ")) {
+        for (String line : lines)
+        {
+            if (line.startsWith("Comment: "))
+            {
                 comment = line.substring("Comment: ".length());
                 break;
             }
         }
 
         isTrue("We did not find the comment line. This MUST never happen.", comment != null);
-        isEquals("Comment was not properly encoded. Expected: "  +utf8WithUmlauts + ", Actual: " + comment, comment, utf8WithUmlauts);
+        isEquals("Comment was not properly encoded. Expected: " + utf8WithUmlauts + ", Actual: " + comment, comment, utf8WithUmlauts);
 
         // round-tripped comment from ascii armor input stream
         isEquals(header[1].substring("Comment: ".length()), utf8WithUmlauts);
