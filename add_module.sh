54664764d2c597d13e220c3ecbc959f35f5cfae5
David Hook <dgh@cryptoworkshop.com>
2022-05-03 11:00:07 +1000

    fixed to deal with src.zip in jmail dist

--- add_module.sh
+++ add_module.sh
@@ -44,8 +44,10 @@ rm -rf module.tmp
 	javac  -target 1.9 --module-path ${provJar}:$utilJar:$pkixJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
     elif [ $1 = "jmail" ]
     then
+        ( cd build/artifacts/jdk1.8/bcjmail-jdk18on-*; mkdir src; cd src; unzip ../src.zip )
         jmailSrc=`echo build/artifacts/jdk1.8/bcjmail-jdk18on-*/src/`
 	javac  -target 1.9 --module-path ${provJar}:$utilJar:$pkixJar:libs/jakarta.mail-2.0.1.jar:libs/jakarta.activation-api-2.0.0.jar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$jmailSrc
+        rm -r build/artifacts/jdk1.8/bcjmail-jdk18on-*/src
     elif [ $1 = "pkix" ]
     then
 	javac  -target 1.9 --module-path ${provJar}:$utilJar -classpath module.tmp/v5 -d module.tmp/v9 `find $1/src/main/jdk1.9 -name "*.java"` -sourcepath $1/src/main/jdk1.9:$1/src/main/java
