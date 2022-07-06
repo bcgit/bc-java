4c67e96381a00a2b3d31dd8284d7dfe840baf0f7
David Hook <dgh@cryptoworkshop.com>
2022-07-06 18:51:06 +1000

    removed public field, minor reformatting.

--- core/src/main/java/org/bouncycastle/pqc/crypto/saber/SABERPrivateKeyParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/saber/SABERPrivateKeyParameters.java
@@ -7,15 +7,15 @@ public class SABERPrivateKeyParameters
 {
     private byte[] privateKey;
 
-    public byte[] getPrivateKey()
-    {
-        return Arrays.clone(privateKey);
-    }
-
     public SABERPrivateKeyParameters(SABERParameters params, byte[] privateKey)
     {
         super(true, params);
         this.privateKey = Arrays.clone(privateKey);
+    }
+
+    public byte[] getPrivateKey()
+    {
+        return Arrays.clone(privateKey);
     }
 
     public byte[] getEncoded()
