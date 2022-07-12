3a524aa4da45c776c3db0dc633cdd3c84647e802
David Hook <dgh@cryptoworkshop.com>
2022-07-12 18:25:12 +1000

    place holder class for Falcon directory

---
+++ core/src/main/java/org/bouncycastle/pqc/crypto/falcon/ComplexNumberWrapper.java
@@ -0,0 +1,11 @@
+package org.bouncycastle.pqc.crypto.falcon;
+
+class ComplexNumberWrapper {
+    FalconFPR re;
+    FalconFPR im;
+
+    ComplexNumberWrapper(FalconFPR re, FalconFPR im) {
+        this.re = re;
+        this.im = im;
+    };
+}
