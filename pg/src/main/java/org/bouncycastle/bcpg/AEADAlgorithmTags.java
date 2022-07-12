1f699d8490e94221ce1e869bf0238362c9ae1776
David Hook <dgh@cryptoworkshop.com>
2022-07-13 06:53:47 +1000

    minor reformating - relates to gihub #1132

--- pg/src/main/java/org/bouncycastle/bcpg/AEADAlgorithmTags.java
+++ pg/src/main/java/org/bouncycastle/bcpg/AEADAlgorithmTags.java
@@ -1,6 +1,7 @@
 package org.bouncycastle.bcpg;
 
-public interface AEADAlgorithmTags {
+public interface AEADAlgorithmTags
+{
     int EAX = 1;    // EAX (IV len: 16 octets, Tag len: 16 octets)
     int OCB = 2;    // OCB (IV len: 15 octets, Tag len: 16 octets)
     int GCM = 3;    // GCM (IV len: 12 octets, Tag len: 16 octets)
