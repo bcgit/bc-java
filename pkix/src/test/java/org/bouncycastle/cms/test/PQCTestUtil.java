0546fc69145af14c657131bf46b4f33b3c130b9f
David Hook <dgh@cryptoworkshop.com>
2022-07-14 15:40:12 +1000

    added Falcon support for CMS

--- pkix/src/test/java/org/bouncycastle/cms/test/PQCTestUtil.java
+++ pkix/src/test/java/org/bouncycastle/cms/test/PQCTestUtil.java
@@ -3,6 +3,7 @@ package org.bouncycastle.cms.test;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.Date;
@@ -17,7 +18,10 @@ import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.X509KeyUsage;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pqc.jcajce.interfaces.FalconKey;
+import org.bouncycastle.pqc.jcajce.spec.FalconParameterSpec;
 import org.bouncycastle.pqc.jcajce.spec.SPHINCS256KeyGenParameterSpec;
+import org.bouncycastle.pqc.jcajce.spec.SPHINCSPlusParameterSpec;
 
 public class PQCTestUtil
 {
@@ -31,13 +35,42 @@ public class PQCTestUtil
         return kpGen.generateKeyPair();
     }
 
+    public static KeyPair makeSphincsPlusKeyPair()
+        throws Exception
+    {
+        KeyPairGenerator kpGen = KeyPairGenerator.getInstance("SPHINCSPlus", "BCPQC");
+
+        kpGen.initialize(SPHINCSPlusParameterSpec.sha2_128f, new SecureRandom());
+
+        return kpGen.generateKeyPair();
+    }
+
+    public static KeyPair makeFalconKeyPair()
+        throws Exception
+    {
+        KeyPairGenerator kpGen = KeyPairGenerator.getInstance("Falcon", "BCPQC");
+
+        kpGen.initialize(FalconParameterSpec.falcon_512, new SecureRandom());
+
+        return kpGen.generateKeyPair();
+    }
+
     public static X509Certificate makeCertificate(KeyPair subKP, String subDN, KeyPair issKP, String issDN)
         throws Exception
     {
         //
         // create base certificate - version 3
         //
-        ContentSigner sigGen = new JcaContentSignerBuilder("SHA512withSPHINCS256").setProvider("BCPQC").build(issKP.getPrivate());
+        ContentSigner sigGen;
+        PrivateKey    issPriv = issKP.getPrivate();
+        if (issPriv instanceof FalconKey)
+        {
+            sigGen = new JcaContentSignerBuilder("Falcon").setProvider("BCPQC").build(issPriv);
+        }
+        else
+        {
+            sigGen = new JcaContentSignerBuilder("SHA512withSPHINCS256").setProvider("BCPQC").build(issPriv);
+        }
 
         X509v3CertificateBuilder certGen = new JcaX509v3CertificateBuilder(new X500Name(issDN), BigInteger.valueOf(1), new Date(System.currentTimeMillis() - 50000), new Date(System.currentTimeMillis() + 50000), new X500Name(subDN), subKP.getPublic())
             .addExtension(new ASN1ObjectIdentifier("2.5.29.15"), true,
