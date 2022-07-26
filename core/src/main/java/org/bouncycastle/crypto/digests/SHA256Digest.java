ff46d6449b670e52b3dbe468517d1329bd61044e
Peter Dettman <peter.dettman@bouncycastle.org>
2022-07-27 01:33:50 +0700

    Refactoring digests

--- core/src/main/java/org/bouncycastle/crypto/digests/SHA256Digest.java
+++ core/src/main/java/org/bouncycastle/crypto/digests/SHA256Digest.java
@@ -103,13 +103,7 @@ public class SHA256Digest
         byte[]  in,
         int     inOff)
     {
-        // Note: Inlined for performance
-//        X[xOff] = Pack.bigEndianToInt(in, inOff);
-        int n = in[inOff] << 24;
-        n |= (in[++inOff] & 0xff) << 16;
-        n |= (in[++inOff] & 0xff) << 8;
-        n |= (in[++inOff] & 0xff);
-        X[xOff] = n;
+        X[xOff] = Pack.bigEndianToInt(in, inOff);
 
         if (++xOff == 16)
         {
@@ -129,9 +123,7 @@ public class SHA256Digest
         X[15] = (int)(bitLength & 0xffffffff);
     }
 
-    public int doFinal(
-        byte[]  out,
-        int     outOff)
+    public int doFinal(byte[] out, int outOff)
     {
         finish();
 
