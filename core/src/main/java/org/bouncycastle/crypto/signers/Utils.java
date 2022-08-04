b50e4e0122f50f8ce52ec104f2b3053e9a6787e0
David Hook <dgh@cryptoworkshop.com>
2022-08-04 19:47:40 +1000

    further work on constraint addition, refactoring.

--- core/src/main/java/org/bouncycastle/crypto/signers/Utils.java
+++ core/src/main/java/org/bouncycastle/crypto/signers/Utils.java
@@ -4,47 +4,24 @@ import java.math.BigInteger;
 
 import org.bouncycastle.crypto.CryptoServiceProperties;
 import org.bouncycastle.crypto.CryptoServicePurpose;
+import org.bouncycastle.crypto.constraints.ConstraintUtils;
 import org.bouncycastle.math.ec.ECCurve;
 
 class Utils
 {
     static CryptoServiceProperties getDefaultProperties(String algorithm, BigInteger p, boolean forSigning)
     {
-        return new DefaultServiceProperties(algorithm, bitsOfSecurity(p), forSigning);
+        return new DefaultServiceProperties(algorithm, ConstraintUtils.bitsOfSecurityFor(p), forSigning);
     }
 
     static CryptoServiceProperties getDefaultProperties(String algorithm, ECCurve curve, boolean forSigning)
     {
-        return new DefaultServiceProperties(algorithm, bitsOfSecurity(curve), forSigning);
+        return new DefaultServiceProperties(algorithm, ConstraintUtils.bitsOfSecurityFor(curve), forSigning);
     }
 
     static CryptoServiceProperties getDefaultProperties(String algorithm, int bitsOfSecurity, boolean forSigning)
     {
         return new DefaultServiceProperties(algorithm, bitsOfSecurity, forSigning);
-    }
-
-    static int bitsOfSecurity(BigInteger p)
-    {
-        int pBits = p.bitLength();
-
-        if (pBits >= 2048)
-        {
-            return (pBits >= 3072) ?
-                        ((pBits >= 7680) ?
-                            ((pBits >= 15360) ? 256
-                            : 192)
-                        : 128)
-                   : 112;
-        }
-
-        return (pBits >= 1024) ? 80 : 20;      // TODO: possibly a bit harsh...
-    }
-
-    static int bitsOfSecurity(ECCurve curve)
-    {
-        int pBits = curve.getFieldSize();
-
-        return (pBits + 1) / 2;  // TODO: not quite right!
     }
 
     // Service Definitions
@@ -65,46 +42,6 @@ class Utils
         public int bitsOfSecurity()
         {
             return bitsOfSecurity;
-        }
-
-        public String getServiceName()
-        {
-            return algorithm;
-        }
-
-        public CryptoServicePurpose getPurpose()
-        {
-            return forSigning ? CryptoServicePurpose.SIGNING : CryptoServicePurpose.VERIFYING;
-        }
-    }
-
-    private static class ECServiceProperties
-        implements CryptoServiceProperties
-    {
-        private boolean forSigning;
-        private final String algorithm;
-        private int pBits;
-
-        ECServiceProperties(String algorithm, BigInteger p, boolean forSigning)
-        {
-            this.algorithm = algorithm;
-            this.pBits = p.bitLength();
-            this.forSigning = forSigning;
-        }
-
-        public int bitsOfSecurity()
-        {
-            if (pBits >= 2048)
-            {
-                return (pBits >= 3072) ?
-                            ((pBits >= 7680) ?
-                                ((pBits >= 15360) ? 256
-                                : 192)
-                            : 128)
-                       : 112;
-            }
-
-            return (pBits >= 1024) ? 80 : 20;      // TODO: possibly a bit harsh...
         }
 
         public String getServiceName()
