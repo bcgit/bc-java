5770c17b9d0144cf5c0392aedbe7189ddd0808a2
David Hook <dgh@cryptoworkshop.com>
2022-08-06 16:52:41 +1000

    added missing AllTests.

---
+++ pkix/src/test/java/org/bouncycastle/pkix/test/AllTests.java
@@ -0,0 +1,46 @@
+package org.bouncycastle.pkix.test;
+
+import java.security.Security;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllTests
+    extends TestCase
+{
+    public static void main (String[] args)
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("PKCS Tests");
+        
+        suite.addTestSuite(CheckerTest.class);
+        suite.addTestSuite(RevocationTest.class);
+
+        return new BCTestSetup(suite);
+    }
+
+    static class BCTestSetup
+        extends TestSetup
+    {
+        public BCTestSetup(Test test)
+        {
+            super(test);
+        }
+
+        protected void setUp()
+        {
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        }
+
+        protected void tearDown()
+        {
+            Security.removeProvider("BC");
+        }
+    }
+}
