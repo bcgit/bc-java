69b81dace57512a429ba996369fba247cfebcd54
Peter Dettman <peter.dettman@bouncycastle.org>
2022-08-26 11:40:54 +0700

    Handle zero-distance rotates correctly

--- core/src/main/jdk1.4/org/bouncycastle/util/Longs.java
+++ core/src/main/jdk1.4/org/bouncycastle/util/Longs.java
@@ -65,12 +65,12 @@ public class Longs
 
     public static long rotateLeft(long i, int distance)
     {
-        return (i << distance) ^ (i >>> -distance);
+        return (i << distance) | (i >>> -distance);
     }
 
     public static long rotateRight(long i, int distance)
     {
-        return (i >>> distance) ^ (i << -distance);
+        return (i >>> distance) | (i << -distance);
     }
 
     public static Long valueOf(long value)
