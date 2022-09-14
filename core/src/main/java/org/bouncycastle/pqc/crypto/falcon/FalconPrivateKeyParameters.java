cef906836b396fdcc9fcb80c6bf73335da012958
David Hook <dgh@cryptoworkshop.com>
2022-09-15 08:01:14 +1000

    added carriage of the public key to Dilithium and Falcon private keys.

--- core/src/main/java/org/bouncycastle/pqc/crypto/falcon/FalconPrivateKeyParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/falcon/FalconPrivateKeyParameters.java
@@ -6,16 +6,23 @@ public class FalconPrivateKeyParameters
     extends FalconKeyParameters
 {
 
-    private byte[] sk;
+    private final byte[] sk;
+    private final byte[] pk;
 
-    public FalconPrivateKeyParameters(FalconParameters parameters, byte[] sk_encoded)
+    public FalconPrivateKeyParameters(FalconParameters parameters, byte[] sk_encoded, byte[] pk_encoded)
     {
         super(true, parameters);
         this.sk = Arrays.clone(sk_encoded);
+        this.pk = Arrays.clone(pk_encoded);
     }
 
     public byte[] getEncoded()
     {
         return Arrays.clone(sk);
+    }
+
+    public byte[] getPublicKey()
+    {
+        return Arrays.clone(pk);
     }
 }
