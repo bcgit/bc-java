45769a45d8f7cd2bdba9cd73694639ec9c8abf7c
David Hook <dgh@cryptoworkshop.com>
2022-09-20 02:56:26 +1000

    Added HQC support to BCPQC provider.

---
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/hqc/BCHQCPublicKey.java
@@ -0,0 +1,125 @@
+package org.bouncycastle.pqc.jcajce.provider.hqc;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.PublicKey;
+
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.pqc.crypto.hqc.HQCPublicKeyParameters;
+import org.bouncycastle.pqc.crypto.util.PublicKeyFactory;
+import org.bouncycastle.pqc.crypto.util.SubjectPublicKeyInfoFactory;
+import org.bouncycastle.pqc.jcajce.interfaces.HQCKey;
+import org.bouncycastle.pqc.jcajce.spec.HQCParameterSpec;
+import org.bouncycastle.util.Arrays;
+
+public class BCHQCPublicKey
+    implements PublicKey, HQCKey
+{
+    private static final long serialVersionUID = 1L;
+
+    private transient HQCPublicKeyParameters params;
+
+    public BCHQCPublicKey(
+        HQCPublicKeyParameters params)
+    {
+        this.params = params;
+    }
+
+    public BCHQCPublicKey(SubjectPublicKeyInfo keyInfo)
+        throws IOException
+    {
+        init(keyInfo);
+    }
+
+    private void init(SubjectPublicKeyInfo keyInfo)
+        throws IOException
+    {
+        this.params = (HQCPublicKeyParameters) PublicKeyFactory.createKey(keyInfo);
+    }
+
+    /**
+     * Compare this HQC public key with another object.
+     *
+     * @param o the other object
+     * @return the result of the comparison
+     */
+    public boolean equals(Object o)
+    {
+        if (o == this)
+        {
+            return true;
+        }
+
+        if (o instanceof BCHQCPublicKey)
+        {
+            BCHQCPublicKey otherKey = (BCHQCPublicKey)o;
+
+            return Arrays.areEqual(params.getEncoded(), otherKey.params.getEncoded());
+        }
+
+        return false;
+    }
+
+    public int hashCode()
+    {
+        return Arrays.hashCode(params.getEncoded());
+    }
+
+    /**
+     * @return name of the algorithm - "HQC"
+     */
+    public final String getAlgorithm()
+    {
+        return "HQC";
+    }
+
+    public byte[] getEncoded()
+    {
+        try
+        {
+            SubjectPublicKeyInfo pki = SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(params);
+
+            return pki.getEncoded();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+    public String getFormat()
+    {
+        return "X.509";
+    }
+
+    public HQCParameterSpec getParameterSpec()
+    {
+        return HQCParameterSpec.fromName(params.getParameters().getName());
+    }
+
+    HQCPublicKeyParameters getKeyParams()
+    {
+        return params;
+    }
+
+    private void readObject(
+        ObjectInputStream in)
+        throws IOException, ClassNotFoundException
+    {
+        in.defaultReadObject();
+
+        byte[] enc = (byte[])in.readObject();
+
+        init(SubjectPublicKeyInfo.getInstance(enc));
+    }
+
+    private void writeObject(
+        ObjectOutputStream out)
+        throws IOException
+    {
+        out.defaultWriteObject();
+
+        out.writeObject(this.getEncoded());
+    }
+}
