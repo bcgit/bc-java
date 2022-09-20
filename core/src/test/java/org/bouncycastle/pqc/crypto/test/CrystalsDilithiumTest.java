92209fbd07cf7274efd3d902334a0619a1f5e10f
royb <roy.basmacier@primekey.com>
2022-09-21 00:00:09 +0200

    made some changes for dilithium aes

--- core/src/test/java/org/bouncycastle/pqc/crypto/test/CrystalsDilithiumTest.java
+++ core/src/test/java/org/bouncycastle/pqc/crypto/test/CrystalsDilithiumTest.java
@@ -72,20 +72,20 @@ public class CrystalsDilithiumTest
         throws Exception
     {
         String[] files = new String[]{
-            "PQCsignKAT_Dilithium2.rsp",
-//            "PQCsignKAT_Dilithium2aes.rsp",
-            "PQCsignKAT_Dilithium3.rsp",
-//            "PQCsignKAT_Dilithium3aes.rsp",
-            "PQCsignKAT_Dilithium5.rsp"
-//            "PQCsignKAT_Dilithium5aes.rsp"
+//            "PQCsignKAT_Dilithium2.rsp",
+            "PQCsignKAT_Dilithium2-AES.rsp",
+//            "PQCsignKAT_Dilithium3.rsp",
+            "PQCsignKAT_Dilithium3-AES.rsp",
+//            "PQCsignKAT_Dilithium5.rsp"
+            "PQCsignKAT_Dilithium5-AES.rsp"
         };
         DilithiumParameters[] parameters = new DilithiumParameters[]{
-            DilithiumParameters.dilithium2,
-//            DilithiumParameters.dilithium2_aes,
-            DilithiumParameters.dilithium3,
-//            DilithiumParameters.dilithium3_aes,
-            DilithiumParameters.dilithium5
-//            DilithiumParameters.dilithium5_aes
+//            DilithiumParameters.dilithium2,
+            DilithiumParameters.dilithium2_aes,
+//            DilithiumParameters.dilithium3,
+            DilithiumParameters.dilithium3_aes,
+//            DilithiumParameters.dilithium5
+            DilithiumParameters.dilithium5_aes
         };
 
         TestSampler sampler = new TestSampler();
