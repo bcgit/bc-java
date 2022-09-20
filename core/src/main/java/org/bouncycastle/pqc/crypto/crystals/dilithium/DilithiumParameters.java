2a3ff10a604e5e3cfcdb6adcbd3aede71bed0cc2
David Hook <dgh@cryptoworkshop.com>
2022-09-20 16:02:34 +1000

    refactored to make randomization dependent on setting of SecureRandom.

--- core/src/main/java/org/bouncycastle/pqc/crypto/crystals/dilithium/DilithiumParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/crystals/dilithium/DilithiumParameters.java
@@ -4,39 +4,30 @@ import java.security.SecureRandom;
 
 public class DilithiumParameters
 {
-    public static final DilithiumParameters dilithium2 = new DilithiumParameters("dilithium2shake", 2, false, false);
-    public static final DilithiumParameters dilithium2R = new DilithiumParameters("dilithium2shakeR", 2, true, false);
-    public static final DilithiumParameters dilithium2aes = new DilithiumParameters("dilithium2aes", 2, false, true);
-    public static final DilithiumParameters dilithium2aesR = new DilithiumParameters("dilithium2aesR", 2, true, true);
+    public static final DilithiumParameters dilithium2 = new DilithiumParameters("dilithium2", 2, false);
+    public static final DilithiumParameters dilithium2_aes = new DilithiumParameters("dilithium2-aes", 2, true);
 
-    public static final DilithiumParameters dilithium3 = new DilithiumParameters("dilithium3shake", 3, false, false);
-    public static final DilithiumParameters dilithium3R = new DilithiumParameters("dilithium3shakeR", 3, true, false);
-    public static final DilithiumParameters dilithium3aes = new DilithiumParameters("dilithium3aes", 3, false, true);
-    public static final DilithiumParameters dilithium3aesR = new DilithiumParameters("dilithium3aesR", 3, true, true);
+    public static final DilithiumParameters dilithium3 = new DilithiumParameters("dilithium3", 3, false);
+    public static final DilithiumParameters dilithium3_aes = new DilithiumParameters("dilithium3-aes", 3, true);
 
-    public static final DilithiumParameters dilithium5 = new DilithiumParameters("dilithium5shake", 5, false, false);
-    public static final DilithiumParameters dilithium5R = new DilithiumParameters("dilithium5shakeR", 5, true, false);
-    public static final DilithiumParameters dilithium5aes = new DilithiumParameters("dilithium5aes", 5, false, true);
-    public static final DilithiumParameters dilithium5aesR = new DilithiumParameters("dilithium5aesR", 5, true, true);
+    public static final DilithiumParameters dilithium5 = new DilithiumParameters("dilithium5", 5, false);
+    public static final DilithiumParameters dilithium5_aes = new DilithiumParameters("dilithium5-aes", 5, true);
 
     private final int k;
     private final String name;
 
-    private final boolean randomizedSigning;
     private final boolean usingAES;// or shake
 
-    private DilithiumParameters(String name, int k, boolean randomizedSigning, boolean usingAES)
+    private DilithiumParameters(String name, int k, boolean usingAES)
     {
         this.name = name;
         this.k = k;
-        this.randomizedSigning = randomizedSigning;
         this.usingAES = usingAES;
-
     }
 
     DilithiumEngine getEngine(SecureRandom random)
     {
-        return new DilithiumEngine(k, random, randomizedSigning, usingAES);
+        return new DilithiumEngine(k, random, usingAES);
     }
 
     public String getName()
