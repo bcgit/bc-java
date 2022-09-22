1e12a5035f7460744c3dafc993bfc06d3327f91c
David Hook <dgh@cryptoworkshop.com>
2022-09-23 07:37:54 +1000

    updated Kyber private key encoding.

--- core/src/main/java/org/bouncycastle/pqc/crypto/crystals/kyber/KyberPrivateKeyParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/crystals/kyber/KyberPrivateKeyParameters.java
@@ -5,28 +5,59 @@ import org.bouncycastle.util.Arrays;
 public class KyberPrivateKeyParameters
     extends KyberKeyParameters
 {
-    final byte[] privateKey;
-    final byte[] publicKey;
+    final byte[] s;
+    final byte[] hpk;
+    final byte[] nonce;
+    final byte[] t;
+    final byte[] rho;
 
-    public KyberPrivateKeyParameters(KyberParameters params, byte[] privateKey, byte[] publicKey)
+    public KyberPrivateKeyParameters(KyberParameters params, byte[] s, byte[] hpk, byte[] nonce, byte[] t, byte[] rho)
     {
         super(true, params);
-        this.privateKey = Arrays.clone(privateKey);
-        this.publicKey = publicKey;
+        this.s = Arrays.clone(s);
+        this.hpk = Arrays.clone(hpk);
+        this.nonce = Arrays.clone(nonce);
+        this.t = Arrays.clone(t);
+        this.rho = Arrays.clone(rho);
+    }
+
+    public byte[] getT()
+    {
+        return Arrays.clone(t);
+    }
+
+    public byte[] getRho()
+    {
+        return Arrays.clone(rho);
     }
 
     public byte[] getPrivateKey()
     {
-        return Arrays.clone(privateKey);
+        return Arrays.concatenate(s, getPublicKey(), hpk, nonce);
     }
 
     public byte[] getEncoded()
     {
-        return Arrays.clone(privateKey);
+        return getPrivateKey();
     }
 
     public byte[] getPublicKey()
     {
-        return Arrays.clone(publicKey);
+        return Arrays.concatenate(t, rho);
+    }
+
+    public byte[] getS()
+    {
+        return Arrays.clone(s);
+    }
+
+    public byte[] getHPK()
+    {
+        return Arrays.clone(hpk);
+    }
+
+    public byte[] getNonce()
+    {
+        return Arrays.clone(nonce);
     }
 }
