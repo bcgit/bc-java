8cd4565b85ef49f48faaba6a59b5ba2b050be576
royb <roy.basmacier@primekey.com>
2022-09-22 15:01:01 -0400

    fixed kyber aes

--- core/src/test/java/org/bouncycastle/pqc/crypto/test/CrystalsKyberTest.java
+++ core/src/test/java/org/bouncycastle/pqc/crypto/test/CrystalsKyberTest.java
@@ -103,18 +103,18 @@ public class CrystalsKyberTest
             KyberParameters.kyber512,
             KyberParameters.kyber768,
             KyberParameters.kyber1024,
-//            KyberParameters.kyber512,
-//            KyberParameters.kyber768,
-//            KyberParameters.kyber1024,
+            KyberParameters.kyber512_aes,
+            KyberParameters.kyber768_aes,
+            KyberParameters.kyber1024_aes,
         };
 
         String[] files = new String[]{
             "kyber512.rsp",
             "kyber768.rsp",
             "kyber1024.rsp",
-//            "kyber512aes.rsp",
-//            "kyber768aes.rsp",
-//            "kyber1024aes.rsp",
+            "kyber512aes.rsp",
+            "kyber768aes.rsp",
+            "kyber1024aes.rsp",
         };
 
         TestSampler sampler = new TestSampler();
