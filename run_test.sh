ab485a5d2d5ad2b2f9caa000c78b69c62d0d6bb3
David Hook <dgh@cryptoworkshop.com>
2022-09-26 15:35:58 +1000

    final 1.72 changes

--- run_test.sh
+++ run_test.sh
@@ -19,8 +19,8 @@ q
   done
 )
 
-javac -d /tmp/classes.jakarta -classpath build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:build/artifacts/jdk1.8/jars/bcutil-jdk18on-171.jar:build/artifacts/jdk1.8/jars/bcpkix-jdk18on-171.jar:build/artifacts/jdk1.8/jars/bcjmail-jdk18on-171.jar:/opt/junit/junit.jar:libs/jakarta.mail-2.0.1.jar:libs/jakarta.activation-api-2.0.0.jar /tmp/jakarta.test/org/bouncycastle/mail/smime/test/*.java
-java -cp /tmp/classes.jakarta:build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:build/artifacts/jdk1.8/jars/bcpkix-jdk18on-171.jar:build/artifacts/jdk1.8/jars/bcutil-jdk18on-171.jar:build/artifacts/jdk1.8/jars/bcjmail-jdk18on-171.jar:/opt/junit/junit.jar:libs/jakarta.mail-2.0.1.jar:libs/jakarta.activation-api-2.0.0.jar:libs/activation.jar org.bouncycastle.mail.smime.test.AllTests
+javac -d /tmp/classes.jakarta -classpath build/artifacts/jdk1.8/jars/bcprov-jdk18on-172.jar:build/artifacts/jdk1.8/jars/bcutil-jdk18on-172.jar:build/artifacts/jdk1.8/jars/bcpkix-jdk18on-172.jar:build/artifacts/jdk1.8/jars/bcjmail-jdk18on-172.jar:/opt/junit/junit.jar:libs/jakarta.mail-2.0.1.jar:libs/jakarta.activation-api-2.0.0.jar /tmp/jakarta.test/org/bouncycastle/mail/smime/test/*.java
+java -cp /tmp/classes.jakarta:build/artifacts/jdk1.8/jars/bcprov-jdk18on-172.jar:build/artifacts/jdk1.8/jars/bcpkix-jdk18on-172.jar:build/artifacts/jdk1.8/jars/bcutil-jdk18on-172.jar:build/artifacts/jdk1.8/jars/bcjmail-jdk18on-172.jar:/opt/junit/junit.jar:libs/jakarta.mail-2.0.1.jar:libs/jakarta.activation-api-2.0.0.jar:libs/activation.jar org.bouncycastle.mail.smime.test.AllTests
 
 )
 
@@ -28,8 +28,8 @@ java -cp /tmp/classes.jakarta:build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar
 export JAVA_HOME=/opt/jdk-11.0.1
 export PATH=$JAVA_HOME/bin:$PATH
 
-javac -d /tmp/classes.jdk11 -classpath build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:/opt/junit/junit.jar prov/src/test/jdk1.11/org/bouncycastle/jcajce/provider/test/XDHKeyTest.java
-java -cp /tmp/classes.jdk11:build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:/opt/junit/junit.jar org.bouncycastle.jcajce.provider.test.XDHKeyTest
+javac -d /tmp/classes.jdk11 -classpath build/artifacts/jdk1.8/jars/bcprov-jdk18on-172.jar:/opt/junit/junit.jar prov/src/test/jdk1.11/org/bouncycastle/jcajce/provider/test/XDHKeyTest.java
+java -cp /tmp/classes.jdk11:build/artifacts/jdk1.8/jars/bcprov-jdk18on-172.jar:/opt/junit/junit.jar org.bouncycastle.jcajce.provider.test.XDHKeyTest
 
 )
 
@@ -37,6 +37,6 @@ java -cp /tmp/classes.jdk11:build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:/
 export JAVA_HOME=/opt/jdk-15
 export PATH=$JAVA_HOME/bin:$PATH
 
-javac -d /tmp/classes.jdk15 -classpath build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:/opt/junit/junit.jar prov/src/test/jdk1.15/org/bouncycastle/jcajce/provider/test/EdDSA15Test.java
-java -cp /tmp/classes.jdk15:build/artifacts/jdk1.8/jars/bcprov-jdk18on-171.jar:/opt/junit/junit.jar org.bouncycastle.jcajce.provider.test.EdDSA15Test
+javac -d /tmp/classes.jdk15 -classpath build/artifacts/jdk1.8/jars/bcprov-jdk18on-172.jar:/opt/junit/junit.jar prov/src/test/jdk1.15/org/bouncycastle/jcajce/provider/test/EdDSA15Test.java
+java -cp /tmp/classes.jdk15:build/artifacts/jdk1.8/jars/bcprov-jdk18on-172.jar:/opt/junit/junit.jar org.bouncycastle.jcajce.provider.test.EdDSA15Test
 )
