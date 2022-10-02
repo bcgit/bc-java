4b505a6a5cb1fcd823efd4fe9c617509f1b77ce3
David Hook <dgh@cryptoworkshop.com>
2022-10-02 12:26:27 +1100

    removed deprecated classes

--- core/src/test/java/org/bouncycastle/crypto/test/RSAKeyEncapsulationTest.java
+++ core/src/test/java/org/bouncycastle/crypto/test/RSAKeyEncapsulationTest.java
@@ -4,12 +4,15 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.SecretWithEncapsulation;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.generators.KDF2BytesGenerator;
 import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
-import org.bouncycastle.crypto.kems.RSAKeyEncapsulation;
+import org.bouncycastle.crypto.kems.RSAKEMExtractor;
+import org.bouncycastle.crypto.kems.RSAKEMGenerator;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
 import org.bouncycastle.util.test.SimpleTest;
 
 /**
@@ -32,20 +35,21 @@ public class RSAKeyEncapsulationTest
         AsymmetricCipherKeyPair    keys   = rsaGen.generateKeyPair();
         
         // Set RSA-KEM parameters
-        RSAKeyEncapsulation     kem;
+        RSAKEMGenerator kemGen;
+        RSAKEMExtractor kemExt;
         KDF2BytesGenerator        kdf = new KDF2BytesGenerator(new SHA1Digest());
         SecureRandom            rnd = new SecureRandom();
         byte[]                    out = new byte[128];
         KeyParameter            key1, key2;
         
         // Test RSA-KEM
-        kem = new RSAKeyEncapsulation(kdf, rnd);
+        kemGen = new RSAKEMGenerator(128 / 8, kdf, rnd);
         
-        kem.init(keys.getPublic());
-        key1 = (KeyParameter)kem.encrypt(out, 128);
+        SecretWithEncapsulation secEnc = kemGen.generateEncapsulated(keys.getPublic());
+        key1 = new KeyParameter(secEnc.getSecret());
         
-        kem.init(keys.getPrivate());
-        key2 = (KeyParameter)kem.decrypt(out, 128);
+        kemExt = new RSAKEMExtractor((RSAKeyParameters)keys.getPrivate(), 128 / 8, kdf);
+        key2 = new KeyParameter(kemExt.extractSecret(secEnc.getEncapsulation()));
 
         if (!areEqual(key1.getKey(), key2.getKey()))
         {
