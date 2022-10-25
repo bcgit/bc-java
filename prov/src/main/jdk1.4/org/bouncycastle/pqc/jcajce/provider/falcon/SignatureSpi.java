ded283a3bfc27b8a19ce7aed669eba57ff339174
David Hook <dgh@cryptoworkshop.com>
2022-10-25 13:47:01 +1100

    Java 4 compatibility files.

---
+++ prov/src/main/jdk1.4/org/bouncycastle/pqc/jcajce/provider/falcon/SignatureSpi.java
@@ -0,0 +1,203 @@
+package org.bouncycastle.pqc.jcajce.provider.falcon;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.SignatureException;
+import java.security.spec.AlgorithmParameterSpec;
+
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.params.ParametersWithRandom;
+import org.bouncycastle.pqc.crypto.falcon.FalconParameters;
+import org.bouncycastle.pqc.crypto.falcon.FalconPrivateKeyParameters;
+import org.bouncycastle.pqc.crypto.falcon.FalconSigner;
+import org.bouncycastle.pqc.jcajce.provider.dilithium.BCDilithiumPublicKey;
+import org.bouncycastle.util.Strings;
+
+public class SignatureSpi
+    extends java.security.Signature
+{
+    private ByteArrayOutputStream bOut;
+    private FalconSigner signer;
+    private SecureRandom random;
+    private FalconParameters parameters;
+
+    protected SignatureSpi(FalconSigner signer)
+    {
+        super("FALCON");
+        
+        this.bOut = new ByteArrayOutputStream();
+        this.signer = signer;
+        this.parameters = null;
+    }
+
+    protected SignatureSpi(FalconSigner signer, FalconParameters parameters)
+    {
+        super(Strings.toUpperCase(parameters.getName()));
+        this.parameters = parameters;
+
+        this.bOut = new ByteArrayOutputStream();
+        this.signer = signer;
+    }
+
+    protected void engineInitVerify(PublicKey publicKey)
+        throws InvalidKeyException
+    {
+        if (!(publicKey instanceof BCFalconPublicKey))
+        {
+            try
+            {
+                publicKey = new BCFalconPublicKey(SubjectPublicKeyInfo.getInstance(publicKey.getEncoded()));
+            }
+            catch (Exception e)
+            {
+                throw new InvalidKeyException("unknown public key passed to Falcon: " + e.getMessage());
+            }
+        }
+
+        BCFalconPublicKey key = (BCFalconPublicKey)publicKey;
+
+        if (parameters != null)
+        {
+            String canonicalAlg = Strings.toUpperCase(parameters.getName());
+            if (!canonicalAlg.equals(key.getAlgorithm()))
+            {
+                throw new InvalidKeyException("signature configured for " + canonicalAlg);
+            }
+        }
+
+        signer.init(false, key.getKeyParams());
+    }
+
+    protected void engineInitSign(PrivateKey privateKey, SecureRandom random)
+        throws InvalidKeyException
+    {
+        this.random = random;
+        engineInitSign(privateKey);
+    }
+
+    protected void engineInitSign(PrivateKey privateKey)
+        throws InvalidKeyException
+    {
+        if (privateKey instanceof BCFalconPrivateKey)
+        {
+            BCFalconPrivateKey key = (BCFalconPrivateKey)privateKey;
+            CipherParameters param = key.getKeyParams();
+
+            if (parameters != null)
+            {
+                String canonicalAlg = Strings.toUpperCase(parameters.getName());
+                if (!canonicalAlg.equals(key.getAlgorithm()))
+                {
+                    throw new InvalidKeyException("signature configured for " + canonicalAlg);
+                }
+            }
+
+            if (random != null)
+            {
+                signer.init(true, new ParametersWithRandom(param, random));
+            }
+            else
+            {
+                signer.init(true, param);
+            }
+        }
+        else
+        {
+            throw new InvalidKeyException("unknown private key passed to Falcon");
+        }
+    }
+
+    protected void engineUpdate(byte b)
+            throws SignatureException
+    {
+        bOut.write(b);
+    }
+
+    protected void engineUpdate(byte[] b, int off, int len)
+            throws SignatureException
+    {
+        bOut.write(b, off, len);
+    }
+
+    protected byte[] engineSign()
+        throws SignatureException
+    {
+        try
+        {
+            byte[] message = bOut.toByteArray();
+
+            bOut.reset();
+
+            return signer.generateSignature(message);
+        }
+        catch (Exception e)
+        {
+            throw new SignatureException(e.toString());
+        }
+    }
+
+    protected boolean engineVerify(byte[] sigBytes)
+        throws SignatureException
+    {
+        byte[] message = bOut.toByteArray();
+
+        bOut.reset();
+
+        return signer.verifySignature(message, sigBytes);
+    }
+
+    protected void engineSetParameter(AlgorithmParameterSpec params)
+    {
+        // TODO
+        throw new UnsupportedOperationException("engineSetParameter unsupported");
+    }
+
+    /**
+     * @deprecated replaced with #engineSetParameter(java.security.spec.AlgorithmParameterSpec)
+     */
+    protected void engineSetParameter(String param, Object value)
+    {
+        throw new UnsupportedOperationException("engineSetParameter unsupported");
+    }
+
+    /**
+     * @deprecated
+     */
+    protected Object engineGetParameter(String param)
+    {
+        throw new UnsupportedOperationException("engineSetParameter unsupported");
+    }
+
+    public static class Base
+        extends SignatureSpi
+    {
+        public Base()
+        {
+            super(new FalconSigner());
+        }
+    }
+
+    public static class Falcon512
+        extends SignatureSpi
+    {
+        public Falcon512()
+        {
+            super(new FalconSigner(), FalconParameters.falcon_512);
+        }
+    }
+
+    public static class Falcon1024
+        extends SignatureSpi
+    {
+        public Falcon1024()
+        {
+            super(new FalconSigner(), FalconParameters.falcon_1024);
+        }
+    }
+}
