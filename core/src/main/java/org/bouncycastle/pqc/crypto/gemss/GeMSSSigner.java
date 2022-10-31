a79a38359b8013d0e11ece094c182f39ddcd48e6
David Hook <dgh@cryptoworkshop.com>
2022-10-31 20:17:20 +1100

    latest updates from GeMSS branch (unreachable branch removal)

--- core/src/main/java/org/bouncycastle/pqc/crypto/gemss/GeMSSSigner.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/gemss/GeMSSSigner.java
@@ -20,8 +20,6 @@ public class GeMSSSigner
 
     }
 
-
-    @Override
     public void init(boolean forSigning, CipherParameters param)
     {
         if (forSigning)
@@ -44,7 +42,6 @@ public class GeMSSSigner
 
     }
 
-    @Override
     public byte[] generateSignature(byte[] message)
     {
         GeMSSEngine engine = privKey.getParameters().getEngine();
@@ -55,68 +52,10 @@ public class GeMSSSigner
         return sm8;
     }
 
-    @Override
     public boolean verifySignature(byte[] message, byte[] signature)
     {
         GeMSSEngine engine = pubKey.getParameters().getEngine();
         int ret = engine.crypto_sign_open(pubKey.getPK(), message, signature);
-//        Pointer pk_tmp = new Pointer(1 + engine.NB_WORD_UNCOMP_EQ * engine.HFEmr8);//if (engine.HFEmr8 != 0)
-//        PointerUnion pk = new PointerUnion(pubKey.getPK());
-//        int i;
-//        long val = 0;
-//        if (engine.HFENr8 != 0 && (engine.HFEmr8 > 1))
-//        {
-//            PointerUnion pk_cp = new PointerUnion(pk);
-//            pk_cp.moveNextBytes(engine.ACCESS_last_equations8 - 1);
-//            for (i = 0; i < engine.HFEmr8 - 1; ++i)
-//            {
-//                /* Last byte of the equation */
-//                pk_cp.moveNextBytes(engine.NB_BYTES_EQUATION);
-//                val ^= ((pk_cp.getByte() & 0xFFL) >>> engine.HFENr8) << (i * engine.HFENr8c);
-//            }
-//        }
-//        if (engine.HFEmr8 != 0)
-//        {
-//            long cst = 0;
-//            PointerUnion pk64 = new PointerUnion(pk);
-//            for (i = 0; i < (engine.HFEmr8 - 1); i++)
-//            {
-//                pk64.setByteIndex(engine.ACCESS_last_equations8 + i * engine.NB_BYTES_EQUATION);
-//                cst ^= engine.convMQ_uncompressL_gf2(new Pointer(pk_tmp, 1 + i * engine.NB_WORD_UNCOMP_EQ), pk64) << i;
-//            }
-//            pk64.setByteIndex(engine.ACCESS_last_equations8 + i * engine.NB_BYTES_EQUATION);
-//            /* The last equation in input is smaller because compressed */
-//            cst ^= engine.convMQ_last_uncompressL_gf2(new Pointer(pk_tmp, 1 + i * engine.NB_WORD_UNCOMP_EQ), pk64) << i;
-//            if (engine.HFENr8 != 0 && (engine.HFEmr8 > 1))
-//            {
-//                /* Number of lost bits by the zero padding of each equation (without the last) */
-//                final int LOST_BITS = ((engine.HFEmr8 - 1) * engine.HFENr8c);
-//                if (engine.HFEnvr == 0)
-//                {
-//                    pk_tmp.setXor(1 + (i + 1) * engine.NB_WORD_UNCOMP_EQ - 1, val << (64 - LOST_BITS));
-//                }
-//                else if (engine.HFEnvr > LOST_BITS)
-//                {
-//                    pk_tmp.setXor(1 + (i + 1) * engine.NB_WORD_UNCOMP_EQ - 1, val << (engine.HFEnvr - LOST_BITS));
-//                }
-//                else if (engine.HFEnvr == LOST_BITS)
-//                {
-//                    pk_tmp.set(1 + (i + 1) * engine.NB_WORD_UNCOMP_EQ - 1, val);
-//                }
-//                else if (engine.HFEnvr < LOST_BITS)
-//                {
-//                    pk_tmp.setXor(1 + (i + 1) * engine.NB_WORD_UNCOMP_EQ - 2, val << (64 - (LOST_BITS - engine.HFEnvr)));
-//                    pk_tmp.set(1 + (i + 1) * engine.NB_WORD_UNCOMP_EQ - 1, val >> (LOST_BITS - engine.HFEnvr));
-//                }
-//            }
-//            cst <<= engine.HFEmr - engine.HFEmr8;
-//            pk_tmp.set(cst);
-//        }
-//        int ret = 0;
-//        if (engine.HFEmr8 != 0)
-//        {
-//            ret = engine.sign_openHFE_huncomp_pk(message, message.length, signature, pk, new PointerUnion(pk_tmp));
-//        }
         return ret != 0;
     }
 }
