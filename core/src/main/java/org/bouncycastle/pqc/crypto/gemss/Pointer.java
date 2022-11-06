80d4579de879aa2427264fc57db4921071fb5c4d
gefeili <gli@keyfactor.com>
2022-11-07 09:30:14 +1030

    rem_gf2n and sqr_gf2n are done. genSecretMQS_gf2_opt is not active

--- core/src/main/java/org/bouncycastle/pqc/crypto/gemss/Pointer.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/gemss/Pointer.java
@@ -412,6 +412,28 @@ class Pointer
         return res;
     }
 
+    public void mul_gf2x(Pointer A, Pointer B){
+        switch (array.length)
+        {
+        case 6:
+            mul192_no_simd_gf2x(array, 0, A.array, A.cp, B.array, B.cp, new long[2], 0);
+            break;
+        case 9:
+            mul288_no_simd_gf2x(array, 0, A.array, A.cp, B.array, B.cp, new long[3], new long[3], new long[7]);
+            break;
+        case 12:
+            mul384_no_simd_gf2x(array, A.array, A.cp, B.array, B.cp, new long[3], new long[3], new long[8]);
+            break;
+        case 13:
+            mul416_no_simd_gf2x(array, A.array, A.cp, B.array, B.cp, new long[4], new long[4], new long[13],
+                new long[2], new long[2]);
+            break;
+        case 17:
+            mul544_no_simd_gf2x(array, A.array, A.cp, B.array, B.cp, new long[5], new long[5], new long[9],
+                new long[3], new long[3], new long[7]);
+            break;
+        }
+    }
     public void mul_gf2x(Pointer A, Pointer B, int HFEnq, int NB_WORD_GFqn, int HFEnr)
     {
         switch (array.length)
@@ -835,7 +857,6 @@ class Pointer
     {
         //Buffer size: 2
         long AA, BB;
-        //long[] RESERVED_BUF2 = new long[2];
         /* A0*B0 */
         MUL64_NO_SIMD_GF2X(C, c_cp, A[a_cp], B[b_cp]);
         /* A2*B2 */
@@ -845,29 +866,15 @@ class Pointer
         C[c_cp + 1] ^= RESERVED_BUF2[buf_cp];
         C[c_cp + 4] ^= RESERVED_BUF2[buf_cp + 1];
         C[c_cp + 2] = C[c_cp + 4];
-        C[c_cp + 3] = C[c_cp + 1];
-    /*  C[0] = C0
-        C[1] = C1^C2
-        C[2] = C4^C3
-        C[3] = C1^C2
-        C[4] = C4^C3
-        C[5] = C5 */
-        C[c_cp + 1] ^= C[c_cp];
         C[c_cp + 4] ^= C[c_cp + 5];
-        C[c_cp + 2] ^= C[c_cp + 1];
-        C[c_cp + 3] ^= C[c_cp + 4];
-    /*  C[0] = C0
-        C[1] = C0^(C1^C2)
-        C[2] = (C0^C1^C2)^(C3^C4)
-        C[3] = (C1^C2)^(C3^C4^C5)
-        C[4] = (C3^C4)^C5
-        C[5] = C5 */
+        C[c_cp + 3] = C[c_cp + 1] ^ C[c_cp + 4];
+        C[c_cp + 1] ^= C[c_cp];
         AA = A[a_cp] ^ A[a_cp + 1];
         BB = B[b_cp] ^ B[b_cp + 1];
-        /* (A0+A1)*(B0+B1) */
         MUL64_NO_SIMD_GF2X(RESERVED_BUF2, buf_cp, AA, BB);
+        C[c_cp + 2] ^= C[c_cp + 1] ^ RESERVED_BUF2[buf_cp + 1];
         C[c_cp + 1] ^= RESERVED_BUF2[buf_cp];
-        C[c_cp + 2] ^= RESERVED_BUF2[buf_cp + 1];
+        //TODO: move forward
         AA = A[a_cp + 1] ^ A[a_cp + 2];
         BB = B[b_cp + 1] ^ B[b_cp + 2];
         /* (A1+A2)*(B1+B2)  */
@@ -1068,267 +1075,4 @@ class Pointer
         C[11] ^= RESERVED_BUF9[7];
         C[12] ^= RESERVED_BUF9[8];
     }
-
-    void REM192_SPECIALIZED_TRINOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k3, int ki, int ki64, int k364, long[] Q, long mask)
-    {
-        Q[0] = (Pol[2] >>> ki) ^ (Pol[3] << ki64);
-        Q[1] = (Pol[3] >>> ki) ^ (Pol[4] << ki64);
-        Q[2] = (Pol[4] >>> ki) ^ (Pol[5] << ki64);
-        P[p_cp] = Pol[0] ^ Q[0] ^ (Q[0] << k3);
-        P[p_cp + 1] = Pol[1] ^ Q[1] ^ (Q[0] >>> k364) ^ (Q[1] << k3);
-        P[p_cp + 2] = Pol[2] ^ Q[2] ^ (Q[1] >>> k364) ^ (Q[2] << k3);
-        /* 64-(k364+ki) == (k3-ki) */
-        long R = (ki >= k3) ? Q[2] >>> (ki - k3) : (Q[1] >>> (k364 + ki)) ^ (Q[2] << (k3 - ki));
-        P[p_cp] ^= R ^ (R << k3);
-        P[p_cp + 2] &= mask;
-    }
-
-    void REM288_TRINOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k3, int ki, int ki64, int k364, long[] Q, long mask)
-    {
-        Q[0] = (Pol[4] >>> ki) ^ (Pol[5] << ki64);
-        Q[1] = (Pol[5] >>> ki) ^ (Pol[6] << ki64);
-        Q[2] = (Pol[6] >>> ki) ^ (Pol[7] << ki64);
-        Q[3] = (Pol[7] >>> ki) ^ (Pol[8] << ki64);
-        Q[4] = Pol[8] >>> ki;
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp] ^= Q[0] << k3;
-        P[p_cp + 1] ^= (Q[0] >>> k364) ^ (Q[1] << k3);
-        P[p_cp + 2] ^= (Q[1] >>> k364) ^ (Q[2] << k3);
-        P[p_cp + 3] ^= (Q[2] >>> k364) ^ (Q[3] << k3);
-        P[p_cp + 4] ^= (Q[3] >>> k364) ^ (Q[4] << k3);
-        /* 64-(k364+ki) == (k3-ki) */
-        long R = (ki >= k3) ? Q[4] >>> (ki - k3) : (Q[3] >>> (k364 + ki)) ^ (Q[4] << (k3 - ki));
-        P[p_cp] ^= R;
-        P[p_cp] ^= R << k3;
-        P[p_cp + 4] &= mask;
-    }
-
-    void REM288_SPECIALIZED_TRINOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k3, int ki, int ki64, int k364, long[] Q, long mask)
-    {
-        Q[0] = (Pol[4] >>> ki) ^ (Pol[5] << ki64);
-        Q[1] = (Pol[5] >>> ki) ^ (Pol[6] << ki64);
-        Q[2] = (Pol[6] >>> ki) ^ (Pol[7] << ki64);
-        Q[3] = (Pol[7] >>> ki) ^ (Pol[8] << ki64);
-        Q[4] = (Pol[8] >>> ki);
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp] ^= Q[0] << k3;
-        P[p_cp + 1] ^= (Q[0] >>> k364) ^ (Q[1] << k3);
-        P[p_cp + 2] ^= (Q[1] >>> k364) ^ (Q[2] << k3);
-        P[p_cp + 3] ^= (Q[2] >>> k364) ^ (Q[3] << k3);
-        P[p_cp + 4] ^= (Q[3] >>> k364) ^ (Q[4] << k3);
-        /* 64-(k364+ki) == (k3-ki) */
-        long R = (ki >= k3) ? Q[4] >>> (ki - k3) : (Q[3] >>> (k364 + ki)) ^ (Q[4] << (k3 - ki));
-        P[p_cp] ^= R;
-        P[p_cp] ^= R << k3;
-        /* This row is the unique difference with REM288_TRINOMIAL_GF2X */
-        P[p_cp + 1] ^= R >>> k364;
-        P[p_cp + 4] &= mask;
-    }
-
-    void REM544_PENTANOMIAL_K3_IS_128_GF2X(long[] P, int p_cp, long[] Pol, int k1, int k2, int ki, int ki64,
-                                           int k164, int k264, long[] Q, long mask)
-    {
-        Q[0] = (Pol[8] >>> ki) ^ (Pol[9] << ki64);
-        Q[1] = (Pol[9] >>> ki) ^ (Pol[10] << ki64);
-        Q[2] = (Pol[10] >>> ki) ^ (Pol[11] << ki64);
-        Q[3] = (Pol[11] >>> ki) ^ (Pol[12] << ki64);
-        Q[4] = (Pol[12] >>> ki) ^ (Pol[13] << ki64);
-        Q[5] = (Pol[13] >>> ki) ^ (Pol[14] << ki64);
-        Q[6] = (Pol[14] >>> ki) ^ (Pol[15] << ki64);
-        Q[7] = (Pol[15] >>> ki) ^ (Pol[16] << ki64);
-        Q[8] = (Pol[16] >>> ki);
-        /* R for K2=3 */
-        Q[0] ^= Pol[16] >>> (k264);
-        /* R for K3=128 */
-        Q[0] ^= (Q[6] >>> ki) ^ (Q[7] << ki64);
-        Q[1] ^= (Q[7] >>> ki) ^ (Q[8] << ki64);
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp + 5] = Pol[5] ^ Q[5];
-        P[p_cp + 6] = Pol[6] ^ Q[6];
-        P[p_cp + 7] = Pol[7] ^ Q[7];
-        P[p_cp + 8] = Pol[8] ^ Q[8];
-        /* K3=128 */
-        P[p_cp + 2] ^= Q[0];
-        P[p_cp + 3] ^= Q[1];
-        P[p_cp + 4] ^= Q[2];
-        P[p_cp + 5] ^= Q[3];
-        P[p_cp + 6] ^= Q[4];
-        P[p_cp + 7] ^= Q[5];
-        P[p_cp + 8] ^= Q[6];
-        P[p_cp] ^= Q[0] << k1;
-        P[p_cp + 1] ^= (Q[0] >>> k164) ^ (Q[1] << k1);
-        P[p_cp + 2] ^= (Q[1] >>> k164) ^ (Q[2] << k1);
-        P[p_cp + 3] ^= (Q[2] >>> k164) ^ (Q[3] << k1);
-        P[p_cp + 4] ^= (Q[3] >>> k164) ^ (Q[4] << k1);
-        P[p_cp + 5] ^= (Q[4] >>> k164) ^ (Q[5] << k1);
-        P[p_cp + 6] ^= (Q[5] >>> k164) ^ (Q[6] << k1);
-        P[p_cp + 7] ^= (Q[6] >>> k164) ^ (Q[7] << k1);
-        P[p_cp + 8] ^= (Q[7] >>> k164) ^ (Q[8] << k1);
-        P[p_cp] ^= Q[0] << k2;
-        P[p_cp + 1] ^= (Q[0] >>> k264) ^ (Q[1] << k2);
-        P[p_cp + 2] ^= (Q[1] >>> k264) ^ (Q[2] << k2);
-        P[p_cp + 3] ^= (Q[2] >>> k264) ^ (Q[3] << k2);
-        P[p_cp + 4] ^= (Q[3] >>> k264) ^ (Q[4] << k2);
-        P[p_cp + 5] ^= (Q[4] >>> k264) ^ (Q[5] << k2);
-        P[p_cp + 6] ^= (Q[5] >>> k264) ^ (Q[6] << k2);
-        P[p_cp + 7] ^= (Q[6] >>> k264) ^ (Q[7] << k2);
-        P[p_cp + 8] ^= (Q[7] >>> k264) ^ (Q[8] << k2);
-        P[p_cp + 8] &= mask;
-    }
-
-    void REM544_PENTANOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k1, int k2, int k3, int ki, int ki64, int k164,
-                                 int k264, int k364, long[] Q, long mask)
-    {
-        Q[0] = (Pol[8] >>> ki) ^ (Pol[9] << ki64);
-        Q[1] = (Pol[9] >>> ki) ^ (Pol[10] << ki64);
-        Q[2] = (Pol[10] >>> ki) ^ (Pol[11] << ki64);
-        Q[3] = (Pol[11] >>> ki) ^ (Pol[12] << ki64);
-        Q[4] = (Pol[12] >>> ki) ^ (Pol[13] << ki64);
-        Q[5] = (Pol[13] >>> ki) ^ (Pol[14] << ki64);
-        Q[6] = (Pol[14] >>> ki) ^ (Pol[15] << ki64);
-        Q[7] = (Pol[15] >>> ki) ^ (Pol[16] << ki64);
-        Q[8] = Pol[16] >>> ki;
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp + 5] = Pol[5] ^ Q[5];
-        P[p_cp + 6] = Pol[6] ^ Q[6];
-        P[p_cp + 7] = Pol[7] ^ Q[7];
-        P[p_cp + 8] = Pol[8] ^ Q[8];
-        P[p_cp] ^= Q[0] << k1;
-        P[p_cp + 1] ^= (Q[0] >>> k164) ^ (Q[1] << k1);
-        P[p_cp + 2] ^= (Q[1] >>> k164) ^ (Q[2] << k1);
-        P[p_cp + 3] ^= (Q[2] >>> k164) ^ (Q[3] << k1);
-        P[p_cp + 4] ^= (Q[3] >>> k164) ^ (Q[4] << k1);
-        P[p_cp + 5] ^= (Q[4] >>> k164) ^ (Q[5] << k1);
-        P[p_cp + 6] ^= (Q[5] >>> k164) ^ (Q[6] << k1);
-        P[p_cp + 7] ^= (Q[6] >>> k164) ^ (Q[7] << k1);
-        P[p_cp + 8] ^= (Q[7] >>> k164) ^ (Q[8] << k1);
-        P[p_cp] ^= Q[0] << k2;
-        P[p_cp + 1] ^= (Q[0] >>> k264) ^ (Q[1] << k2);
-        P[p_cp + 2] ^= (Q[1] >>> k264) ^ (Q[2] << k2);
-        P[p_cp + 3] ^= (Q[2] >>> k264) ^ (Q[3] << k2);
-        P[p_cp + 4] ^= (Q[3] >>> k264) ^ (Q[4] << k2);
-        P[p_cp + 5] ^= (Q[4] >>> k264) ^ (Q[5] << k2);
-        P[p_cp + 6] ^= (Q[5] >>> k264) ^ (Q[6] << k2);
-        P[p_cp + 7] ^= (Q[6] >>> k264) ^ (Q[7] << k2);
-        P[p_cp + 8] ^= (Q[7] >>> k264) ^ (Q[8] << k2);
-        P[p_cp] ^= Q[0] << k3;
-        P[p_cp + 1] ^= (Q[0] >>> k364) ^ (Q[1] << k3);
-        P[p_cp + 2] ^= (Q[1] >>> k364) ^ (Q[2] << k3);
-        P[p_cp + 3] ^= (Q[2] >>> k364) ^ (Q[3] << k3);
-        P[p_cp + 4] ^= (Q[3] >>> k364) ^ (Q[4] << k3);
-        P[p_cp + 5] ^= (Q[4] >>> k364) ^ (Q[5] << k3);
-        P[p_cp + 6] ^= (Q[5] >>> k364) ^ (Q[6] << k3);
-        P[p_cp + 7] ^= (Q[6] >>> k364) ^ (Q[7] << k3);
-        P[p_cp + 8] ^= (Q[7] >>> k364) ^ (Q[8] << k3);
-        /* 64-(k364+ki) == (k3-ki) */
-        long R = (ki >= k3) ? Q[8] >>> (ki - k3) : (Q[7] >>> (k364 + ki)) ^ (Q[8] << (k3 - ki));
-        R ^= (ki >= k2) ? Q[8] >>> (ki - k2) : (Q[7] >>> (k264 + ki)) ^ (Q[8] << (k2 - ki));
-        R ^= (ki >= k1) ? Q[8] >>> (ki - k1) : (Q[7] >>> (k164 + ki)) ^ (Q[8] << (k1 - ki));
-        P[p_cp] ^= R;
-        P[p_cp] ^= R << k1;
-        P[p_cp] ^= R << k2;
-        P[p_cp] ^= R << k3;
-        P[p_cp + 8] &= mask;
-    }
-
-    void REM384_SPECIALIZED_TRINOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k3, int ki, int ki64, int k364, long[] Q, long mask)
-    {
-        long R2;
-        Q[0] = (Pol[5] >>> ki) ^ (Pol[6] << ki64);
-        Q[1] = (Pol[6] >>> ki) ^ (Pol[7] << ki64);
-        Q[2] = (Pol[7] >>> ki) ^ (Pol[8] << ki64);
-        Q[3] = (Pol[8] >>> ki) ^ (Pol[9] << ki64);
-        Q[4] = (Pol[9] >>> ki) ^ (Pol[10] << ki64);
-        Q[5] = (Pol[10] >>> ki) ^ (Pol[11] << ki64);
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp + 5] = Pol[5] ^ Q[5];
-        /* 64-(k364+ki) == (k3-ki) */
-        long R = (Q[3] >>> (k364 + ki)) ^ (Q[4] << (k3 - ki));
-        P[p_cp] ^= R;
-        R2 = (Q[4] >>> (k364 + ki)) ^ (Q[5] << (k3 - ki));
-        P[p_cp + 1] ^= R2;
-        P[p_cp + 1] ^= (R ^ Q[0]) << k3;
-        P[p_cp + 2] ^= ((R ^ Q[0]) >>> k364) ^ ((R2 ^ Q[1]) << k3);
-        P[p_cp + 3] ^= ((R2 ^ Q[1]) >>> k364) ^ (Q[2] << k3);
-        P[p_cp + 4] ^= (Q[2] >>> k364) ^ (Q[3] << k3);
-        P[p_cp + 5] ^= Q[3] >>> k364;
-        P[p_cp + 5] &= mask;
-    }
-
-    void REM384_SPECIALIZED358_TRINOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k3, int ki, int ki64, int k364, long[] Q, long mask)
-    {
-        Q[0] = (Pol[5] >>> ki) ^ (Pol[6] << ki64);
-        Q[1] = (Pol[6] >>> ki) ^ (Pol[7] << ki64);
-        Q[2] = (Pol[7] >>> ki) ^ (Pol[8] << ki64);
-        Q[3] = (Pol[8] >>> ki) ^ (Pol[9] << ki64);
-        Q[4] = (Pol[9] >>> ki) ^ (Pol[10] << ki64);
-        Q[5] = (Pol[10] >>> ki) ^ (Pol[11] << ki64);
-        /* 64-(k364+ki) == (k3-ki) */
-        long R = (Q[4] >>> (k364 + ki)) ^ (Q[5] << (k3 - ki));
-        Q[0] ^= R;
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp + 5] = Pol[5] ^ Q[5];
-        P[p_cp] ^= Q[0] << k3;
-        P[p_cp + 1] ^= (Q[0] >>> k364) ^ (Q[1] << k3);
-        P[p_cp + 2] ^= (Q[1] >>> k364) ^ (Q[2] << k3);
-        P[p_cp + 3] ^= (Q[2] >>> k364) ^ (Q[3] << k3);
-        P[p_cp + 4] ^= (Q[3] >>> k364) ^ (Q[4] << k3);
-        P[p_cp + 5] ^= (Q[4] >>> k364);
-        P[p_cp + 5] &= mask;
-    }
-
-    void REM402_SPECIALIZED_TRINOMIAL_GF2X(long[] P, int p_cp, long[] Pol, int k3, int ki, int ki64, int k364, long[] Q, long mask)
-    {
-        Q[0] = (Pol[6] >>> ki) ^ (Pol[7] << ki64);
-        Q[1] = (Pol[7] >>> ki) ^ (Pol[8] << ki64);
-        Q[2] = (Pol[8] >>> ki) ^ (Pol[9] << ki64);
-        Q[3] = (Pol[9] >>> ki) ^ (Pol[10] << ki64);
-        Q[4] = (Pol[10] >>> ki) ^ (Pol[11] << ki64);
-        Q[5] = (Pol[11] >>> ki) ^ (Pol[12] << ki64);
-        Q[6] = (Pol[12] >>> ki);
-
-        Q[0] ^= (Q[3] >>> 39) ^ (Q[4] << 25);
-        Q[1] ^= (Q[4] >>> 39) ^ (Q[5] << 25);
-        Q[2] ^= (Q[5] >>> 39) ^ (Q[6] << 25);
-        P[p_cp] = Pol[0] ^ Q[0];
-        P[p_cp + 1] = Pol[1] ^ Q[1];
-        P[p_cp + 2] = Pol[2] ^ Q[2];
-        P[p_cp + 3] = Pol[3] ^ Q[3];
-        P[p_cp + 4] = Pol[4] ^ Q[4];
-        P[p_cp + 5] = Pol[5] ^ Q[5];
-        P[p_cp + 6] = Pol[6] ^ Q[6];
-
-        P[p_cp + 2] ^= (Q[0] << k3);
-        P[p_cp + 3] ^= (Q[0] >>> k364) ^ (Q[1] << k3);
-        P[p_cp + 4] ^= (Q[1] >>> k364) ^ (Q[2] << k3);
-        P[p_cp + 5] ^= (Q[2] >>> k364) ^ (Q[3] << k3);
-        P[p_cp + 6] ^= Q[3] >>> k364;
-        P[p_cp + 6] &= mask;
-    }
-
-
 }
