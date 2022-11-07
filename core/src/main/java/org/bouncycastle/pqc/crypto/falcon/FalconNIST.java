546e5142dc90b115d6fe5fcf4803029ca8cd6e4e
David Hook <david.hook@keyfactor.com>
2022-11-07 20:45:46 +1100

    rolled back wrong file, updated correct one...

--- core/src/main/java/org/bouncycastle/pqc/crypto/falcon/FalconNIST.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/falcon/FalconNIST.java
@@ -4,7 +4,6 @@ package org.bouncycastle.pqc.crypto.falcon;
 import java.security.SecureRandom;
 
 import org.bouncycastle.util.Arrays;
-import org.bouncycastle.util.encoders.Hex;
 
 class FalconNIST
 {
@@ -268,7 +267,7 @@ class FalconNIST
         int sig_len, msg_len;
         FalconVrfy vrfy = new FalconVrfy();
         FalconCommon common = new FalconCommon();
-System.err.println(Hex.toHexString(srcpk));
+
         /*
          * Decode public key.
          */
