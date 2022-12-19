75fd40cffe0eb614b464a2ae05266f9f56946d89
gefeili <gli@keyfactor.com>
2022-12-19 16:17:08 +1030

    ISAP code is done.

--- core/src/test/java/org/bouncycastle/crypto/test/ISAPTest.java
+++ core/src/test/java/org/bouncycastle/crypto/test/ISAPTest.java
@@ -6,12 +6,14 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.digests.ISAPDigest;
 import org.bouncycastle.crypto.engines.ISAPEngine;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
 import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.util.test.SimpleTest;
+
 import static org.bouncycastle.crypto.engines.ISAPEngine.IsapType;
 
 public class ISAPTest
@@ -29,6 +31,7 @@ public class ISAPTest
         testVectors("isapa128v20", IsapType.ISAP_A_128);
         testVectors("isapk128av20", IsapType.ISAP_K_128A);
         testVectors("isapk128v20", IsapType.ISAP_K_128);
+        testVectors();
     }
 
     private void testVectors(String filename, IsapType isapType)
@@ -47,7 +50,7 @@ public class ISAPTest
             int a = line.indexOf('=');
             if (a < 0)
             {
-//                if (!map.get("Count").equals("628"))
+//                if (!map.get("Count").equals("265"))
 //                {
 //                    continue;
 //                }
@@ -64,10 +67,6 @@ public class ISAPTest
                 {
                     mismatch("Keystream " + map.get("Count"), (String)map.get("CT"), Arrays.concatenate(rv, mac));
                 }
-//                else
-//                {
-//                    System.out.println("Keystream " + map.get("Count") + " pass");
-//                }
                 map.clear();
             }
             else
@@ -75,9 +74,49 @@ public class ISAPTest
                 map.put(line.substring(0, a).trim(), line.substring(a + 1).trim());
             }
         }
-        System.out.println(filename+" pass");
+        System.out.println(filename + " pass");
     }
 
+    private void testVectors()
+        throws Exception
+    {
+        ISAPDigest isap = new ISAPDigest();
+        InputStream src = ISAPTest.class.getResourceAsStream("/org/bouncycastle/crypto/test/isap/LWC_HASH_KAT_256.txt");
+        BufferedReader bin = new BufferedReader(new InputStreamReader(src));
+        String line;
+        byte[] ptByte;
+        HashMap<String, String> map = new HashMap<String, String>();
+        while ((line = bin.readLine()) != null)
+        {
+            int a = line.indexOf('=');
+            if (a < 0)
+            {
+//                if (!map.get("Count").equals("10"))
+//                {
+//                    continue;
+//                }
+                ptByte = Hex.decode((String)map.get("Msg"));
+                isap.update(ptByte, 0, ptByte.length);
+                byte[] hash = new byte[32];
+                isap.doFinal(hash, 0);
+                if (!areEqual(hash, Hex.decode((String)map.get("MD"))))
+                {
+                    mismatch("Keystream " + map.get("Count"), (String)map.get("MD"), hash);
+                }
+//                else
+//                {
+//                    System.out.println(map.get("Count") + " pass");
+//                }
+                map.clear();
+                isap.reset();
+            }
+            else
+            {
+                map.put(line.substring(0, a).trim(), line.substring(a + 1).trim());
+            }
+        }
+        System.out.println("ISAP Hash pass");
+    }
 
     private void mismatch(String name, String expected, byte[] found)
     {
