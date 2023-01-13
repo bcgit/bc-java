ad08ba36d77a63f1a92dc13aec8faafafa563694
David Hook <dgh@cryptoworkshop.com>
2023-01-14 09:55:02 +1100

    refactor to deal with classloader issues, added class monitor

---
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/drbg/EntropyDaemon.java
@@ -0,0 +1,68 @@
+package org.bouncycastle.jcajce.provider.drbg;
+
+import java.lang.ref.WeakReference;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class EntropyDaemon
+    implements Runnable
+{
+    private static final Logger LOG = Logger.getLogger(EntropyDaemon.class.getName());
+
+    private final LinkedList<Runnable> tasks = new LinkedList<Runnable>();
+    private final WeakReference parentClassMonitor;
+
+    public EntropyDaemon(Class parentClass)
+    {
+        this.parentClassMonitor = new WeakReference(parentClass);
+    }
+
+    void addTask(Runnable task)
+    {
+        synchronized (tasks)
+        {
+            tasks.add(task);
+        }
+    }
+
+    public void run()
+    {
+        while (!Thread.currentThread().isInterrupted() && parentClassMonitor.get() != null)
+        {
+            Runnable task;
+            synchronized (tasks)
+            {
+                task = tasks.poll();
+            }
+
+            if (task != null)
+            {
+                try
+                {
+                    task.run();
+                }
+                catch (Throwable e)
+                {
+                    // ignore
+                }
+            }
+            else
+            {
+                try
+                {
+                    Thread.sleep(5000);
+                }
+                catch (InterruptedException e)
+                {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+
+        if (LOG.isLoggable(Level.FINE))
+        {
+            LOG.fine("entropy thread interrupted - exiting");
+        }
+    }
+}
