ab25216a2a35bf76404299600c455b9faae7fb82
gefeili <gli@keyfactor.com>
2023-02-09 11:03:15 +1030

    Add Ascon Hash to the master branch

---
+++ core/src/main/java/org/bouncycastle/crypto/digests/AsconDigest.java
@@ -0,0 +1,214 @@
+package org.bouncycastle.crypto.digests;
+
+import java.io.ByteArrayOutputStream;
+
+import org.bouncycastle.crypto.DataLengthException;
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.OutputLengthException;
+
+public class AsconDigest
+    implements Digest
+{
+    public enum AsconParameters
+    {
+        AsconHash,
+        AsconHashA,
+        AsconXof,
+        AsconXofA,
+    }
+
+    public AsconDigest(AsconParameters parameters)
+    {
+        switch (parameters)
+        {
+        case AsconHash:
+            ASCON_PB_ROUNDS = 12;
+            ASCON_IV = (((long)(ASCON_HASH_RATE * 8) << 48) |
+                ((long)(ASCON_PA_ROUNDS) << 40) |
+                ((long)(ASCON_HASH_BYTES * 8)));
+            algorithmName = "Ascon-Hash";
+            break;
+        case AsconHashA:
+            ASCON_PB_ROUNDS = 8;
+            ASCON_IV = (((long)(ASCON_HASH_RATE * 8) << 48) |
+                ((long)(ASCON_PA_ROUNDS) << 40) |
+                ((long)(ASCON_PA_ROUNDS - ASCON_PB_ROUNDS) << 32) |
+                ((long)(ASCON_HASH_BYTES * 8)));
+            algorithmName = "Ascon-HashA";
+            break;
+        case AsconXof:
+            ASCON_PB_ROUNDS = 12;
+            ASCON_IV = (((long)(ASCON_HASH_RATE * 8) << 48) |
+                ((long)(ASCON_PA_ROUNDS) << 40));
+            algorithmName = "Ascon-Xof";
+            break;
+        case AsconXofA:
+            ASCON_PB_ROUNDS = 8;
+            ASCON_IV = (((long)(ASCON_HASH_RATE * 8) << 48) |
+                ((long)(ASCON_PA_ROUNDS) << 40) |
+                ((long)(ASCON_PA_ROUNDS - ASCON_PB_ROUNDS) << 32));
+            algorithmName = "Ascon-XofA";
+            break;
+        default:
+            throw new IllegalArgumentException("Invalid parameter settings for Ascon Hash");
+        }
+    }
+
+    private String algorithmName;
+
+    private final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    private long x0;
+    private long x1;
+    private long x2;
+    private long x3;
+    private long x4;
+    private final int CRYPTO_BYTES = 32;
+    private final long ASCON_IV;
+    private final int ASCON_HASH_RATE = 8;
+    private final int ASCON_PA_ROUNDS = 12;
+    private int ASCON_PB_ROUNDS;
+
+
+    private int ASCON_HASH_BYTES = 32;
+
+    private long ROR(long x, int n)
+    {
+        return x >>> n | x << (64 - n);
+    }
+
+    private void ROUND(long C)
+    {
+        long t0 = x0 ^ x1 ^ x2 ^ x3 ^ C ^ (x1 & (x0 ^ x2 ^ x4 ^ C));
+        long t1 = x0 ^ x2 ^ x3 ^ x4 ^ C ^ ((x1 ^ x2 ^ C) & (x1 ^ x3));
+        long t2 = x1 ^ x2 ^ x4 ^ C ^ (x3 & x4);
+        long t3 = x0 ^ x1 ^ x2 ^ C ^ ((~x0) & (x3 ^ x4));
+        long t4 = x1 ^ x3 ^ x4 ^ ((x0 ^ x4) & x1);
+        x0 = t0 ^ ROR(t0, 19) ^ ROR(t0, 28);
+        x1 = t1 ^ ROR(t1, 39) ^ ROR(t1, 61);
+        x2 = ~(t2 ^ ROR(t2, 1) ^ ROR(t2, 6));
+        x3 = t3 ^ ROR(t3, 10) ^ ROR(t3, 17);
+        x4 = t4 ^ ROR(t4, 7) ^ ROR(t4, 41);
+    }
+
+    private void P(int nr)
+    {
+        if (nr == 12)
+        {
+            ROUND(0xf0L);
+            ROUND(0xe1L);
+            ROUND(0xd2L);
+            ROUND(0xc3L);
+        }
+        if (nr >= 8)
+        {
+            ROUND(0xb4L);
+            ROUND(0xa5L);
+        }
+        ROUND(0x96L);
+        ROUND(0x87L);
+        ROUND(0x78L);
+        ROUND(0x69L);
+        ROUND(0x5aL);
+        ROUND(0x4bL);
+    }
+
+    private long PAD(int i)
+    {
+        return 0x80L << (56 - (i << 3));
+    }
+
+    private long LOADBYTES(final byte[] bytes, int inOff, int n)
+    {
+        long x = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            x |= (bytes[i + inOff] & 0xFFL) << ((7 - i) << 3);
+        }
+        return x;
+    }
+
+    private void STOREBYTES(byte[] bytes, int inOff, long w, int n)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            bytes[i + inOff] = (byte)(w >>> ((7 - i) << 3));
+        }
+    }
+
+    @Override
+    public String getAlgorithmName()
+    {
+        return algorithmName;
+    }
+
+    @Override
+    public int getDigestSize()
+    {
+        return CRYPTO_BYTES;
+    }
+
+    @Override
+    public void update(byte in)
+    {
+        buffer.write(in);
+    }
+
+    @Override
+    public void update(byte[] input, int inOff, int len)
+    {
+        if ((inOff + len) > input.length)
+        {
+            throw new DataLengthException("input buffer too short");
+        }
+        buffer.write(input, inOff, len);
+    }
+
+    @Override
+    public int doFinal(byte[] output, int outOff)
+    {
+        if (CRYPTO_BYTES + outOff > output.length)
+        {
+            throw new OutputLengthException("output buffer is too short");
+        }
+        byte[] input = buffer.toByteArray();
+        int len = buffer.size();
+        int inOff = 0;
+        /* initialize */
+        x0 = ASCON_IV;
+        x1 = 0;
+        x2 = 0;
+        x3 = 0;
+        x4 = 0;
+        P(ASCON_PA_ROUNDS);
+        /* absorb full plaintext blocks */
+        while (len >= ASCON_HASH_RATE)
+        {
+            x0 ^= LOADBYTES(input, inOff, 8);
+            P(ASCON_PB_ROUNDS);
+            inOff += ASCON_HASH_RATE;
+            len -= ASCON_HASH_RATE;
+        }
+        /* absorb final plaintext block */
+        x0 ^= LOADBYTES(input, inOff, len);
+        x0 ^= PAD(len);
+        P(ASCON_PA_ROUNDS);
+        /* squeeze full output blocks */
+        len = CRYPTO_BYTES;
+        while (len > ASCON_HASH_RATE)
+        {
+            STOREBYTES(output, outOff, x0, 8);
+            P(ASCON_PB_ROUNDS);
+            outOff += ASCON_HASH_RATE;
+            len -= ASCON_HASH_RATE;
+        }
+        /* squeeze final output block */
+        STOREBYTES(output, outOff, x0, len);
+        return CRYPTO_BYTES;
+    }
+
+    @Override
+    public void reset()
+    {
+        buffer.reset();
+    }
+}
