415b66ae5b2981fec8f1778fed1a8c0754e6d62c
David Hook <dgh@cryptoworkshop.com>
2023-02-18 16:33:35 +1100

    fixed getInstance in SubjectAltPublicKeyInfo added X.509 Section 9.8.3 and 9.8.4

---
+++ core/src/main/java/org/bouncycastle/asn1/x509/AltSignatureAlgorithm.java
@@ -0,0 +1,86 @@
+package org.bouncycastle.asn1.x509;
+
+import org.bouncycastle.asn1.ASN1Object;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.ASN1TaggedObject;
+
+/**
+ * X.509 Section 9.8.3.
+ * <br/>
+ * This extension may be used as a public-key certificate extension, a CRL extension or an AVL extension. It shall contain
+ * the algorithm identifier for the alternative digital signature algorithm used by the signer when creating an alternative
+ * digital signature and by the relying party when validating the alternative digital signature.
+ * <pre>
+ * altSignatureAlgorithm EXTENSION ::= {
+ *     SYNTAX AltSignatureAlgorithm
+ *     IDENTIFIED BY id-ce-altSignatureAlgorithm }
+ *
+ * AltSignatureAlgorithm ::= AlgorithmIdentifier{{SupportedAlgorithms}}
+ * </pre>
+ * When the altSignatureAlgorithm extension is included in a particular value that is an instance of a data type that
+ * supports extensions, the altSignatureValue extension shall also be included.
+ * <br/>
+ * NOTE 1 – By having a separate altSignatureAlgorithm extension, instead of having it combined with the
+ * altSignatureValue extension, the alternative digital signature algorithm is protected by the alternative signature.
+ * This extension may be flagged either as critical or as non-critical.
+ * <br/>
+ * NOTE 2 – It is recommended that it be flagged as non-critical. Flagging it as critical would require all relying parties to understand
+ * the extension and the alternative public-key algorithms
+ */
+public class AltSignatureAlgorithm
+    extends ASN1Object
+{
+    private AlgorithmIdentifier algorithm;
+
+    public static AltSignatureAlgorithm getInstance(
+        ASN1TaggedObject obj,
+        boolean          explicit)
+    {
+        return getInstance(ASN1Sequence.getInstance(obj, explicit));
+    }
+
+    public static AltSignatureAlgorithm getInstance(
+        Object obj)
+    {
+        if (obj instanceof AltSignatureAlgorithm)
+        {
+            return (AltSignatureAlgorithm)obj;
+        }
+        else if (obj != null)
+        {
+            return new AltSignatureAlgorithm(ASN1Sequence.getInstance(obj));
+        }
+
+        return null;
+    }
+
+    public static AltSignatureAlgorithm fromExtensions(Extensions extensions)
+    {
+        return getInstance(Extensions.getExtensionParsedValue(extensions, Extension.altSignatureAlgorithm));
+    }
+
+    private AltSignatureAlgorithm(ASN1Sequence s)
+    {
+        if (s.size() != 2)
+        {
+            throw new IllegalArgumentException("extension should contain only 2 elements");
+        }
+        algorithm = AlgorithmIdentifier.getInstance(s);
+    }
+
+    public AltSignatureAlgorithm(AlgorithmIdentifier algorithm)
+    {
+        this.algorithm = algorithm;
+    }
+
+    public AlgorithmIdentifier getAlgorithm()
+    {
+        return algorithm;
+    }
+
+    public ASN1Primitive toASN1Primitive()
+    {
+        return algorithm.toASN1Primitive();
+    }
+}
