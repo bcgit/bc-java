1d75286870ecc25e558e26b9edb8239f17cac05d
David Hook <dgh@cryptoworkshop.com>
2023-02-20 06:24:02 +1100

    added UKM processing

--- util/src/main/java/org/bouncycastle/asn1/cms/CMSORIforKEMOtherInfo.java
+++ util/src/main/java/org/bouncycastle/asn1/cms/CMSORIforKEMOtherInfo.java
@@ -4,7 +4,9 @@ import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.DERTaggedObject;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 
 /**
@@ -14,6 +16,8 @@ import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
  *     kekLength INTEGER (1..MAX),
  *     ukm [0] EXPLICIT UserKeyingMaterial OPTIONAL
  *   }
+ *
+ *   UserKeyingMaterial ::= OCTET STRING
  * </pre>
  */
 public class CMSORIforKEMOtherInfo
@@ -21,13 +25,20 @@ public class CMSORIforKEMOtherInfo
 {
     private final AlgorithmIdentifier wrap;
     private final int kekLength;
+    private final byte[] ukm;
 
     public CMSORIforKEMOtherInfo(AlgorithmIdentifier wrap, int kekLength)
     {
+        this(wrap, kekLength, null);
+    }
+
+    public CMSORIforKEMOtherInfo(AlgorithmIdentifier wrap, int kekLength, byte[] ukm)
+    {
         this.wrap = wrap;
         this.kekLength = kekLength;
+        this.ukm = ukm;
     }
-    
+
     public ASN1Primitive toASN1Primitive()
     {
         ASN1EncodableVector v = new ASN1EncodableVector();
@@ -35,6 +46,10 @@ public class CMSORIforKEMOtherInfo
         v.add(wrap);
         v.add(new ASN1Integer(kekLength));
 
+        if (ukm != null)
+        {
+            v.add(new DERTaggedObject(true, 0, new DEROctetString(ukm)));
+        }
         return new DERSequence(v);
     }
 }
