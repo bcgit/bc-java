670f57990dc2806d590bcc489cbdac2fcb3b51e3
Peter Dettman <peter.dettman@bouncycastle.org>
2023-03-01 17:11:13 +0700

    Add Integers.bitCount

--- core/src/test/java/org/bouncycastle/util/utiltest/IntegersTest.java
+++ core/src/test/java/org/bouncycastle/util/utiltest/IntegersTest.java
@@ -1,5 +1,7 @@
 package org.bouncycastle.util.utiltest;
 
+import java.util.Random;
+
 import org.bouncycastle.util.Integers;
 
 import junit.framework.TestCase;
@@ -7,6 +9,17 @@ import junit.framework.TestCase;
 public class IntegersTest
     extends TestCase
 {
+    public void testBitCount()
+    {
+    	Random random = new Random();
+
+    	for (int pos = 0; pos <= 24; ++pos)
+    	{
+    		int seed = random.nextInt();
+    		implTestBitCountRange(seed, pos, 256);
+    	}
+    }
+
     public void testNumberOfLeadingZeros()
     {
         for (int i = 0; i < 31; ++i)
@@ -29,5 +42,24 @@ public class IntegersTest
 
         assertEquals(31, Integers.numberOfTrailingZeros(Integer.MIN_VALUE));
         assertEquals(32, Integers.numberOfTrailingZeros(0));
+    }
+
+    private static void implTestBitCountRange(int seed, int pos, int count)
+    {
+    	for (int i = 0; i < count; ++i)
+    	{
+    		int n = seed + (i << pos);
+            assertEquals(simpleBitCount(n), Integers.bitCount(n));
+    	}
+    }
+
+    private static int simpleBitCount(int n)
+    {
+    	int count = 0;
+    	for (int i = 0; i < 32; ++i)
+    	{
+    		count += (n >>> i) & 1;
+    	}
+    	return count;
     }
 }
