5b77b67cd90102dd4510da2c77362f2427fefc4c
David Hook <dgh@cryptoworkshop.com>
2023-03-24 20:05:36 +1100

    updated version number

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/BouncyCastlePQCProvider.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/BouncyCastlePQCProvider.java
@@ -22,7 +22,7 @@ public class BouncyCastlePQCProvider
     extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Post-Quantum Security Provider v1.73b";
+    private static String info = "BouncyCastle Post-Quantum Security Provider v1.73";
 
     public static String PROVIDER_NAME = "BCPQC";
 
@@ -50,7 +50,7 @@ public class BouncyCastlePQCProvider
      */
     public BouncyCastlePQCProvider()
     {
-        super(PROVIDER_NAME, 1.7299, info);
+        super(PROVIDER_NAME, 1.73, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
