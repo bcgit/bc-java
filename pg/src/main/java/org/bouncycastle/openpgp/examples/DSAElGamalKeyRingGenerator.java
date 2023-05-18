0c7bd00b1500955c0a796b3b361bbd254fa972fc
David Hook <dgh@cryptoworkshop.com>
2023-05-18 13:35:41 +1000

    minor refactoring, update - relates to github #1394, github #1346

--- pg/src/main/java/org/bouncycastle/openpgp/examples/DSAElGamalKeyRingGenerator.java
+++ pg/src/main/java/org/bouncycastle/openpgp/examples/DSAElGamalKeyRingGenerator.java
@@ -41,31 +41,31 @@ import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyEncryptorBuilder;
  * <p>
  * usage: DSAElGamalKeyRingGenerator [-a] identity passPhrase
  * <p>
- * Where identity is the name to be associated with the public key. The keys are placed 
+ * Where identity is the name to be associated with the public key. The keys are placed
  * in the files pub.[asc|bpg] and secret.[asc|bpg].
  */
 public class DSAElGamalKeyRingGenerator
 {
 
     private static final int SIG_HASH = HashAlgorithmTags.SHA512;
-    private static final int[] HASH_PREFERENCES = new int[] {
-            HashAlgorithmTags.SHA512, HashAlgorithmTags.SHA384, HashAlgorithmTags.SHA256, HashAlgorithmTags.SHA224
+    private static final int[] HASH_PREFERENCES = new int[]{
+        HashAlgorithmTags.SHA512, HashAlgorithmTags.SHA384, HashAlgorithmTags.SHA256, HashAlgorithmTags.SHA224
     };
-    private static final int[] SYM_PREFERENCES = new int[] {
-            SymmetricKeyAlgorithmTags.AES_256, SymmetricKeyAlgorithmTags.AES_192, SymmetricKeyAlgorithmTags.AES_128
+    private static final int[] SYM_PREFERENCES = new int[]{
+        SymmetricKeyAlgorithmTags.AES_256, SymmetricKeyAlgorithmTags.AES_192, SymmetricKeyAlgorithmTags.AES_128
     };
-    private static final int[] COMP = new int[] {
-            CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.BZIP2, CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.UNCOMPRESSED
+    private static final int[] COMP = new int[]{
+        CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.BZIP2, CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.UNCOMPRESSED
     };
 
     private static void exportKeyPair(
-        OutputStream    secretOut,
-        OutputStream    publicOut,
-        KeyPair         dsaKp,
-        KeyPair         elgKp,
-        String          identity,
-        char[]          passPhrase,
-        boolean         armor)
+        OutputStream secretOut,
+        OutputStream publicOut,
+        KeyPair dsaKp,
+        KeyPair elgKp,
+        String identity,
+        char[] passPhrase,
+        boolean armor)
         throws IOException, PGPException
     {
         if (armor)
@@ -73,46 +73,48 @@ public class DSAElGamalKeyRingGenerator
             secretOut = new ArmoredOutputStream(secretOut);
         }
 
-        PGPKeyPair        dsaKeyPair = new JcaPGPKeyPair(PGPPublicKey.DSA, dsaKp, new Date());
-        PGPKeyPair        elgKeyPair = new JcaPGPKeyPair(PGPPublicKey.ELGAMAL_ENCRYPT, elgKp, new Date());
+        PGPKeyPair dsaKeyPair = new JcaPGPKeyPair(PGPPublicKey.DSA, dsaKp, new Date());
+        PGPKeyPair elgKeyPair = new JcaPGPKeyPair(PGPPublicKey.ELGAMAL_ENCRYPT, elgKp, new Date());
         PGPDigestCalculator sha1Calc = new JcaPGPDigestCalculatorProviderBuilder().build().get(HashAlgorithmTags.SHA1);
 
         PGPContentSignerBuilder contentSignerBuilder = new JcaPGPContentSignerBuilder(
-                dsaKeyPair.getPublicKey().getAlgorithm(), SIG_HASH);
+            dsaKeyPair.getPublicKey().getAlgorithm(), SIG_HASH);
         PBESecretKeyEncryptor secretKeyEncryptor = new JcePBESecretKeyEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256, sha1Calc)
-                .setProvider("BC")
-                .build(passPhrase);
+            .setProvider("BC")
+            .build(passPhrase);
 
         PGPSignatureSubpacketGenerator primaryHashedSubpackets = getPrimaryKeyHashedSubpackets();
         primaryHashedSubpackets.setIssuerFingerprint(false, dsaKeyPair.getPublicKey());
-        PGPKeyRingGenerator    keyRingGen = new PGPKeyRingGenerator(PGPSignature.POSITIVE_CERTIFICATION, dsaKeyPair,
-                 identity, sha1Calc, primaryHashedSubpackets.generate(), null, contentSignerBuilder, secretKeyEncryptor);
+        PGPKeyRingGenerator keyRingGen = new PGPKeyRingGenerator(PGPSignature.POSITIVE_CERTIFICATION, dsaKeyPair,
+            identity, sha1Calc, primaryHashedSubpackets.generate(), null, contentSignerBuilder, secretKeyEncryptor);
 
         PGPSignatureSubpacketGenerator subkeyHashedSubpackets = getEncryptionKeyHashedSubpackets();
         subkeyHashedSubpackets.setIssuerFingerprint(false, dsaKeyPair.getPublicKey());
         keyRingGen.addSubKey(elgKeyPair, subkeyHashedSubpackets.generate(), null);
-        
+
         keyRingGen.generateSecretKeyRing().encode(secretOut);
-        
+
         secretOut.close();
-        
+
         if (armor)
         {
             publicOut = new ArmoredOutputStream(publicOut);
         }
-        
+
         keyRingGen.generatePublicKeyRing().encode(publicOut);
-        
+
         publicOut.close();
     }
 
-    private static PGPSignatureSubpacketGenerator getEncryptionKeyHashedSubpackets() {
+    private static PGPSignatureSubpacketGenerator getEncryptionKeyHashedSubpackets()
+    {
         PGPSignatureSubpacketGenerator gen = new PGPSignatureSubpacketGenerator();
         gen.setKeyFlags(true, KeyFlags.ENCRYPT_COMMS | KeyFlags.ENCRYPT_STORAGE);
         return gen;
     }
 
-    private static PGPSignatureSubpacketGenerator getPrimaryKeyHashedSubpackets() {
+    private static PGPSignatureSubpacketGenerator getPrimaryKeyHashedSubpackets()
+    {
         PGPSignatureSubpacketGenerator gen = new PGPSignatureSubpacketGenerator();
         gen.setPreferredHashAlgorithms(false, HASH_PREFERENCES);
         gen.setPreferredSymmetricAlgorithms(false, SYM_PREFERENCES);
@@ -122,11 +124,13 @@ public class DSAElGamalKeyRingGenerator
         return gen;
     }
 
-    private static KeyPair generateElGamalKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-        KeyPairGenerator    elgKpg = KeyPairGenerator.getInstance("ELGAMAL", "BC");
+    private static KeyPair generateElGamalKeyPair()
+        throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException
+    {
+        KeyPairGenerator elgKpg = KeyPairGenerator.getInstance("ELGAMAL", "BC");
         // 3072 bit parameters from https://datatracker.ietf.org/doc/html/rfc3526#section-4
-        BigInteger          g = new BigInteger("2", 16);
-        BigInteger          p = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF", 16);
+        BigInteger g = new BigInteger("2", 16);
+        BigInteger p = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF", 16);
 
         DHParameterSpec elParams = new DHParameterSpec(p, g);
 
@@ -135,12 +139,14 @@ public class DSAElGamalKeyRingGenerator
         //
         // this is quicker because we are using pregenerated parameters.
         //
-        KeyPair                    elgKp = elgKpg.generateKeyPair();
+        KeyPair elgKp = elgKpg.generateKeyPair();
         return elgKp;
     }
 
-    private static KeyPair generateDSAKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException {
-        KeyPairGenerator    dsaKpg = KeyPairGenerator.getInstance("DSA", "BC");
+    private static KeyPair generateDSAKeyPair()
+        throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        KeyPairGenerator dsaKpg = KeyPairGenerator.getInstance("DSA", "BC");
 
         dsaKpg.initialize(3072);
 
@@ -148,7 +154,7 @@ public class DSAElGamalKeyRingGenerator
         // this takes a while as the key generator has to generate some DSA params
         // before it generates the key.
         //
-        KeyPair             dsaKp = dsaKpg.generateKeyPair();
+        KeyPair dsaKp = dsaKpg.generateKeyPair();
         return dsaKp;
     }
 
@@ -175,17 +181,17 @@ public class DSAElGamalKeyRingGenerator
                 System.out.println("DSAElGamalKeyRingGenerator [-a] identity passPhrase");
                 System.exit(0);
             }
-            
-            FileOutputStream    out1 = new FileOutputStream("secret.asc");
-            FileOutputStream    out2 = new FileOutputStream("pub.asc");
-            
+
+            FileOutputStream out1 = new FileOutputStream("secret.asc");
+            FileOutputStream out2 = new FileOutputStream("pub.asc");
+
             exportKeyPair(out1, out2, dsaKp, elgKp, args[1], args[2].toCharArray(), true);
         }
         else
         {
-            FileOutputStream    out1 = new FileOutputStream("secret.bpg");
-            FileOutputStream    out2 = new FileOutputStream("pub.bpg");
-            
+            FileOutputStream out1 = new FileOutputStream("secret.bpg");
+            FileOutputStream out2 = new FileOutputStream("pub.bpg");
+
             exportKeyPair(out1, out2, dsaKp, elgKp, args[0], args[1].toCharArray(), false);
         }
     }
--- pg/src/main/java/org/bouncycastle/openpgp/examples/EllipticCurveKeyPairGenerator.java
+++ pg/src/main/java/org/bouncycastle/openpgp/examples/EllipticCurveKeyPairGenerator.java
@@ -1,5 +1,19 @@
 package org.bouncycastle.openpgp.examples;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
 import org.bouncycastle.bcpg.ArmoredOutputStream;
 import org.bouncycastle.bcpg.CompressionAlgorithmTags;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
@@ -16,7 +30,6 @@ import org.bouncycastle.openpgp.PGPPublicKey;
 import org.bouncycastle.openpgp.PGPPublicKeyRing;
 import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.bouncycastle.openpgp.PGPSignature;
-import org.bouncycastle.openpgp.PGPSignatureGenerator;
 import org.bouncycastle.openpgp.PGPSignatureSubpacketGenerator;
 import org.bouncycastle.openpgp.operator.PBESecretKeyEncryptor;
 import org.bouncycastle.openpgp.operator.PGPContentSignerBuilder;
@@ -26,50 +39,36 @@ import org.bouncycastle.openpgp.operator.jcajce.JcaPGPDigestCalculatorProviderBu
 import org.bouncycastle.openpgp.operator.jcajce.JcaPGPKeyPair;
 import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyEncryptorBuilder;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.Security;
-import java.security.spec.AlgorithmParameterSpec;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-
 /**
  * A simple utility class that generates an RSA key ring.
  * <p>
  * usage: RSAKeyPairGenerator [-a] identity passPhrase
  * <p>
- * Where identity is the name to be associated with the public key. The keys are placed 
+ * Where identity is the name to be associated with the public key. The keys are placed
  * in the files pub.[asc|bpg] and secret.[asc|bpg].
  */
 public class EllipticCurveKeyPairGenerator
 {
 
     private static final int SIG_HASH = HashAlgorithmTags.SHA512;
-    private static final int[] HASH_PREFERENCES = new int[] {
-            HashAlgorithmTags.SHA512, HashAlgorithmTags.SHA384, HashAlgorithmTags.SHA256, HashAlgorithmTags.SHA224
+    private static final int[] HASH_PREFERENCES = new int[]{
+        HashAlgorithmTags.SHA512, HashAlgorithmTags.SHA384, HashAlgorithmTags.SHA256, HashAlgorithmTags.SHA224
     };
-    private static final int[] SYM_PREFERENCES = new int[] {
-            SymmetricKeyAlgorithmTags.AES_256, SymmetricKeyAlgorithmTags.AES_192, SymmetricKeyAlgorithmTags.AES_128
+    private static final int[] SYM_PREFERENCES = new int[]{
+        SymmetricKeyAlgorithmTags.AES_256, SymmetricKeyAlgorithmTags.AES_192, SymmetricKeyAlgorithmTags.AES_128
     };
-    private static final int[] COMP_PREFERENCES = new int[] {
-            CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.BZIP2, CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.UNCOMPRESSED
+    private static final int[] COMP_PREFERENCES = new int[]{
+        CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.BZIP2, CompressionAlgorithmTags.ZLIB, CompressionAlgorithmTags.UNCOMPRESSED
     };
 
     private static void generateAndExportKeyRing(
-        OutputStream    secretOut,
-        OutputStream    publicOut,
-        String          identity,
-        char[]          passPhrase,
-        boolean         armor)
-            throws IOException, NoSuchProviderException, PGPException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        OutputStream secretOut,
+        OutputStream publicOut,
+        String identity,
+        char[] passPhrase,
+        boolean armor)
+        throws IOException, NoSuchProviderException, PGPException, NoSuchAlgorithmException, InvalidAlgorithmParameterException
+    {
         if (armor)
         {
             secretOut = new ArmoredOutputStream(secretOut);
@@ -82,7 +81,7 @@ public class EllipticCurveKeyPairGenerator
 
         PGPContentSignerBuilder contentSignerBuilder = new JcaPGPContentSignerBuilder(PublicKeyAlgorithmTags.EDDSA_LEGACY, SIG_HASH);
         PBESecretKeyEncryptor secretKeyEncryptor = new JcePBESecretKeyEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256, sha1Calc)
-                .build(passPhrase);
+            .build(passPhrase);
 
         Date now = new Date();
 
@@ -112,45 +111,46 @@ public class EllipticCurveKeyPairGenerator
         encryptionKeySubpackets.setIssuerFingerprint(false, primaryKey.getPublicKey());
 
         PGPKeyRingGenerator gen = new PGPKeyRingGenerator(PGPSignature.POSITIVE_CERTIFICATION, primaryKey, identity,
-                sha1Calc, primarySubpackets.generate(), null, contentSignerBuilder, secretKeyEncryptor);
+            sha1Calc, primarySubpackets.generate(), null, contentSignerBuilder, secretKeyEncryptor);
         gen.addSubKey(signingKey, signingKeySubpacket.generate(), null, contentSignerBuilder);
         gen.addSubKey(encryptionKey, encryptionKeySubpackets.generate(), null);
 
         PGPSecretKeyRing secretKeys = gen.generateSecretKeyRing();
         secretKeys.encode(secretOut);
-        
+
         secretOut.close();
-        
+
         if (armor)
         {
             publicOut = new ArmoredOutputStream(publicOut);
         }
 
-        List<PGPPublicKey> publicKeyList = new ArrayList<>();
+        List<PGPPublicKey> publicKeyList = new ArrayList<PGPPublicKey>();
         Iterator<PGPPublicKey> it = secretKeys.getPublicKeys();
-        while (it.hasNext()) {
+        while (it.hasNext())
+        {
             publicKeyList.add(it.next());
         }
 
         PGPPublicKeyRing publicKeys = new PGPPublicKeyRing(publicKeyList);
-        
+
         publicKeys.encode(publicOut);
-        
+
         publicOut.close();
     }
-    
+
     public static void main(
         String[] args)
         throws Exception
     {
         Security.addProvider(new BouncyCastleProvider());
-        
+
         if (args.length < 2)
         {
             System.out.println("EllipticCurveKeyPairGenerator [-a] identity passPhrase");
             System.exit(0);
         }
-        
+
         if (args[0].equals("-a"))
         {
             if (args.length < 3)
@@ -158,17 +158,17 @@ public class EllipticCurveKeyPairGenerator
                 System.out.println("EllipticCurveKeyPairGenerator [-a] identity passPhrase");
                 System.exit(0);
             }
-            
-            FileOutputStream    out1 = new FileOutputStream("secret.asc");
-            FileOutputStream    out2 = new FileOutputStream("pub.asc");
-            
+
+            FileOutputStream out1 = new FileOutputStream("secret.asc");
+            FileOutputStream out2 = new FileOutputStream("pub.asc");
+
             generateAndExportKeyRing(out1, out2, args[1], args[2].toCharArray(), true);
         }
         else
         {
-            FileOutputStream    out1 = new FileOutputStream("secret.bpg");
-            FileOutputStream    out2 = new FileOutputStream("pub.bpg");
-            
+            FileOutputStream out1 = new FileOutputStream("secret.bpg");
+            FileOutputStream out2 = new FileOutputStream("pub.bpg");
+
             generateAndExportKeyRing(out1, out2, args[0], args[1].toCharArray(), false);
         }
     }
