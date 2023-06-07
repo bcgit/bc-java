c8ef1d67ffb9f93404c40819b599c4d735bd72d5
David Hook <dgh@cryptoworkshop.com>
2023-06-07 12:30:53 +1000

    1.74 release updates

--- prov/src/main/jdk1.1/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/jdk1.1/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -43,7 +43,7 @@ import org.bouncycastle.jcajce.provider.util.AsymmetricKeyInfoConverter;
 public final class BouncyCastleProvider extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Security Provider v1.73";
+    private static String info = "BouncyCastle Security Provider v1.74";
 
     public static final String PROVIDER_NAME = "BC";
 
@@ -118,7 +118,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.73, info);
+        super(PROVIDER_NAME, 1.74, info);
 
         setup();
     }
