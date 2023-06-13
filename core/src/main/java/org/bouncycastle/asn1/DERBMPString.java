c13cf199449485366174ffdf7e0f0a8a60760668
Peter Dettman <peter.dettman@bouncycastle.org>
2023-06-13 17:24:01 +0700

    Remove deprecated DERBMPString.getInstance methods

--- core/src/main/java/org/bouncycastle/asn1/DERBMPString.java
+++ core/src/main/java/org/bouncycastle/asn1/DERBMPString.java
@@ -12,69 +12,6 @@ public class DERBMPString
     extends ASN1BMPString
 {
     /**
-     * Return a BMP String from the given object.
-     *
-     * @param obj the object we want converted.
-     * @exception IllegalArgumentException if the object cannot be converted.
-     * @return a DERBMPString instance, or null.
-     * 
-     * @deprecated Use {@link ASN1BMPString#getInstance(Object)} instead.
-     */
-    public static DERBMPString getInstance(
-        Object  obj)
-    {
-        if (obj == null || obj instanceof DERBMPString)
-        {
-            return (DERBMPString)obj;
-        }
-        if (obj instanceof ASN1BMPString)
-        {
-            return new DERBMPString(((ASN1BMPString)obj).string);
-        }
-        if (obj instanceof byte[])
-        {
-            try
-            {
-                return (DERBMPString)fromByteArray((byte[])obj);
-            }
-            catch (Exception e)
-            {
-                throw new IllegalArgumentException("encoding error in getInstance: " + e.toString());
-            }
-        }
-
-        throw new IllegalArgumentException("illegal object in getInstance: " + obj.getClass().getName());
-    }
-
-    /**
-     * Return a BMP String from a tagged object.
-     *
-     * @param obj      the tagged object holding the object we want
-     * @param explicit true if the object is meant to be explicitly tagged false
-     *                 otherwise.
-     * @exception IllegalArgumentException if the tagged object cannot be converted.
-     * @return a DERBMPString instance.
-     * 
-     * @deprecated Use {@link ASN1BMPString#getInstance(ASN1TaggedObject, boolean)}
-     *             instead.
-     */
-    public static DERBMPString getInstance(
-        ASN1TaggedObject obj,
-        boolean          explicit)
-    {
-        ASN1Primitive o = obj.getObject();
-
-        if (explicit || o instanceof DERBMPString)
-        {
-            return getInstance(o);
-        }
-        else
-        {
-            return new DERBMPString(ASN1OctetString.getInstance(o).getOctets());
-        }
-    }
-
-    /**
      * Basic constructor
      * @param string a String to wrap as a BMP STRING.
      */
