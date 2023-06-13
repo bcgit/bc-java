41655fb880bb57f6e4ac5ab694f1ce0275a7eb40
Peter Dettman <peter.dettman@bouncycastle.org>
2023-06-13 18:18:04 +0700

    Remove deprecated DERT61String.getInstance methods

--- core/src/main/java/org/bouncycastle/asn1/DERT61String.java
+++ core/src/main/java/org/bouncycastle/asn1/DERT61String.java
@@ -8,69 +8,6 @@ public class DERT61String
     extends ASN1T61String
 {
     /**
-     * Return a T61 string from the passed in object.
-     *
-     * @param obj a DERT61String or an object that can be converted into one.
-     * @exception IllegalArgumentException if the object cannot be converted.
-     * @return a DERT61String instance, or null
-     * 
-     * @deprecated Use {@link ASN1T61String#getInstance(Object)} instead.
-     */
-    public static DERT61String getInstance(
-        Object  obj)
-    {
-        if (obj == null || obj instanceof DERT61String)
-        {
-            return (DERT61String)obj;
-        }
-        if (obj instanceof ASN1T61String)
-        {
-            return new DERT61String(((ASN1T61String)obj).contents, false);
-        }
-        if (obj instanceof byte[])
-        {
-            try
-            {
-                return (DERT61String)fromByteArray((byte[])obj);
-            }
-            catch (Exception e)
-            {
-                throw new IllegalArgumentException("encoding error in getInstance: " + e.toString());
-            }
-        }
-
-        throw new IllegalArgumentException("illegal object in getInstance: " + obj.getClass().getName());
-    }
-
-    /**
-     * Return an T61 String from a tagged object.
-     *
-     * @param obj      the tagged object holding the object we want
-     * @param explicit true if the object is meant to be explicitly tagged false
-     *                 otherwise.
-     * @exception IllegalArgumentException if the tagged object cannot be converted.
-     * @return a DERT61String instance, or null
-     * 
-     * @deprecated Use {@link ASN1T61String#getInstance(ASN1TaggedObject, boolean)}
-     *             instead.
-     */
-    public static DERT61String getInstance(
-        ASN1TaggedObject obj,
-        boolean          explicit)
-    {
-        ASN1Primitive o = obj.getObject();
-
-        if (explicit || o instanceof DERT61String)
-        {
-            return getInstance(o);
-        }
-        else
-        {
-            return new DERT61String(ASN1OctetString.getInstance(o).getOctets(), true);
-        }
-    }
-
-    /**
      * Basic constructor - with string 8 bit assumed.
      *
      * @param string the string to be wrapped.
