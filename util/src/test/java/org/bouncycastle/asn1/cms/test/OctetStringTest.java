6b514312a5b1c8b2830bcbfdbf161e30f72eba0f
Peter Dettman <peter.dettman@bouncycastle.org>
2023-06-14 15:36:43 +0700

    Refactoring in asn1 tests

--- util/src/test/java/org/bouncycastle/asn1/cms/test/OctetStringTest.java
+++ util/src/test/java/org/bouncycastle/asn1/cms/test/OctetStringTest.java
@@ -144,16 +144,11 @@ public class OctetStringTest
                 new BEROctetString(new byte[20])
             });
 
-        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-        ASN1OutputStream aOut = ASN1OutputStream.create(bOut);
+        byte[] encoding = str.getEncoded();
 
-        aOut.writeObject(str);
+        ASN1OctetString decoded = ASN1OctetString.getInstance(encoding);
 
-        aOut.close();
-
-        ASN1InputStream aIn = new ASN1InputStream(bOut.toByteArray());
-
-        assertTrue(Arrays.areEqual(new byte[30], ASN1OctetString.getInstance(aIn.readObject()).getOctets()));
+        assertTrue(Arrays.areEqual(new byte[30], decoded.getOctets()));
     }
 
     public void testNestedStructure()
