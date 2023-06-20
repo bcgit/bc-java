29a51f39ea609390456be9309cffad5490b7a693
David Hook <dgh@cryptoworkshop.com>
2023-06-20 14:19:50 +1000

    removed use of GCMParameterSpec

--- pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JceAEADCipherUtil.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JceAEADCipherUtil.java
@@ -2,18 +2,39 @@ package org.bouncycastle.openpgp.operator.jcajce;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidParameterSpecException;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.GCMParameterSpec;
+
+import org.bouncycastle.internal.asn1.cms.GCMParameters;
+import org.bouncycastle.jcajce.provider.symmetric.util.GcmSpecUtil;
+import org.bouncycastle.jcajce.spec.AEADParameterSpec;
 
 public class JceAEADCipherUtil
 {
     static void setUpAeadCipher(Cipher aead, SecretKey secretKey, int mode, byte[] nonce, int aeadMacLen, byte[] aad)
         throws InvalidKeyException, InvalidAlgorithmParameterException
     {
-        GCMParameterSpec parameters = new GCMParameterSpec(aeadMacLen, nonce);
-        aead.init(mode, secretKey, parameters);
-        aead.updateAAD(aad);
+        if (GcmSpecUtil.gcmSpecExtractable())
+        {
+            AlgorithmParameterSpec parameters;
+            try
+            {
+                parameters = GcmSpecUtil.extractGcmSpec(new GCMParameters(nonce, (aeadMacLen + 7) / 8).toASN1Primitive());
+            }
+            catch (InvalidParameterSpecException e)
+            {
+                throw new InvalidAlgorithmParameterException(e.getMessage());
+            }
+            aead.init(mode, secretKey, parameters);
+            aead.updateAAD(aad);
+        }
+        else
+        {
+            AEADParameterSpec parameters = new AEADParameterSpec(nonce, aeadMacLen, aad);
+            aead.init(mode, secretKey, parameters);
+        }
     }
 }
