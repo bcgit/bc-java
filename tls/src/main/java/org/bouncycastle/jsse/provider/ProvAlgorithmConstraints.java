781d5672810506d1692768f4168fabffa5dcab10
Peter Dettman <peter.dettman@bouncycastle.org>
2023-06-26 19:01:13 +0700

    BCJSSE: Update default disabled algorithms properties

--- tls/src/main/java/org/bouncycastle/jsse/provider/ProvAlgorithmConstraints.java
+++ tls/src/main/java/org/bouncycastle/jsse/provider/ProvAlgorithmConstraints.java
@@ -23,9 +23,11 @@ class ProvAlgorithmConstraints
     private static final String PROPERTY_TLS_DISABLED_ALGORITHMS = "jdk.tls.disabledAlgorithms";
 
     private static final String DEFAULT_CERTPATH_DISABLED_ALGORITHMS =
-        "MD2, MD5, SHA1 jdkCA & usage TLSServer, RSA keySize < 1024, DSA keySize < 1024, EC keySize < 224";
+        "MD2, MD5, SHA1 jdkCA & usage TLSServer, RSA keySize < 1024, DSA keySize < 1024, EC keySize < 224, " +
+        "SHA1 usage SignedJAR & denyAfter 2019-01-01";    
     private static final String DEFAULT_TLS_DISABLED_ALGORITHMS =
-        "SSLv3, TLSv1, TLSv1.1, RC4, DES, MD5withRSA, DH keySize < 1024, EC keySize < 224, 3DES_EDE_CBC, anon, NULL";
+        "SSLv3, TLSv1, TLSv1.1, DTLSv1.0, RC4, DES, MD5withRSA, DH keySize < 1024, EC keySize < 224, 3DES_EDE_CBC, " +
+        "anon, NULL, ECDH";    
     private static final DisabledAlgorithmConstraints provTlsDisabledAlgorithms = DisabledAlgorithmConstraints.create(
         ProvAlgorithmDecomposer.INSTANCE_TLS, PROPERTY_TLS_DISABLED_ALGORITHMS, DEFAULT_TLS_DISABLED_ALGORITHMS);
     private static final DisabledAlgorithmConstraints provX509DisabledAlgorithms = DisabledAlgorithmConstraints.create(
