a65b216821ab2c241f75fa95906e87819b71d230
Peter Dettman <peter.dettman@bouncycastle.org>
2023-06-27 12:48:48 +0700

    Restore jdk1.9 MR override for GCMUtil
    - simplify jdk1.4 fallback

--- tls/src/main/jdk1.9/org/bouncycastle/tls/crypto/impl/jcajce/GCMUtil.java
+++ tls/src/main/jdk1.9/org/bouncycastle/tls/crypto/impl/jcajce/GCMUtil.java
@@ -2,16 +2,18 @@ package org.bouncycastle.tls.crypto.impl.jcajce;
 
 import java.security.spec.AlgorithmParameterSpec;
 
+import javax.crypto.spec.GCMParameterSpec;
+
 class GCMUtil
 {
     static AlgorithmParameterSpec createGCMParameterSpec(final int tLen, final byte[] src)
         throws Exception
     {
-        throw new IllegalStateException();
+        return new GCMParameterSpec(tLen, src);
     }
 
     static boolean isGCMParameterSpecAvailable()
     {
-        return false;
+        return true;
     }
 }
