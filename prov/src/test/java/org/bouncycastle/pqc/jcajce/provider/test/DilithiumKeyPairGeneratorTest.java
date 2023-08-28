bf631134b7b11b89e32f118a7b164afc3ed37e98
David Hook <dgh@cryptoworkshop.com>
2023-08-28 12:59:25 +1000

    added test on algorithm names

--- prov/src/test/java/org/bouncycastle/pqc/jcajce/provider/test/DilithiumKeyPairGeneratorTest.java
+++ prov/src/test/java/org/bouncycastle/pqc/jcajce/provider/test/DilithiumKeyPairGeneratorTest.java
@@ -1,10 +1,13 @@
 package org.bouncycastle.pqc.jcajce.provider.test;
 
 import java.security.KeyFactory;
+import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 import java.security.Security;
 
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.bc.BCObjectIdentifiers;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.pqc.jcajce.spec.DilithiumParameterSpec;
 
@@ -27,6 +30,32 @@ public class DilithiumKeyPairGeneratorTest
         throws Exception
     {
         kf = KeyFactory.getInstance("Dilithium", "BC");
+    }
+
+    public void testKeyPairGeneratorNames()
+        throws Exception
+    {
+        ASN1ObjectIdentifier[] oids = new ASN1ObjectIdentifier[] {
+            BCObjectIdentifiers.dilithium2,
+            BCObjectIdentifiers.dilithium3,
+            BCObjectIdentifiers.dilithium5
+        };
+
+        String[] algs = new String[]{
+            "DILITHIUM2",
+            "DILITHIUM3",
+            "DILITHIUM5"
+        };
+
+        for (int i = 0; i != oids.length; i++)
+        {
+            KeyPairGenerator kpGen = KeyPairGenerator.getInstance(oids[i].getId(), "BC");
+
+            KeyPair kp = kpGen.generateKeyPair();
+
+            assertEquals(algs[i], kp.getPrivate().getAlgorithm());
+            assertEquals(algs[i], kp.getPublic().getAlgorithm());
+        }
     }
 
     public void testKeyPairEncoding()
