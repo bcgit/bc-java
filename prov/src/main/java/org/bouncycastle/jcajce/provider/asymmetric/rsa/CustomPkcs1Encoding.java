e0569dcb1dea9d421d84fc4c5c5688fe101afa2d
Peter Dettman <peter.dettman@bouncycastle.org>
2023-11-10 19:57:47 +0700

    Improvements to PKCS1Encoding

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/rsa/CustomPkcs1Encoding.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/rsa/CustomPkcs1Encoding.java
@@ -1,4 +1,4 @@
-package org.bouncycastle.crypto.encodings;
+package org.bouncycastle.jcajce.provider.asymmetric.rsa;
 
 import java.security.SecureRandom;
 
@@ -6,6 +6,7 @@ import org.bouncycastle.crypto.AsymmetricBlockCipher;
 import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.CryptoServicesRegistrar;
 import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.encodings.PKCS1Encoding;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithRandom;
 import org.bouncycastle.util.Arrays;
@@ -15,24 +16,9 @@ import org.bouncycastle.util.Properties;
  * this does your basic PKCS 1 v1.5 padding - whether or not you should be using this
  * depends on your application - see PKCS1 Version 2 for details.
  */
-public class PKCS1Encoding
+class CustomPKCS1Encoding
     implements AsymmetricBlockCipher
 {
-    /**
-     * @deprecated use NOT_STRICT_LENGTH_ENABLED_PROPERTY
-     */
-    public static final String STRICT_LENGTH_ENABLED_PROPERTY = "org.bouncycastle.pkcs1.strict";
-
-    /**
-     * some providers fail to include the leading zero in PKCS1 encoded blocks. If you need to
-     * work with one of these set the system property org.bouncycastle.pkcs1.not_strict to true.
-     * <p>
-     * The system property is checked during construction of the encoding object, it is set to
-     * false by default.
-     * </p>
-     */
-    public static final String NOT_STRICT_LENGTH_ENABLED_PROPERTY = "org.bouncycastle.pkcs1.not_strict";
-
     private static final int HEADER_LENGTH = 10;
 
     private SecureRandom random;
@@ -40,8 +26,6 @@ public class PKCS1Encoding
     private boolean forEncryption;
     private boolean forPrivateKey;
     private boolean useStrictLength;
-    private int pLen = -1;
-    private byte[] fallback = null;
     private byte[] blockBuffer;
 
     /**
@@ -49,57 +33,23 @@ public class PKCS1Encoding
      *
      * @param cipher
      */
-    public PKCS1Encoding(
-        AsymmetricBlockCipher cipher)
+    CustomPKCS1Encoding(AsymmetricBlockCipher cipher)
     {
         this.engine = cipher;
         this.useStrictLength = useStrict();
     }
-
-    /**
-     * Constructor for decryption with a fixed plaintext length.
-     *
-     * @param cipher The cipher to use for cryptographic operation.
-     * @param pLen   Length of the expected plaintext.
-     */
-    public PKCS1Encoding(
-        AsymmetricBlockCipher cipher,
-        int pLen)
-    {
-        this.engine = cipher;
-        this.useStrictLength = useStrict();
-        this.pLen = pLen;
-    }
-
-    /**
-     * Constructor for decryption with a fixed plaintext length and a fallback
-     * value that is returned, if the padding is incorrect.
-     *
-     * @param cipher   The cipher to use for cryptographic operation.
-     * @param fallback The fallback value, we don't do an arraycopy here.
-     */
-    public PKCS1Encoding(
-        AsymmetricBlockCipher cipher,
-        byte[] fallback)
-    {
-        this.engine = cipher;
-        this.useStrictLength = useStrict();
-        this.fallback = fallback;
-        this.pLen = fallback.length;
-    }
-
 
     //
     // for J2ME compatibility
     //
     private boolean useStrict()
     {
-        if (Properties.isOverrideSetTo(NOT_STRICT_LENGTH_ENABLED_PROPERTY, true))
+        if (Properties.isOverrideSetTo(PKCS1Encoding.NOT_STRICT_LENGTH_ENABLED_PROPERTY, true))
         {
             return false;
         }
 
-        return !Properties.isOverrideSetTo(STRICT_LENGTH_ENABLED_PROPERTY, false);
+        return !Properties.isOverrideSetTo(PKCS1Encoding.STRICT_LENGTH_ENABLED_PROPERTY, false);
     }
 
     public AsymmetricBlockCipher getUnderlyingCipher()
@@ -107,9 +57,7 @@ public class PKCS1Encoding
         return engine;
     }
 
-    public void init(
-        boolean forEncryption,
-        CipherParameters param)
+    public void init(boolean forEncryption, CipherParameters param)
     {
         AsymmetricKeyParameter kParam;
 
@@ -134,11 +82,6 @@ public class PKCS1Encoding
         this.forPrivateKey = kParam.isPrivate();
         this.forEncryption = forEncryption;
         this.blockBuffer = new byte[engine.getOutputBlockSize()];
-
-        if (pLen > 0 && fallback == null && random == null)
-        {
-           throw new IllegalArgumentException("encoder requires random");
-        }
     }
 
     public int getInputBlockSize()
@@ -169,11 +112,7 @@ public class PKCS1Encoding
         }
     }
 
-    public byte[] processBlock(
-        byte[] in,
-        int inOff,
-        int inLen)
-        throws InvalidCipherTextException
+    public byte[] processBlock(byte[] in, int inOff, int inLen) throws InvalidCipherTextException
     {
         if (forEncryption)
         {
@@ -185,11 +124,7 @@ public class PKCS1Encoding
         }
     }
 
-    private byte[] encodeBlock(
-        byte[] in,
-        int inOff,
-        int inLen)
-        throws InvalidCipherTextException
+    private byte[] encodeBlock(byte[] in, int inOff, int inLen) throws InvalidCipherTextException
     {
         if (inLen > getInputBlockSize())
         {
@@ -289,105 +224,11 @@ public class PKCS1Encoding
     }
 
     /**
-     * Check the argument is a valid encoding with type 2 of a plaintext with the given length. Returns 0 if
-     * valid, or -1 if invalid.
-     */
-    private static int checkPkcs1Encoding2(byte[] buf, int plaintextLength)
-    {
-        // The first byte should be 0x02
-        int badPadSign = -((buf[0] & 0xFF) ^ 0x02);
-
-        int lastPadPos = buf.length - 1 - plaintextLength;
-
-        // The header should be at least 10 bytes
-        badPadSign |= lastPadPos - 9;
-
-        // All pad bytes before the last one should be non-zero
-        for (int i = 1; i < lastPadPos; ++i)
-        {
-            badPadSign |= (buf[i] & 0xFF) - 1;
-        }
-
-        // Last pad byte should be zero
-        badPadSign |= -(buf[lastPadPos] & 0xFF);
-
-        return badPadSign >> 31;
-    }
-
-    /**
-     * Decode PKCS#1.5 encoding, and return a random value if the padding is not correct.
-     *
-     * @param in    The encrypted block.
-     * @param inOff Offset in the encrypted block.
-     * @param inLen Length of the encrypted block.
-     *              //@param pLen Length of the desired output.
-     * @return The plaintext without padding, or a random value if the padding was incorrect.
-     * @throws InvalidCipherTextException
-     */
-    private byte[] decodeBlockOrRandom(byte[] in, int inOff, int inLen)
-        throws InvalidCipherTextException
-    {
-        if (!forPrivateKey)
-        {
-            throw new InvalidCipherTextException("sorry, this method is only for decryption, not for signing");
-        }
-
-        int plaintextLength = this.pLen;
-
-        byte[] random = fallback;
-        if (fallback == null)
-        {
-            random = new byte[plaintextLength];
-            this.random.nextBytes(random);
-        }
-
-        int badPadMask = 0;
-        int strictBlockSize = engine.getOutputBlockSize();
-        byte[] block = engine.processBlock(in, inOff, inLen);
-
-        byte[] data = block;
-        if (block.length != strictBlockSize)
-        {
-            if (useStrictLength || block.length < strictBlockSize)
-            {
-                data = blockBuffer;
-            }
-        }
-
-        badPadMask |= checkPkcs1Encoding2(data, plaintextLength);
-
-        /*
-         * Now, to a constant time constant memory copy of the decrypted value
-         * or the random value, depending on the validity of the padding.
-         */
-        int dataOff = data.length - plaintextLength; 
-        byte[] result = new byte[plaintextLength];
-        for (int i = 0; i < plaintextLength; ++i)
-        {
-            result[i] = (byte)((data[dataOff + i] & ~badPadMask) | (random[i] & badPadMask));
-        }
-
-        Arrays.fill(block, (byte)0);
-        Arrays.fill(blockBuffer, 0, Math.max(0, blockBuffer.length - block.length), (byte)0);
-
-        return result;
-    }
-
-    /**
      * @throws InvalidCipherTextException if the decrypted block is not in PKCS1 format.
      */
     private byte[] decodeBlock(byte[] in, int inOff, int inLen)
         throws InvalidCipherTextException
     {
-        /*
-         * If the length of the expected plaintext is known, we use a constant-time decryption.
-         * If the decryption fails, we return a random value.
-         */
-        if (forPrivateKey && this.pLen != -1)
-        {
-            return this.decodeBlockOrRandom(in, inOff, inLen);
-        }
-
         int strictBlockSize = engine.getOutputBlockSize();
         byte[] block = engine.processBlock(in, inOff, inLen);
 
@@ -403,13 +244,10 @@ public class PKCS1Encoding
 
         try
         {
-            if (plaintextLength < 0)
+            if (plaintextLength < 0 | incorrectLength)
             {
-                throw new InvalidCipherTextException("block incorrect");
-            }
-            if (incorrectLength)
-            {
-                throw new InvalidCipherTextException("block incorrect size");
+                // Special behaviour to avoid throw/catch/throw in CipherSpi
+                return null;
             }
 
             byte[] result = new byte[plaintextLength];
