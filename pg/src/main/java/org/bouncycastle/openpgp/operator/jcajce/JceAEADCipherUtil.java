e8cf62d2d5ea30f63046a217b41c7a16da96d183
David Hook <dgh@cryptoworkshop.com>
2023-11-14 15:27:24 +1100

    fixed dependency issue

--- pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JceAEADCipherUtil.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JceAEADCipherUtil.java
@@ -8,9 +8,17 @@ import java.security.spec.InvalidParameterSpecException;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 
-import org.bouncycastle.internal.asn1.cms.GCMParameters;
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.ASN1Object;
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.jcajce.provider.symmetric.util.GcmSpecUtil;
 import org.bouncycastle.jcajce.spec.AEADParameterSpec;
+import org.bouncycastle.util.Arrays;
 
 public class JceAEADCipherUtil
 {
@@ -35,6 +43,45 @@ public class JceAEADCipherUtil
         {
             AEADParameterSpec parameters = new AEADParameterSpec(nonce, aeadMacLen, aad);
             aead.init(mode, secretKey, parameters);
+        }
+    }
+
+    static class GCMParameters
+        extends ASN1Object
+    {
+        private byte[] nonce;
+        private int icvLen;
+
+        public GCMParameters(
+            byte[] nonce,
+            int    icvLen)
+        {
+            this.nonce = Arrays.clone(nonce);
+            this.icvLen = icvLen;
+        }
+
+        public byte[] getNonce()
+        {
+            return Arrays.clone(nonce);
+        }
+
+        public int getIcvLen()
+        {
+            return icvLen;
+        }
+
+        public ASN1Primitive toASN1Primitive()
+        {
+            ASN1EncodableVector v = new ASN1EncodableVector(2);
+
+            v.add(new DEROctetString(nonce));
+
+            if (icvLen != 12)
+            {
+                v.add(new ASN1Integer(icvLen));
+            }
+
+            return new DERSequence(v);
         }
     }
 }
