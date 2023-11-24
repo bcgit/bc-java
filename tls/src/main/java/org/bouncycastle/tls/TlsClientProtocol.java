394112f4ce1186369ecbde2ca14afdf73bae06cf
Peter Dettman <peter.dettman@bouncycastle.org>
2023-11-24 12:29:56 +0700

    Fix RFC reference

--- tls/src/main/java/org/bouncycastle/tls/TlsClientProtocol.java
+++ tls/src/main/java/org/bouncycastle/tls/TlsClientProtocol.java
@@ -1853,7 +1853,7 @@ public class TlsClientProtocol
             TlsExtensionsUtils.addSupportedVersionsExtensionClient(clientExtensions, supportedVersions);
 
             /*
-             * RFC 8446 4.2.1. In compatibility mode [..], this field MUST be non-empty, so a client
+             * RFC 8446 4.1.2. In compatibility mode [..], this field MUST be non-empty, so a client
              * not offering a pre-TLS 1.3 session MUST generate a new 32-byte value.
              */
             if (legacy_session_id.length < 1 && tlsClient.shouldUseCompatibilityMode())
