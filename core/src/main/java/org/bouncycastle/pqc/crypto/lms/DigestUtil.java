06de4c08ed5f559ba094f45e56d1a30dfc69d602
Peter Dettman <peter.dettman@bouncycastle.org>
2023-12-13 01:25:51 +0700

    LMS updates

--- core/src/main/java/org/bouncycastle/pqc/crypto/lms/DigestUtil.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/lms/DigestUtil.java
@@ -1,13 +1,9 @@
 package org.bouncycastle.pqc.crypto.lms;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA256Digest;
-import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.bouncycastle.crypto.digests.SHAKEDigest;
 
 /**
@@ -15,44 +11,27 @@ import org.bouncycastle.crypto.digests.SHAKEDigest;
  */
 class DigestUtil
 {
-    private static Map<String, ASN1ObjectIdentifier> nameToOid = new HashMap<String, ASN1ObjectIdentifier>();
-    private static Map<ASN1ObjectIdentifier, String> oidToName = new HashMap<ASN1ObjectIdentifier, String>();
-
-    static
-    {
-        nameToOid.put("SHA-256", NISTObjectIdentifiers.id_sha256);
-        nameToOid.put("SHA-512", NISTObjectIdentifiers.id_sha512);
-        nameToOid.put("SHAKE128", NISTObjectIdentifiers.id_shake128);
-        nameToOid.put("SHAKE256", NISTObjectIdentifiers.id_shake256);
-
-        oidToName.put(NISTObjectIdentifiers.id_sha256, "SHA-256");
-        oidToName.put(NISTObjectIdentifiers.id_sha512, "SHA-512");
-        oidToName.put(NISTObjectIdentifiers.id_shake128, "SHAKE128");
-        oidToName.put(NISTObjectIdentifiers.id_shake256, "SHAKE256");
-    }
-
     static Digest getDigest(LMOtsParameters otsParameters)
     {
         return createDigest(otsParameters.getDigestOID(), otsParameters.getN());
     }
 
-    static Digest getDigest(LMSigParameters lmSigParameters)
+    static Digest getDigest(LMSigParameters sigParameters)
     {
-        return createDigest(lmSigParameters.getDigestOID(), lmSigParameters.getM());
+        return createDigest(sigParameters.getDigestOID(), sigParameters.getM());
     }
 
-    private static Digest createDigest(ASN1ObjectIdentifier digOid, int digLen)
+    private static Digest createDigest(ASN1ObjectIdentifier oid, int length)
     {
-        Digest dig = createDigest(digOid);
-        if (digOid.equals(NISTObjectIdentifiers.id_shake256_len))
+        Digest digest = createDigest(oid);
+
+        if (NISTObjectIdentifiers.id_shake256_len.equals(oid) ||
+            digest.getDigestSize() != length)
         {
-            return new WrapperDigest(dig, digLen);
+            return new WrapperDigest(digest, length);
         }
-        if (digLen == 24)
-        {
-            return new WrapperDigest(dig, digLen);
-        }
-        return dig;
+
+        return digest;
     }
 
     private static Digest createDigest(ASN1ObjectIdentifier oid)
@@ -60,18 +39,6 @@ class DigestUtil
         if (oid.equals(NISTObjectIdentifiers.id_sha256))
         {
             return new SHA256Digest();
-        }
-        if (oid.equals(NISTObjectIdentifiers.id_sha512))
-        {
-            return new SHA512Digest();
-        }
-        if (oid.equals(NISTObjectIdentifiers.id_shake128))
-        {
-            return new SHAKEDigest(128);
-        }
-        if (oid.equals(NISTObjectIdentifiers.id_shake256))
-        {
-            return new SHAKEDigest(256);
         }
         if (oid.equals(NISTObjectIdentifiers.id_shake256_len))
         {
@@ -84,55 +51,47 @@ class DigestUtil
     static class WrapperDigest
         implements Digest
     {
-
-        private final Digest dig;
+        private final Digest digest;
         private final int length;
 
-        WrapperDigest(Digest dig, int length)
+        WrapperDigest(Digest digest, int length)
         {
-            this.dig = dig;
+            this.digest = digest;
             this.length = length;
         }
 
-        @Override
         public String getAlgorithmName()
         {
-            return dig.getAlgorithmName() + "/" + length * 8;
+            return digest.getAlgorithmName() + "/" + length * 8;
         }
 
-        @Override
         public int getDigestSize()
         {
             return length;
         }
 
-        @Override
         public void update(byte in)
         {
-             dig.update(in);
+             digest.update(in);
         }
 
-        @Override
         public void update(byte[] in, int inOff, int len)
         {
-            dig.update(in, inOff, len);
+            digest.update(in, inOff, len);
         }
 
-        @Override
         public int doFinal(byte[] out, int outOff)
         {
-            byte[] digOut = new byte[dig.getDigestSize()];
-
-            dig.doFinal(digOut, 0);
+            byte[] digOut = new byte[digest.getDigestSize()];
+            digest.doFinal(digOut, 0);
 
             System.arraycopy(digOut, 0, out, outOff, length);
             return length;
         }
 
-        @Override
         public void reset()
         {
-            dig.reset();
+            digest.reset();
         }
     }
 }
