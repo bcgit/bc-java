a5049d4dde7f6bc0cf66a7f3e4d5417f0e7dca5c
David Hook <dgh@cryptoworkshop.com>
2023-12-22 14:34:37 +1100

    moved NTRUPrime to always produce 256 bit secrets - relates to github #1554

--- core/src/main/java/org/bouncycastle/pqc/crypto/ntruprime/SNTRUPrimeParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/ntruprime/SNTRUPrimeParameters.java
@@ -9,15 +9,15 @@ public class SNTRUPrimeParameters
     implements KEMParameters
 {
     public static final SNTRUPrimeParameters sntrup653 = new SNTRUPrimeParameters("sntrup653", 653, 4621, 288,
-                                                                    994, 865, 994, 1518, 16);
+                                                                    994, 865, 994, 1518, 32);
     public static final SNTRUPrimeParameters sntrup761 = new SNTRUPrimeParameters("sntrup761", 761, 4591, 286,
-                                                                    1158, 1007, 1158, 1763, 16);
+                                                                    1158, 1007, 1158, 1763, 32);
     public static final SNTRUPrimeParameters sntrup857 = new SNTRUPrimeParameters("sntrup857", 857, 5167, 322,
-                                                                    1322, 1152, 1322, 1999, 16);
+                                                                    1322, 1152, 1322, 1999, 32);
     public static final SNTRUPrimeParameters sntrup953 = new SNTRUPrimeParameters("sntrup953", 953, 6343, 396,
-                                                                    1505, 1317, 1505, 2254, 24);
+                                                                    1505, 1317, 1505, 2254, 32);
     public static final SNTRUPrimeParameters sntrup1013 = new SNTRUPrimeParameters("sntrup1013", 1013, 7177, 448,
-                                                                    1623, 1423, 1623, 2417, 24);
+                                                                    1623, 1423, 1623, 2417, 32);
     public static final SNTRUPrimeParameters sntrup1277 = new SNTRUPrimeParameters("sntrup1277", 1277, 7879, 492,
                                                                     2067, 1815, 2067, 3059, 32);
 
