9ec49a7fef4ad221bc023f76450848b19bbf4619
royb <roy.basmacier@primekey.com>
2024-02-07 17:12:29 -0500

    Using kdf for KEM Encapsulator/Decapsulator

--- prov/src/main/jdk21/org/bouncycastle/pqc/jcajce/provider/ntruprime/SNTRUPrimeEncapsulatorSpi.java
+++ prov/src/main/jdk21/org/bouncycastle/pqc/jcajce/provider/ntruprime/SNTRUPrimeEncapsulatorSpi.java
@@ -1,16 +1,25 @@
 package org.bouncycastle.pqc.jcajce.provider.ntruprime;
 
+import org.bouncycastle.asn1.bc.BCObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.crypto.SecretWithEncapsulation;
+import org.bouncycastle.crypto.Wrapper;
+import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.jcajce.spec.KTSParameterSpec;
 import org.bouncycastle.pqc.crypto.ntruprime.SNTRUPrimeKEMGenerator;
+import org.bouncycastle.pqc.jcajce.provider.util.WrapUtil;
+import org.bouncycastle.util.Arrays;
 
 import javax.crypto.KEM;
 import javax.crypto.KEMSpi;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.SecureRandom;
 import java.util.Objects;
+
+import static org.bouncycastle.pqc.jcajce.provider.Util.makeKeyBytes;
 
 class SNTRUPrimeEncapsulatorSpi
     implements KEMSpi.EncapsulatorSpi
@@ -36,11 +45,30 @@ class SNTRUPrimeEncapsulatorSpi
         Objects.checkFromToIndex(from, to, engineEncapsulationSize());
         Objects.requireNonNull(algorithm, "null algorithm");
 
+        KTSParameterSpec.Builder builder = new KTSParameterSpec.Builder(parameterSpec.getKeyAlgorithmName(), parameterSpec.getKeySize());
+
+        if (!algorithm.equals("Generic"))
+        {
+            //TODO:
+//            builder.withKdfAlgorithm(AlgorithmIdentifier.getInstance(algorithm));
+        }
+        KTSParameterSpec spec = builder.build();
+
         SecretWithEncapsulation secEnc = kemGen.generateEncapsulated(publicKey.getKeyParams());
 
-        // TODO: parameters...
+        byte[] encapsulation = secEnc.getEncapsulation();
         byte[] secret = secEnc.getSecret();
-        return new KEM.Encapsulated(new SecretKeySpec(secret, 0, secret.length, algorithm), secEnc.getEncapsulation(), null);
+
+        byte[] kdfKey = Arrays.copyOfRange(secret, from, to);
+
+        try
+        {
+            return new KEM.Encapsulated(new SecretKeySpec(makeKeyBytes(spec, kdfKey), algorithm), encapsulation , null);
+        }
+        catch (InvalidKeyException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
