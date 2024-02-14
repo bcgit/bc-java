2fe3820f12b5c937e3964948616ee6f8cbf1eddf
gefeili <gli@keyfactor.com>
2024-02-14 15:49:57 +1030

    Add tests for PGPUserAttributeSubpacketVector, PGPCompressedData, PGPMarker, PGPOnePassSignature, PGPPadding, PGPSignature and PGPLiteralDataGenerator.

--- pg/src/test/java/org/bouncycastle/openpgp/test/OpenpgpTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/OpenpgpTest.java
@@ -1,17 +1,36 @@
 package org.bouncycastle.openpgp.test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.security.Security;
+import java.util.Date;
 
+import org.bouncycastle.bcpg.BCPGInputStream;
+import org.bouncycastle.bcpg.BCPGOutputStream;
 import org.bouncycastle.bcpg.CompressionAlgorithmTags;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
 import org.bouncycastle.bcpg.PublicKeyAlgorithmTags;
 import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
 import org.bouncycastle.crypto.CryptoServicesRegistrar;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openpgp.PGPCanonicalizedDataGenerator;
+import org.bouncycastle.openpgp.PGPCompressedData;
 import org.bouncycastle.openpgp.PGPCompressedDataGenerator;
+import org.bouncycastle.openpgp.PGPEncryptedDataList;
 import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPLiteralData;
+import org.bouncycastle.openpgp.PGPLiteralDataGenerator;
+import org.bouncycastle.openpgp.PGPMarker;
+import org.bouncycastle.openpgp.PGPOnePassSignature;
+import org.bouncycastle.openpgp.PGPPadding;
+import org.bouncycastle.openpgp.PGPPublicKeyRing;
+import org.bouncycastle.openpgp.PGPSignature;
 import org.bouncycastle.openpgp.PGPUtil;
+import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+import org.bouncycastle.util.Strings;
 import org.bouncycastle.util.test.SimpleTest;
+import org.bouncycastle.util.test.UncloseableOutputStream;
 
 public class OpenpgpTest
     extends SimpleTest
@@ -33,6 +52,8 @@ public class OpenpgpTest
     public void performTest()
         throws Exception
     {
+        testPGPLiteralDataGenerator();
+        testContruction();
         testPGPUtil();
         testPGPCompressedDataGenerator();
     }
@@ -79,6 +100,63 @@ public class OpenpgpTest
 
         isTrue(PGPUtil.makeRandomKey(SymmetricKeyAlgorithmTags.DES, CryptoServicesRegistrar.getSecureRandom()).length == 8);
         testException("unknown symmetric algorithm: ", "PGPException", ()->PGPUtil.makeRandomKey(SymmetricKeyAlgorithmTags.NULL, CryptoServicesRegistrar.getSecureRandom()));
+
+    }
+
+    public void testContruction()
+        throws Exception
+    {
+        String data = "Now is the time for all good men\nTo come to the aid of the party\n";
+        PGPCanonicalizedDataGenerator canGen = new PGPCanonicalizedDataGenerator();
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+        OutputStream out = canGen.open(bOut, PGPLiteralData.TEXT, PGPLiteralData.CONSOLE, new Date());
+
+        out.write(Strings.toByteArray(data));
+
+        out.close();
+        byte[] input = bOut.toByteArray();
+        //PGPLiteralData lData = new PGPLiteralData(new ByteArrayInputStream(bOut.toByteArray()));
+
+        //PGPLiteralData
+        testException("unexpected packet in stream: ", "IOException", ()-> new PGPCompressedData(new BCPGInputStream(new ByteArrayInputStream(input))));
+        //testException("unexpected packet in stream: ", "IOException", ()-> new PGPEncryptedDataList(new BCPGInputStream(new ByteArrayInputStream(input))));
+        testException("unexpected packet in stream: ", "IOException", ()-> new PGPMarker(new BCPGInputStream(new ByteArrayInputStream(input))));
+        testException("unexpected packet in stream: ", "IOException", ()-> new PGPOnePassSignature(new BCPGInputStream(new ByteArrayInputStream(input))));
+        testException("unexpected packet in stream: ", "IOException", ()-> new PGPPadding(new BCPGInputStream(new ByteArrayInputStream(input))));
+        //testException("unexpected packet in stream: ", "IOException", ()-> new PGPPublicKeyRing(new BCPGInputStream(new ByteArrayInputStream(input)), new BcKeyFingerprintCalculator()));
+        testException("unexpected packet in stream: ", "IOException", ()-> new PGPSignature(new BCPGInputStream(new ByteArrayInputStream(input))));
+    }
+
+    public void testPGPLiteralDataGenerator()
+        throws Exception
+    {
+        PGPLiteralDataGenerator lGen = new PGPLiteralDataGenerator();
+        String data = "Now is the time for all good men\nTo come to the aid of the party\n";
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+        PGPCompressedDataGenerator cGen = new PGPCompressedDataGenerator(
+            PGPCompressedData.ZIP);
+        BCPGOutputStream bcOut = new BCPGOutputStream(
+            cGen.open(new UncloseableOutputStream(bOut)));
+        Date testDate = new Date((System.currentTimeMillis() / 1000) * 1000);
+        lGen.open(
+            new UncloseableOutputStream(bcOut),
+            PGPLiteralData.BINARY,
+            "_CONSOLE",
+            data.getBytes().length,
+            testDate);
+        testException("generator already in open state", "IllegalStateException", ()->lGen.open(
+            new UncloseableOutputStream(bcOut),
+            PGPLiteralData.BINARY,
+            "_CONSOLE",
+            data.getBytes().length,
+            testDate));
+        testException("generator already in open state", "IllegalStateException", ()->lGen.open(
+            new UncloseableOutputStream(bcOut),
+            PGPLiteralData.BINARY,
+            "_CONSOLE",
+            testDate,
+            new byte[10]));
 
     }
 }
