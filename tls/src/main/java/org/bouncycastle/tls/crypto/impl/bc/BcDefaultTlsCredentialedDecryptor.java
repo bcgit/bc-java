d37128e62b282f61df663430e5a97a55de97302d
Peter Dettman <peter.dettman@bouncycastle.org>
2024-02-26 19:19:36 +0700

    Factor out TlsRsaKeyExchange class into core

--- tls/src/main/java/org/bouncycastle/tls/crypto/impl/bc/BcDefaultTlsCredentialedDecryptor.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/impl/bc/BcDefaultTlsCredentialedDecryptor.java
@@ -1,19 +1,15 @@
 package org.bouncycastle.tls.crypto.impl.bc;
 
 import java.io.IOException;
-import java.security.SecureRandom;
 
-import org.bouncycastle.crypto.encodings.PKCS1Encoding;
-import org.bouncycastle.crypto.engines.RSABlindedEngine;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithRandom;
 import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.tls.TlsRsaKeyExchange;
 import org.bouncycastle.tls.Certificate;
 import org.bouncycastle.tls.ProtocolVersion;
 import org.bouncycastle.tls.TlsCredentialedDecryptor;
 import org.bouncycastle.tls.crypto.TlsCryptoParameters;
 import org.bouncycastle.tls.crypto.TlsSecret;
-import org.bouncycastle.util.Arrays;
 
 /**
  * Credentialed class decrypting RSA encrypted secrets sent from a peer for our end of the TLS connection using the BC light-weight API.
@@ -26,7 +22,7 @@ public class BcDefaultTlsCredentialedDecryptor
     protected AsymmetricKeyParameter privateKey;
 
     public BcDefaultTlsCredentialedDecryptor(BcTlsCrypto crypto, Certificate certificate,
-                                           AsymmetricKeyParameter privateKey)
+            AsymmetricKeyParameter privateKey)
     {
         if (crypto == null)
         {
@@ -81,56 +77,10 @@ public class BcDefaultTlsCredentialedDecryptor
     protected TlsSecret safeDecryptPreMasterSecret(TlsCryptoParameters cryptoParams,
         RSAKeyParameters rsaServerPrivateKey, byte[] encryptedPreMasterSecret)
     {
-        SecureRandom secureRandom = crypto.getSecureRandom();
-
-        /*
-         * RFC 5246 7.4.7.1.
-         */
         ProtocolVersion expectedVersion = cryptoParams.getRSAPreMasterSecretVersion();
 
-        /*
-         * Generate 48 random bytes we can use as a Pre-Master-Secret, if the PKCS1 padding check should fail.
-         */
-        byte[] fallback = new byte[48];
-        secureRandom.nextBytes(fallback);
-
-        byte[] M = Arrays.clone(fallback);
-        try
-        {
-            PKCS1Encoding encoding = new PKCS1Encoding(new RSABlindedEngine(), fallback);
-            encoding.init(false, new ParametersWithRandom(rsaServerPrivateKey, secureRandom));
-
-            M = encoding.processBlock(encryptedPreMasterSecret, 0, encryptedPreMasterSecret.length);
-        }
-        catch (Exception e)
-        {
-            /*
-             * This should never happen since the decryption should never throw an exception and return a
-             * random value instead.
-             *
-             * In any case, a TLS server MUST NOT generate an alert if processing an RSA-encrypted premaster
-             * secret message fails, or the version number is not as expected. Instead, it MUST continue the
-             * handshake with a randomly generated premaster secret.
-             */
-        }
-
-        /*
-         * Compare the version number in the decrypted Pre-Master-Secret with the legacy_version field from
-         * the ClientHello. If they don't match, continue the handshake with the randomly generated 'fallback'
-         * value.
-         *
-         * NOTE: The comparison and replacement must be constant-time.
-         */
-        int mask = (expectedVersion.getMajorVersion() ^ (M[0] & 0xFF))
-                 | (expectedVersion.getMinorVersion() ^ (M[1] & 0xFF));
-
-        // 'mask' will be all 1s if the versions matched, or else all 0s.
-        mask = (mask - 1) >> 31;
-
-        for (int i = 0; i < 48; i++)
-        {
-            M[i] = (byte)((M[i] & mask) | (fallback[i] & ~mask));
-        }
+        byte[] M = TlsRsaKeyExchange.decryptPreMasterSecret(encryptedPreMasterSecret, rsaServerPrivateKey,
+            expectedVersion.getFullVersion(), crypto.getSecureRandom());
 
         return crypto.createSecret(M);
     }
