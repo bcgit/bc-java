148255e7db302cfa2a20b24ea1cc611b094676d2
David Hook <dgh@cryptoworkshop.com>
2024-03-18 11:57:57 +1100

    added additional NTRU OIDs to provider.

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/NTRU.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/NTRU.java
@@ -27,7 +27,9 @@ public class NTRU
             provider.addAlgorithm("Alg.Alias.KeyGenerator." + BCObjectIdentifiers.ntruhps2048509, "NTRU");
             provider.addAlgorithm("Alg.Alias.KeyGenerator." + BCObjectIdentifiers.ntruhps2048677, "NTRU");
             provider.addAlgorithm("Alg.Alias.KeyGenerator." + BCObjectIdentifiers.ntruhps4096821, "NTRU");
+            provider.addAlgorithm("Alg.Alias.KeyGenerator." + BCObjectIdentifiers.ntruhps40961229, "NTRU");
             provider.addAlgorithm("Alg.Alias.KeyGenerator." + BCObjectIdentifiers.ntruhrss701, "NTRU");
+            provider.addAlgorithm("Alg.Alias.KeyGenerator." + BCObjectIdentifiers.ntruhrss1373, "NTRU");
 
             AsymmetricKeyInfoConverter keyFact = new NTRUKeyFactorySpi();
 
@@ -36,13 +38,17 @@ public class NTRU
             provider.addAlgorithm("Alg.Alias.Cipher." + BCObjectIdentifiers.ntruhps2048509, "NTRU");
             provider.addAlgorithm("Alg.Alias.Cipher." + BCObjectIdentifiers.ntruhps2048677, "NTRU");
             provider.addAlgorithm("Alg.Alias.Cipher." + BCObjectIdentifiers.ntruhps4096821, "NTRU");
+            provider.addAlgorithm("Alg.Alias.Cipher." + BCObjectIdentifiers.ntruhps40961229, "NTRU");
             provider.addAlgorithm("Alg.Alias.Cipher." + BCObjectIdentifiers.ntruhrss701, "NTRU");
+            provider.addAlgorithm("Alg.Alias.Cipher." + BCObjectIdentifiers.ntruhrss1373, "NTRU");
 
             registerOid(provider, BCObjectIdentifiers.pqc_kem_ntru, "NTRU", keyFact);
             registerOid(provider, BCObjectIdentifiers.ntruhps2048509, "NTRU", keyFact);
             registerOid(provider, BCObjectIdentifiers.ntruhps2048677, "NTRU", keyFact);
             registerOid(provider, BCObjectIdentifiers.ntruhps4096821, "NTRU", keyFact);
+            registerOid(provider, BCObjectIdentifiers.ntruhps40961229, "NTRU", keyFact);
             registerOid(provider, BCObjectIdentifiers.ntruhrss701, "NTRU", keyFact);
+            registerOid(provider, BCObjectIdentifiers.ntruhrss1373, "NTRU", keyFact);
         }
     }
 }
