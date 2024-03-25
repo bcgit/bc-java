93f8b3416b3285bb98dd845647a0c798fb02805a
mwcw <megan@cryptoworkshop.com>
2024-03-26 09:02:59 +1100

    Added running ant build to CI.

---
+++ ci/build_1_8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+#
+# This script is for running inside the docker container
+#
+
+cd /workspace/bc-java
+source ci/common.sh
+
+export JAVA_HOME=`openjdk_8`
+export export PATH=$JAVA_HOME/bin:$PATH
+export JDKPATH=$JAVA_HOME
+
+sh build1-8+
