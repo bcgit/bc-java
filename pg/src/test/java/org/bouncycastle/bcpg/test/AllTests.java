f70cfbb4916ac038832d31861a789b8c7e1a415b
Paul Schaub <vanitasvitae@fsfe.org>
2024-04-24 11:58:45 +0200

    Add tests for v6 OPS and SignaturePackets

---
+++ pg/src/test/java/org/bouncycastle/bcpg/test/AllTests.java
@@ -0,0 +1,70 @@
+package org.bouncycastle.bcpg.test;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.test.PrintTestResult;
+import org.bouncycastle.util.test.SimpleTestResult;
+
+import java.security.Security;
+
+public class AllTests
+        extends TestCase {
+
+    public void testPacketParsing()
+    {
+        Security.addProvider(new BouncyCastleProvider());
+
+        org.bouncycastle.util.test.Test[] tests = new org.bouncycastle.util.test.Test[] {
+                new SignaturePacketTest(),
+                new OnePassSignaturePacketTest(),
+                new OpenPgpMessageTest()
+        };
+
+        for (int i = 0; i != tests.length; i++)
+        {
+            SimpleTestResult result = (SimpleTestResult)tests[i].perform();
+
+            if (!result.isSuccessful())
+            {
+                fail(result.toString());
+            }
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        PrintTestResult.printResult(junit.textui.TestRunner.run(suite()));
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("OpenPGP Packet Tests");
+
+        suite.addTestSuite(AllTests.class);
+
+        return new BCPacketTests(suite);
+    }
+
+    static class BCPacketTests
+            extends TestSetup
+    {
+        public BCPacketTests(Test test)
+        {
+            super(test);
+        }
+
+        protected void setUp()
+        {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+
+        protected void tearDown()
+        {
+            Security.removeProvider("BC");
+        }
+    }
+}
