b8f84aa0539a583ef505fdfd8639137b9051e639
Paul Schaub <vanitasvitae@fsfe.org>
2024-05-08 11:07:31 +0200

    Add documentation to ECDH,ECDSA,EdDSA BCPGKeys

--- pg/src/main/java/org/bouncycastle/bcpg/Ed25519SecretBCPGKey.java
+++ pg/src/main/java/org/bouncycastle/bcpg/Ed25519SecretBCPGKey.java
@@ -2,9 +2,20 @@ package org.bouncycastle.bcpg;
 
 import java.io.IOException;
 
+/**
+ * Secret key of type {@link PublicKeyAlgorithmTags#Ed25519}.
+ * This type was introduced with Crypto-Refresh and can be used with v4, v6 keys.
+ * Note however, that legacy implementations might not understand this key type yet.
+ * For a key type compatible with legacy v4 implementations, see {@link EdDSAPublicBCPGKey} with
+ * {@link PublicKeyAlgorithmTags#EDDSA_LEGACY}.
+ *
+ * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-algorithm-specific-part-for-ed2">
+ *     Crypto-Refresh - Algorithm-Specific Part for Ed25519 Keys</a>
+ */
 public class Ed25519SecretBCPGKey
     extends OctetArrayBCPGKey
 {
+    // 32 octets of the native secret key
     public static final int LENGTH = 32;
 
     public Ed25519SecretBCPGKey(BCPGInputStream in)
--- pg/src/main/java/org/bouncycastle/bcpg/X448SecretBCPGKey.java
+++ pg/src/main/java/org/bouncycastle/bcpg/X448SecretBCPGKey.java
@@ -2,9 +2,20 @@ package org.bouncycastle.bcpg;
 
 import java.io.IOException;
 
+/**
+ * Secret key of type {@link PublicKeyAlgorithmTags#X448}.
+ * This type was introduced with Crypto-Refresh and can be used with v4, v6 keys.
+ * Note however, that legacy implementations might not understand this key type yet.
+ * For a key type compatible with legacy v4 implementations, see {@link ECDHPublicBCPGKey} with
+ * {@link PublicKeyAlgorithmTags#ECDH}.
+ *
+ * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-algorithm-specific-part-for-x4">
+ *     Crypto-Refresh - Algorithm-Specific Part for X448 Keys</a>
+ */
 public class X448SecretBCPGKey
     extends OctetArrayBCPGKey
 {
+    // 56 octets of the native secret key
     public static final int LENGTH = 56;
 
     public X448SecretBCPGKey(BCPGInputStream in)
