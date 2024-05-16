ac06988598635aa97f489ee28c80ed3b0822fad6
Paul Schaub <vanitasvitae@fsfe.org>
2024-05-16 10:53:02 +0200

    Use Pack class for writing and reading key-IDs from byte arrays

--- pg/src/main/java/org/bouncycastle/bcpg/sig/IssuerKeyID.java
+++ pg/src/main/java/org/bouncycastle/bcpg/sig/IssuerKeyID.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.bcpg.sig;
 
+import org.bouncycastle.bcpg.FingerprintUtil;
 import org.bouncycastle.bcpg.SignatureSubpacket;
 import org.bouncycastle.bcpg.SignatureSubpacketTags;
 
@@ -13,16 +14,7 @@ public class IssuerKeyID
         long    keyId)
     {
         byte[]    data = new byte[8];
-        
-        data[0] = (byte)(keyId >> 56);
-        data[1] = (byte)(keyId >> 48);
-        data[2] = (byte)(keyId >> 40);
-        data[3] = (byte)(keyId >> 32);
-        data[4] = (byte)(keyId >> 24);
-        data[5] = (byte)(keyId >> 16);
-        data[6] = (byte)(keyId >> 8);
-        data[7] = (byte)keyId;
-        
+        FingerprintUtil.writeKeyID(keyId, data);
         return data;
     }
     
@@ -43,9 +35,6 @@ public class IssuerKeyID
     
     public long getKeyID()
     {
-        long    keyID = ((long)(data[0] & 0xff) << 56) | ((long)(data[1] & 0xff) << 48) | ((long)(data[2] & 0xff) << 40) | ((long)(data[3] & 0xff) << 32)
-                                | ((long)(data[4] & 0xff) << 24) | ((data[5] & 0xff) << 16) | ((data[6] & 0xff) << 8) | (data[7] & 0xff);
-        
-        return keyID;
+        return FingerprintUtil.readKeyID(data);
     }
 }
