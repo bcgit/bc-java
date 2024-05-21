f23fc367e6aded2d2aaea37583695ab7c8ddd878
Paul Schaub <vanitasvitae@fsfe.org>
2024-05-21 15:02:47 +0200

    Document reversed MPI encoding / little-endian native encoding

--- pg/src/main/java/org/bouncycastle/bcpg/ECSecretBCPGKey.java
+++ pg/src/main/java/org/bouncycastle/bcpg/ECSecretBCPGKey.java
@@ -8,6 +8,9 @@ import java.math.BigInteger;
  * This type is for use with {@link PublicKeyAlgorithmTags#ECDH} or {@link PublicKeyAlgorithmTags#ECDSA}.
  * The specific curve is identified by providing an OID.
  * Regarding X25519, X448, consider the following:
+ * ECDH keys using curve448 are unspecified.
+ * ECDH secret keys using curve25519 use big-endian MPI encoding, contrary to {@link X25519SecretBCPGKey} which uses
+ * native encoding.
  * Modern implementations use dedicated key types {@link X25519SecretBCPGKey}, {@link X448SecretBCPGKey} along with
  * dedicated algorithm tags {@link PublicKeyAlgorithmTags#X25519}, {@link PublicKeyAlgorithmTags#X448}.
  * If you want to be compatible with legacy applications however, you should use this class instead.
@@ -17,6 +20,8 @@ import java.math.BigInteger;
  *     Crypto-Refresh - Algorithm-Specific Parts for ECDH Keys</a>
  * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-algorithm-specific-part-for-ec">
  *     Crypto-Refresh - Algorithm-Specific Parts for ECDSA Keys</a>
+ * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-curve25519legacy-ecdh-secre">
+ *     Crypto-Refresh - Curve25519Legacy ECDH Secret Key Material (deprecated)</a>
  */
 public class ECSecretBCPGKey
     extends BCPGObject
