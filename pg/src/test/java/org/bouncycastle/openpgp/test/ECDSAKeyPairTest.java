897caef2bcfdbd369fcd0692b923dc95df328260
Paul Schaub <vanitasvitae@fsfe.org>
2024-05-23 10:39:49 +0200

    Do not register BouncyCastleProvider in ECDSAKeyPairTest

--- pg/src/test/java/org/bouncycastle/openpgp/test/ECDSAKeyPairTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/ECDSAKeyPairTest.java
@@ -93,7 +93,6 @@ public class ECDSAKeyPairTest
     public void performTest()
             throws Exception
     {
-        Security.addProvider(new BouncyCastleProvider());
         testConversionOfFreshJcaKeyPair();
         testConversionOfParsedJcaKeyPair();
         testConversionOfParsedBcKeyPair();
