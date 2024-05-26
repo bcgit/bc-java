5b53ecc3da544ef8ada76a71b6423362ed8bae06
wegoo <zhenwei1108@163.com>
2024-05-26 09:05:29 +0800

    update max range while generator sm2 keypair. error when signature data the private key = 'n-1'

--- core/src/main/java/org/bouncycastle/crypto/generators/SM2KeyPairGenerator.java
+++ core/src/main/java/org/bouncycastle/crypto/generators/SM2KeyPairGenerator.java
@@ -1,24 +1,6 @@
 package org.bouncycastle.crypto.generators;
 
 import java.math.BigInteger;
-import java.security.SecureRandom;
-
-import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
-import org.bouncycastle.crypto.AsymmetricCipherKeyPairGenerator;
-import org.bouncycastle.crypto.CryptoServicePurpose;
-import org.bouncycastle.crypto.CryptoServicesRegistrar;
-import org.bouncycastle.crypto.KeyGenerationParameters;
-import org.bouncycastle.crypto.constraints.ConstraintUtils;
-import org.bouncycastle.crypto.constraints.DefaultServiceProperties;
-import org.bouncycastle.crypto.params.ECDomainParameters;
-import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
-import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
-import org.bouncycastle.crypto.params.ECPublicKeyParameters;
-import org.bouncycastle.math.ec.ECConstants;
-import org.bouncycastle.math.ec.ECMultiplier;
-import org.bouncycastle.math.ec.ECPoint;
-import org.bouncycastle.math.ec.FixedPointCombMultiplier;
-import org.bouncycastle.math.ec.WNafUtil;
 import org.bouncycastle.util.BigIntegers;
 
 public class SM2KeyPairGenerator
@@ -31,6 +13,6 @@ public class SM2KeyPairGenerator
 
     protected boolean isOutOfRangeD(BigInteger d, BigInteger n)
     {
-        return d.compareTo(ONE) < 0 || (d.compareTo(n.subtract(BigIntegers.ONE)) >= 0);
+        return d.compareTo(ONE) < 0 || (d.compareTo(n.subtract(BigIntegers.TWO)) >= 0);
     }
 }
