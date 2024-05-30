d9c1644e43ad0c3cf7663553bd462f8c1d5b0c33
Paul Schaub <vanitasvitae@fsfe.org>
2024-05-30 13:29:32 +0200

    Add debug info for prefix mismatch to BCEdDSAPublicKey constructor

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/edec/BCEdDSAPublicKey.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/edec/BCEdDSAPublicKey.java
@@ -14,6 +14,7 @@ import org.bouncycastle.internal.asn1.edec.EdECObjectIdentifiers;
 import org.bouncycastle.jcajce.interfaces.EdDSAPublicKey;
 import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.Properties;
+import org.bouncycastle.util.encoders.Hex;
 
 public class BCEdDSAPublicKey
     implements EdDSAPublicKey
@@ -54,7 +55,9 @@ public class BCEdDSAPublicKey
         }
         else
         {
-            throw new InvalidKeySpecException("raw key data not recognised");
+            throw new InvalidKeySpecException("raw key data not recognised (prefix mismatch:\n" +
+                    "Expected: " + Hex.toHexString(prefix) + "\n" +
+                    "Got:      " + Hex.toHexString(java.util.Arrays.copyOf(rawData, prefixLength)) + ")");
         }
     }
 
