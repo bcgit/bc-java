f7808a37d7f7274ab6987abbf56eaf34c388b2bb
Paul Schaub <vanitasvitae@fsfe.org>
2024-06-11 16:46:10 +0200

    Add test for creating AEAD encrypted keys

--- pg/src/test/java/org/bouncycastle/openpgp/test/AEADProtectedPGPSecretKeyTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/AEADProtectedPGPSecretKeyTest.java
@@ -1,18 +1,36 @@
 package org.bouncycastle.openpgp.test;
 
+import org.bouncycastle.bcpg.AEADAlgorithmTags;
 import org.bouncycastle.bcpg.ArmoredInputStream;
 import org.bouncycastle.bcpg.BCPGInputStream;
+import org.bouncycastle.bcpg.HashAlgorithmTags;
+import org.bouncycastle.bcpg.PublicKeyAlgorithmTags;
+import org.bouncycastle.bcpg.S2K;
+import org.bouncycastle.bcpg.SecretKeyPacket;
+import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator;
+import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPKeyPair;
+import org.bouncycastle.openpgp.PGPObjectFactory;
+import org.bouncycastle.openpgp.PGPPrivateKey;
+import org.bouncycastle.openpgp.PGPSecretKey;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.bouncycastle.openpgp.bc.BcPGPObjectFactory;
+import org.bouncycastle.openpgp.operator.bc.BcAEADSecretKeyEncryptorBuilder;
 import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyDecryptorBuilder;
 import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
+import org.bouncycastle.openpgp.operator.bc.BcPGPKeyPair;
 import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
 import org.bouncycastle.util.encoders.Hex;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Iterator;
 
 public class AEADProtectedPGPSecretKeyTest
@@ -30,6 +48,7 @@ public class AEADProtectedPGPSecretKeyTest
             throws Exception
     {
         unlockTestVector();
+        lockGeneratedV4Key();
     }
 
     private void unlockTestVector()
@@ -78,6 +97,37 @@ public class AEADProtectedPGPSecretKeyTest
         JcePBESecretKeyDecryptorBuilder jceDecryptor = new JcePBESecretKeyDecryptorBuilder().setProvider(new BouncyCastleProvider());
         PGPPrivateKey privSubKey = subkey.extractPrivateKey(jceDecryptor.build(passphrase));
         isEncodingEqual(plainSubkey, privSubKey.getPrivateKeyDataPacket().getEncoded());
+    }
+
+    private void lockGeneratedV4Key() throws PGPException {
+        Ed25519KeyPairGenerator gen = new Ed25519KeyPairGenerator();
+        gen.init(new Ed25519KeyGenerationParameters(new SecureRandom()));
+        AsymmetricCipherKeyPair kp = gen.generateKeyPair();
+        Date creationTime = currentTimeRounded();
+        PGPKeyPair keyPair = new BcPGPKeyPair(PublicKeyAlgorithmTags.Ed25519, kp, creationTime);
+
+        BcAEADSecretKeyEncryptorBuilder encBuilder = new BcAEADSecretKeyEncryptorBuilder(
+                AEADAlgorithmTags.OCB, SymmetricKeyAlgorithmTags.AES_256,
+                S2K.Argon2Params.memoryConstrainedParameters()
+        );
+
+        PGPSecretKey sk = new PGPSecretKey(
+                keyPair.getPrivateKey(),
+                keyPair.getPublicKey(),
+                new BcPGPDigestCalculatorProvider().get(HashAlgorithmTags.SHA1),
+                true,
+                encBuilder.build("Hello".toCharArray(), keyPair.getPublicKey().getPublicKeyPacket()));
+
+        isEquals(SecretKeyPacket.USAGE_AEAD, sk.getS2KUsage());
+        isEquals(S2K.ARGON_2, sk.getS2K().getType());
+        isEquals(3, sk.getS2K().getPasses());
+        isEquals(4, sk.getS2K().getParallelism());
+        isEquals(16, sk.getS2K().getMemorySizeExponent());
+
+        BcPBESecretKeyDecryptorBuilder decBuilder = new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider());
+
+        PGPPrivateKey dec = sk.extractPrivateKey(decBuilder.build("Hello".toCharArray()));
+        isEncodingEqual(keyPair.getPrivateKey().getPrivateKeyDataPacket().getEncoded(), dec.getPrivateKeyDataPacket().getEncoded());
     }
 
     public static void main(String[] args)
