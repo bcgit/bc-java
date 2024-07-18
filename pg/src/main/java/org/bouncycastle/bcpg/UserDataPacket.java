ac581f9eb98ca61f71b72d5b134a8f09fcf5c69a
Paul Schaub <vanitasvitae@fsfe.org>
2024-07-18 16:15:25 +0200

    Document the purpose of the UserDataPacket interface

--- pg/src/main/java/org/bouncycastle/bcpg/UserDataPacket.java
+++ pg/src/main/java/org/bouncycastle/bcpg/UserDataPacket.java
@@ -1,5 +1,9 @@
 package org.bouncycastle.bcpg;
 
+/**
+ * Superclass for user identities ({@link UserIDPacket}, {@link UserAttributePacket}).
+ * The superclass is used to hold different user identity objects in the same collection.
+ */
 public interface UserDataPacket
 {
 
