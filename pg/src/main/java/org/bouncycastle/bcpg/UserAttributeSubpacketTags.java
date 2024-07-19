f6e7e1f05adc7264e3f47aec1a47f1b7c74068db
Paul Schaub <vanitasvitae@fsfe.org>
2024-07-19 15:16:01 +0200

    Add documentation to UserAttributeSubpacketTags

--- pg/src/main/java/org/bouncycastle/bcpg/UserAttributeSubpacketTags.java
+++ pg/src/main/java/org/bouncycastle/bcpg/UserAttributeSubpacketTags.java
@@ -2,8 +2,16 @@ package org.bouncycastle.bcpg;
 
 /**
  * Basic PGP user attribute sub-packet tag types.
+ *
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc4880#section-5.12">
+ *     RFC4880 - User Attribute Packet</a>
+ * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-user-attribute-packet-type-">
+ *     C-R - User Attribute Packet</a>
  */
 public interface UserAttributeSubpacketTags 
 {
+    /**
+     * Tag for an {@link org.bouncycastle.bcpg.attr.ImageAttribute}.
+     */
     int IMAGE_ATTRIBUTE = 1;
 }
