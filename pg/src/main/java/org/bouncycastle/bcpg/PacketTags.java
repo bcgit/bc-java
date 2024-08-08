d6c52317ebbc150a3474db5301838ba59a050de6
Paul Schaub <vanitasvitae@fsfe.org>
2024-08-08 13:26:26 +0200

    Update rfc reference

--- pg/src/main/java/org/bouncycastle/bcpg/PacketTags.java
+++ pg/src/main/java/org/bouncycastle/bcpg/PacketTags.java
@@ -12,7 +12,7 @@ public interface PacketTags
        * Packet class: {@link PublicKeyEncSessionPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPPublicKeyEncryptedData}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-public-key-encrypted-sessio">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-public-key-encrypted-sessio">
        *     Public-Key Encrypted Session Key Packet</a>
        */
       int PUBLIC_KEY_ENC_SESSION = 1;    // Public-Key Encrypted Session Key Packet
@@ -22,7 +22,7 @@ public interface PacketTags
        * Packet class: {@link SignaturePacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPSignature}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-signature-packet-type-id-2">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-signature-packet-type-id-2">
        *     Signature Packet</a>
        */
       int SIGNATURE = 2;                 // Signature Packet
@@ -32,7 +32,7 @@ public interface PacketTags
        * Packet class: {@link SymmetricKeyEncSessionPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPSymmetricKeyEncryptedData}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-symmetric-key-encrypted-ses">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-symmetric-key-encrypted-ses">
        *     Symmetric-Key Encrypted Session-Key Packet</a>
        */
       int SYMMETRIC_KEY_ENC_SESSION = 3; // Symmetric-Key Encrypted Session Key Packet
@@ -43,7 +43,7 @@ public interface PacketTags
        * Business logic: {@link org.bouncycastle.openpgp.PGPOnePassSignature},
        * {@link org.bouncycastle.openpgp.PGPOnePassSignatureList}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-one-pass-signature-packet-t">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-one-pass-signature-packet-t">
        *     One-Pass-Signature Packet</a>
        */
       int ONE_PASS_SIGNATURE = 4 ;       // One-Pass Signature Packet
@@ -53,7 +53,7 @@ public interface PacketTags
        * Packet class: {@link SecretKeyPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPSecretKey}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-secret-key-packet-type-id-5">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-secret-key-packet-type-id-5">
        *     Secret-Key Packet</a>
        */
       int SECRET_KEY = 5;                // Secret Key Packet
@@ -63,7 +63,7 @@ public interface PacketTags
        * Packet class: {@link PublicKeyPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPPublicKey}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-public-key-packet-type-id-6">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-public-key-packet-type-id-6">
        *     Public-Key Packet</a>
        */
       int PUBLIC_KEY = 6 ;               // Public Key Packet
@@ -73,7 +73,7 @@ public interface PacketTags
        * Packet class: {@link SecretSubkeyPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPSecretKey}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-secret-subkey-packet-type-i">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-secret-subkey-packet-type-i">
        *     Secret-Subkey Packet</a>
        */
       int SECRET_SUBKEY = 7;             // Secret Subkey Packet
@@ -83,7 +83,7 @@ public interface PacketTags
        * Packet class: {@link CompressedDataPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPCompressedData}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-compressed-data-packet-type">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-compressed-data-packet-type">
        *     Compressed Data Packet</a>
        */
       int COMPRESSED_DATA = 8;           // Compressed Data Packet
@@ -94,7 +94,7 @@ public interface PacketTags
        * Business logic: {@link org.bouncycastle.openpgp.PGPSymmetricKeyEncryptedData}
        * Note: This encrypted data packet in favor of {@link #SYM_ENC_INTEGRITY_PRO}.
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-symmetrically-encrypted-dat">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-symmetrically-encrypted-dat">
        *     Symmetrically Encrypted Data Packet</a>
        */
       int SYMMETRIC_KEY_ENC = 9;         // Symmetrically Encrypted Data Packet
@@ -105,7 +105,7 @@ public interface PacketTags
        * Business logic: {@link org.bouncycastle.openpgp.PGPMarker}
        * This packet is deprecated and MUST be ignored.
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-marker-packet-type-id-10">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-marker-packet-type-id-10">
        *     Marker Packet</a>
        */
       int MARKER = 10;                   // Marker Packet
@@ -115,7 +115,7 @@ public interface PacketTags
        * Packet class: {@link LiteralDataPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPLiteralData}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-literal-data-packet-type-id">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-literal-data-packet-type-id">
        *     Literal Data Packet</a>
        */
       int LITERAL_DATA = 11;             // Literal Data Packet
@@ -125,7 +125,7 @@ public interface PacketTags
        * Packet class: {@link TrustPacket}
        * This class has no dedicated business logic implementation.
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-trust-packet-type-id-12">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-trust-packet-type-id-12">
        *     Trust Packet</a>
        */
       int TRUST = 12;                    // Trust Packet
@@ -134,7 +134,7 @@ public interface PacketTags
        * User ID Packet.
        * Packet class: {@link UserIDPacket}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-user-id-packet-type-id-13">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-user-id-packet-type-id-13">
        *     User ID Packet</a>
        */
       int USER_ID = 13;                  // User ID Packet
@@ -144,7 +144,7 @@ public interface PacketTags
        * Packet class: {@link PublicSubkeyPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPPublicKey}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-public-subkey-packet-type-i">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-public-subkey-packet-type-i">
        *     Public-Subkey Packet</a>
        */
       int PUBLIC_SUBKEY = 14;            // Public Subkey Packet
@@ -154,7 +154,7 @@ public interface PacketTags
        * Packet class: {@link UserAttributePacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPUserAttributeSubpacketVector}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-user-attribute-packet-type-">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-user-attribute-packet-type-">
        *     User Attribute Packet</a>
        */
       int USER_ATTRIBUTE = 17;           // User attribute
@@ -164,7 +164,7 @@ public interface PacketTags
        * Packet class: {@link SymmetricEncIntegrityPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPSymmetricKeyEncryptedData}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-symmetrically-encrypted-int">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-symmetrically-encrypted-int">
        *     Symmetrically Encrypted Integrity Protected Data Packet</a>
        */
       int SYM_ENC_INTEGRITY_PRO = 18;    // Symmetric encrypted, integrity protected
@@ -173,7 +173,7 @@ public interface PacketTags
        * Modification Detection Code Packet.
        * This is no longer a stand-alone packet and has been integrated into the {@link #SYM_ENC_INTEGRITY_PRO}.
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-terminology-changes">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-terminology-changes">
        *     Terminology Changes</a>
        */
       int MOD_DETECTION_CODE = 19;       // Modification detection code
@@ -194,7 +194,7 @@ public interface PacketTags
        * Packet class: {@link PaddingPacket}
        * Business logic: {@link org.bouncycastle.openpgp.PGPPadding}
        *
-       * @see <a href="https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-padding-packet-type-id-21">
+       * @see <a href="https://www.rfc-editor.org/rfc/rfc9580.html#name-padding-packet-type-id-21">
        *     Padding Packet</a>
        */
       int PADDING = 21;                  // Padding Packet
