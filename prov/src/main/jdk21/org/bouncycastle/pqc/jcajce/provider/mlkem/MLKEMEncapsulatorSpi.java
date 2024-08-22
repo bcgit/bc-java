93c9379f73074b5d207500d36248d51375fa128b
royb <roy.basmacier@primekey.com>
2024-08-22 15:40:07 -0400

    added MLKEM to jdk21

---
+++ prov/src/main/jdk21/org/bouncycastle/pqc/jcajce/provider/mlkem/MLKEMEncapsulatorSpi.java
@@ -0,0 +1,93 @@
+package org.bouncycastle.pqc.jcajce.provider.mlkem;
+
+import org.bouncycastle.crypto.SecretWithEncapsulation;
+import org.bouncycastle.jcajce.provider.asymmetric.mlkem.BCMLKEMPublicKey;
+import org.bouncycastle.jcajce.spec.KTSParameterSpec;
+import org.bouncycastle.pqc.crypto.crystals.kyber.KyberKEMGenerator;
+import org.bouncycastle.pqc.jcajce.provider.Util;
+
+import javax.crypto.KEM;
+import javax.crypto.KEMSpi;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MLKEMEncapsulatorSpi
+    implements KEMSpi.EncapsulatorSpi
+{
+    private final BCMLKEMPublicKey publicKey;
+    private final KTSParameterSpec parameterSpec;
+    private final KyberKEMGenerator kemGen;
+
+    public MLKEMEncapsulatorSpi(BCMLKEMPublicKey publicKey, KTSParameterSpec parameterSpec, SecureRandom random)
+    {
+        this.publicKey = publicKey;
+        this.parameterSpec = parameterSpec;
+
+        this.kemGen = new KyberKEMGenerator(random);
+    }
+
+
+    @Override
+    public KEM.Encapsulated engineEncapsulate(int from, int to, String algorithm)
+    {
+        Objects.checkFromToIndex(from, to, engineSecretSize());
+        Objects.requireNonNull(algorithm, "null algorithm");
+
+        // if algorithm is Generic then use parameterSpec to wrap key
+        if (!parameterSpec.getKeyAlgorithmName().equals("Generic") &&
+                algorithm.equals("Generic"))
+        {
+            algorithm = parameterSpec.getKeyAlgorithmName();
+        }
+
+        // check spec algorithm mismatch provided algorithm
+        if (!parameterSpec.getKeyAlgorithmName().equals("Generic") &&
+                !parameterSpec.getKeyAlgorithmName().equals(algorithm))
+        {
+            throw new UnsupportedOperationException(parameterSpec.getKeyAlgorithmName() + " does not match " + algorithm);
+        }
+
+        // Only use KDF when ktsParameterSpec is provided
+        // Considering any ktsParameterSpec with "Generic" as ktsParameterSpec not provided
+        boolean useKDF = parameterSpec.getKdfAlgorithm() != null;
+
+        SecretWithEncapsulation secEnc = kemGen.generateEncapsulated(publicKey.getKeyParams());
+
+        byte[] encapsulation = secEnc.getEncapsulation();
+        byte[] secret = secEnc.getSecret();
+
+        byte[] secretKey;
+
+        if (useKDF)
+        {
+            try
+            {
+                secret = Util.makeKeyBytes(parameterSpec, secret);
+            }
+            catch (InvalidKeyException e)
+            {
+                throw new IllegalStateException(e);
+            }
+        }
+
+        secretKey = Arrays.copyOfRange(secret, from, to);
+
+        return new KEM.Encapsulated(new SecretKeySpec(secretKey, algorithm), encapsulation, null); //TODO: DER encoding for params    }
+    }
+
+    @Override
+    public int engineSecretSize()
+    {
+        return parameterSpec.getKeySize() / 8;
+    }
+
+
+    @Override
+    public int engineEncapsulationSize()
+    {
+        return kemGen.getEncapsulationSize(publicKey.getKeyParams());
+    }
+}
