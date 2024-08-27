21e8460556526eb6e06c880379563f825f51f8d3
David Hook <dgh@cryptoworkshop.com>
2024-08-27 11:16:50 +1000

    added use of old dilithium OIDs for hash-ml to avoid side affects.

--- core/src/main/java/org/bouncycastle/pqc/crypto/mldsa/MLDSAParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/mldsa/MLDSAParameters.java
@@ -7,6 +7,7 @@ public class MLDSAParameters
     public static final MLDSAParameters ml_dsa_44 = new MLDSAParameters("ml-dsa-44", 2, false);
     public static final MLDSAParameters ml_dsa_65 = new MLDSAParameters("ml-dsa-65", 3, false);
     public static final MLDSAParameters ml_dsa_87 = new MLDSAParameters("ml-dsa-87", 5, false);
+
     public static final MLDSAParameters hash_ml_dsa_44 = new MLDSAParameters("hash-ml-dsa-44", 2, true);
     public static final MLDSAParameters hash_ml_dsa_65 = new MLDSAParameters("hash-ml-dsa-65", 3, true);
     public static final MLDSAParameters hash_ml_dsa_87 = new MLDSAParameters("hash-ml-dsa-87", 5, true);
