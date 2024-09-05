61149407526d8d14add394ce7d9e15e7adb00d68
gefeili <gli@keyfactor.com>
2024-09-05 14:27:21 +0930

    revert the wrong commit #1768 Implement message decryption using SEIPDv2 and PKESKv6 packets

--- pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
@@ -74,8 +74,6 @@ public class RegressionTest
         new LegacyX25519KeyPairTest(),
         new LegacyX448KeyPairTest(),
 
-        new PGPv6MessageDecryptionTest(),
-
         new Curve25519PrivateKeyEncodingTest(),
         new EdDSAKeyConversionWithLeadingZeroTest(),
         new ECDSAKeyPairTest()
