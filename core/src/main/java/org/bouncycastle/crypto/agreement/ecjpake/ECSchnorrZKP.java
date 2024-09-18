f38b7acb788c3363bd144ea4db4319b00544752a
DawidM <96344755+dawmit@users.noreply.github.com>
2024-09-18 10:10:02 +0100

    Update ECSchnorrZKP.java

--- core/src/main/java/org/bouncycastle/crypto/agreement/ecjpake/ECSchnorrZKP.java
+++ core/src/main/java/org/bouncycastle/crypto/agreement/ecjpake/ECSchnorrZKP.java
@@ -4,15 +4,21 @@ import java.math.BigInteger;
 import org.bouncycastle.math.ec.ECPoint;
 
 /**
- * Package protected class for the zero knowledge proof, for an EC J-PAKE exchange.
- * <p>
- * V = G x [v]
- * r = v - d * c mod n
- * <p>
- */
+ * Package protected class containing zero knowledge proof, for an EC J-PAKE exchange.
+ *</p>
+ *
+ *
+ */ 
 class ECSchnorrZKP {
-    
+
+    /**
+     * The value of V = G x [v].
+     */
     private final ECPoint V;
+
+    /**
+     * The value of r = v - d * c mod n
+     */
     private final BigInteger r;
 
     ECSchnorrZKP(ECPoint V, BigInteger r) 
