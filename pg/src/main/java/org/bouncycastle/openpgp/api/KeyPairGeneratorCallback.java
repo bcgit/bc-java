b1817acd620e11834823d47c486b85e36c98d01c
Paul Schaub <vanitasvitae@fsfe.org>
2024-10-03 11:53:18 +0200

    Implement high-level OpenPGPV6KeyGenerator class

---
+++ pg/src/main/java/org/bouncycastle/openpgp/api/KeyPairGeneratorCallback.java
@@ -0,0 +1,21 @@
+package org.bouncycastle.openpgp.api;
+
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPKeyPair;
+import org.bouncycastle.openpgp.operator.PGPKeyPairGenerator;
+
+/**
+ * Callback to generate a {@link PGPKeyPair} from a {@link PGPKeyPairGenerator} instance.
+ */
+@FunctionalInterface
+public interface KeyPairGeneratorCallback {
+    /**
+     * Generate a {@link PGPKeyPair} by calling a factory method on a given generator instance.
+     *
+     * @param generator PGPKeyPairGenerator
+     * @return generated key pair
+     * @throws PGPException
+     */
+    PGPKeyPair generateFrom(PGPKeyPairGenerator generator)
+            throws PGPException;
+}
