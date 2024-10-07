e407d8d7a8f3baa6716eb8226f453e032ea9f43f
Paul Schaub <vanitasvitae@fsfe.org>
2024-10-07 12:26:41 +0200

    Introduce BC implementation of OpenPGPV6KeyGenerator

--- pg/src/test/java/org/bouncycastle/openpgp/api/test/BcOpenPGPV6KeyGeneratorTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/api/test/BcOpenPGPV6KeyGeneratorTest.java
@@ -1,28 +1,25 @@
-package org.bouncycastle.openpgp.test;
+package org.bouncycastle.openpgp.api.test;
 
-import org.bouncycastle.bcpg.ArmoredOutputStream;
-import org.bouncycastle.bcpg.BCPGOutputStream;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
-import org.bouncycastle.bcpg.PacketFormat;
 import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKey;
 import org.bouncycastle.openpgp.PGPSecretKeyRing;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.api.BcOpenPGPV6KeyGenerator;
 import org.bouncycastle.openpgp.api.OpenPGPV6KeyGenerator;
 import org.bouncycastle.openpgp.operator.PGPKeyPairGenerator;
-import org.bouncycastle.openpgp.operator.bc.BcPGPContentSignerBuilderProvider;
-import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
-import org.bouncycastle.openpgp.operator.bc.BcPGPKeyPairGeneratorProvider;
+import org.bouncycastle.openpgp.test.AbstractPgpKeyPairTest;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Date;
+import java.util.Iterator;
 
-public class PGPV6KeyRingGeneratorTest
+public class BcOpenPGPV6KeyGeneratorTest
         extends AbstractPgpKeyPairTest
 {
     @Override
     public String getName()
     {
-        return "PGPV6KeyRingGeneratorTest";
+        return "OpenPGPV6KeyGeneratorTest";
     }
 
     @Override
@@ -33,13 +30,11 @@ public class PGPV6KeyRingGeneratorTest
     }
 
     private void testGenerateMinimalKey()
-            throws PGPException, IOException
+            throws PGPException
     {
         Date creationTime = currentTimeRounded();
-        OpenPGPV6KeyGenerator gen = new OpenPGPV6KeyGenerator(
-                new BcPGPKeyPairGeneratorProvider(),
-                new BcPGPContentSignerBuilderProvider(HashAlgorithmTags.SHA3_512),
-                new BcPGPDigestCalculatorProvider(),
+        OpenPGPV6KeyGenerator gen = new BcOpenPGPV6KeyGenerator(
+                HashAlgorithmTags.SHA3_512,
                 creationTime
         );
         PGPSecretKeyRing secretKeys = gen.withPrimaryKey(
@@ -55,17 +50,27 @@ public class PGPV6KeyRingGeneratorTest
                 .addSigningSubkey(null)
                 .build();
 
-        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-        ArmoredOutputStream aOut = new ArmoredOutputStream(bOut);
-        BCPGOutputStream pOut = new BCPGOutputStream(aOut, PacketFormat.CURRENT);
-        secretKeys.encode(pOut);
-        pOut.close();
-        aOut.close();
-        System.out.println(bOut);
+        // Test creation time
+        for (PGPPublicKey key : secretKeys.toCertificate())
+        {
+            isEquals(creationTime, key.getCreationTime());
+            for (Iterator<PGPSignature> it = key.getSignatures(); it.hasNext(); ) {
+                PGPSignature sig = it.next();
+                isEquals(creationTime, sig.getCreationTime());
+            }
+        }
+
+        PGPPublicKey primaryKey = secretKeys.getPublicKey();
+        // Test UIDs
+        Iterator<String> uids = primaryKey.getUserIDs();
+        isEquals("Alice <alice@example.org>", uids.next());
+        isFalse(uids.hasNext());
+
+
     }
 
     public static void main(String[] args)
     {
-        runTest(new PGPV6KeyRingGeneratorTest());
+        runTest(new BcOpenPGPV6KeyGeneratorTest());
     }
 }
