1f9e3b4d47c7891dafb40e5e333ff6804195b34b
Paul Schaub <vanitasvitae@fsfe.org>
2024-10-07 12:39:54 +0200

    Add BcOpenPGPV6KeyGeneratorTest to RegressionTests

--- pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
@@ -3,6 +3,7 @@ package org.bouncycastle.openpgp.test;
 import java.security.Security;
 
 import org.bouncycastle.bcpg.test.SignatureSubpacketsTest;
+import org.bouncycastle.openpgp.api.test.BcOpenPGPV6KeyGeneratorTest;
 import org.bouncycastle.util.test.SimpleTest;
 import org.bouncycastle.util.test.Test;
 
@@ -85,7 +86,8 @@ public class RegressionTest
         new PGPv5KeyTest(),
         new PGPv5MessageDecryptionTest(),
         new PGPv6SignatureTest(),
-        new PGPKeyPairGeneratorTest()
+        new PGPKeyPairGeneratorTest(),
+        new BcOpenPGPV6KeyGeneratorTest()
     };
 
     public static void main(String[] args)
