02353f3c284a61d30113d9f97862b623a9d3d753
David Hook <dgh@cryptoworkshop.com>
2024-10-25 09:13:43 +1100

    moved to lower-case names as with slhdsa.

--- prov/src/main/java/org/bouncycastle/jcajce/spec/MLDSAParameterSpec.java
+++ prov/src/main/java/org/bouncycastle/jcajce/spec/MLDSAParameterSpec.java
@@ -12,24 +12,24 @@ import org.bouncycastle.util.Strings;
 public class MLDSAParameterSpec
     implements AlgorithmParameterSpec
 {
-    public static final MLDSAParameterSpec ml_dsa_44 = new MLDSAParameterSpec("ML-DSA-44");
-    public static final MLDSAParameterSpec ml_dsa_65 = new MLDSAParameterSpec("ML-DSA-65");
-    public static final MLDSAParameterSpec ml_dsa_87 = new MLDSAParameterSpec("ML-DSA-87");
+    public static final MLDSAParameterSpec ml_dsa_44 = new MLDSAParameterSpec("ml-dsa-44");
+    public static final MLDSAParameterSpec ml_dsa_65 = new MLDSAParameterSpec("ml-dsa-65");
+    public static final MLDSAParameterSpec ml_dsa_87 = new MLDSAParameterSpec("ml-dsa-87");
 
-    public static final MLDSAParameterSpec ml_dsa_44_with_sha512 = new MLDSAParameterSpec("ML-DSA-44-WITH-SHA512");
-    public static final MLDSAParameterSpec ml_dsa_65_with_sha512 = new MLDSAParameterSpec("ML-DSA-65-WITH-SHA512");
-    public static final MLDSAParameterSpec ml_dsa_87_with_sha512 = new MLDSAParameterSpec("ML-DSA-87-WITH-SHA512");
+    public static final MLDSAParameterSpec ml_dsa_44_with_sha512 = new MLDSAParameterSpec("ml-dsa-44-with-sha512");
+    public static final MLDSAParameterSpec ml_dsa_65_with_sha512 = new MLDSAParameterSpec("ml-dsa-65-with-sha512");
+    public static final MLDSAParameterSpec ml_dsa_87_with_sha512 = new MLDSAParameterSpec("ml-dsa-87-with-sha512");
 
     private static Map parameters = new HashMap();
 
     static
     {
-        parameters.put("ml-dsa-44", MLDSAParameterSpec.ml_dsa_44);
-        parameters.put("ml-dsa-65", MLDSAParameterSpec.ml_dsa_65);
-        parameters.put("ml-dsa-87", MLDSAParameterSpec.ml_dsa_87);
-        parameters.put("ml-dsa-44-with-sha512", MLDSAParameterSpec.ml_dsa_44_with_sha512);
-        parameters.put("ml-dsa-65-with-sha512", MLDSAParameterSpec.ml_dsa_65_with_sha512);
-        parameters.put("ml-dsa-87-with-sha512", MLDSAParameterSpec.ml_dsa_87_with_sha512);
+        parameters.put(ml_dsa_44.name, MLDSAParameterSpec.ml_dsa_44);
+        parameters.put(ml_dsa_65.name, MLDSAParameterSpec.ml_dsa_65);
+        parameters.put(ml_dsa_87.name, MLDSAParameterSpec.ml_dsa_87);
+        parameters.put(ml_dsa_44_with_sha512.name, MLDSAParameterSpec.ml_dsa_44_with_sha512);
+        parameters.put(ml_dsa_65_with_sha512.name, MLDSAParameterSpec.ml_dsa_65_with_sha512);
+        parameters.put(ml_dsa_87_with_sha512.name, MLDSAParameterSpec.ml_dsa_87_with_sha512);
     }
 
     private final String name;
