842a6bc914bd813999323d925699c0916b099496
David Hook <dgh@cryptoworkshop.com>
2024-11-01 18:57:49 +1100

    updated for 1.80

--- prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -76,7 +76,7 @@ public final class BouncyCastleProvider extends Provider
 {
     private static final Logger LOG = Logger.getLogger(BouncyCastleProvider.class.getName());
 
-    private static String info = "BouncyCastle Security Provider v1.79";
+    private static String info = "BouncyCastle Security Provider v1.80b";
 
     public static final String PROVIDER_NAME = "BC";
 
@@ -169,7 +169,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.7900, info);
+        super(PROVIDER_NAME, 1.7999, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
