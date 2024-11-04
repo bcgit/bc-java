53a563a004c0d246bfae475b506b6500b48b3c25
David Hook <dgh@cryptoworkshop.com>
2024-11-04 13:07:39 +1100

    refactoring - removed duplicate KDF construction.

--- prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/util/WrapUtil.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/provider/util/WrapUtil.java
@@ -2,32 +2,14 @@ package org.bouncycastle.pqc.jcajce.provider.util;
 
 import java.security.InvalidKeyException;
 
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.ASN1OctetString;
-import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
-import org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
-import org.bouncycastle.crypto.DerivationFunction;
-import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.Wrapper;
-import org.bouncycastle.crypto.Xof;
-import org.bouncycastle.crypto.agreement.kdf.ConcatenationKDFGenerator;
-import org.bouncycastle.crypto.digests.SHA256Digest;
-import org.bouncycastle.crypto.digests.SHA384Digest;
-import org.bouncycastle.crypto.digests.SHA512Digest;
-import org.bouncycastle.crypto.digests.SHAKEDigest;
 import org.bouncycastle.crypto.engines.AESEngine;
 import org.bouncycastle.crypto.engines.ARIAEngine;
 import org.bouncycastle.crypto.engines.CamelliaEngine;
 import org.bouncycastle.crypto.engines.RFC3394WrapEngine;
 import org.bouncycastle.crypto.engines.RFC5649WrapEngine;
 import org.bouncycastle.crypto.engines.SEEDEngine;
-import org.bouncycastle.crypto.generators.HKDFBytesGenerator;
-import org.bouncycastle.crypto.generators.KDF2BytesGenerator;
-import org.bouncycastle.crypto.macs.KMAC;
-import org.bouncycastle.crypto.params.HKDFParameters;
-import org.bouncycastle.crypto.params.KDFParameters;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.jcajce.spec.KTSParameterSpec;
 import org.bouncycastle.util.Arrays;
@@ -112,141 +94,13 @@ public class WrapUtil
     private static byte[] makeKeyBytes(KTSParameterSpec ktsSpec, byte[] secret)
         throws InvalidKeyException
     {
-        AlgorithmIdentifier kdfAlgorithm = ktsSpec.getKdfAlgorithm();
-        byte[] otherInfo = ktsSpec.getOtherInfo();
-        byte[] keyBytes = new byte[(ktsSpec.getKeySize() + 7) / 8];
-
-        if (X9ObjectIdentifiers.id_kdf_kdf2.equals(kdfAlgorithm.getAlgorithm()))
+        try
         {
-            AlgorithmIdentifier digAlg = AlgorithmIdentifier.getInstance(kdfAlgorithm.getParameters());
-            DerivationFunction kdf = new KDF2BytesGenerator(getDigest(digAlg.getAlgorithm()));
-
-            kdf.init(new KDFParameters(secret, otherInfo));
-
-            kdf.generateBytes(keyBytes, 0, keyBytes.length);
+            return KdfUtil.makeKeyBytes(ktsSpec.getKdfAlgorithm(), secret, ktsSpec.getOtherInfo(), ktsSpec.getKeySize());
         }
-        else if (X9ObjectIdentifiers.id_kdf_kdf3.equals(kdfAlgorithm.getAlgorithm()))
+        catch (IllegalArgumentException e)
         {
-            AlgorithmIdentifier digAlg = AlgorithmIdentifier.getInstance(kdfAlgorithm.getParameters());
-            DerivationFunction kdf = new ConcatenationKDFGenerator(getDigest(digAlg.getAlgorithm()));
-
-            kdf.init(new KDFParameters(secret, otherInfo));
-
-            kdf.generateBytes(keyBytes, 0, keyBytes.length);
+            throw new InvalidKeyException(e.getMessage(), e);
         }
-        else if (PKCSObjectIdentifiers.id_alg_hkdf_with_sha256.equals(kdfAlgorithm.getAlgorithm()))
-        {
-            if (kdfAlgorithm.getParameters() == null)
-            {
-                DerivationFunction kdf = new HKDFBytesGenerator(new SHA256Digest());
-
-                kdf.init(new HKDFParameters(secret, null, otherInfo));
-
-                kdf.generateBytes(keyBytes, 0, keyBytes.length);
-            }
-            else
-            {
-                throw new IllegalStateException("HDKF parameter support not added");
-            }
-        }
-        else if (PKCSObjectIdentifiers.id_alg_hkdf_with_sha384.equals(kdfAlgorithm.getAlgorithm()))
-        {
-            if (kdfAlgorithm.getParameters() == null)
-            {
-                DerivationFunction kdf = new HKDFBytesGenerator(new SHA384Digest());
-
-                kdf.init(new HKDFParameters(secret, null, otherInfo));
-
-                kdf.generateBytes(keyBytes, 0, keyBytes.length);
-            }
-            else
-            {
-                throw new IllegalStateException("HDKF parameter support not added");
-            }
-        }
-        else if (PKCSObjectIdentifiers.id_alg_hkdf_with_sha512.equals(kdfAlgorithm.getAlgorithm()))
-        {
-            if (kdfAlgorithm.getParameters() == null)
-            {
-                DerivationFunction kdf = new HKDFBytesGenerator(new SHA512Digest());
-
-                kdf.init(new HKDFParameters(secret, null, otherInfo));
-
-                kdf.generateBytes(keyBytes, 0, keyBytes.length);
-            }
-            else
-            {
-                throw new IllegalStateException("HDKF parameter support not added");
-            }
-        }
-        else if (NISTObjectIdentifiers.id_Kmac128.equals(kdfAlgorithm.getAlgorithm()))
-        {
-            byte[] customStr = new byte[0];
-            if (kdfAlgorithm.getParameters() != null)
-            {
-                customStr = ASN1OctetString.getInstance(kdfAlgorithm.getParameters()).getOctets();
-            }
-
-            KMAC mac = new KMAC(128, customStr);
-
-            mac.init(new KeyParameter(secret, 0, secret.length));
-
-            mac.update(otherInfo, 0, otherInfo.length);
-
-            mac.doFinal(keyBytes, 0, keyBytes.length);
-        }
-        else if (NISTObjectIdentifiers.id_Kmac256.equals(kdfAlgorithm.getAlgorithm()))
-        {
-            byte[] customStr = new byte[0];
-            if (kdfAlgorithm.getParameters() != null)
-            {
-                customStr = ASN1OctetString.getInstance(kdfAlgorithm.getParameters()).getOctets();
-            }
-
-            KMAC mac = new KMAC(256, customStr);
-
-            mac.init(new KeyParameter(secret, 0, secret.length));
-
-            mac.update(otherInfo, 0, otherInfo.length);
-
-            mac.doFinal(keyBytes, 0, keyBytes.length);
-        }
-        else if (NISTObjectIdentifiers.id_shake256.equals(kdfAlgorithm.getAlgorithm()))
-        {
-             Xof xof = new SHAKEDigest(256);
-
-             xof.update(secret, 0, secret.length);
-             xof.update(otherInfo, 0, otherInfo.length);
-
-             xof.doFinal(keyBytes, 0, keyBytes.length);
-        }
-        else
-        {
-            throw new InvalidKeyException("Unrecognized KDF: " + kdfAlgorithm.getAlgorithm());
-        }
-
-        return keyBytes;
-    }
-
-    static Digest getDigest(ASN1ObjectIdentifier oid)
-    {
-        if (oid.equals(NISTObjectIdentifiers.id_sha256))
-        {
-            return new SHA256Digest();
-        }
-        if (oid.equals(NISTObjectIdentifiers.id_sha512))
-        {
-            return new SHA512Digest();
-        }
-        if (oid.equals(NISTObjectIdentifiers.id_shake128))
-        {
-            return new SHAKEDigest(128);
-        }
-        if (oid.equals(NISTObjectIdentifiers.id_shake256))
-        {
-            return new SHAKEDigest(256);
-        }
-
-        throw new IllegalArgumentException("unrecognized digest OID: " + oid);
     }
 }
