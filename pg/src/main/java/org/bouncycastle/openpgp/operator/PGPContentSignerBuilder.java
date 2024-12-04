db6a986114577e73c0f044a1a6fee58c6c294560
gefeili <gli@keyfactor.com>
2024-12-04 16:25:57 +1030

    Update from #1926

--- pg/src/main/java/org/bouncycastle/openpgp/operator/PGPContentSignerBuilder.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/PGPContentSignerBuilder.java
@@ -3,6 +3,11 @@ package org.bouncycastle.openpgp.operator;
 import org.bouncycastle.openpgp.PGPException;
 import org.bouncycastle.openpgp.PGPPrivateKey;
 
+/**
+ * Builder for {@link PGPContentSigner} objects.
+ * The purpose of this class is to act as an abstract factory, whose subclasses can decide, which concrete
+ * implementation to use for the {@link PGPContentSigner}.
+ */
 public interface PGPContentSignerBuilder
 {
     PGPContentSigner build(final int signatureType, final PGPPrivateKey privateKey)
