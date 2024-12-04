0cd9945c356dbc207dd9579292b4808c4233da70
gefeili <gli@keyfactor.com>
2024-12-04 18:01:35 +1030

    Remove SecretSplitter.recombineShares

--- core/src/main/java/org/bouncycastle/crypto/threshold/SplitSecret.java
+++ core/src/main/java/org/bouncycastle/crypto/threshold/SplitSecret.java
@@ -4,5 +4,10 @@ public interface SplitSecret
 {
     SecretShare[] getSecretShare();
 
+    /**
+     * Recombines secret shares to reconstruct the original secret.
+     *
+     * @return A byte array containing the reconstructed secret.
+     */
     byte[] recombine();
 }
