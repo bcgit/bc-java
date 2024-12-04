74b0afc24648002f20404d98838eb35b86631440
gefeili <gli@keyfactor.com>
2024-12-04 16:25:57 +1030

    Update from #1926

--- pg/src/main/java/org/bouncycastle/openpgp/operator/PGPContentVerifierBuilderProvider.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/PGPContentVerifierBuilderProvider.java
@@ -2,6 +2,12 @@ package org.bouncycastle.openpgp.operator;
 
 import org.bouncycastle.openpgp.PGPException;
 
+/**
+ * Provider for {@link PGPContentVerifierBuilder} instances.
+ * The purpose of this class is to act as an abstract factory, whose subclasses can decide, which concrete
+ * implementation of {@link PGPContentVerifierBuilder} (builder for objects check signatures for correctness)
+ * to provide.
+ */
 public interface PGPContentVerifierBuilderProvider
 {
     PGPContentVerifierBuilder get(int keyAlgorithm, int hashAlgorithm)
