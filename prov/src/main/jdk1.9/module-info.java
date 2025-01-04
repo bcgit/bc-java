2a4ee490f7bceccf91ff2f6cc5d4be2845e2b884
David Hook <dgh@cryptoworkshop.com>
2025-01-05 10:39:22 +1100

    added tls/threshold packages

--- prov/src/main/jdk1.9/module-info.java
+++ prov/src/main/jdk1.9/module-info.java
@@ -60,6 +60,8 @@ module org.bouncycastle.provider
     exports org.bouncycastle.crypto.prng;
     exports org.bouncycastle.crypto.prng.drbg;
     exports org.bouncycastle.crypto.signers;
+    exports org.bouncycastle.crypto.threshold;
+    exports org.bouncycastle.crypto.tls;
     exports org.bouncycastle.crypto.util;
     exports org.bouncycastle.i18n;
     exports org.bouncycastle.i18n.filter;
