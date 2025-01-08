2d22d5aef3da98d813b4af1f9d6203aa624f7db2
Paul Schaub <vanitasvitae@fsfe.org>
2025-01-08 13:16:05 +0100

    Add javadoc to OpenPGPDetachedSignatureGenerator

--- pg/src/main/java/org/bouncycastle/openpgp/api/OpenPGPDetachedSignatureGenerator.java
+++ pg/src/main/java/org/bouncycastle/openpgp/api/OpenPGPDetachedSignatureGenerator.java
@@ -13,6 +13,18 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * High-Level OpenPGP Signature Generator for Detached Signatures.
+ * Detached signatures can be stored and distributed as a distinct object alongside the signed data.
+ * They are used for example to sign Release files of some Linux software distributions.
+ * <p>
+ * To use this class, instantiate it, optionally providing a concrete {@link OpenPGPImplementation} and
+ * {@link OpenPGPPolicy} for algorithm policing.
+ * Then, add the desired {@link OpenPGPKey} you want to use for signing the data via one or more
+ * calls to {@link #addSigningKey(OpenPGPKey, char[])}.
+ * Lastly, retrieve a list of detached {@link OpenPGPSignature.OpenPGPDocumentSignature signatures} by calling
+ * {@link #sign(InputStream)}, passing in an {@link InputStream} containing the data you want to sign.
+ */
 public class OpenPGPDetachedSignatureGenerator
 {
     private final OpenPGPImplementation implementation;
@@ -22,34 +34,77 @@ public class OpenPGPDetachedSignatureGenerator
     private final List<PGPSignatureGenerator> signatureGenerators = new ArrayList<>();
     private final List<OpenPGPCertificate.OpenPGPComponentKey> signingKeys = new ArrayList<>();
 
+    /**
+     * Instantiate a signature generator using the default {@link OpenPGPImplementation} and its {@link OpenPGPPolicy}.
+     */
     public OpenPGPDetachedSignatureGenerator()
     {
         this(OpenPGPImplementation.getInstance());
     }
 
+    /**
+     * Instantiate a signature generator using the passed in {@link OpenPGPImplementation} and its {@link OpenPGPPolicy}.
+     *
+     * @param implementation custom OpenPGP implementation
+     */
     public OpenPGPDetachedSignatureGenerator(OpenPGPImplementation implementation)
     {
         this(implementation, implementation.policy());
     }
 
+    /**
+     * Instantiate a signature generator using a custom {@link OpenPGPImplementation} and custom {@link OpenPGPPolicy}.
+     *
+     * @param implementation custom OpenPGP implementation
+     * @param policy custom OpenPGP policy
+     */
     public OpenPGPDetachedSignatureGenerator(OpenPGPImplementation implementation, OpenPGPPolicy policy)
     {
         this.implementation = implementation;
         this.policy = policy;
     }
 
+    /**
+     * Set the type of generated signatures to {@link PGPSignature#BINARY_DOCUMENT}.
+     * Binary signatures are calculated over the plaintext as is.
+     * Binary signatures are the default.
+     *
+     * @return this
+     */
     public OpenPGPDetachedSignatureGenerator setBinarySignature()
     {
         this.signatureType = PGPSignature.BINARY_DOCUMENT;
         return this;
     }
 
+    /**
+     * Set the type of generated signatures to {@link PGPSignature#CANONICAL_TEXT_DOCUMENT}.
+     * Text signatures are calculated over modified plaintext, which is first transformed by canonicalizing
+     * line endings to CR-LF (<pre>0x0D0A</pre>).
+     * This is useful, if the plaintext is transported via a channel that may not retain the original message
+     * encoding.
+     *
+     * @return this
+     */
     public OpenPGPDetachedSignatureGenerator setCanonicalTextDocument()
     {
         this.signatureType = PGPSignature.CANONICAL_TEXT_DOCUMENT;
         return this;
     }
 
+    /**
+     * Add an {@link OpenPGPKey} as signing key.
+     * If no (sub-)key in the signing key is capable of creating signatures, or if the key is expired or revoked,
+     * this method will throw an {@link InvalidSigningKeyException}.
+     * Otherwise, all capable signing subkeys will be used to create detached signatures.
+     *
+     * @param key OpenPGP key
+     * @param passphrase passphrase to unlock the signing key
+     * @return this
+     *
+     * @throws InvalidSigningKeyException if the OpenPGP key does not contain a usable signing subkey
+     * @throws PGPException if signing fails
+     */
     public OpenPGPDetachedSignatureGenerator addSigningKey(OpenPGPKey key, char[] passphrase)
             throws PGPException
     {
@@ -90,6 +145,16 @@ public class OpenPGPDetachedSignatureGenerator
         return policy.getDefaultDocumentSignatureHashAlgorithm();
     }
 
+    /**
+     * Pass in an {@link InputStream} containing the data that shall be signed and return a list of detached
+     * signatures.
+     *
+     * @param inputStream data to be signed
+     * @return detached signatures
+     *
+     * @throws IOException if something goes wrong processing the data
+     * @throws PGPException if signing fails
+     */
     public List<OpenPGPSignature.OpenPGPDocumentSignature> sign(InputStream inputStream)
             throws IOException, PGPException
     {
