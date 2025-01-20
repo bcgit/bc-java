0ac93c9dbd9bc4c97a0a1d3630296bd0ad818f63
Peter Dettman <peter.dettman@bouncycastle.org>
2025-01-20 23:59:39 +0700

    Add validatilon methods to Arrays

--- core/src/main/java/org/bouncycastle/util/Arrays.java
+++ core/src/main/java/org/bouncycastle/util/Arrays.java
@@ -1240,4 +1240,30 @@ public final class Arrays
     {
         return null == array || array.length < 1;
     }
+
+    public static void validateRange(byte[] buf, int from, int to)
+    {
+        if (buf == null)
+        {
+            throw new NullPointerException("'buf' cannot be null");
+        }
+        if ((from | (buf.length - from) | (to - from) | (buf.length - to)) < 0)
+        {
+            throw new IndexOutOfBoundsException("buf.length: " + buf.length + ", from: " + from + ", to: " + to);
+        }
+    }
+
+    public static void validateSegment(byte[] buf, int off, int len)
+    {
+        if (buf == null)
+        {
+            throw new NullPointerException("'buf' cannot be null");
+        }
+        int available = buf.length - off;
+        int remaining = available - len;
+        if ((off | len | available | remaining) < 0)
+        {
+            throw new IndexOutOfBoundsException("buf.length: " + buf.length + ", off: " + off + ", len: " + len);
+        }
+    }
 }
