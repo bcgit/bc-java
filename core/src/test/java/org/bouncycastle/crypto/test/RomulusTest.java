9bf85333df7941d5cfa5da9472b9ed09f0b00183
gefeili <gli@keyfactor.com>
2025-01-21 15:01:45 +1030

    Add tests for RomulusEngine, RomulusM will be done in the future.

--- core/src/test/java/org/bouncycastle/crypto/test/RomulusTest.java
+++ core/src/test/java/org/bouncycastle/crypto/test/RomulusTest.java
@@ -14,6 +14,7 @@ import org.bouncycastle.crypto.digests.RomulusDigest;
 import org.bouncycastle.crypto.engines.GiftCofbEngine;
 import org.bouncycastle.crypto.engines.RomulusEngine;
 import org.bouncycastle.crypto.modes.AEADBlockCipher;
+import org.bouncycastle.crypto.modes.AEADCipher;
 import org.bouncycastle.crypto.params.AEADParameters;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
@@ -31,9 +32,64 @@ public class RomulusTest
     public void performTest()
         throws Exception
     {
-        CipherTest.implTestVectorsEngine(new RomulusEngine(RomulusEngine.RomulusParameters.RomulusM), "crypto/romulus", "m_LWC_AEAD_KAT_128_128.txt", this);
+        //CipherTest.implTestVectorsEngine(new RomulusEngine(RomulusEngine.RomulusParameters.RomulusM), "crypto/romulus", "m_LWC_AEAD_KAT_128_128.txt", this);
         CipherTest.implTestVectorsEngine(new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT), "crypto/romulus", "t_LWC_AEAD_KAT_128_128.txt", this);
         CipherTest.implTestVectorsEngine(new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN), "crypto/romulus", "n_LWC_AEAD_KAT_128_128.txt", this);
+
+        CipherTest.implTestBufferingEngine(16, 16, 128, this, new CipherTest.Instance()
+        {
+            @Override
+            public AEADCipher createInstance()
+            {
+                return new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT);
+            }
+        });
+        CipherTest.implTestBufferingEngine(16, 16, 128, this, new CipherTest.Instance()
+        {
+            @Override
+            public AEADCipher createInstance()
+            {
+                return new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN);
+            }
+        });
+        CipherTest.implTestExceptionsEngine(16, 16, this, new CipherTest.Instance()
+        {
+            @Override
+            public AEADCipher createInstance()
+            {
+                return new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT);
+            }
+        });
+        CipherTest.implTestExceptionsEngine(16, 16, this, new CipherTest.Instance()
+        {
+            @Override
+            public AEADCipher createInstance()
+            {
+                return new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN);
+            }
+        });
+        implTestParametersEngine(new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT), 16, 16, 16);
+        implTestParametersEngine(new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN), 16, 16, 16);
+        CipherTest.checkAEADParemeter(this, 16, 16, 16, 16, new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT));
+        CipherTest.checkAEADParemeter(this, 16, 16, 16, 16, new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN));
+        CipherTest.checkAEADCipherMultipleBlocks(this, 1025, 33, 16, 128, 16, new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT));
+        CipherTest.checkAEADCipherMultipleBlocks(this, 1025, 33, 16, 128, 16, new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN));
+
+        CipherTest.checkCipher(16, 16, 40, 128, new CipherTest.Instance()
+        {
+            public AEADCipher createInstance()
+            {
+                return new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT);
+            }
+        });
+
+        CipherTest.checkCipher(16, 16, 40, 128, new CipherTest.Instance()
+        {
+            public AEADCipher createInstance()
+            {
+                return new RomulusEngine(RomulusEngine.RomulusParameters.RomulusN);
+            }
+        });
 
 //        RomulusEngine romulus = new RomulusEngine(RomulusEngine.RomulusParameters.RomulusT);
 //        testExceptions(romulus, romulus.getKeyBytesSize(), romulus.getIVBytesSize(), romulus.getBlockSize());
@@ -48,6 +104,32 @@ public class RomulusTest
 //        testVectors(RomulusEngine.RomulusParameters.RomulusN, "n");
     }
 
+    private void implTestParametersEngine(RomulusEngine cipher, int keySize, int ivSize,
+                                          int macSize)
+    {
+        if (cipher.getKeyBytesSize() != keySize)
+        {
+            fail("key bytes of " + cipher.getAlgorithmName() + " is not correct");
+        }
+        if (cipher.getIVBytesSize() != ivSize)
+        {
+            fail("iv bytes of " + cipher.getAlgorithmName() + " is not correct");
+        }
+
+        CipherParameters parameters = new ParametersWithIV(new KeyParameter(new byte[keySize]), new byte[ivSize]);
+
+        cipher.init(true, parameters);
+        if (cipher.getOutputSize(0) != macSize)
+        {
+            fail("getOutputSize of " + cipher.getAlgorithmName() + " is incorrect for encryption");
+        }
+
+        cipher.init(false, parameters);
+        if (cipher.getOutputSize(macSize) != 0)
+        {
+            fail("getOutputSize of " + cipher.getAlgorithmName() + " is incorrect for decryption");
+        }
+    }
 
 
     private void testVectorsHash()
