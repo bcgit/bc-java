6e8ad394604c2a4b4d5afc074c04990d1dd6dc62
gefeili <gli@keyfactor.com>
2025-04-02 11:10:45 +1030

    Update AEAD modes

--- core/src/main/java/org/bouncycastle/crypto/modes/EAXBlockCipher.java
+++ core/src/main/java/org/bouncycastle/crypto/modes/EAXBlockCipher.java
@@ -224,6 +224,12 @@ public class EAXBlockCipher
         {
             throw new DataLengthException("Input buffer too short");
         }
+        if (in == out && segmentsOverlap(inOff, len, outOff, getUpdateOutputSize(len)))
+        {
+            in = new byte[len];
+            System.arraycopy(out, inOff, in, 0, len);
+            inOff = 0;
+        }
 
         int resultLen = 0;
 
@@ -383,5 +389,11 @@ public class EAXBlockCipher
         }
 
         return nonEqual == 0;
+    }
+
+    protected boolean segmentsOverlap(int inOff, int inLen, int outOff, int outLen)
+    {
+        // please ensure a valid check for inLen > 0 and outLen > 0 outside this function
+        return inOff <= outOff + outLen && outOff <= inOff + inLen;
     }
 }
