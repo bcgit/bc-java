e172b1479894a86ac3c26c34d770b75dc6b1c37e
Paul Schaub <vanitasvitae@fsfe.org>
2025-05-13 15:31:09 +0200

    PGPSignature.join(): Retain signature packet format

--- pg/src/main/java/org/bouncycastle/openpgp/PGPSignature.java
+++ pg/src/main/java/org/bouncycastle/openpgp/PGPSignature.java
@@ -197,7 +197,7 @@ public class PGPSignature
      */
     public static final int THIRD_PARTY_CONFIRMATION = 0x50;
 
-    private final SignaturePacket sigPck;
+    final SignaturePacket sigPck;
     private final TrustPacket trustPck;
 
     private volatile PGPContentVerifier verifier;
@@ -1033,6 +1033,8 @@ public class PGPSignature
         SignatureSubpacket[] unhashed = (SignatureSubpacket[])merged.toArray(new SignatureSubpacket[0]);
         return new PGPSignature(
             new SignaturePacket(
+                sig1.getVersion(),
+                sig1.sigPck.hasNewPacketFormat(),
                 sig1.getSignatureType(),
                 sig1.getKeyID(),
                 sig1.getKeyAlgorithm(),
