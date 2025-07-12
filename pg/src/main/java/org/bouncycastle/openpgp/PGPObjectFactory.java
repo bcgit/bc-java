956735194ed5fea3949a7deaa237ab07d0e814eb
Paul Schaub <vanitasvitae@fsfe.org>
2025-07-12 11:36:28 +0200

    PGPObjectFactory: Prevent ClassCastException when parsing dangling ModDetectionCodePacket

--- pg/src/main/java/org/bouncycastle/openpgp/PGPObjectFactory.java
+++ pg/src/main/java/org/bouncycastle/openpgp/PGPObjectFactory.java
@@ -167,6 +167,8 @@ public class PGPObjectFactory
             return new PGPMarker(in);
         case PacketTags.PADDING:
             return new PGPPadding(in);
+        case PacketTags.MOD_DETECTION_CODE:
+            return new UnknownPacket(PacketTags.MOD_DETECTION_CODE, in);
         }
 
         int tag = in.nextPacketTag();
