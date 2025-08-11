da4a329194cad9bb9efb80a5862dfb6ca1bb286f
David Hook <dgh@cryptoworkshop.com>
2025-08-11 15:21:21 +1000

    refactoring of ML-KEM public key validation into PublicKeyParameters - relates to github #1974

--- core/src/main/java/org/bouncycastle/pqc/crypto/mlkem/MLKEMPublicKeyParameters.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/mlkem/MLKEMPublicKeyParameters.java
@@ -16,6 +16,9 @@ public class MLKEMPublicKeyParameters
     public MLKEMPublicKeyParameters(MLKEMParameters params, byte[] t, byte[] rho)
     {
         super(false, params);
+
+        validatePublicKey(params.getEngine(), getEncoded(t, rho));
+
         this.t = Arrays.clone(t);
         this.rho = Arrays.clone(rho);
     }
@@ -23,8 +26,32 @@ public class MLKEMPublicKeyParameters
     public MLKEMPublicKeyParameters(MLKEMParameters params, byte[] encoding)
     {
         super(false, params);
+
+        validatePublicKey(params.getEngine(), encoding);
+
         this.t = Arrays.copyOfRange(encoding, 0, encoding.length - MLKEMEngine.KyberSymBytes);
         this.rho = Arrays.copyOfRange(encoding, encoding.length - MLKEMEngine.KyberSymBytes, encoding.length);
+    }
+
+    private static void validatePublicKey(MLKEMEngine engine, byte[] publicKeyInput)
+    {
+        // Input validation (6.2 ML-KEM Encaps)
+        // length Check
+        if (publicKeyInput.length != engine.getKyberIndCpaPublicKeyBytes())
+        {
+            throw new IllegalArgumentException("length check failed for ml-kem public key construction");
+        }
+
+        // Modulus Check
+        PolyVec polyVec = new PolyVec(engine);
+        MLKEMIndCpa indCpa = engine.getIndCpa();
+
+        byte[] seed = indCpa.unpackPublicKey(polyVec, publicKeyInput);
+        byte[] ek = indCpa.packPublicKey(polyVec, seed);
+        if (!Arrays.areEqual(ek, publicKeyInput))
+        {
+            throw new IllegalArgumentException("modulus check failed for ml-kem public key construction");
+        }
     }
 
     public byte[] getEncoded()
