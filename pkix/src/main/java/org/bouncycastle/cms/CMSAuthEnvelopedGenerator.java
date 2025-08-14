92a328681329dfb91d91fefca7430f14d41788a1
gefeili <gli@keyfactor.com>
2025-08-14 10:30:23 +0930

    remove unused imports in CMSAuthEnvelopedGenerator

--- pkix/src/main/java/org/bouncycastle/cms/CMSAuthEnvelopedGenerator.java
+++ pkix/src/main/java/org/bouncycastle/cms/CMSAuthEnvelopedGenerator.java
@@ -1,8 +1,5 @@
 package org.bouncycastle.cms;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bouncycastle.asn1.cms.OriginatorInfo;
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 
