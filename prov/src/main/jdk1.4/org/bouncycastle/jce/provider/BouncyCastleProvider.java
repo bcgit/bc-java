6058e18cfdff0db8968d9ffa11becec62af38540
David Hook <dgh@cryptoworkshop.com>
2025-09-12 16:00:21 +1000

    moved to 1.82

--- prov/src/main/jdk1.4/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/jdk1.4/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -51,7 +51,7 @@ import org.bouncycastle.pqc.jcajce.provider.dilithium.DilithiumKeyFactorySpi;
 public final class BouncyCastleProvider extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Security Provider v1.81";
+    private static String info = "BouncyCastle Security Provider v1.82";
 
     public static final String PROVIDER_NAME = "BC";
 
@@ -135,7 +135,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.8100, info);
+        super(PROVIDER_NAME, 1.8200, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
