0fb2ec1aa037ce239270cb2757fb569feed3b858
Peter Dettman <peter.dettman@bouncycastle.org>
2025-09-22 20:58:34 +0700

    Fix javadoc

--- core/src/main/java/org/bouncycastle/asn1/x509/X509ObjectIdentifiers.java
+++ core/src/main/java/org/bouncycastle/asn1/x509/X509ObjectIdentifiers.java
@@ -31,7 +31,7 @@ public interface X509ObjectIdentifiers
      * id-SHA1 OBJECT IDENTIFIER ::=    
      *   {iso(1) identified-organization(3) oiw(14) secsig(3) algorithms(2) 26 }
      * <p>
-     * OID: 1.3.14.3.2.27
+     * OID: 1.3.14.3.2.26
      */
     static final ASN1ObjectIdentifier    id_SHA1                 = new ASN1ObjectIdentifier("1.3.14.3.2.26").intern();
 
