8caae47b1ca89f449f39f2dc8469a56432203bac
David Hook <dgh@cryptoworkshop.com>
2025-10-24 18:45:29 +1100

    relocated class - added getContextFrom()

--- prov/src/main/java/org/bouncycastle/jcajce/util/SpecUtil.java
+++ prov/src/main/java/org/bouncycastle/jcajce/util/SpecUtil.java
@@ -1,14 +1,10 @@
-package org.bouncycastle.pqc.jcajce.provider.util;
+package org.bouncycastle.jcajce.util;
 
 import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.spec.AlgorithmParameterSpec;
 
-/**
- * @deprecated use org.bouncycastle.jcajce.util.SpecUtil
- */
-@Deprecated
 public class SpecUtil
 {
     private static Class[] NO_PARAMS = new Class[0];
@@ -23,6 +19,28 @@ public class SpecUtil
                 try
                 {
                     Method m = paramSpec.getClass().getMethod("getName", NO_PARAMS);
+
+                    return m.invoke(paramSpec, NO_ARGS);
+                }
+                catch (Exception e)
+                {
+                    // ignore - maybe log?
+                }
+
+                return null;
+            }
+        });
+    }
+
+    public static byte[] getContextFrom(final AlgorithmParameterSpec paramSpec)
+    {
+        return (byte[])AccessController.doPrivileged(new PrivilegedAction()
+        {
+            public Object run()
+            {
+                try
+                {
+                    Method m = paramSpec.getClass().getMethod("getContext", NO_PARAMS);
 
                     return m.invoke(paramSpec, NO_ARGS);
                 }
