dbaed5e291c3df0c2bb00568f86491d2dc12c577
Peter Dettman <peter.dettman@bouncycastle.org>
2026-02-10 23:27:24 +0700

    HQC: Add missing mappings, fix OID mappings
    - unify HQC mappings using SpiUtil
    - restrict keys by parameter set where appropriate

--- prov/src/main/jdk17/org/bouncycastle/pqc/jcajce/provider/hqc/HQCKEMSpi.java
+++ prov/src/main/jdk17/org/bouncycastle/pqc/jcajce/provider/hqc/HQCKEMSpi.java
@@ -10,49 +10,105 @@ import java.security.spec.AlgorithmParameterSpec;
 import javax.crypto.KEMSpi;
 
 import org.bouncycastle.jcajce.spec.KTSParameterSpec;
+import org.bouncycastle.pqc.crypto.hqc.HQCKeyParameters;
+import org.bouncycastle.pqc.crypto.hqc.HQCParameters;
 
 public class HQCKEMSpi
     implements KEMSpi
 {
+    private final HQCParameters hqcParameters;
+
+    HQCKEMSpi(HQCParameters hqcParameters)
+    {
+        this.hqcParameters = hqcParameters;
+    }
 
     @Override
     public EncapsulatorSpi engineNewEncapsulator(PublicKey publicKey, AlgorithmParameterSpec spec,
-                                                 SecureRandom secureRandom)
-        throws InvalidAlgorithmParameterException, InvalidKeyException
+        SecureRandom secureRandom) throws InvalidAlgorithmParameterException, InvalidKeyException
     {
-        if (!(publicKey instanceof BCHQCPublicKey))
+        if (!(publicKey instanceof BCHQCPublicKey bcPublicKey))
         {
-            throw new InvalidKeyException("unsupported key");
+            throw new InvalidKeyException("unsupported key type");
         }
+
+        checkKeyParameters(bcPublicKey.getKeyParams());
+
         if (spec == null)
         {
             // Do not wrap key, no KDF
             spec = new KTSParameterSpec.Builder("Generic", 256).withNoKdf().build();
         }
-        if (!(spec instanceof KTSParameterSpec))
+        else if (!(spec instanceof KTSParameterSpec))
         {
             throw new InvalidAlgorithmParameterException("HQC can only accept KTSParameterSpec");
         }
-        return new HQCEncapsulatorSpi((BCHQCPublicKey)publicKey, (KTSParameterSpec)spec, secureRandom);
+
+        return new HQCEncapsulatorSpi(bcPublicKey, (KTSParameterSpec)spec, secureRandom);
     }
 
     @Override
     public DecapsulatorSpi engineNewDecapsulator(PrivateKey privateKey, AlgorithmParameterSpec spec)
         throws InvalidAlgorithmParameterException, InvalidKeyException
     {
-        if (!(privateKey instanceof BCHQCPrivateKey))
+        if (!(privateKey instanceof BCHQCPrivateKey bcPrivateKey))
         {
-            throw new InvalidKeyException("unsupported key");
+            throw new InvalidKeyException("unsupported key type");
         }
+
+        checkKeyParameters(bcPrivateKey.getKeyParams());
+
         if (spec == null)
         {
             // Do not unwrap key, no KDF
             spec = new KTSParameterSpec.Builder("Generic", 256).withNoKdf().build();
         }
-        if (!(spec instanceof KTSParameterSpec))
+        else if (!(spec instanceof KTSParameterSpec))
         {
             throw new InvalidAlgorithmParameterException("HQC can only accept KTSParameterSpec");
         }
-        return new HQCDecapsulatorSpi((BCHQCPrivateKey)privateKey, (KTSParameterSpec)spec);
+
+        return new HQCDecapsulatorSpi(bcPrivateKey, (KTSParameterSpec)spec);
+    }
+
+    private void checkKeyParameters(HQCKeyParameters key) throws InvalidKeyException
+    {
+        if (hqcParameters != null && hqcParameters != key.getParameters())
+        {
+            throw new InvalidKeyException("HQC key mismatch");
+        }
+    }
+
+    public static class HQC extends HQCKEMSpi
+    {
+        public HQC()
+        {
+            // NOTE: Unrestricted parameters/keys
+            super(null);
+        }
+    }
+
+    public static class HQC128 extends HQCKEMSpi
+    {
+        public HQC128()
+        {
+            super(HQCParameters.hqc128);
+        }
+    }
+
+    public static class HQC192 extends HQCKEMSpi
+    {
+        public HQC192()
+        {
+            super(HQCParameters.hqc192);
+        }
+    }
+
+    public static class HQC256 extends HQCKEMSpi
+    {
+        public HQC256()
+        {
+            super(HQCParameters.hqc256);
+        }
     }
 }
