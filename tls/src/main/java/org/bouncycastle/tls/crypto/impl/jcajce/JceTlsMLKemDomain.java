970d8d4a0de31c2100413619cc6705e18c26b611
Peter Dettman <peter.dettman@bouncycastle.org>
2026-04-09 15:39:09 +0700

    Delegate JceTlsMLKemDomain functionality to KemUtil

--- tls/src/main/java/org/bouncycastle/tls/crypto/impl/jcajce/JceTlsMLKemDomain.java
+++ tls/src/main/java/org/bouncycastle/tls/crypto/impl/jcajce/JceTlsMLKemDomain.java
@@ -1,18 +1,11 @@
 package org.bouncycastle.tls.crypto.impl.jcajce;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
-import javax.crypto.KeyGenerator;
-
 import org.bouncycastle.jcajce.SecretKeyWithEncapsulation;
-import org.bouncycastle.jcajce.spec.KEMExtractSpec;
-import org.bouncycastle.jcajce.spec.KEMGenerateSpec;
-import org.bouncycastle.jcajce.spec.MLKEMParameterSpec;
 import org.bouncycastle.tls.NamedGroup;
 import org.bouncycastle.tls.crypto.TlsAgreement;
 import org.bouncycastle.tls.crypto.TlsKemConfig;
@@ -43,17 +36,7 @@ public class JceTlsMLKemDomain implements TlsKemDomain
 
     public JceTlsSecret decapsulate(PrivateKey privateKey, byte[] ciphertext)
     {
-        try
-        {
-            KeyGenerator keyGenerator = crypto.getHelper().createKeyGenerator(kemName);
-            keyGenerator.init(new KEMExtractSpec.Builder(privateKey, ciphertext, "DEF", 256).withNoKdf().build());
-            SecretKeyWithEncapsulation secEnc = (SecretKeyWithEncapsulation)keyGenerator.generateKey();
-            return adoptLocalSecret(secEnc.getEncoded());
-        }
-        catch (Exception e)
-        {
-            throw Exceptions.illegalArgumentException("invalid key: " + e.getMessage(), e);
-        }
+        return KemUtil.decapsulate(crypto, kemName, privateKey, ciphertext);
     }
 
     public PublicKey decodePublicKey(byte[] encoding)
@@ -64,16 +47,7 @@ public class JceTlsMLKemDomain implements TlsKemDomain
 
     public SecretKeyWithEncapsulation encapsulate(PublicKey publicKey)
     {
-        try
-        {
-            KeyGenerator keyGenerator = crypto.getHelper().createKeyGenerator(kemName);
-            keyGenerator.init(new KEMGenerateSpec.Builder(publicKey, "DEF", 256).withNoKdf().build());
-            return (SecretKeyWithEncapsulation)keyGenerator.generateKey();
-        }
-        catch (Exception e)
-        {
-            throw Exceptions.illegalArgumentException("invalid key: " + e.getMessage(), e);
-        }
+        return KemUtil.encapsulate(crypto, kemName, publicKey);
     }
 
     public byte[] encodePublicKey(PublicKey publicKey)
@@ -84,21 +58,7 @@ public class JceTlsMLKemDomain implements TlsKemDomain
 
     public KeyPair generateKeyPair()
     {
-        try
-        {
-            // TODO How to pass only the SecureRandom to initialize if we use the full name in the getInstance?
-//            KeyPairGenerator keyPairGenerator = KemUtil.getKeyPairGenerator(crypto, kemName);
-//            keyPairGenerator.initialize((AlgorithmParameterSpec)null, crypto.getSecureRandom());
-//            return keyPairGenerator.generateKeyPair();
-
-            KeyPairGenerator keyPairGenerator = crypto.getHelper().createKeyPairGenerator("ML-KEM");
-            keyPairGenerator.initialize(MLKEMParameterSpec.fromName(kemName), crypto.getSecureRandom());
-            return keyPairGenerator.generateKeyPair();
-        }
-        catch (GeneralSecurityException e)
-        {
-            throw Exceptions.illegalStateException("unable to create key pair: " + e.getMessage(), e);
-        }
+        return KemUtil.generateKeyPair(crypto, kemName);
     }
 
     public boolean isServer()
