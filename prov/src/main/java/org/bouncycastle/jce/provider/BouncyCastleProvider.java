1ad44540b95e9fc7547a1044921aa151b7edebf7
David Hook <dgh@cryptoworkshop.com>
2026-04-12 09:07:30 +1000

    marked SPHINCSPlus and Dilithium for deprecation.

--- prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -128,7 +128,8 @@ public final class BouncyCastleProvider extends Provider
 
     private static final String[] ASYMMETRIC_CIPHERS =
         {
-            "DSA", "DH", "EC", "RSA", "GOST", "ECGOST", "ElGamal", "DSTU4145", "GM", "EdEC", "LMS", "NTRU", "Falcon", "CONTEXT", "SLHDSA", "MLDSA", "MLKEM", "SPHINCSPlus"
+            "DSA", "DH", "EC", "RSA", "GOST", "ECGOST", "ElGamal", "DSTU4145", "GM", "EdEC", "LMS", "NTRU", "Falcon", "CONTEXT", "SLHDSA", "MLDSA", "MLKEM",
+            "Dilithium", "SPHINCSPlus"
         };
 
     /*
--- prov/src/main/java/org/bouncycastle/pqc/jcajce/interfaces/DilithiumPublicKey.java
+++ prov/src/main/java/org/bouncycastle/pqc/jcajce/interfaces/DilithiumPublicKey.java
@@ -2,6 +2,10 @@ package org.bouncycastle.pqc.jcajce.interfaces;
 
 import java.security.PublicKey;
 
+/**
+ * @deprecated to be deleted - use ML-DSA instead.
+ */
+@Deprecated
 public interface DilithiumPublicKey
     extends PublicKey, DilithiumKey
 {
