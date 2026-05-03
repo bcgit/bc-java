b4d7ea0094edd391d2b0f8cecf1a575f0ff2e995
David Hook <dgh@cryptoworkshop.com>
2026-05-03 15:49:22 +1000

    Added basic KeyGrip calculator for RSA keys, relates to github #676

--- pg/src/test/java/org/bouncycastle/gpg/test/RegressionTest.java
+++ pg/src/test/java/org/bouncycastle/gpg/test/RegressionTest.java
@@ -8,7 +8,8 @@ import org.bouncycastle.util.test.Test;
 public class RegressionTest
 {
     public static Test[] tests = {
-        new KeyBoxTest()
+        new KeyBoxTest(),
+        new KeyGripCalculatorTest()
     };
 
     public static void main(String[] args)
