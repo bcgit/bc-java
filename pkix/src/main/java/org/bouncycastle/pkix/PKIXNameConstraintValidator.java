425e4c6ec29b2cb2162dcbbfcb5adc38de40168a
David Hook <dgh@cryptoworkshop.com>
2026-05-13 13:56:58 +1000

    refactor of X.509 name constraints validator down to a single class. fixed TODO on multi-valued RDNs in RFC3280CertPathUtilities.

--- pkix/src/main/java/org/bouncycastle/pkix/PKIXNameConstraintValidator.java
+++ pkix/src/main/java/org/bouncycastle/pkix/PKIXNameConstraintValidator.java
@@ -8,7 +8,8 @@ import org.bouncycastle.asn1.x509.NameConstraintValidatorException;
 
 public class PKIXNameConstraintValidator
 {
-    ASN1PKIXNameConstraintValidator validator = new ASN1PKIXNameConstraintValidator();
+    org.bouncycastle.asn1.x509.PKIXNameConstraintValidator validator =
+        new org.bouncycastle.asn1.x509.PKIXNameConstraintValidator();
 
     public PKIXNameConstraintValidator()
     {
