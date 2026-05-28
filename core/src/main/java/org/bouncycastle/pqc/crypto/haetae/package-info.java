631a9461f3290cda8dbbc8d4578a33df3183d8e4
David Hook <dgh@cryptoworkshop.com>
2026-05-28 23:30:25 +1000

    added missing package-info for HAETAE

---
+++ core/src/main/java/org/bouncycastle/pqc/crypto/haetae/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Lightweight implementation of HAETAE, a module-lattice-based signature scheme in the
+ * NIST PQC additional-digital-signatures round.
+ */
+package org.bouncycastle.pqc.crypto.haetae;
