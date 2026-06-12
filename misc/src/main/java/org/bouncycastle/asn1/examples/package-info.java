5229ede45c391edbc82c1c784477883b8ba94198
David Hook <dgh@cryptoworkshop.com>
2026-06-13 07:07:56 +1000

    added missing package-info files

---
+++ misc/src/main/java/org/bouncycastle/asn1/examples/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Simple examples of ASN.1 API usage, including a command line utility for dumping the structure of an encoded object.
+ */
+package org.bouncycastle.asn1.examples;
---
+++ misc/src/main/java/org/bouncycastle/cades/examples/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Examples of producing and upgrading CAdES baseline signatures (ETSI EN 319 122-1 / RFC 5126) using the org.bouncycastle.cades high-level builders.
+ */
+package org.bouncycastle.cades.examples;
