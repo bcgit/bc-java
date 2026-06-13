bcd03cbba454b7eeec94a92a3bdb9da08e5062cd
David Hook <dgh@cryptoworkshop.com>
2026-06-14 01:27:17 +1000

    added Java 9 names for PLAIN-ECDSA, relates to github #751

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/EC.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/EC.java
@@ -388,6 +388,22 @@ public class EC
             addSignatureAlgorithm(provider, "SHA3-256", "PLAIN-ECDSA", PREFIX + "SignatureSpi$ecCVCDSA3_256", BSIObjectIdentifiers.ecdsa_plain_SHA3_256, generalEcAttributes);
             addSignatureAlgorithm(provider, "SHA3-384", "PLAIN-ECDSA", PREFIX + "SignatureSpi$ecCVCDSA3_384", BSIObjectIdentifiers.ecdsa_plain_SHA3_384, generalEcAttributes);
             addSignatureAlgorithm(provider, "SHA3-512", "PLAIN-ECDSA", PREFIX + "SignatureSpi$ecCVCDSA3_512", BSIObjectIdentifiers.ecdsa_plain_SHA3_512, generalEcAttributes);
+
+            // Java 9+ standard algorithm names for ECDSA producing IEEE P1363 format (fixed-width
+            // r || s) output - the same encoding as PLAIN-ECDSA / CVC-ECDSA. Registered directly
+            // (with the EC key attributes) so Signature.getInstance(...) resolves them, including
+            // the auto-provider-selection path used by the JDK's built-in XML signature support
+            // (github #751).
+            provider.addAlgorithm("Signature.NONEwithECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSANone", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA1withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA224withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA224", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA256withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA256", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA384withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA384", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA512withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA512", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA3-224withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA3_224", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA3-256withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA3_256", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA3-384withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA3_384", generalEcAttributes);
+            provider.addAlgorithm("Signature.SHA3-512withECDSAinP1363Format", PREFIX + "SignatureSpi$ecCVCDSA3_512", generalEcAttributes);
         }
     }
 }
