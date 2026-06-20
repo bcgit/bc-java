e2a367bb76db7b3aa07c63275fd15a20d24a34c7
Peter Dettman <peter.dettman@bouncycastle.org>
2026-06-20 19:21:23 +0700

    Validate RFC 5084 ICV length in CcmParameters/GcmParameters

--- core/src/main/java/org/bouncycastle/internal/asn1/cms/GCMParameters.java
+++ core/src/main/java/org/bouncycastle/internal/asn1/cms/GCMParameters.java
@@ -22,6 +22,8 @@ import org.bouncycastle.util.Arrays;
 public class GCMParameters
     extends ASN1Object
 {
+    private static final int DEFAULT_ICVLEN = 12;
+
     private byte[] nonce;
     private int icvLen;
 
@@ -56,16 +58,16 @@ public class GCMParameters
     private GCMParameters(
         ASN1Sequence seq)
     {
+        int count = seq.size();
+        if (count < 1 || count > 2)
+        {
+            throw new IllegalArgumentException("Bad sequence size: " + count);
+        }
+        
         this.nonce = ASN1OctetString.getInstance(seq.getObjectAt(0)).getOctets();
+        ASN1Integer icvLen = seq.size() < 2 ? null : ASN1Integer.getInstance(seq.getObjectAt(1)); 
 
-        if (seq.size() == 2)
-        {
-            this.icvLen = ASN1Integer.getInstance(seq.getObjectAt(1)).intValueExact();
-        }
-        else
-        {
-            this.icvLen = 12;
-        }
+        this.icvLen = validateICVLen(icvLen == null ? DEFAULT_ICVLEN : icvLen.intValueExact());
     }
 
     public GCMParameters(
@@ -73,7 +75,7 @@ public class GCMParameters
         int    icvLen)
     {
         this.nonce = Arrays.clone(nonce);
-        this.icvLen = icvLen;
+        this.icvLen = validateICVLen(icvLen);
     }
 
     public byte[] getNonce()
@@ -92,11 +94,20 @@ public class GCMParameters
 
         v.add(new DEROctetString(nonce));
 
-        if (icvLen != 12)
+        if (icvLen != DEFAULT_ICVLEN)
         {
             v.add(ASN1Integer.valueOf(icvLen));
         }
 
         return new DERSequence(v);
+    }
+
+    // RFC 5084: AES-GCM-ICVlen ::= INTEGER (12 | 13 | 14 | 15 | 16)
+    private static int validateICVLen(int icvLen)
+    {
+        if (icvLen < 12 || icvLen > 16)
+            throw new IllegalArgumentException("Invalid ICV length: " + icvLen);
+
+        return icvLen;
     }
 }
