012bcebd030720d40eab9971e602818b5ac8ef3a
David Hook <dgh@cryptoworkshop.com>
2026-07-06 16:21:33 +1000

    Fixed jdk18+/legacy Ant test builds: signer-info clash on package-private tests MayoRetryTest and HttpUtilTest which sit in signed main packages. Moved them out of the shared pqc.crypto.test/est.test AllTests into package-local AllTests (run by Gradle) and dropped those from the signed-jar test staging, matching the lms/keybox pattern.

---
+++ pkix/src/test/java/org/bouncycastle/est/AllTests.java
@@ -0,0 +1,44 @@
+package org.bouncycastle.est;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.bouncycastle.test.PrintTestResult;
+
+public class AllTests
+    extends TestCase
+{
+    public static void main(String[] args)
+    {
+        PrintTestResult.printResult(junit.textui.TestRunner.run(suite()));
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("EST package-private tests");
+
+        suite.addTestSuite(HttpUtilTest.class);
+
+        return new BCTestSetup(suite);
+    }
+
+    static class BCTestSetup
+        extends TestSetup
+    {
+        public BCTestSetup(Test test)
+        {
+            super(test);
+        }
+
+        protected void setUp()
+        {
+
+        }
+
+        protected void tearDown()
+        {
+
+        }
+    }
+}
