73fcb0ae69ae37822a67a71f1e31b0e61f28d177
David Hook <dgh@cryptoworkshop.com>
2026-07-06 12:48:36 +1000

    Brought internal CMSOBjectIdentifiers in line with util, corrected references to oid is provider code

--- core/src/main/java/org/bouncycastle/internal/asn1/cms/CMSObjectIdentifiers.java
+++ core/src/main/java/org/bouncycastle/internal/asn1/cms/CMSObjectIdentifiers.java
@@ -2,29 +2,31 @@ package org.bouncycastle.internal.asn1.cms;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
 
 public interface CMSObjectIdentifiers
 {
     /** PKCS#7: 1.2.840.113549.1.7.1 */
-    static final ASN1ObjectIdentifier    data = PKCSObjectIdentifiers.data;
+    ASN1ObjectIdentifier    data = PKCSObjectIdentifiers.data;
     /** PKCS#7: 1.2.840.113549.1.7.2 */
-    static final ASN1ObjectIdentifier    signedData = PKCSObjectIdentifiers.signedData;
+    ASN1ObjectIdentifier    signedData = PKCSObjectIdentifiers.signedData;
     /** PKCS#7: 1.2.840.113549.1.7.3 */
-    static final ASN1ObjectIdentifier    envelopedData = PKCSObjectIdentifiers.envelopedData;
+    ASN1ObjectIdentifier    envelopedData = PKCSObjectIdentifiers.envelopedData;
     /** PKCS#7: 1.2.840.113549.1.7.4 */
-    static final ASN1ObjectIdentifier    signedAndEnvelopedData = PKCSObjectIdentifiers.signedAndEnvelopedData;
+    ASN1ObjectIdentifier    signedAndEnvelopedData = PKCSObjectIdentifiers.signedAndEnvelopedData;
     /** PKCS#7: 1.2.840.113549.1.7.5 */
-    static final ASN1ObjectIdentifier    digestedData = PKCSObjectIdentifiers.digestedData;
+    ASN1ObjectIdentifier    digestedData = PKCSObjectIdentifiers.digestedData;
     /** PKCS#7: 1.2.840.113549.1.7.6 */
-    static final ASN1ObjectIdentifier    encryptedData = PKCSObjectIdentifiers.encryptedData;
+    ASN1ObjectIdentifier    encryptedData = PKCSObjectIdentifiers.encryptedData;
     /** PKCS#9: 1.2.840.113549.1.9.16.1.2 -- smime ct authData */
-    static final ASN1ObjectIdentifier    authenticatedData = PKCSObjectIdentifiers.id_ct_authData;
+    ASN1ObjectIdentifier    authenticatedData = PKCSObjectIdentifiers.id_ct_authData;
     /** PKCS#9: 1.2.840.113549.1.9.16.1.9 -- smime ct compressedData */
-    static final ASN1ObjectIdentifier    compressedData = PKCSObjectIdentifiers.id_ct_compressedData;
+    ASN1ObjectIdentifier    compressedData = PKCSObjectIdentifiers.id_ct_compressedData;
     /** PKCS#9: 1.2.840.113549.1.9.16.1.23 -- smime ct authEnvelopedData */
-    static final ASN1ObjectIdentifier    authEnvelopedData = PKCSObjectIdentifiers.id_ct_authEnvelopedData;
+    ASN1ObjectIdentifier    authEnvelopedData = PKCSObjectIdentifiers.id_ct_authEnvelopedData;
     /** PKCS#9: 1.2.840.113549.1.9.16.1.31 -- smime ct timestampedData*/
-    static final ASN1ObjectIdentifier    timestampedData = PKCSObjectIdentifiers.id_ct_timestampedData;
+    ASN1ObjectIdentifier    timestampedData = PKCSObjectIdentifiers.id_ct_timestampedData;
+    ASN1ObjectIdentifier    zlibCompress = PKCSObjectIdentifiers.id_alg_zlibCompress;
 
     /**
      * The other Revocation Info arc
@@ -34,22 +36,34 @@ public interface CMSObjectIdentifiers
      *        dod(6) internet(1) security(5) mechanisms(5) pkix(7) ri(16) }
      * </pre>
      */
-    static final ASN1ObjectIdentifier    id_ri = new ASN1ObjectIdentifier("1.3.6.1.5.5.7.16");
+    ASN1ObjectIdentifier    id_ri = X509ObjectIdentifiers.id_pkix.branch("16");
 
     /** 1.3.6.1.5.5.7.16.2 */
-    static final ASN1ObjectIdentifier    id_ri_ocsp_response = id_ri.branch("2");
+    ASN1ObjectIdentifier    id_ri_ocsp_response = id_ri.branch("2");
     /** 1.3.6.1.5.5.7.16.4 */
-    static final ASN1ObjectIdentifier    id_ri_scvp = id_ri.branch("4");
+    ASN1ObjectIdentifier    id_ri_scvp = id_ri.branch("4");
 
     /** 1.3.6.1.5.5.7.6 */
-    static final ASN1ObjectIdentifier id_alg  = new ASN1ObjectIdentifier("1.3.6.1.5.5.7.6");
+    ASN1ObjectIdentifier id_alg = X509ObjectIdentifiers.pkix_algorithms;
 
-    static final ASN1ObjectIdentifier id_RSASSA_PSS_SHAKE128 = id_alg.branch("30");
+    ASN1ObjectIdentifier id_RSASSA_PSS_SHAKE128 = X509ObjectIdentifiers.id_rsassa_pss_shake128;
 
-    static final ASN1ObjectIdentifier id_RSASSA_PSS_SHAKE256 = id_alg.branch("31");
+    ASN1ObjectIdentifier id_RSASSA_PSS_SHAKE256 = X509ObjectIdentifiers.id_rsassa_pss_shake256;
 
-    static final ASN1ObjectIdentifier id_ecdsa_with_shake128 = id_alg.branch("32");
+    ASN1ObjectIdentifier id_ecdsa_with_shake128 = X509ObjectIdentifiers.id_ecdsa_with_shake128;
 
-    static final ASN1ObjectIdentifier id_ecdsa_with_shake256 = id_alg.branch("33");
+    ASN1ObjectIdentifier id_ecdsa_with_shake256 = X509ObjectIdentifiers.id_ecdsa_with_shake256;
 
+    /**
+     * OtherRecipientInfo types
+     */
+    ASN1ObjectIdentifier id_ori = PKCSObjectIdentifiers.id_smime.branch("13");
+
+    ASN1ObjectIdentifier id_ori_kem = id_ori.branch("3");
+
+    /**
+     *    id-alg-cek-hkdf-sha256 OBJECT IDENTIFIER ::= { iso(1) member-body(2)
+     *        us(840) rsadsi(113549) pkcs(1) pkcs-9(9) smime(16) alg(3) 31 }
+     */
+    ASN1ObjectIdentifier id_alg_cek_hkdf_sha256 = PKCSObjectIdentifiers.smime_alg.branch("31");
 }
