edfeb7f4747f322ef200d719b3ab1304ae03ff02
David Hook <dgh@cryptoworkshop.com>
2026-07-06 12:20:23 +1000

    Removed the unused org.bouncycastle.iana.AEADAlgorithm constants class, a leftover of the expired draft-zauner-tls-aes-ocb, together with its module exports

--- core/src/main/java/org/bouncycastle/iana/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Constants drawn from various IANA registries (algorithm-number / hash-function /
- * AEAD identifiers) used by HKDF, TLS and CMS callers.
- */
-package org.bouncycastle.iana;
