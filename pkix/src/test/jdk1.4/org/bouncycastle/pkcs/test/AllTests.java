b9fa2cb27873e97132e251c1aef75edac748216f
David Hook <dgh@cryptoworkshop.com>
2026-07-26 16:43:37 +1000

    Resync the three jdk1.4 cert-path overlays (jce/provider/CertPathValidatorUtilities, jce/provider/RFC3280CertPathUtilities, x509/CertPathValidatorUtilities) with base, folding in the ReasonsMask API rename, the PKIXCRLValidator/PKIXPolicyNode extraction, and the ExtException-to-java.security.cert migration those overlays had missed since each landed - build1-4 has been broken since the first of these changes regardless of anything else. Also fix jdk1.4 test-tree compile breaks left by unrelated recent commits (SM9 Destroyable, a PKIXRevocationChecker test, an EST import pulled into ParseTest, String.contains in PBETest, a stale PKIXCertPathReviewer test reference, and two getDeclaredMethod varargs call sites) so build-test compiles and the full jdk1.4 suite runs again.

---
+++ pkix/src/test/jdk1.4/org/bouncycastle/pkcs/test/AllTests.java
@@ -0,0 +1,28 @@
+package org.bouncycastle.pkcs.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.bouncycastle.test.PrintTestResult;
+
+public class AllTests
+    extends TestCase
+{
+    public static void main (String[] args)
+    {
+        PrintTestResult.printResult(junit.textui.TestRunner.run(suite()));
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("PKCS Tests");
+
+        suite.addTestSuite(PfxPduTest.class);
+        suite.addTestSuite(PKCS10Test.class);
+        suite.addTestSuite(PKCS8Test.class);
+        suite.addTestSuite(PKCS12UtilTest.class);
+        suite.addTestSuite(PKCS12PfxPduSecretKeyTest.class);
+
+        return new BCTestSetup(suite);
+    }
+}
