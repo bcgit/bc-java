895bc7739dacc34950ef56828c8acd454b37f3ad
David Hook <dgh@cryptoworkshop.com>
2026-07-26 10:39:56 +1000

    PKIX: add regression tests pinning the corrected ReasonsMask.hasNewReasons semantics in both the prov and pkix copies, with legacy-build excludes for the package-private pkix.jcajce test, and release-note the ReasonsMask fixes.

---
+++ pkix/src/test/java/org/bouncycastle/pkix/jcajce/AllTests.java
@@ -0,0 +1,28 @@
+package org.bouncycastle.pkix.jcajce;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.bouncycastle.test.PrintTestResult;
+
+/**
+ * Suite for tests that need package access to org.bouncycastle.pkix.jcajce - excluded from
+ * the legacy Ant builds, where the test jar cannot share the package with the signed bcpkix jar.
+ */
+public class AllTests
+    extends TestCase
+{
+    public static void main(String[] args)
+    {
+        PrintTestResult.printResult(junit.textui.TestRunner.run(suite()));
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("PKIX JcaJce Tests");
+
+        suite.addTestSuite(ReasonsMaskTest.class);
+
+        return suite;
+    }
+}
