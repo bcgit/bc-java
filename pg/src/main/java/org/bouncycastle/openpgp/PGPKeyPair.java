504462507493818581bd0b24db0061a52f46fb6e
Paul Schaub <vanitasvitae@fsfe.org>
2026-07-28 16:52:07 +0200

    Fix NPE in PGPKeyPair.asSubKey()

--- pg/src/main/java/org/bouncycastle/openpgp/PGPKeyPair.java
+++ pg/src/main/java/org/bouncycastle/openpgp/PGPKeyPair.java
@@ -79,8 +79,16 @@ public class PGPKeyPair
             pub.getAlgorithm(),
             pub.getCreationTime(),
             pub.getPublicKeyPacket().getKey());
+
+        PGPPrivateKey privateKey = null;
+        if (priv != null)
+        {
+            // reconstruct private key using public subkey packet
+            privateKey = new PGPPrivateKey(pub.getKeyID(), pubSubPkt, priv.getPrivateKeyDataPacket());
+        }
+
         return new PGPKeyPair(
             new PGPPublicKey(pubSubPkt, fingerPrintCalculator),
-            new PGPPrivateKey(pub.getKeyID(), pubSubPkt, priv.getPrivateKeyDataPacket()));
+            privateKey);
     }
 }
